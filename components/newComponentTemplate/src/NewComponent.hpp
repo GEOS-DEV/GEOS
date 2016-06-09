ae5bee925e134728e6e41399712abbac2994df2c
Randolph R. Settgast <settgast1@llnl.gov>
2016-06-08 22:19:33 -0700

    added component template

---
+++ components/newComponentTemplate/src/NewComponent.hpp
@@ -0,0 +1,23 @@
+/*
+ * NewComponent.hpp
+ *
+ *  Created on: Jun 8, 2016
+ *      Author: settgast
+ */
+
+#ifndef COMPONENTS_NEWCOMPONENTTEMPLATE_SRC_NEWCOMPONENT_HPP_
+#define COMPONENTS_NEWCOMPONENTTEMPLATE_SRC_NEWCOMPONENT_HPP_
+
+namespace geosx
+{
+
+class NewComponent
+{
+public:
+  NewComponent();
+  virtual ~NewComponent();
+};
+
+} /* namespace geosx */
+
+#endif /* COMPONENTS_NEWCOMPONENTTEMPLATE_SRC_NEWCOMPONENT_HPP_ */
---
+++ components/newComponentTemplate/tests/testNewComponent.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2015, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ *
+ * All rights reserved.
+ *
+ * This source code cannot be distributed without permission and
+ * further review from Lawrence Livermore National Laboratory.
+ */
+
+#include "gtest/gtest.h"
+//#include <cstring>
+//#include "slic/slic.hpp"
+//#include "../src/codingUtilities/stackTrace.hpp"
+
+// API coverage tests
+// Each test should be documented with the interface functions being tested
+
+//------------------------------------------------------------------------------
+// getName()
+//------------------------------------------------------------------------------
+TEST(testNewComponent,feature)
+{
+//	signal(SIGSEGV, stacktrace::handler);   // install our handler
+//	stacktrace::foo(); // this will call foo, bar, and baz.  baz segfaults.
+//  geosx::stacktrace::handler( SIGSEGV, 0 );
+}
+
