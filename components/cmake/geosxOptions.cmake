7991d7559ed9d23670a9c269227ccac79e9e4311
Randolph R. Settgast <rrsettgast@gmail.com>
2016-06-21 16:43:39 -0700

    modified build system to error if c++14 is not enabled. Also throw error if c++11 is enabled. Also added default hostconfig file to include into other hostconfig files in order to enable c++14.

--- components/cmake/geosxOptions.cmake
+++ components/cmake/geosxOptions.cmake
@@ -1,3 +1,15 @@
-set(ENABLE_FORTRAN OFF CACHE BOOL "Enables Fortran" FORCE)
-set(ENABLE_CXX11 OFF CACHE BOOL "Enables C++11 language support" FORCE)
-set(ENABLE_CXX14 ON CACHE BOOL "Enables C++14 language support" FORCE)
+
+#set(ENABLE_FORTRAN OFF CACHE BOOL "Enables Fortran" FORCE)
+#set(ENABLE_CXX11 OFF CACHE BOOL "Enables C++11 language support" FORCE)
+#set(ENABLE_CXX14 ON CACHE BOOL "Enables C++14 language support" FORCE)
+
+if(ENABLE_CXX11)
+    MESSAGE(FATAL_ERROR "c++11 is enabled. GEOSX requires c++14")
+endif(ENABLE_CXX11)
+
+if(NOT ENABLE_CXX14)
+    MESSAGE(FATAL_ERROR "c++14 is NOT enabled. GEOSX requires c++14")
+endif(NOT ENABLE_CXX14)
+
+blt_append_custom_compiler_flag(FLAGS_VAR CMAKE_CXX_FLAGS DEFAULT -pedantic)
+blt_append_custom_compiler_flag(FLAGS_VAR CMAKE_CXX_FLAGS DEFAULT -Wabi)
