d5170ff2a523afd8e5af570caac311795392be4d
Randolph R. Settgast <settgast1@llnl.gov>
2016-06-29 16:45:32 -0700

    fixed bug in geosxOptions.cmake

--- components/cmake/geosxOptions.cmake
+++ components/cmake/geosxOptions.cmake
@@ -3,13 +3,13 @@
 #set(ENABLE_CXX11 OFF CACHE BOOL "Enables C++11 language support" FORCE)
 #set(ENABLE_CXX14 ON CACHE BOOL "Enables C++14 language support" FORCE)
 
-if(ENABLE_CXX11)
+if(CXX_STD STREQUAL c++11)
     MESSAGE(FATAL_ERROR "c++11 is enabled. GEOSX requires c++14")
-endif(ENABLE_CXX11)
+endif(CXX_STD STREQUAL c++11)
 
-if(NOT ENABLE_CXX14)
+if(NOT CXX_STD STREQUAL c++14)
     MESSAGE(FATAL_ERROR "c++14 is NOT enabled. GEOSX requires c++14")
-endif(NOT ENABLE_CXX14)
+endif(NOT CXX_STD STREQUAL c++14)
 
 blt_append_custom_compiler_flag(FLAGS_VAR CMAKE_CXX_FLAGS DEFAULT -pedantic)
 blt_append_custom_compiler_flag(FLAGS_VAR CMAKE_CXX_FLAGS DEFAULT -Wabi)
