ad20f1955a4b042b6d84fdaa2703c7270769f8ae
Randolph R. Settgast <settgast1@llnl.gov>
2016-07-17 12:38:41 -0700

    modify stacktrace to demangle names on OSX

--- components/core/src/codingUtilities/stackTrace.cpp
+++ components/core/src/codingUtilities/stackTrace.cpp
@@ -16,27 +16,32 @@ namespace stacktrace
 
 void handler(int sig, int exitFlag, int exitCode )
 {
-  void *array[20];
+  fprintf(stderr,"executing stackTrace.cpp::handler(%d,%d,%d)\n", sig, exitFlag, exitCode );
+  void *array[100];
   int size;
 
   // get void*'s for all entries on the stack
-  size = backtrace(array, 20);
+  size = backtrace(array, 100);
 
   // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  backtrace_symbols_fd(array, size, STDERR_FILENO);
+//  fprintf(stderr, "Error: signal %d:\n", sig);
+//  backtrace_symbols_fd(array, size, STDERR_FILENO);
 
-  fprintf(stderr,"attempting unmangled trace: \n");
 
 //  char** mangled_name = backtrace_symbols(array, size);
   char ** messages    = backtrace_symbols(array, size);
+//  fprintf(stderr,"attempting unmangled trace: \n");
+//  fprintf(stderr,"0         1         2         3         4         5         6         7         8         9         : \n");
+//  fprintf(stderr,"0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789: \n");
 
   // skip first stack frame (points here)
   for ( int i = 1; i < size && messages != NULL; ++i)
   {
       char *mangled_name = 0, *offset_begin = 0, *offset_end = 0;
+//      std::cout<<messages[i]<<std::endl;
 
-      // find parantheses and +address offset surrounding mangled name
+#if 0
+      // find parentheses and +address offset surrounding mangled name
       for (char *p = messages[i]; *p; ++p)
       {
           if (*p == '(')
@@ -53,22 +58,36 @@ void handler(int sig, int exitFlag, int exitCode )
               break;
           }
       }
+#else
+      mangled_name = &(messages[i][58]);
+//      std::cout<<mangled_name<<std::endl;
+      for (char *p = messages[i]; *p; ++p)
+      {
+          if (*p == '+')
+          {
+              offset_begin = p;
+          }
+          offset_end = p;
+      }
+#endif
 
       // if the line could be processed, attempt to demangle the symbol
       if (mangled_name && offset_begin && offset_end &&
           mangled_name < offset_begin)
       {
           *mangled_name++ = '\0';
+          *(offset_begin-1) = '\0';
           *offset_begin++ = '\0';
           *offset_end++ = '\0';
+//          std::cout<<mangled_name<<std::endl;
 
           int status;
-          char * real_name = abi::__cxa_demangle(mangled_name, 0, 0, &status);
-
+          char * real_name = abi::__cxa_demangle(mangled_name, nullptr, nullptr, &status);
+//          std::cout<<status<<std::endl;
           // if demangling is successful, output the demangled function name
           if (status == 0)
           {
-              std::cerr << "[bt]: (" << i << ") " << messages[i] << " : "
+              std::cerr << messages[i] << " : "
                         << real_name << "+" << offset_begin << offset_end
                         << std::endl;
 
@@ -76,7 +95,7 @@ void handler(int sig, int exitFlag, int exitCode )
           // otherwise, output the mangled function name
           else
           {
-              std::cerr << "[bt]: (" << i << ") " << messages[i] << " : "
+              std::cerr << messages[i] << " : "
                         << mangled_name << "+" << offset_begin << offset_end
                         << std::endl;
           }
@@ -85,7 +104,7 @@ void handler(int sig, int exitFlag, int exitCode )
       // otherwise, print the whole line
       else
       {
-          std::cerr << "[bt]: (" << i << ") " << messages[i] << std::endl;
+          std::cerr << messages[i] << std::endl;
       }
   }
   std::cerr << std::endl;
@@ -95,11 +114,6 @@ void handler(int sig, int exitFlag, int exitCode )
     exit(exitCode);
 
 }
-
-
-
 }
-
-
 }
 
