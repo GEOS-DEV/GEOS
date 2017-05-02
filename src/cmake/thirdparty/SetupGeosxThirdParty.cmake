6c08e67233485a461f0c36ae557da8b1671ae5c1
Randolph R. Settgast <settgast1@llnl.gov>
2017-05-01 23:12:58 -0700

    forced static library build for caliper

--- src/cmake/thirdparty/SetupGeosxThirdParty.cmake
+++ src/cmake/thirdparty/SetupGeosxThirdParty.cmake
@@ -199,6 +199,7 @@ ExternalProject_Add( caliper
                      INSTALL_COMMAND make install
                      CMAKE_ARGS -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
                                 -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+                                -DBUILD_SHARED_LIBS:BOOL=OFF
                                 -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> )
 
 #include(${CALIPER_DIR}/share/cmake/caliper/caliper-config.cmake OPTIONAL)
