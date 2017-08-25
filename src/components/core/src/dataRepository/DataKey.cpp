af910d4d194dc095b0ebd2a1e56d03acc27ba6ae
Randolph R. Settgast <settgast1@llnl.gov>
2017-08-25 14:39:57 -0700

    added DataKey object

---
+++ src/components/core/src/dataRepository/DataKey.cpp
@@ -0,0 +1,20 @@
+/*
+ * DataKey.cpp
+ *
+ *  Created on: Aug 23, 2017
+ *      Author: settgast
+ */
+
+#include "DataKey.hpp"
+
+DataKey::DataKey()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+DataKey::~DataKey()
+{
+  // TODO Auto-generated destructor stub
+}
+
