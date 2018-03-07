7a1a46a49953b6cfb7fc7e3548b278a9c89ce43f
Arturo Vargas <artv3.89@gmail.com>
2018-03-07 15:32:09 -0800

    addressed Randys comments and moved make_unique (not availble for intel)

--- src/components/core/src/common/DataTypes.hpp
+++ src/components/core/src/common/DataTypes.hpp
@@ -43,6 +43,21 @@
 #define CONTAINERARRAY_RETURN_PTR 1
 #endif
 
+#include <iostream>
+#include <memory>
+
+#ifdef __INTEL_COMPILER
+namespace std
+{
+  template<typename T, typename... Args>
+  std::unique_ptr<T> make_unique(Args&&... args)
+  {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+  }
+
+}
+#endif
+
 
 namespace geosx
 {
