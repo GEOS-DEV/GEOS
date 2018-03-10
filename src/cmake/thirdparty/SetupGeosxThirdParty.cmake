d9f5a60e546e1527508163ec4c4ed83d219ec18d
Benjamin Curtice Corbett <corbett5@llnl.gov>
2018-03-09 17:59:57 -0800

    Fixed regex replacing debug with release in setting up tpl path

--- src/cmake/thirdparty/SetupGeosxThirdParty.cmake
+++ src/cmake/thirdparty/SetupGeosxThirdParty.cmake
@@ -62,7 +62,7 @@ endif()
 
 get_filename_component( TEMP_DIR "${CMAKE_INSTALL_PREFIX}" NAME)
 string(REGEX REPLACE "debug" "release" TEMP_DIR2 ${TEMP_DIR})
-set( GEOSX_TPL_DIR "${GEOSX_TPL_ROOT_DIR}/${TEMP_DIR}" )
+set( GEOSX_TPL_DIR "${GEOSX_TPL_ROOT_DIR}/${TEMP_DIR2}" )
 message("GEOSX_TPL_DIR=${GEOSX_TPL_DIR}")
 
 
