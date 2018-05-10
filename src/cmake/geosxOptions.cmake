4227ec7646aa038ba5f42cabdd260f101b4dd2a8
Randolph R. Settgast <settgast1@llnl.gov>
2018-05-10 12:12:39 -0700

    rearranged cmake many cmake options/variables to get them out of the hostconfig

--- src/cmake/geosxOptions.cmake
+++ src/cmake/geosxOptions.cmake
@@ -40,23 +40,13 @@ if(NOT BLT_CXX_STD STREQUAL c++14)
     MESSAGE(FATAL_ERROR "c++14 is NOT enabled. GEOSX requires c++14")
 endif(NOT BLT_CXX_STD STREQUAL c++14)
 
-if( (CMAKE_CXX_STANDARD EQUAL 11) OR (CMAKE_CXX_STANDARD EQUAL 14) )
-  add_definitions("-DUSE_CXX11")
-endif()
-
-
-
-
 message("CMAKE_CXX_COMPILER_ID = ${CMAKE_CXX_COMPILER_ID}")
-
-
 
 blt_append_custom_compiler_flag( FLAGS_VAR CMAKE_CXX_FLAGS DEFAULT "${OpenMP_CXX_FLAGS}")
 blt_append_custom_compiler_flag( FLAGS_VAR CMAKE_CXX_FLAGS
                                  GNU "-Wall -pedantic-errors -Wno-abi -Wextra  -Wshadow -Wfloat-equal	-Wcast-align	-Wpointer-arith	-Wwrite-strings	-Wcast-qual	-Wswitch-default  -Wno-vla  -Wno-switch-default  -Wno-unused-parameter  -Wno-unused-variable  -Wno-unused-function" 
                                  CLANG "-Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-padded -Wno-missing-prototypes -Wno-covered-switch-default -Wno-double-promotion -Wno-documentation -Wno-switch-enum -Wno-sign-conversion -Wno-unused-parameter -Wno-unused-variable -Wno-reserved-id-macro" 
                                )
-
 
 if( CMAKE_HOST_APPLE )
     set(GEOSX_LINK_PREPEND_FLAG "-Wl,-force_load" CACHE PATH "" FORCE)
