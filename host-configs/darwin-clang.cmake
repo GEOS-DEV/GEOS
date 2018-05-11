a24ff364b9e816d385b9a623eef1bc5c36a41007
Randolph R. Settgast <settgast1@llnl.gov>
2018-05-11 14:30:07 -0700

    more modifications to the cmake build

--- host-configs/darwin-clang.cmake
+++ host-configs/darwin-clang.cmake
@@ -3,18 +3,6 @@ site_name(HOST_NAME)
 set(CONFIG_NAME "${HOST_NAME}-darwin-x86_64-clang@apple-mp" CACHE PATH "") 
 message( "CONFIG_NAME = ${CONFIG_NAME}" )
 
-
-#set(TPL_DIR "${CMAKE_SOURCE_DIR}/../../axom_tpl" CACHE PATH "" )
-#message("TPL_DIR = ${TPL_DIR}")
-#include("${TPL_DIR}/${CONFIG_NAME}.cmake")
-
-
-
-#set(ATK_DIR "${CMAKE_SOURCE_DIR}/../../axom/install-${CONFIG_NAME}-debug" CACHE PATH "")
-#set(ATK_CMAKE "${ATK_DIR}/lib/cmake" CACHE PATH "")
-
-set( GEOSX_TPL_ROOT_DIR "../../thirdPartyLibs/" CACHE PATH "" )
-
 set(CMAKE_C_COMPILER "/usr/bin/clang" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "/usr/bin/clang++" CACHE PATH "")
 set(ENABLE_FORTRAN OFF CACHE BOOL "" FORCE)
@@ -24,23 +12,11 @@ set(MPI_CXX_COMPILER "/opt/local/bin/mpicxx-openmpi-clang" CACHE PATH "")
 set(MPI_Fortran_COMPILER "/opt/local/bin/mpifort-openmpi-clang" CACHE PATH "")
 set(MPIEXEC "mpirun-openmpi-gcc7" CACHE PATH "")
 
-
-include("${CMAKE_CURRENT_LIST_DIR}/hc-defaults.cmake")
-
-
-set(GEOSX_LINK_PREPEND_FLAG "-Wl,-force_load" CACHE PATH "" FORCE)
-set(GEOSX_LINK_POSTPEND_FLAG "" CACHE PATH "" FORCE)
-
-#set(GEOSX_LINK_PREPEND_FLAG  "-Wl,--whole-archive"    CACHE PATH "" FORCE)
-#set(GEOSX_LINK_POSTPEND_FLAG "-Wl,--no-whole-archive" CACHE PATH "" FORCE)
-
 #######################################
 # RAJA/CHAI SETUP
 #######################################
 #set( CHAI_DIR "${CMAKE_SOURCE_DIR}/../../chai" CACHE PATH "")
 #set( RAJA_DIR "${CMAKE_SOURCE_DIR}/../../raja" CACHE PATH "")
-option( BUILD_LOCAL_CHAI "Use the local mirrored CHAI" OFF )
-option( BUILD_LOCAL_RAJA "Use the local mirrored RAJA" OFF )
 
 option( RAJA_ENABLE_TBB "" OFF)
 option( ENABLE_CALIPER "Enables CALIPER" OFF )
@@ -49,5 +25,3 @@ set(CUDA_ENABLED      "OFF"       CACHE PATH "" FORCE)
 set(ENABLE_OPENMP     "OFF"        CACHE PATH "" FORCE)
 set(CHAI_BUILD_TYPE   "cpu-no-rm" CACHE PATH "" FORCE)
 set(CHAI_ARGS         ""          CACHE PATH "" FORCE)
-set(CALIPER_INSTALL   ""          CACHE PATH "" FORCE)
-set(RAJA_ENABLE_TESTS "OFF"       CACHE PATH "" FORCE)
--- host-configs/darwin-gcc7.cmake
+++ host-configs/darwin-gcc7.cmake
@@ -3,48 +3,20 @@ site_name(HOST_NAME)
 set(CONFIG_NAME "${HOST_NAME}-darwin-x86_64-gcc@mp7" CACHE PATH "") 
 message( "CONFIG_NAME = ${CONFIG_NAME}" )
 
-
-#set(TPL_DIR "${CMAKE_SOURCE_DIR}/../../axom_tpl" CACHE PATH "" )
-#message("TPL_DIR = ${TPL_DIR}")
-#include("${TPL_DIR}/${CONFIG_NAME}.cmake")
-
-
-
-#set(ATK_DIR "${CMAKE_SOURCE_DIR}/../../axom/install-${CONFIG_NAME}-debug" CACHE PATH "")
-#set(ATK_CMAKE "${ATK_DIR}/lib/cmake" CACHE PATH "")
-
-set( GEOSX_TPL_ROOT_DIR "../../thirdPartyLibs/" CACHE PATH "" )
-
-
-
 set(CMAKE_C_COMPILER "/opt/local/bin/gcc-mp-7" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "/opt/local/bin/g++-mp-7" CACHE PATH "")
 set(ENABLE_FORTRAN OFF CACHE BOOL "" FORCE)
 set(ENABLE_MPI ON CACHE PATH "")
-#set(MPI_C_COMPILER "/opt/local/bin/mpicc-openmpi-gcc7" CACHE PATH "")
-#set(MPI_CXX_COMPILER "/opt/local/bin/mpicxx-openmpi-gcc7" CACHE PATH "")
-#set(MPI_Fortran_COMPILER "/opt/local/bin/mpifort-openmpi-gcc7" CACHE PATH "")
-#set(MPIEXEC "mpirun-openmpi-gcc7" CACHE PATH "")
-
-set(MPI_C_COMPILER "/opt/local/bin/mpicc-mpich-gcc7" CACHE PATH "")
-set(MPI_CXX_COMPILER "/opt/local/bin/mpicxx-mpich-gcc7" CACHE PATH "")
-set(MPI_Fortran_COMPILER "/opt/local/bin/mpifort-mpich-gcc7" CACHE PATH "")
-set(MPIEXEC "mpirun-mpich-gcc7" CACHE PATH "")
-
-
-
-include("${CMAKE_CURRENT_LIST_DIR}/hc-defaults.cmake")
-
-
-set(ENABLE_MATHPRESSO ON CACHE BOOL  "Enables mathpresso Plugin")
+set(MPI_C_COMPILER "/opt/local/bin/mpicc-openmpi-gcc7" CACHE PATH "")
+set(MPI_CXX_COMPILER "/opt/local/bin/mpicxx-openmpi-gcc7" CACHE PATH "")
+set(MPI_Fortran_COMPILER "/opt/local/bin/mpifort-openmpi-gcc7" CACHE PATH "")
+set(MPIEXEC "mpirun-openmpi-gcc7" CACHE PATH "")
 
 #######################################
 # RAJA/CHAI SETUP
 #######################################
 #set( CHAI_DIR "${CMAKE_SOURCE_DIR}/../../chai" CACHE PATH "")
 #set( RAJA_DIR "${CMAKE_SOURCE_DIR}/../../raja" CACHE PATH "")
-option( BUILD_LOCAL_CHAI "Use the local mirrored CHAI" OFF )
-option( BUILD_LOCAL_RAJA "Use the local mirrored RAJA" OFF )
 
 option( RAJA_ENABLE_TBB "" OFF)
 option( ENABLE_CALIPER "Enables CALIPER" OFF )
@@ -53,5 +25,4 @@ set(CUDA_ENABLED      "OFF"        CACHE PATH "" FORCE)
 set(ENABLE_OPENMP     "ON"        CACHE PATH "" FORCE)
 set(CHAI_BUILD_TYPE   "cpu-no-rm" CACHE PATH "" FORCE)
 set(CHAI_ARGS         ""          CACHE PATH "" FORCE)
-set(CALIPER_INSTALL   ""          CACHE PATH "" FORCE)
-set(RAJA_ENABLE_TESTS "OFF"       CACHE PATH "" FORCE)
+set(RAJA_ENABLE_OPENMP "ON"        CACHE PATH "" FORCE)
