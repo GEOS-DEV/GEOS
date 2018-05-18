32da72f86469fd6598f88bdecce715b122d8c359
Randolph R. Settgast <settgast1@llnl.gov>
2018-05-17 23:13:34 -0700

    modified clang4 hostconfig for toss 3

--- host-configs/toss_3_x86_64_ib-clang@4.0.0.cmake
+++ host-configs/toss_3_x86_64_ib-clang@4.0.0.cmake
@@ -2,14 +2,28 @@
 
 set(CONFIG_NAME "quartz-toss_3_x86_64_ib-clang@4.0.0" CACHE PATH "") 
 
-set(TPL_DIR "/usr/gapps/GEOS/geosx/2017_10_04_13_56_50" CACHE PATH "" )
-include("${TPL_DIR}/${CONFIG_NAME}.cmake")
+#set(TPL_DIR "/usr/gapps/GEOS/geosx/2017_10_04_13_56_50" CACHE PATH "" )
+#include("${TPL_DIR}/${CONFIG_NAME}.cmake")
 
-set(ATK_DIR "/usr/gapps/GEOS/geosx/axom/toss_3_x86_64_ib-clang\@4.0.0-release" CACHE PATH "")
+#set(ATK_DIR "/usr/gapps/GEOS/geosx/axom/toss_3_x86_64_ib-clang\@4.0.0-release" CACHE PATH "")
+
+set(CMAKE_C_COMPILER "/usr/tce/packages/clang/clang-4.0.0/bin/clang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-4.0.0/bin/clang++" CACHE PATH "")
 
 
-set( GEOSX_TPL_ROOT_DIR "/usr/gapps/GEOS/geosx/thirdPartyLibs/" CACHE PATH "" FORCE)
+set(ENABLE_FORTRAN OFF CACHE BOOL "" FORCE)
+set(ENABLE_MPI ON CACHE BOOL "" FORCE)
 
+
+set(MPI_HOME             "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-4.0.0" CACHE PATH "")
+set(MPI_C_COMPILER       "${MPI_HOME}/bin/mpicc"   CACHE PATH "")
+set(MPI_CXX_COMPILER     "${MPI_HOME}/bin/mpicxx"  CACHE PATH "")
+set(MPI_Fortran_COMPILER "${MPI_HOME}/bin/mpifort" CACHE PATH "")
+
+set(MPIEXEC              "/usr/bin/srun" CACHE PATH "")
+set(MPIEXEC_NUMPROC_FLAG "-n" CACHE PATH "")
+
+set( GEOSX_TPL_ROOT_DIR "/usr/gapps/GEOS/geosx/thirdPartyLibs/" CACHE PATH "" )
 
 
 #######################################
