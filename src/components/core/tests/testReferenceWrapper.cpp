09d92127413234b165c5dfcb9df56e8d71ce741b
Randolph R. Settgast <settgast1@llnl.gov>
2018-06-25 15:56:22 -0700

    remove extra header text from testReferenceWrapper.cpp

--- src/components/core/tests/testReferenceWrapper.cpp
+++ src/components/core/tests/testReferenceWrapper.cpp
@@ -8,15 +8,6 @@
 // GEOSX is free software; you can redistribute it and/or modify it under the
 // terms of the GNU Lesser General Public License (as published by the Free
 // Software Foundation) version 2.1 dated February 1999.
-/*
- * Copyright (c) 2015, Lawrence Livermore National Security, LLC.
- * Produced at the Lawrence Livermore National Laboratory.
- *
- * All rights reserved.
- *
- * This source code cannot be distributed without permission and
- * further review from Lawrence Livermore National Laboratory.
- */
 
 
 #ifdef __clang__
