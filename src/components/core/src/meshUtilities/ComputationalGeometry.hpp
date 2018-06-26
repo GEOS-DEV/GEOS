e9e15e15ec20aaa4019349d66945ad703d348d33
Randolph R. Settgast <settgast1@llnl.gov>
2018-06-26 16:56:08 -0700

    added ComputationalGeometry files that contain computationalGeometry namespace and functions

---
+++ src/components/core/src/meshUtilities/ComputationalGeometry.hpp
@@ -0,0 +1,54 @@
+/*
+ * ComputationalGeometry.hpp
+ *
+ *  Created on: Jun 26, 2018
+ *      Author: settgast
+ */
+
+#ifndef SRC_COMPONENTS_CORE_SRC_MESHUTILITIES_COMPUTATIONALGEOMETRY_HPP_
+#define SRC_COMPONENTS_CORE_SRC_MESHUTILITIES_COMPUTATIONALGEOMETRY_HPP_
+
+#include "common/DataTypes.hpp"
+
+namespace geosx
+{
+namespace computationalGeometry
+{
+
+/**
+ * @author settgast
+ * Calculates the centroid of a convex 3D polygon as well as the normal
+ * @param[in] pointIndices list of index references for the points array in
+ * order (CW or CCW) about the polygon loop
+ * @param[in] points 3D point list
+ * @param[out] center 3D center of the given ordered polygon point list
+ * @param[out] normal Normal to the face
+ * @return area of the convex 3D polygon
+ */
+real64 Centroid_3DPolygon(const localIndex_array& pointsIndices,
+                         const array<R1Tensor>& points,
+                         R1Tensor& center,
+                         R1Tensor& normal );
+
+/**
+ * @author settgast
+ * Calculates the centroid of a convex 3D polygon as well as the normal
+ * @param[in] pointIndices list of index references for the points array in
+ * order (CW or CCW) about the polygon loop
+ * @param[in] pointReferences 3D reference point list
+ * @param[in] pointDisplacements 3D displacement list
+ * @param[out] center 3D center of the given ordered polygon point list
+ * @param[out] normal Normal to the face
+ * @return area of the convex 3D polygon
+ */
+real64 Centroid_3DPolygon(const localIndex_array& pointsIndices,
+                         const array<R1Tensor>& pointReferences,
+                         const array<R1Tensor>& pointDisplacements,
+                         R1Tensor& center,
+                         R1Tensor& normal );
+
+
+}
+} /* namespace geosx */
+
+#endif /* SRC_COMPONENTS_CORE_SRC_MESHUTILITIES_COMPUTATIONALGEOMETRY_HPP_ */
