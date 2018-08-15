fa9dd917f7cc70647706d6b5185f850d5b10f10f
Randolph R. Settgast <settgast1@llnl.gov>
2018-08-15 11:34:24 -0700

    modify and test user defined conversion operators to reduce dimension of an array

--- src/components/core/src/managers/BoundaryConditions/BoundaryConditionManager.hpp
+++ src/components/core/src/managers/BoundaryConditions/BoundaryConditionManager.hpp
@@ -78,6 +78,7 @@ public:
                                ARGS & ... args );
 
 
+#if 0
   template< typename LAMBDA >
   void ApplyBoundaryCondition( real64 const time,
                                dataRepository::ManagedGroup * object,
@@ -104,6 +105,69 @@ public:
       }
     });
   }
+#else
+  template< typename LAMBDA >
+  void ApplyBoundaryCondition( real64 const time,
+                               dataRepository::ManagedGroup * domain,
+                               string const & fieldName,
+                               LAMBDA && lambda )
+  {
+    for( auto & subGroup : this->GetSubGroups() )
+    {
+      BoundaryConditionBase const * bc = subGroup.second->group_cast<BoundaryConditionBase const *>();
+      if( time >= bc->GetStartTime() && time < bc->GetEndTime() && ( bc->GetFieldName()==fieldName) )
+      {
+        string_array const objectPath = stringutilities::Tokenize( bc->GetObjectPath(), "/");
+        localIndex const targetPathLength = objectPath.size();
+
+        MeshLevel * const meshLevel = domain->group_cast<DomainPartition*>()->
+                                      getMeshBody(0)->getMeshLevel(0);
+
+        dataRepository::ManagedGroup * targetGroup = domain->GetGroup(objectPath[0]);
+        GEOS_ASSERT( meshLevel->hasGroup())
+
+          string fieldName = bc->GetFieldName();
+
+          if( objectPath.size()>1 )
+          {
+            GEOS_ASSERT( !( !bc->GetFieldName().empty() && !objectPath[1].empty() ) ,
+                         "fieldName specified in both fieldName entry ("<<bc->GetFieldName()
+                         <<") and objectPath ("<<objectPath[1]<<")");
+
+            GEOS_ASSERT( !( bc->GetFieldName().empty() && objectPath[1].empty() ),
+                         "fieldName not specified in either fieldName entry or objectPath");
+
+            if( !objectPath[1].empty() )
+            {
+              fieldName = objectPath[1];
+            }
+          }
+          else
+          {
+            GEOS_ASSERT( !bc->GetFieldName().empty(),
+                         "fieldName not specified in either fieldName entry or objectPath" );
+          }
+
+
+          dataRepository::ManagedGroup const * setGroup = targetGroup->GetGroup(dataRepository::keys::sets);
+          string_array setNames = bc->GetSetNames();
+          for( auto & setName : setNames )
+          {
+            dataRepository::ViewWrapper<set<localIndex>> const * const setWrapper = setGroup->getWrapper<set<localIndex>>(setName);
+            if( setWrapper != nullptr )
+            {
+              set<localIndex> const & set = setWrapper->reference();
+              bc->ApplyBounaryConditionDefaultMethod<rtTypes::equateValue>( set, 0.0, targetGroup, fieldName );
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+
+#endif
 
   template< typename LAMBDA >
   void ApplyBoundaryCondition( real64 const time,
