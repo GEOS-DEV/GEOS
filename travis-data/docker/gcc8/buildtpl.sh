4ec307e40594bbda78427e159fd946453480ae44
Randolph R. Settgast <rrsettgast@gmail.com>
2018-09-07 08:57:00 -0700

    adding ubuntu18 and gcc8 images

---
+++ travis-data/docker/gcc8/buildtpl.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+set -x
+
+git clone https://github.com/GEOSX/thirdPartyLibs.git
+cd thirdPartyLibs
+git checkout bugfix/siloBuild
+git submodule update --init --recursive
+python scripts/config-build.py -hc host-configs/default.cmake -bt Release
+cd build-default-release
+make
+cd ..
+git submodule deinit .
+rm -rf build-default-release
