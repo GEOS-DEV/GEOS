23cfeac0a86594c57a005bcc43f9505b0c73c33b
Antoine Mazuyer <antoine.mazuyer@protonmail.com>
2018-09-10 16:44:55 -0700

    add test for volume computation

--- src/coreComponents/unitTests/geometry/testVolumeComputation.cpp
+++ src/coreComponents/unitTests/geometry/testVolumeComputation.cpp
@@ -76,24 +76,50 @@ int main(int argc, char** argv)
   return result;
 }
 
-
 TEST(testXML,testXML)
 {
     R1Tensor hex[8] = {R1Tensor(3), R1Tensor(3), R1Tensor(3), R1Tensor(3), 
                        R1Tensor(3), R1Tensor(3), R1Tensor(3), R1Tensor(3)};
     hex[0][0] = 0.; hex[0][1] = 0. ; hex[0][2]= 0;
-    hex[1][0] = 1.; hex[1][1] = 0. ; hex[0][2]= 0;
-    hex[2][0] = 0.; hex[3][1] = 1. ; hex[0][2]= 0;
-    hex[3][0] = 1.; hex[2][1] = 1. ; hex[0][2]= 0;
-    hex[4][0] = 0.; hex[4][1] = 0. ; hex[0][2]= 1;
-    hex[5][0] = 1.; hex[5][1] = 0. ; hex[0][2]= 1;
-    hex[6][0] = 0.; hex[7][1] = 1. ; hex[0][2]= 1;
-    hex[7][0] = 1.; hex[6][1] = 1. ; hex[0][2]= 1;
-
-    if( geosx::computationalGeometry::HexVolume( hex ) != 1.) {
-        std::cout << geosx::computationalGeometry::HexVolume( hex ) << std::endl;
+    hex[1][0] = 1.; hex[1][1] = 0. ; hex[1][2]= 0;
+    hex[2][0] = 0.; hex[2][1] = 1. ; hex[2][2]= 0;
+    hex[3][0] = 1.; hex[3][1] = 1. ; hex[3][2]= 0;
+    hex[4][0] = 0.; hex[4][1] = 0. ; hex[4][2]= 1;
+    hex[5][0] = 1.; hex[5][1] = 0. ; hex[5][2]= 1;
+    hex[6][0] = 0.; hex[6][1] = 1. ; hex[6][2]= 1;
+    hex[7][0] = 1.; hex[7][1] = 1. ; hex[7][2]= 1;
+    if( std::fabs(geosx::computationalGeometry::HexVolume( hex ) - 1.) > 1e-9) {
         GEOS_ERROR("Problem detected in the volume computation of hex");
     }
 
+    R1Tensor tet[4] = {R1Tensor(3), R1Tensor(3), R1Tensor(3), R1Tensor(3)};
+    tet[0][0] = 0.; tet[0][1] = 0. ; tet[0][2]= 0;
+    tet[1][0] = 1.; tet[1][1] = 0. ; tet[1][2]= 0;
+    tet[2][0] = 0.; tet[2][1] = 1. ; tet[2][2]= 0;
+    tet[3][0] = 0.; tet[3][1] = 0. ; tet[3][2]= 1;
+    if( std::fabs(geosx::computationalGeometry::TetVolume( tet ) - 1./6.) > 1e-9) {
+        GEOS_ERROR("Problem detected in the volume computation of tet");
+    }
 
+    R1Tensor wedge[6] = {R1Tensor(3), R1Tensor(3), R1Tensor(3),
+                         R1Tensor(3), R1Tensor(3), R1Tensor(3)};
+    wedge[0][0] = 1.; wedge[0][1] = 0. ; wedge[0][2]= 0;
+    wedge[1][0] = 0.; wedge[1][1] = 1. ; wedge[1][2]= 0;
+    wedge[2][0] = 0.; wedge[2][1] = 0. ; wedge[2][2]= 0;
+    wedge[3][0] = 1.; wedge[3][1] = 0. ; wedge[3][2]= 1;
+    wedge[4][0] = 0.; wedge[4][1] = 1. ; wedge[4][2]= 1;
+    wedge[5][0] = 0.; wedge[5][1] = 0. ; wedge[5][2]= 1;
+    if( std::fabs(geosx::computationalGeometry::WedgeVolume( wedge ) - 0.5) > 1e-9) {
+        GEOS_ERROR("Problem detected in the volume computation of wedge");
+    }
+
+    R1Tensor pyramid[5] = {R1Tensor(3), R1Tensor(3), R1Tensor(3), R1Tensor(3), R1Tensor(3)};
+    pyramid[0][0] = 0.; pyramid[0][1] = 0. ; pyramid[0][2]= 0;
+    pyramid[1][0] = 1.; pyramid[1][1] = 0. ; pyramid[1][2]= 0;
+    pyramid[2][0] = 1.; pyramid[2][1] = 2. ; pyramid[2][2]= 0;
+    pyramid[3][0] = 0.; pyramid[3][1] = 2. ; pyramid[3][2]= 0;
+    pyramid[4][0] = 0.5; pyramid[4][1] = 0.5 ; pyramid[4][2]= 1;
+    if( std::fabs(geosx::computationalGeometry::PyramidVolume( pyramid ) - 2./3.) > 1e-9) {
+        GEOS_ERROR("Problem detected in the volume computation of pyramid");
+    }
 }
