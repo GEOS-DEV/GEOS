7b935f7b3013eb281298b2cf0503b2084110c3be
Randolph R. Settgast <rrsettgast@gmail.com>
2018-09-13 08:27:54 -0700

    changes to docker images to force NUM_PROC=2

--- travis-data/docker/clang5-ubuntu18/buildtpl.sh
+++ travis-data/docker/clang5-ubuntu18/buildtpl.sh
@@ -4,9 +4,9 @@ set -x
 
 git clone https://github.com/GEOSX/thirdPartyLibs.git
 cd thirdPartyLibs
-git checkout bugfix/klevzoff/fix-mathpresso
+git checkout bugfix/addLumberjackAcom
 git submodule update --init --recursive
-python scripts/config-build.py -hc host-configs/default.cmake -bt Release
+python scripts/config-build.py -hc host-configs/default.cmake -bt Release -DNUM_PROC:STRING=2
 cd build-default-release
 make
 cd ..
