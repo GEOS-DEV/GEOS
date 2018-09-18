64c20e0066154bd5feab6c6203ff4b963bd7f499
Randolph R. Settgast <settgast1@llnl.gov>
2018-09-18 00:07:20 -0700

    fixes after last merge with develop

--- src/coreComponents/linearAlgebraInterface/tests/testLAOperations.cpp
+++ src/coreComponents/linearAlgebraInterface/tests/testLAOperations.cpp
@@ -16,8 +16,16 @@
  *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 
+#if __clang_major__ >= 5
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant"
+#endif
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
+
+#if __clang_major__ >= 5
+#pragma clang diagnostic pop
+#endif
 
 #include <iostream>
 #include <vector>
@@ -249,7 +257,6 @@ void testLaplaceOperator()
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wglobal-constructors"
 #pragma clang diagnostic ignored "-Wexit-time-destructors"
-#pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant"
 #endif
 
 TEST(testLAOperations,testEpetraLAOperations)
