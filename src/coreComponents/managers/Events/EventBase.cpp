a0296e108bc1497fb62cfa0fe64263732cd00907
Christopher Sherman <sherman27@llnl.gov>
2019-01-10 12:50:17 -0800

    Filtering out unwanted schema elements

--- src/coreComponents/managers/Events/EventBase.cpp
+++ src/coreComponents/managers/Events/EventBase.cpp
@@ -54,6 +54,8 @@ EventBase::EventBase( const std::string& name,
   m_lastCycle(0),
   m_target(nullptr)
 {
+  setSchemaFlags(SchemaFlags::NODE);
+  
   RegisterViewWrapper(viewKeyStruct::eventTargetString, &m_eventTarget, false )->
     setInputFlag(InputFlags::REQUIRED)->
     setDescription("event target");
