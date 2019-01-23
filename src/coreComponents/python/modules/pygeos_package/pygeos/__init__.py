0e21fd1f46901baae52a2d4520ffe98eb48efbff
Christopher Sherman <sherman27@llnl.gov>
2019-01-23 11:01:50 -0800

    Updating the python xml processing package

--- src/coreComponents/python/modules/pygeos_package/pygeos/__init__.py
+++ src/coreComponents/python/modules/pygeos_package/pygeos/__init__.py
@@ -6,7 +6,6 @@ from .regex_config import regexConfig, symbolicMathRegexHandler, parameterHandle
 from .unit_manager import unitManager 
 from .xml_processor import preprocessGEOSXML
 from .table_generator import writeGEOSTable, readGEOSTable
-from .format_xml import format_xml_file
 from .test_manager import runUnitTests
-
+from .format_xml import format_xml_file
 
