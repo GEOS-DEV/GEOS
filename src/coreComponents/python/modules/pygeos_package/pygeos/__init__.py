f90743879a8cd2af8ca251d68550b98367b17089
Christopher Sherman <sherman27@llnl.gov>
2019-01-31 09:41:58 -0800

    Adding documentation for the schema, virtual python setup script

--- src/coreComponents/python/modules/pygeos_package/pygeos/__init__.py
+++ src/coreComponents/python/modules/pygeos_package/pygeos/__init__.py
@@ -1,10 +1,10 @@
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 
 from .regex_config import regexConfig, symbolicMathRegexHandler, parameterHandler, DictRegexHandler
 from .unit_manager import unitManager 
-from .xml_processor import preprocessGEOSXML
+from .xml_processor import preprocessGEOSXML, validateXML
 from .table_generator import writeGEOSTable, readGEOSTable
 from .test_manager import runUnitTests
 from .format_xml import format_xml_file
