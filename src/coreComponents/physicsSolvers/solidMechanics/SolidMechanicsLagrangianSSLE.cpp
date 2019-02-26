ff7ca5003af069773b0c347285e19eddbf2a2434
Randolph R. Settgast <settgast1@llnl.gov>
2019-02-26 10:45:02 -0800

    commented out stiffness based damping in explicit time step

--- src/coreComponents/physicsSolvers/solidMechanics/SolidMechanicsLagrangianSSLE.cpp
+++ src/coreComponents/physicsSolvers/solidMechanics/SolidMechanicsLagrangianSSLE.cpp
@@ -38,20 +38,21 @@ SolidMechanics_LagrangianSSLE::~SolidMechanics_LagrangianSSLE()
 {}
 
 template< localIndex NUM_NODES_PER_ELEM, localIndex NUM_QUADRATURE_POINTS >
-real64 SolidMechanics_LagrangianSSLE::ExplicitElementKernelLaunch( localIndex const er,
-                                                            localIndex const esr,
-                                                            set<localIndex> const & elementList,
-                                                            arrayView2d<localIndex> const & elemsToNodes,
-                                                            arrayView3d< R1Tensor > const & dNdX,
-                                                            arrayView2d<real64> const & detJ,
-                                                            arrayView1d<R1Tensor> const & u,
-                                                            arrayView1d<R1Tensor> const & uhat,
-                                                            arrayView1d<R1Tensor> const & vel,
-                                                            arrayView1d<R1Tensor> & acc,
-                                                            ElementRegionManager::ConstitutiveRelationAccessor<ConstitutiveBase> constitutiveRelations,
-                                                            ElementRegionManager::MaterialViewAccessor< arrayView2d<real64> > const & meanStress,
-                                                            ElementRegionManager::MaterialViewAccessor< arrayView2d<R2SymTensor> > const & devStress,
-                                                            real64 const dt )
+real64 SolidMechanics_LagrangianSSLE::
+ExplicitElementKernelLaunch( localIndex const er,
+                             localIndex const esr,
+                             set<localIndex> const & elementList,
+                             arrayView2d<localIndex> const & elemsToNodes,
+                             arrayView3d< R1Tensor > const & dNdX,
+                             arrayView2d<real64> const & detJ,
+                             arrayView1d<R1Tensor> const & u,
+                             arrayView1d<R1Tensor> const & uhat,
+                             arrayView1d<R1Tensor> const & vel,
+                             arrayView1d<R1Tensor> & acc,
+                             ElementRegionManager::ConstitutiveRelationAccessor<ConstitutiveBase> constitutiveRelations,
+                             ElementRegionManager::MaterialViewAccessor< arrayView2d<real64> > const & meanStress,
+                             ElementRegionManager::MaterialViewAccessor< arrayView2d<R2SymTensor> > const & devStress,
+                             real64 const dt )
 {
 
   ConstitutiveBase::UpdateFunctionPointer update = constitutiveRelations[er][esr][0]->GetStateUpdateFunctionPointer();
@@ -62,51 +63,70 @@ real64 SolidMechanics_LagrangianSSLE::ExplicitElementKernelLaunch( localIndex co
                              GEOSX_LAMBDA ( localIndex const k)
   {
     R1Tensor uhat_local[ NUM_NODES_PER_ELEM ];
-    R1Tensor v_local[ NUM_NODES_PER_ELEM ];
+//    R1Tensor v_local[ NUM_NODES_PER_ELEM ];
     R1Tensor u_local[ NUM_NODES_PER_ELEM ];
     R1Tensor f_local[ NUM_NODES_PER_ELEM ];
 
     real64 c[6][6];
     constitutiveRelations[er][esr][0]->GetStiffness( c );
 
+//    CopyGlobalToLocal<NUM_NODES_PER_ELEM, R1Tensor>( elemsToNodes[k],
+//                                           u, uhat, vel,
+//                                           u_local, uhat_local, v_local );
+
     CopyGlobalToLocal<NUM_NODES_PER_ELEM, R1Tensor>( elemsToNodes[k],
-                                           u, uhat, vel,
-                                           u_local, uhat_local, v_local );
+                                                     u, uhat,
+                                                     u_local, uhat_local );
 
     //Compute Quadrature
     for( localIndex q = 0 ; q<NUM_QUADRATURE_POINTS ; ++q)
     {
 
+      real64 p_stress[6] = {0};
+//      real64 p_Cdamp[6] = {0};
       for( localIndex a=0 ; a<NUM_NODES_PER_ELEM ; ++a )
       {
         R1Tensor const & uhat_a = uhat_local[a];
-        R1Tensor const & v_a = v_local[a];
+//        R1Tensor const & v_a = v_local[a];
 
         const R1Tensor& dNdXb = dNdX[k][q][a];
         const realT u0_x_dNdXb0 = uhat_a[0]*dNdXb[0];
         const realT u1_x_dNdXb1 = uhat_a[1]*dNdXb[1];
 
-        const realT v0_x_dNdXb0 = v_a[0]*dNdXb[0];
-        const realT v1_x_dNdXb1 = v_a[1]*dNdXb[1];
+//        const realT v0_x_dNdXb0 = v_a[0]*dNdXb[0];
+//        const realT v1_x_dNdXb1 = v_a[1]*dNdXb[1];
 
         const realT u2_x_dNdXb2 = uhat_a[2]*dNdXb[2];
-        const realT v2_x_dNdXb2 = v_a[2]*dNdXb[2];
+//        const realT v2_x_dNdXb2 = v_a[2]*dNdXb[2];
 
-        real64 p_stress[6], p_Cdamp[6];
         p_stress[0] += u0_x_dNdXb0*c[0][0] + u1_x_dNdXb1*c[0][1] + u2_x_dNdXb2*c[0][2];
-        p_stress[2] += u0_x_dNdXb0*c[1][0] + u1_x_dNdXb1*c[1][1] + u2_x_dNdXb2*c[1][2];
-        p_stress[5] += u0_x_dNdXb0*c[2][0] + u1_x_dNdXb1*c[2][1] + u2_x_dNdXb2*c[2][2];
-        p_stress[4] += ( uhat_a[2]*dNdXb[1] + uhat_a[1]*dNdXb[2] )*c[3][3];
-        p_stress[3] += ( uhat_a[2]*dNdXb[0] + uhat_a[0]*dNdXb[2] )*c[4][4];
-        p_stress[1] += ( uhat_a[1]*dNdXb[0] + uhat_a[0]*dNdXb[1] )*c[5][5];
+        p_stress[1] += u0_x_dNdXb0*c[1][0] + u1_x_dNdXb1*c[1][1] + u2_x_dNdXb2*c[1][2];
+        p_stress[2] += u0_x_dNdXb0*c[2][0] + u1_x_dNdXb1*c[2][1] + u2_x_dNdXb2*c[2][2];
+        p_stress[3] += ( uhat_a[2]*dNdXb[1] + uhat_a[1]*dNdXb[2] )*c[3][3];
+        p_stress[4] += ( uhat_a[2]*dNdXb[0] + uhat_a[0]*dNdXb[2] )*c[4][4];
+        p_stress[5] += ( uhat_a[1]*dNdXb[0] + uhat_a[0]*dNdXb[1] )*c[5][5];
 
-        p_Cdamp[0] += v0_x_dNdXb0*c[0][0] + v1_x_dNdXb1*c[0][1] + v2_x_dNdXb2*c[0][2];
-        p_Cdamp[2] += v0_x_dNdXb0*c[1][0] + v1_x_dNdXb1*c[1][1] + v2_x_dNdXb2*c[1][2];
-        p_Cdamp[5] += v0_x_dNdXb0*c[2][0] + v1_x_dNdXb1*c[2][1] + v2_x_dNdXb2*c[2][2];
-        p_Cdamp[4] += ( v_a[2]*dNdXb[1] + v_a[1]*dNdXb[2] )*c[3][3];
-        p_Cdamp[3] += ( v_a[2]*dNdXb[0] + v_a[0]*dNdXb[2] )*c[4][4];
-        p_Cdamp[1] += ( v_a[1]*dNdXb[0] + v_a[0]*dNdXb[1] )*c[5][5];
+//        p_Cdamp[0] += v0_x_dNdXb0*c[0][0] + v1_x_dNdXb1*c[0][1] + v2_x_dNdXb2*c[0][2];
+//        p_Cdamp[1] += v0_x_dNdXb0*c[1][0] + v1_x_dNdXb1*c[1][1] + v2_x_dNdXb2*c[1][2];
+//        p_Cdamp[2] += v0_x_dNdXb0*c[2][0] + v1_x_dNdXb1*c[2][1] + v2_x_dNdXb2*c[2][2];
+//        p_Cdamp[3] += ( v_a[2]*dNdXb[1] + v_a[1]*dNdXb[2] )*c[3][3];
+//        p_Cdamp[4] += ( v_a[2]*dNdXb[0] + v_a[0]*dNdXb[2] )*c[4][4];
+//        p_Cdamp[5] += ( v_a[1]*dNdXb[0] + v_a[0]*dNdXb[1] )*c[5][5];
       }
+      real64 const dMeanStress = ( p_stress[0] + p_stress[1] + p_stress[2] )/3.0;
+      meanStress[er][esr][0][k][q] += dMeanStress;
+
+      p_stress[0] -= dMeanStress;
+      p_stress[1] -= dMeanStress;
+      p_stress[2] -= dMeanStress;
+
+      real64 * const restrict p_devStres = devStress[er][esr][0][k][q].Data();
+      p_devStres[0] += p_stress[0];
+      p_devStres[2] += p_stress[1];
+      p_devStres[5] += p_stress[2];
+      p_devStres[4] += p_stress[3];
+      p_devStres[3] += p_stress[4];
+      p_devStres[1] += p_stress[5];
 
     }//quadrature loop
 
