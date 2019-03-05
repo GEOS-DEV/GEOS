4470e79196e227c0470a60ee08834d0d0cfd1bb9
Christopher Sherman <sherman27@llnl.gov>
2019-03-05 15:42:44 -0800

    Adding the capability to output non-schema documentation

--- src/coreComponents/fileIO/schema/SchemaToRSTDocumentation.py
+++ src/coreComponents/fileIO/schema/SchemaToRSTDocumentation.py
@@ -47,18 +47,63 @@ def writeTableRST(file_name, values):
     f.write('\n\n')
 
 
+def parseSchemaNode(node):
+  type_name = child_node.get('name')[:-4]
+  table_values = [['Name', 'Type', 'Default', 'Description']]
+
+  # Parse comments
+  attribute_comments = {}
+  for comment_node in child_node.iterchildren(etree.Comment):
+    tmp = str(comment_node)[4:-3].split(' = ', 1)
+    attribute_comments[tmp[0]] = tmp[1].replace('\\\\', '\\').replace('\n', '\\n')
+
+  # Parse attributes
+  for attribute_node in child_node.findall(xsd + 'attribute'):
+    table_row = [attribute_node.get(v, default=' ') for v in ['name', 'type', 'default']]
+    useValue = attribute_node.get('use')
+    if useValue:
+      table_row[2] = useValue
+    table_values.append(table_row)
+
+    k = table_values[-1][0]
+    if k in attribute_comments:
+      table_values[-1].append(attribute_comments[k])
+    else:
+      table_values[-1].append(' ')
+
+  # Parse nodes
+  for choice_node in child_node.findall(xsd + 'choice'):
+    for sub_node in choice_node.findall(xsd + 'element'):
+      sub_name = sub_node.get('name')
+      sub_required = sub_node.get('minOccurs')
+      sub_unique = sub_node.get('maxOccurs')
+      node_use = ''
+      if sub_required:
+        if sub_unique:
+          node_use = 'unique, required'
+        else:
+          node_use = 'required'
+      elif (sub_unique):
+        node_use = 'unique'
+
+      table_values.append([sub_name, 'node', node_use, ':ref:`XML_%s`' % (sub_name)])
+
+  return type_name, table_values
+
+
 # Config
-input_name = 'schema.xsd'
+schema_name = 'schema.xsd'
+additional_documentation_name = 'schema.xsd.other'
 complete_output = '../../../docs/sphinx/CompleteXMLSchema'
 output_folder = 'docs'
 sphinx_path = '../../coreComponents/fileIO/schema/docs'
 xsd = '{http://www.w3.org/2001/XMLSchema}'
 
 
-# Parse schema, build docs
+# Parse the schema, build documentation tables
 os.system('mkdir -p %s' % (output_folder))
 
-include_tree = etree.parse(input_name)
+include_tree = etree.parse(schema_name)
 include_root = include_tree.getroot()
 
 with open('%s.rst' % (complete_output), 'w') as output_handle:
@@ -70,46 +115,8 @@ with open('%s.rst' % (complete_output), 'w') as output_handle:
   output_handle.write('********************************\n\n')
 
   for child_node in include_root.findall(xsd + 'complexType'):
-    type_name = child_node.get('name')[:-4]
-    table_values = [['Name', 'Type', 'Default', 'Description']]
-
-    # Parse comments
-    attribute_comments = {}
-    for comment_node in child_node.iterchildren(etree.Comment):
-      tmp = str(comment_node)[4:-3].split(' = ', 1)
-      attribute_comments[tmp[0]] = tmp[1].replace('\\\\', '\\').replace('\n', '\\n')
-
-    # Parse attributes
-    for attribute_node in child_node.findall(xsd + 'attribute'):
-      table_row = [attribute_node.get(v, default=' ') for v in ['name', 'type', 'default']]
-      useValue = attribute_node.get('use')
-      if useValue:
-        table_row[2] = useValue
-
-      table_values.append(table_row)
-
-      k = table_values[-1][0]
-      if k in attribute_comments:
-        table_values[-1].append(attribute_comments[k])
-      else:
-        table_values[-1].append(' ')
-
-    # Parse nodes
-    for choice_node in child_node.findall(xsd + 'choice'):
-      for sub_node in choice_node.findall(xsd + 'element'):
-        sub_name = sub_node.get('name')
-        sub_required = sub_node.get('minOccurs')
-        sub_unique = sub_node.get('maxOccurs')
-        node_use = ''
-        if sub_required:
-          if sub_unique:
-            node_use = 'unique, required'
-          else:
-            node_use = 'required'
-        elif (sub_unique):
-          node_use = 'unique'
-
-        table_values.append([sub_name, 'node', node_use, ':ref:`XML_%s`' % (sub_name)])
+    # Parse schema into a list of key parameters
+    type_name, table_values = parseSchemaNode(child_node)
 
     # Write table
     writeTableRST('%s/%s.rst' % (output_folder, type_name), table_values)
@@ -119,4 +126,15 @@ with open('%s.rst' % (complete_output), 'w') as output_handle:
     output_handle.write('%s\n' % (element_header))
     output_handle.write('='*len(element_header) + '\n')
     output_handle.write('.. include:: %s/%s.rst\n\n' % (sphinx_path, type_name))
+
+
+# Parse the additional documentation
+include_tree = etree.parse(additional_documentation_name)
+include_root = include_tree.getroot()
+for child_node in include_root.findall(xsd + 'complexType'):
+  # The additional documentation uses the same format as the schema
+  type_name, table_values = parseSchemaNode(child_node)
+
+  # Write table
+  writeTableRST('%s/%s_other.rst' % (output_folder, type_name), table_values)
 
