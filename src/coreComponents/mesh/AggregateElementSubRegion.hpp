62b632891ec130650d3cea346def7183fd0a569a
mazuyer <antoine.mazuyer@univ-lorraine.fr>
2019-03-28 16:25:44 -0700

    Add an include to cxx-utilities/src/src/ChaiVector.hpp

--- src/coreComponents/mesh/AggregateElementSubRegion.hpp
+++ src/coreComponents/mesh/AggregateElementSubRegion.hpp
@@ -21,6 +21,7 @@
 
 #include "ElementSubRegionBase.hpp"
 #include "InterObjectRelation.hpp"
+#include "cxx-utilities/src/src/ChaiVector.hpp"
 #include "cxx-utilities/src/src/SparsityPattern.hpp"
 
 #include "metis.h"
