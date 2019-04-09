8aa983229bd007b43451b130b2beb1ddd54cbb2f
Nicola Castelletto <castelletto1@llnl.gov>
2019-04-09 15:25:07 -0700

    Adding cblas methods for array1d

---
+++ src/coreComponents/linearAlgebraInterface/src/BlasLapackLA.cpp
@@ -0,0 +1,106 @@
+/*
+ * LapackLA.cpp
+ *
+ *  Created on: Apr 8, 2019
+ *      Author: castelletto1
+ */
+
+// Include the corresponding header file.
+#include "BlasLapackLA.hpp"
+
+// Put everything under the geosx namespace.
+namespace geosx
+{
+
+//-------------------------------------------------------Mathematical methods---
+
+real64 BlasLapackLA::norm1(array1d<real64> const & X) const
+{
+  return cblas_dasum( integer_conversion<int>(X.size()),
+                      X.data(),
+                      1);
+}
+
+real64 BlasLapackLA::norm2(array1d<real64> const & X) const
+{
+  return cblas_dnrm2( integer_conversion<int>(X.size()),
+                      X.data(),
+                      1);
+}
+
+real64 BlasLapackLA::normInf(array1d<real64> const & X) const
+{
+  int ind = cblas_idamax( integer_conversion<int>(X.size()),
+                          X.data(),
+                          1);
+  return std::abs(X[ind]);
+}
+
+void BlasLapackLA::vectorVectorAdd( array1d< real64> const & X,
+                                    array1d< real64> & Y,
+                                    real64 const alpha)
+{
+
+  GEOS_ASSERT_MSG( X.size() == Y.size(),
+                   "Vector dimensions not compatible for sum" );
+
+  cblas_daxpy( integer_conversion<int>( X.size() ),
+               alpha,
+               X.data(),
+               1,
+               Y.data(),
+               1 );
+  return;
+}
+
+void BlasLapackLA::vectorScale(array1d< real64> & X,
+                               real64 alpha)
+{
+
+  cblas_dscal(integer_conversion<int>(X.size()),
+              alpha,
+              X.data(),
+              1);
+  return;
+}
+
+real64 BlasLapackLA::vectorDot( array1d<real64> const & X,
+                                array1d<real64> const & Y)
+{
+  GEOS_ASSERT_MSG( X.size() == Y.size(),
+                   "Vector dimensions not compatible for dot product" );
+
+  return cblas_ddot(integer_conversion<int>(X.size()),
+                    X.data(),
+                    1,
+                    Y.data(),
+                    1);
+}
+
+void BlasLapackLA::vectorCopy(array1d< real64> const & X,
+                              array1d< real64> & Y)
+{
+  GEOS_ASSERT_MSG( X.size() == Y.size(),
+                   "Vector dimensions not compatible for copying" );
+
+  // Call to BLAS using CBLAS interface
+  cblas_dcopy(integer_conversion<int>(X.size()),
+              X.data(),
+              1,
+              Y.data(),
+              1);
+  return;
+}
+
+//----------------------------------------------------------------I/O methods---
+// matrix nice output
+void BlasLapackLA::print(array1d<real64> const & X)
+{
+  for( int i = 0 ; i < X.size() ; ++i )
+  {
+    printf( "%10.2e ", X[i] );
+    printf( "\n" );
+  }
+}
+
+} // end geosx namespace
---
+++ src/coreComponents/linearAlgebraInterface/src/BlasLapackLA.hpp
@@ -0,0 +1,125 @@
+/*
+ * BlasLapackLA.hpp
+ *
+ *  Created on: Apr 8, 2019
+ *      Author: castelletto1
+ */
+
+#ifndef CORECOMPONENTS_LINEARALGEBRAINTERFACE_SRC_BLASLAPACKLA_HPP_
+#define CORECOMPONENTS_LINEARALGEBRAINTERFACE_SRC_BLASLAPACKLA_HPP_
+
+#include "common/DataTypes.hpp"
+#include "Logger.hpp"
+
+#include "cblas.h"
+
+namespace geosx
+{
+
+class BlasLapackLA
+{
+  /**
+   * \class BlasLapackLA
+   * \brief This class contains a collection of BLAS and LAPACK linear
+   *        algebra operations (dense) for GEOSX array1d and array2d
+   */
+
+public:
+
+  //----------------------------------------------------------------------------
+  //! @name Mathematical methods
+  //@{
+
+  /**
+   * @brief Returns the 1-norm of the vector.
+   */
+  real64 norm1( array1d<real64> const & X ) const;
+
+  /**
+   * @brief Returns the two norm of the vector.
+   */
+  real64 norm2( array1d<real64> const & X ) const;
+
+  /**
+   * @brief Infinity-norm of the vector.
+   */
+  real64 normInf( array1d<real64> const & X ) const;
+
+  /**
+   * @brief Vector-Vector sum;
+   * <tt>y</tt> = alpha*<tt>x</tt> + <tt>y</tt>.
+   *
+   * Computes (alpha*<tt>x</tt> + <tt>y</tt>) and overwrites the result on
+   * <tt>y</tt>, with optional scaling.
+   *
+   * \param IN
+   * <tt>x</tt> - GEOSX array1d.
+   * \param [IN]
+   * alpha - Optional scalar to multiply with <tt>Vec</tt>.
+   *
+   * \param INOUT
+   * <tt>y</tt> - GEOSX array1d.
+   *
+   * @warning
+   * Assumes that <tt>x</tt> and <tt>y</tt> have the same size.
+   */
+  void vectorVectorAdd( array1d<real64> const & X,
+                        array1d<real64> & Y,
+                        real64 const alpha = 1. );
+
+  /**
+   * @brief In-place scalar-vector product;
+   * <tt>x</tt> = alpha*<tt>x<tt>
+   *
+   * \param IN
+   * scalarThis - Scalar to multiply with \a this.
+   */
+  void vectorScale( array1d<real64> & X,
+                    real64 alpha );
+
+  /**
+   * @brief Dot product of two vectors.
+   *
+   * \param IN
+   * <tt>x</tt> - GEOSX array1d.
+   * \param IN
+   * <tt>y</tt> - GEOSX array1d.
+   *
+   */
+  real64 vectorDot( array1d<real64> const & X,
+                    array1d<real64> const & Y);
+
+  /**
+   * @brief Vector copy;
+   * <tt>y</tt> = <tt>x<tt>
+   *
+   * \param IN
+   * <tt>x</tt> - GEOSX array1d.
+   *
+   * \param INOUT
+   * <tt>y</tt> - GEOSX array1d.
+   *
+   * @warning
+   * Assumes that <tt>x</tt> and <tt>y</tt> have the same size.
+   *
+   */
+  void vectorCopy( array1d<real64> const & X,
+                   array1d<real64> & Y );
+  //@}
+
+  //----------------------------------------------------------------------------
+  //! @name I/O methods
+  //@{
+
+  /**
+   * @brief Print service method; defines behavior of ostream << operator.
+   */
+  void print(array1d<real64> const & X);
+
+  //@}
+
+};
+
+}
+
+#endif /* CORECOMPONENTS_LINEARALGEBRAINTERFACE_SRC_BLASLAPACKLA_HPP_ */
