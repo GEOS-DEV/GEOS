faace1507bc619905a501ad0d24bec932cc88ae8
Sergey Klevtsov <s.klevzoff@gmail.com>
2019-05-13 09:28:55 +0300

    Add a simple unit test for SPF kernel (to be continued)

--- src/coreComponents/physicsSolvers/unitTests/testCompMultiphaseFlow.cpp
+++ src/coreComponents/physicsSolvers/unitTests/testCompMultiphaseFlow.cpp
@@ -16,16 +16,6 @@
  *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 
-/*
- * Copyright (c) 2015, Lawrence Livermore National Security, LLC.
- * Produced at the Lawrence Livermore National Laboratory.
- *
- * All rights reserved.
- *
- * This source code cannot be distributed without permission and
- * further review from Lawrence Livermore National Laboratory.
- */
-
 #ifdef __clang__
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wglobal-constructors"
