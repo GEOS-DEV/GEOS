72ab8fa040a5b0b19ffb9774936556ea2db1c957
Benjamin Curtice Corbett <corbett5@llnl.gov>
2019-06-06 17:11:50 -0700

    CMake changes.

--- src/cmake/thirdparty/SetupGeosxThirdParty.cmake
+++ src/cmake/thirdparty/SetupGeosxThirdParty.cmake
@@ -1,4 +1,4 @@
-message("\nProcessing SetupGeosxThirdParty.cmake")
+message(STATUS "\nProcessing SetupGeosxThirdParty.cmake")
 
 
 ####################################
@@ -7,9 +7,9 @@ message("\nProcessing SetupGeosxThirdParty.cmake")
 include(ExternalProject)
 
 
-message("GEOSX_TPL_DIR=${GEOSX_TPL_DIR}")
+message(STATUS "GEOSX_TPL_DIR=${GEOSX_TPL_DIR}")
 if( NOT GEOSX_TPL_DIR )
-    message("GEOSX_TPL_ROOT_DIR=${GEOSX_TPL_ROOT_DIR}")
+    message(STATUS "GEOSX_TPL_ROOT_DIR=${GEOSX_TPL_ROOT_DIR}")
     get_filename_component( TEMP_DIR "${CMAKE_INSTALL_PREFIX}" NAME)
     if( CMAKE_BUILD_TYPE MATCHES "Debug" )
         string(REGEX REPLACE "debug" "release" TEMP_DIR2 ${TEMP_DIR})
@@ -22,7 +22,7 @@ if( NOT GEOSX_TPL_DIR )
     endif()
     set( GEOSX_TPL_DIR "${GEOSX_TPL_ROOT_DIR}/${TEMP_DIR2}" )
 endif()
-message("GEOSX_TPL_DIR=${GEOSX_TPL_DIR}")
+message(STATUS "GEOSX_TPL_DIR=${GEOSX_TPL_DIR}")
 
 set( thirdPartyLibs "")
 
@@ -31,12 +31,12 @@ set( thirdPartyLibs "")
 # Conduit
 ################################
 if (NOT EXISTS ${CONDUIT_DIR})
-    message(INFO ": Using conduit from thirdPartyLibs")
+    message(STATUS "Using conduit from thirdPartyLibs")
     set(CONDUIT_DIR ${GEOSX_TPL_DIR}/conduit CACHE PATH "")
 endif()
 
 if (EXISTS ${CONDUIT_DIR})
-  message( "CONDUIT_DIR = ${CONDUIT_DIR}" )
+  message(STATUS "CONDUIT_DIR = ${CONDUIT_DIR}" )
   include( cmake/thirdparty/FindConduit.cmake )
   blt_register_library( NAME conduit
                         INCLUDES ${CONDUIT_INCLUDE_DIRS} 
@@ -57,7 +57,7 @@ if (EXISTS ${CONDUIT_DIR})
   set( thirdPartyLibs ${thirdPartyLibs} conduit conduit_blueprint conduit_relay )
 else()
   set( CONDUIT_FOUND OFF CACHE BOOL "" )
-  message( "Not using conduit" )
+  message(STATUS "Not using conduit" )
 endif()
 
 ################################
@@ -74,9 +74,9 @@ set(UNCRUSTIFY_EXECUTABLE "${GEOSX_TPL_DIR}/uncrustify/bin/uncrustify" CACHE PAT
 # HDF5
 ################################
 if( EXISTS ${HDF5_DIR})
-    message("Using system HDF5 found at ${HDF5_DIR}")
+    message(STATUS "Using system HDF5 found at ${HDF5_DIR}")
 else()
-    message(INFO ": Using HDF5 from thirdPartyLibs")
+    message(STATUS "Using HDF5 from thirdPartyLibs")
     set(HDF5_DIR ${GEOSX_TPL_DIR}/hdf5)
 endif()
 
@@ -95,15 +95,15 @@ endif()
 # SILO
 ################################
 if( EXISTS ${SILO_DIR})
-    message("Using system SILO found at ${SILO_DIR}")
+    message(STATUS "Using system SILO found at ${SILO_DIR}")
 else()
-    message(INFO ": Using SILO from thirdPartyLibs")
+    message(STATUS "Using SILO from thirdPartyLibs")
     set(SILO_DIR ${GEOSX_TPL_DIR}/silo)
 endif()
 
 include(${CMAKE_SOURCE_DIR}/cmake/thirdparty/FindSilo.cmake)
 if (NOT SILO_FOUND)
-    message(FATAL_ERROR ": SILO not found in ${SILO_DIR}. Maybe you need to build it")
+    message(FATAL_ERROR "SILO not found in ${SILO_DIR}. Maybe you need to build it")
 endif()
 blt_register_library( NAME silo
                       INCLUDES ${SILO_INCLUDE_DIRS}
@@ -117,15 +117,15 @@ set( thirdPartyLibs ${thirdPartyLibs} silo )
 # RAJA
 ################################
 if( EXISTS ${RAJA_DIR})
-    message("Using system RAJA found at ${RAJA_DIR}")
+    message(STATUS "Using system RAJA found at ${RAJA_DIR}")
 else()
-    message(INFO ": Using RAJA from thirdPartyLibs")
+    message(STATUS "Using RAJA from thirdPartyLibs")
     set(RAJA_DIR ${GEOSX_TPL_DIR}/raja)
 endif()
 
 include(${CMAKE_SOURCE_DIR}/cmake/thirdparty/FindRAJA.cmake)
 if (NOT RAJA_FOUND)
-    message(FATAL_ERROR ": RAJA not found in ${RAJA_DIR}. Maybe you need to build it")
+    message(FATAL_ERROR "RAJA not found in ${RAJA_DIR}. Maybe you need to build it")
 endif()    
 blt_register_library( NAME raja
                       INCLUDES ${RAJA_INCLUDE_DIRS}
@@ -144,7 +144,7 @@ include(${CMAKE_SOURCE_DIR}/cmake/thirdparty/FindCHAI.cmake)
 ################################
 if( USE_FPARSER )
 
-message(INFO ": Using FPARSER from thirdPartyLibs")
+message(STATUS "Using FPARSER from thirdPartyLibs")
 set(FPARSER_INSTALL_DIR ${GEOSX_TPL_DIR}/fparser)
 
 find_path( FPARSER_INCLUDE_DIRS fparser.h
@@ -168,7 +168,7 @@ find_package_handle_standard_args(FPARSER  DEFAULT_MSG
                                   FPARSER_INCLUDE_DIRS
                                   FPARSER_LIBRARY )
 if (NOT FPARSER_FOUND)
-    message(FATAL_ERROR ": FPARSER not found in ${FPARSER_DIR}. Maybe you need to build it")
+    message(STATUS "FPARSER not found in ${FPARSER_DIR}. Maybe you need to build it")
 endif()
 
 blt_register_library( NAME fparser
@@ -184,14 +184,14 @@ endif()
 # CALIPER
 ################################
 if( ENABLE_CALIPER )
-    message( INFO ": setting up caliper" )
+    message( STATUS "setting up caliper" )
 
     if( EXISTS ${CALIPER_DIR} )
-        message( INFO "Found system caliper" )
-        message("Using system CALIPER found at ${CALIPER_DIR}")
+        message( STATUS "Found system caliper" )
+        message( STATUS "Using system CALIPER found at ${CALIPER_DIR}")
         set(CALIPER_FOUND TRUE)    
     else()
-        message(INFO ": Using CALIPER from thirdPartyLibs")
+        message(STATUS "Using CALIPER from thirdPartyLibs")
         set(CALIPER_DIR ${GEOSX_TPL_DIR}/caliper)
         set(CALIPER_FOUND TRUE)
     endif()
@@ -206,7 +206,7 @@ if( ENABLE_CALIPER )
 
     set( caliper_lib_list caliper caliper-reader caliper-common gotcha caliper-mpi )
                         
-    message(INFO "looking for libs in ${CALIPER_DIR}")
+    message(STATUS "looking for libs in ${CALIPER_DIR}")
     blt_find_libraries( FOUND_LIBS CALIPER_LIBRARIES
                         NAMES ${caliper_lib_list}
                         PATHS ${CALIPER_DIR}/lib ${CALIPER_DIR}/lib64
@@ -218,10 +218,10 @@ if( ENABLE_CALIPER )
                                     CALIPER_LIBRARIES )
 
     if (NOT CALIPER_FOUND)
-        message(FATAL_ERROR ": CALIPER not found in ${CALIPER_DIR}. Maybe you need to build it")
+        message(FATAL_ERROR "CALIPER not found in ${CALIPER_DIR}. Maybe you need to build it")
     else() 
-        message("CALIPER_INCLUDE_DIRS = ${CALIPER_INCLUDE_DIRS}")
-        message("CALIPER_LIBRARIES = ${CALIPER_LIBRARIES}")    
+        message(STATUS "CALIPER_INCLUDE_DIRS = ${CALIPER_INCLUDE_DIRS}")
+        message(STATUS "CALIPER_LIBRARIES = ${CALIPER_LIBRARIES}")    
     endif()
     blt_register_library( NAME caliper
                         INCLUDES ${CALIPER_INCLUDE_DIRS}
@@ -235,16 +235,16 @@ endif()
 # ASMJIT / MATHPRESSO
 ################################
 if (NOT EXISTS ${MATHPRESSO_DIR})
-    message(INFO ": Using mathpresso from thirdPartyLibs")
+    message(STATUS "Using mathpresso from thirdPartyLibs")
     set(MATHPRESSO_DIR ${GEOSX_TPL_DIR}/mathpresso CACHE PATH "")
 endif()
 
 if (EXISTS ${MATHPRESSO_DIR})
-    message( INFO ": setting up asmjit" )
+    message( STATUS "setting up asmjit" )
     set(ASMJIT_LOCAL_DIR ${PROJECT_BINARY_DIR}/thirdparty/asmjit/src/asmjit)
     set(ASMJIT_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/thirdparty/asmjit)
 
-    message( INFO ": setting up MathPresso" )
+    message( STATUS "setting up MathPresso" )
     find_path( MATHPRESSO_INCLUDE_DIRS mathpresso/mathpresso.h
             PATHS  ${MATHPRESSO_DIR}/include
             NO_DEFAULT_PATH
@@ -268,7 +268,7 @@ if (EXISTS ${MATHPRESSO_DIR})
                                     MATHPRESSO_LIBRARY )
                                     
     if (NOT MATHPRESSO_FOUND)
-        message(FATAL_ERROR ": MATHPRESSO not found in ${MATHPRESSO_DIR}. Maybe you need to build it")
+        message(FATAL_ERROR "MATHPRESSO not found in ${MATHPRESSO_DIR}. Maybe you need to build it")
     endif()
 
     blt_register_library( NAME mathpresso
@@ -280,13 +280,13 @@ if (EXISTS ${MATHPRESSO_DIR})
     set( thirdPartyLibs ${thirdPartyLibs} mathpresso )
 else()
     set(MATHPRESSO_FOUND OFF CACHE BOOL "")
-    message(INFO ": Not using mathpresso")
+    message(STATUS "Not using mathpresso")
 endif()
 
 ################################
 # PUGIXML
 ################################
-message( INFO ": setting up pugixml" )
+message( STATUS "setting up pugixml" )
 set(PUGIXML_DIR ${GEOSX_TPL_DIR}/pugixml)
 
 
@@ -324,7 +324,7 @@ find_package_handle_standard_args(PUGIXML  DEFAULT_MSG
 
 
 if (NOT PUGIXML_FOUND)
-    message(FATAL_ERROR ": PUGIXML not found in ${PUGIXML_DIR}. Maybe you need to build it")
+    message(FATAL_ERROR "PUGIXML not found in ${PUGIXML_DIR}. Maybe you need to build it")
 endif()
 
 blt_register_library( NAME pugixml
@@ -342,7 +342,7 @@ if( ENABLE_TRILINOS )
   if(EXISTS ${TRILINOS_DIR})
   
   else()
-      message( INFO ": setting up TRILINOS" )
+      message( STATUS "setting up TRILINOS" )
       set(TRILINOS_DIR ${GEOSX_TPL_DIR}/trilinos)
   endif()
   
@@ -365,7 +365,7 @@ endif()
 # METIS
 ################################
 if( ENABLE_METIS )
-    message( INFO ": setting up METIS" )
+    message( STATUS "setting up METIS" )
 
     set(METIS_DIR ${GEOSX_TPL_DIR}/metis)
 
@@ -385,12 +385,12 @@ if( ENABLE_METIS )
               NO_SYSTEM_ENVIRONMENT_PATH
               NO_CMAKE_SYSTEM_PATH)
 
-    message( "METIS_INCLUDE_DIRS = ${METIS_INCLUDE_DIRS}" )
-    message( "METIS_LIBRARY = ${METIS_LIBRARY}" )
+    message(STATUS "METIS_INCLUDE_DIRS = ${METIS_INCLUDE_DIRS}" )
+    message(STATUS "METIS_LIBRARY = ${METIS_LIBRARY}" )
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(METIS DEFAULT_MSG METIS_INCLUDE_DIRS METIS_LIBRARY )
     if (NOT METIS_FOUND)
-        message(FATAL_ERROR ": METIS not found in ${METIS_DIR}. Maybe you need to build it")
+        message(FATAL_ERROR "METIS not found in ${METIS_DIR}. Maybe you need to build it")
     endif()
 
     blt_register_library( NAME metis
@@ -405,7 +405,7 @@ endif()
 # PARMETIS
 ################################
 if( ENABLE_PARMETIS )
-    message( INFO ": setting up PARMETIS" )
+    message( STATUS "setting up PARMETIS" )
 
     set(PARMETIS_DIR ${GEOSX_TPL_DIR}/parmetis)
 
@@ -425,14 +425,14 @@ if( ENABLE_PARMETIS )
               NO_SYSTEM_ENVIRONMENT_PATH
               NO_CMAKE_SYSTEM_PATH)
 
-    message( "PARMETIS_INCLUDE_DIRS = ${PARMETIS_INCLUDE_DIRS}" )
-    message( "PARMETIS_LIBRARY = ${PARMETIS_LIBRARY}" )
+    message(STATUS "PARMETIS_INCLUDE_DIRS = ${PARMETIS_INCLUDE_DIRS}" )
+    message(STATUS "PARMETIS_LIBRARY = ${PARMETIS_LIBRARY}" )
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(PARMETIS  DEFAULT_MSG
                                     PARMETIS_INCLUDE_DIRS
                                     PARMETIS_LIBRARY )
     if (NOT PARMETIS_FOUND)
-        message(FATAL_ERROR ": PARMETIS not found in ${PARMETIS_DIR}. Maybe you need to build it")
+        message(FATAL_ERROR "PARMETIS not found in ${PARMETIS_DIR}. Maybe you need to build it")
     endif()
 
     blt_register_library( NAME parmetis
@@ -447,7 +447,7 @@ endif()
 # SUPERLU_DIST
 ################################
 if( ENABLE_SUPERLU_DIST)
-    message( INFO ": setting up SUPERLU_DIST" )
+    message( STATUS "setting up SUPERLU_DIST" )
 
     set(SUPERLU_DIST_DIR ${GEOSX_TPL_DIR}/superlu_dist)
 
@@ -467,14 +467,14 @@ if( ENABLE_SUPERLU_DIST)
               NO_SYSTEM_ENVIRONMENT_PATH
               NO_CMAKE_SYSTEM_PATH)
 
-    message( "SUPERLU_DIST_INCLUDE_DIRS = ${SUPERLU_DIST_INCLUDE_DIRS}" )
-    message( "SUPERLU_DIST_LIBRARY = ${SUPERLU_DIST_LIBRARY}" )
+    message(STATUS "SUPERLU_DIST_INCLUDE_DIRS = ${SUPERLU_DIST_INCLUDE_DIRS}" )
+    message(STATUS "SUPERLU_DIST_LIBRARY = ${SUPERLU_DIST_LIBRARY}" )
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(SUPERLU_DIST  DEFAULT_MSG
                                 SUPERLU_DIST_INCLUDE_DIRS
                     SUPERLU_DIST_LIBRARY )
     if (NOT SUPERLU_DIST_FOUND)
-        message(FATAL_ERROR ": SUPERLU_DIST not found in ${SUPERLU_DIST_DIR}. Maybe you need to build it")
+        message(FATAL_ERROR "SUPERLU_DIST not found in ${SUPERLU_DIST_DIR}. Maybe you need to build it")
     endif()
 
     blt_register_library( NAME superlu_dist
@@ -489,7 +489,7 @@ endif()
 # HYPRE
 ################################
 if( ENABLE_HYPRE )
-    message( INFO ": setting up HYPRE" )
+    message( STATUS "setting up HYPRE" )
 
     set(HYPRE_DIR ${GEOSX_TPL_DIR}/hypre)
 
@@ -509,14 +509,14 @@ if( ENABLE_HYPRE )
                 NO_SYSTEM_ENVIRONMENT_PATH
                 NO_CMAKE_SYSTEM_PATH)
 
-    message( "HYPRE_INCLUDE_DIRS = ${HYPRE_INCLUDE_DIRS}" )
-    message( "HYPRE_LIBRARY = ${HYPRE_LIBRARY}" )
+    message(STATUS "HYPRE_INCLUDE_DIRS = ${HYPRE_INCLUDE_DIRS}" )
+    message(STATUS "HYPRE_LIBRARY = ${HYPRE_LIBRARY}" )
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(HYPRE  DEFAULT_MSG
                                     HYPRE_INCLUDE_DIRS
                                     HYPRE_LIBRARY )
     if (NOT HYPRE_FOUND)
-        message(FATAL_ERROR ": HYPRE not found in ${HYPRE_DIR}. Maybe you need to build it")
+        message(FATAL_ERROR "HYPRE not found in ${HYPRE_DIR}. Maybe you need to build it")
     endif()
 
     blt_register_library( NAME hypre
@@ -529,9 +529,32 @@ endif()
 
 
 ################################
+# Intel MKKL
+################################
+if (ENABLE_MKL AND EXISTS ${MKL_ROOT})
+    message( STATUS "setting up Intel MKL" )
+
+    find_library( MKL_LIBRARY NAMES ${MKL_LIBRARY_NAMES}
+                  PATHS ${MKL_ROOT}/lib
+                  NO_DEFAULT_PATH
+                  NO_CMAKE_ENVIRONMENT_PATH
+                  NO_CMAKE_PATH
+                  NO_SYSTEM_ENVIRONMENT_PATH
+                  NO_CMAKE_SYSTEM_PATH)
+
+    blt_register_library( NAME mkl
+                          INCLUDES ${MKL_ROOT}/include
+                          LIBRARIES ${MKL_LIBRARY}
+                          TREAT_INCLUDES_AS_SYSTEM ON )
+    
+    set( thirdPartyLibs ${thirdPartyLibs} mkl )
+endif()
+
+
+################################
 # unsrustify
 ################################
-message("UNCRUSTIFY_FOUND = ${UNCRUSTIFY_FOUND}")
+message(STATUS "UNCRUSTIFY_FOUND = ${UNCRUSTIFY_FOUND}")
 if(UNCRUSTIFY_FOUND)
     # targets for verifying formatting
     if( NOT TARGET uncrustify_check )
@@ -547,4 +570,4 @@ if(UNCRUSTIFY_FOUND)
     
 endif()
 
-message("Leaving SetupGeosxThirdParty.cmake\n")
+message(STATUS "Leaving SetupGeosxThirdParty.cmake\n")
