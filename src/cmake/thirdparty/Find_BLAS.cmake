0bcaf6a00411c8707712643ffb44c43a441ff0a9
Randolph R. Settgast <settgast1@llnl.gov>
2019-06-22 16:35:15 -0700

    re-enable specification of MKL independent of BLAS/LAPACK

--- src/cmake/thirdparty/Find_BLAS.cmake
+++ src/cmake/thirdparty/Find_BLAS.cmake
@@ -18,14 +18,18 @@ else()
                NO_SYSTEM_ENVIRONMENT_PATH
                NO_CMAKE_SYSTEM_PATH)
      
-     find_library( BLAS_LIBRARIES 
-              NAMES blas libmkl_rt.so 
-              PATHS ${BLAS_DIR}/lib ${BLAS_DIR}/lib64
-              NO_DEFAULT_PATH
-              NO_CMAKE_ENVIRONMENT_PATH
-              NO_CMAKE_PATH
-              NO_SYSTEM_ENVIRONMENT_PATH
-              NO_CMAKE_SYSTEM_PATH)
+    if( NOT DEFINED BLAS_LIBRARY_NAMES )
+        set( BLAS_LIBRARY_NAMES "blas;cblas;libmkl_rt.so" CACHE STRING "")
+    endif()
+    
+    find_library( BLAS_LIBRARIES 
+                  NAMES ${BLAS_LIBRARY_NAMES}
+                  PATHS ${BLAS_DIR}/lib ${BLAS_DIR}/lib64
+                  NO_DEFAULT_PATH
+                  NO_CMAKE_ENVIRONMENT_PATH
+                  NO_CMAKE_PATH
+                  NO_SYSTEM_ENVIRONMENT_PATH
+                  NO_CMAKE_SYSTEM_PATH)
               
     list( GET BLAS_LIBRARIES 0 BLAS_LIB0 )
     get_filename_component( BLAS_LIBRARY_DIR ${BLAS_LIB0} DIRECTORY CACHE )
@@ -40,7 +44,7 @@ find_package_handle_standard_args( BLAS
                                    BLAS_LIBRARIES
                                    )
 
-set(BLAS_LIBRARY_NAMES "" CACHE PATH "")
+set(BLAS_LIBRARY_NAMES "" CACHE STRING "" FORCE)
 foreach( lib ${BLAS_LIBRARIES} )
     get_filename_component( libname ${lib} NAME )
     list( APPEND BLAS_LIBRARY_NAMES ${libname} )
