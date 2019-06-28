9a101e2f42c7809f047d2e689e979b58781c7ab7
Antoine Mazuyer <amazuyer@stanford.edu>
2019-06-27 18:43:03 -0700

    ebgin work

---
+++ src/coreComponents/managers/Outputs/VTKOutput.cpp
@@ -0,0 +1,70 @@
+/*
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Copyright (c) 2019, Lawrence Livermore National Security, LLC.
+ *
+ * Produced at the Lawrence Livermore National Laboratory
+ *
+ * LLNL-CODE-746361
+ *
+ * All rights reserved. See COPYRIGHT for details.
+ *
+ * This file is part of the GEOSX Simulation Framework.
+ *
+ * GEOSX is a free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License (as published by the
+ * Free Software Foundation) version 2.1 dated February 1999.
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+/**
+ * @file VTKOutput.cpp
+ */
+
+#include "VTKOutput.hpp"
+#include "fileIO/vtk/VTKFile.hpp"
+
+namespace geosx
+{
+
+using namespace dataRepository;
+using namespace cxx_utilities;
+
+VTKOutput::VTKOutput( std::string const & name,
+                        ManagedGroup * const parent ):
+  OutputBase( name, parent),
+  m_plotFileRoot(),
+  m_writeFaceMesh(),
+  m_plotLevel()
+{
+  RegisterViewWrapper(viewKeysStruct::plotFileRoot, &m_plotFileRoot, false )->
+    setInputFlag(InputFlags::OPTIONAL)->
+    setDescription("");
+
+  RegisterViewWrapper(viewKeysStruct::writeFEMFaces, &m_writeFaceMesh, false )->
+    setInputFlag(InputFlags::OPTIONAL)->
+    setDescription("");
+
+  RegisterViewWrapper(viewKeysStruct::plotLevel, &m_plotLevel, false )->
+    setApplyDefaultValue(1)->
+    setInputFlag(InputFlags::OPTIONAL)->
+    setDescription("");
+
+}
+
+VTKOutput::~VTKOutput()
+{}
+
+
+
+void VTKOutput::Execute(real64 const time_n,
+                         real64 const dt,
+                         integer const cycleNumber,
+                         integer const eventCounter,
+                         real64 const eventProgress,
+                         ManagedGroup * domain)
+{
+}
+
+
+REGISTER_CATALOG_ENTRY( OutputBase, VTKOutput, std::string const &, ManagedGroup * const )
+} /* namespace geosx */
