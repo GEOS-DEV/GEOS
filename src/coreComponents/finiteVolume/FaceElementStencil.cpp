52d837bc8fc64cc99da505c9fd498d30e9635f85
Randolph R. Settgast <settgast1@llnl.gov>
2019-07-08 10:37:27 -0700

    stuck on FluxStencil modifications and the FaceStencil. Enable ArrayOfArray for ToElement relations in another branch off of develop in hopes of a better interface. rebase this commit message out

---
+++ src/coreComponents/finiteVolume/FaceElementStencil.cpp
@@ -0,0 +1,24 @@
+/*
+ * FaceElementStencil.cpp
+ *
+ *  Created on: Jul 1, 2019
+ *      Author: settgast
+ */
+
+#include "FaceElementStencil.hpp"
+
+namespace geosx
+{
+
+FaceElementStencil::FaceElementStencil()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+FaceElementStencil::~FaceElementStencil()
+{
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace geosx */
