847d7a5e3ee9751baa271cfbbdadf131051411e2
Benjamin Curtice Corbett <corbett5@llnl.gov>
2019-07-23 11:12:22 -0700

    Fixed host device errors.

--- src/coreComponents/common/GeosxMacros.hpp
+++ src/coreComponents/common/GeosxMacros.hpp
@@ -29,21 +29,21 @@
 #define GEOSX_LAMBDA [=]
 
 #if defined(__CUDACC__)
+  #define GEOSX_HOST __host__
+  #define GEOSX_DEVICE __device__
+  #define GEOSX_HOST_DEVICE __host__ __device__
 
-#define GEOSX_HOST __host__
-#define GEOSX_DEVICE __device__
-#define GEOSX_HOST_DEVICE __host__ __device__
+  #define GEOSX_DEVICE_LAMBDA [=] __device__
 
-#define GEOSX_DEVICE_LAMBDA [=] __device__
-
+  #define GEOSX_FORCE_INLINE __forceinline__
 #else
+  #define GEOSX_HOST
+  #define GEOSX_DEVICE
+  #define GEOSX_HOST_DEVICE
 
-#define GEOSX_HOST
-#define GEOSX_DEVICE
-#define GEOSX_HOST_DEVICE
+  #define GEOSX_DEVICE_LAMBDA [=]
 
-#define GEOSX_DEVICE_LAMBDA [=]
-
+  #define GEOSX_FORCE_INLINE inline
 #endif
 
 #endif // COMMON_GEOSXMACROS_HPP_
