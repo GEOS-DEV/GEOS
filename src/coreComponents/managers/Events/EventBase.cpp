35454f0201f8f129b31a38df7d1aaff388781caa
Randolph R. Settgast <settgast1@llnl.gov>
2019-08-06 14:18:06 -0700

    1) moved block matrix scaling to its own function 2) instrumented some funcitons with caliper marks

--- src/coreComponents/managers/Events/EventBase.cpp
+++ src/coreComponents/managers/Events/EventBase.cpp
@@ -217,6 +217,7 @@ void EventBase::Execute(real64 const time_n,
 {
   GEOSX_MARK_FUNCTION;
   
+  GEOSX_MARK_BEGIN("EventBase::Execute() 1");
   // If m_targetExecFlag is set, then the code has resumed at a point
   // after the target has executed. 
   if ((m_target != nullptr) && (m_targetExecFlag == 0))
@@ -224,7 +225,10 @@ void EventBase::Execute(real64 const time_n,
     m_targetExecFlag = 1;
     m_target->Execute(time_n, dt, cycleNumber, m_eventCount, m_eventProgress, domain);
   }
+  GEOSX_MARK_END("EventBase::Execute() 1");
   
+
+  GEOSX_MARK_BEGIN("EventBase::Execute() 2");
   // Iterate through the sub-event list using the managed integer m_currentSubEvent
   // This allows for  restart runs to pick up where they left off.
   for ( ; m_currentSubEvent < this->numSubGroups(); ++m_currentSubEvent)
@@ -242,6 +246,7 @@ void EventBase::Execute(real64 const time_n,
       subEvent->Execute(time_n, dt, cycleNumber, m_eventCount, m_eventProgress, domain);
     }
   }
+  GEOSX_MARK_END("EventBase::Execute() 2");
 
   // Update the event status
   m_targetExecFlag = 0;
