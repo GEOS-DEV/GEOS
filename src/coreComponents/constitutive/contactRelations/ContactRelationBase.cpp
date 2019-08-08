3845bff69ab09adf485f319101d4aea9e604902d
Randolph R. Settgast <settgast1@llnl.gov>
2019-08-07 17:54:37 -0700

    added doxygen to ContactRelationBase

--- src/coreComponents/constitutive/contactRelations/ContactRelationBase.cpp
+++ src/coreComponents/constitutive/contactRelations/ContactRelationBase.cpp
@@ -1,8 +1,23 @@
 /*
- * ContactRelationBase.cpp
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Copyright (c) 2019, Lawrence Livermore National Security, LLC.
  *
- *  Created on: Aug 3, 2019
- *      Author: settgast
+ * Produced at the Lawrence Livermore National Laboratory
+ *
+ * LLNL-CODE-746361
+ *
+ * All rights reserved. See COPYRIGHT for details.
+ *
+ * This file is part of the GEOSX Simulation Framework.
+ *
+ * GEOSX is a free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License (as published by the
+ * Free Software Foundation) version 2.1 dated February 1999.
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+/**
+ * @file ContactRelationBase.cpp
  */
 
 #include "ContactRelationBase.hpp"
