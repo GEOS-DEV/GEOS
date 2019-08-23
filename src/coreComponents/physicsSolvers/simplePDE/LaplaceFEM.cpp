46ebcce439daa1e45ce5370f0b9028c7d5339b4e
Andrea Franceschini <franc90@dmsa.unipd.it>
2019-08-23 16:18:40 -0700

    Added restart check to SimpleSolver LagrageFEM

--- src/coreComponents/physicsSolvers/simplePDE/LaplaceFEM.cpp
+++ src/coreComponents/physicsSolvers/simplePDE/LaplaceFEM.cpp
@@ -291,7 +291,7 @@ void LaplaceFEM::ApplySystemSolution( DofManager const & dofManager,
   MeshLevel * const mesh = domain->getMeshBody( 0 )->getMeshLevel( 0 );
   NodeManager * const nodeManager = mesh->getNodeManager();
 
-  dofManager.copyVectorToField( solution, m_fieldName, scalingFactor, nodeManager, m_fieldName );
+  dofManager.addVectorToField( solution, m_fieldName, scalingFactor, nodeManager, m_fieldName );
 
   // Syncronize ghost nodes
   std::map<string, string_array> fieldNames;
