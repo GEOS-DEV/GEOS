f5eba4c2cffcc60d09f1fc6c943efd3c0e40a114
Herve Gross <herve.gross@total.com>
2019-09-07 23:16:52 -0700

    More cleanup

--- src/coreComponents/fileIO/unitTests/testXmlWrapper.cpp
+++ src/coreComponents/fileIO/unitTests/testXmlWrapper.cpp
@@ -12,15 +12,6 @@
  * ------------------------------------------------------------------------------------------------------------
  */
 
-/*
- * Copyright (c) 2015, Lawrence Livermore National Security, LLC.
- * Produced at the Lawrence Livermore National Laboratory.
- *
- * All rights reserved.
- *
- * This source code cannot be distributed without permission and
- * further review from Lawrence Livermore National Laboratory.
- */
 
 #include <gtest/gtest.h>
 
@@ -163,4 +154,3 @@ int main( int argc, char* argv[] )
 
   return result;
 }
-
--- src/coreComponents/physicsSolvers/unitTests/testLaplaceFEM.cpp
+++ src/coreComponents/physicsSolvers/unitTests/testLaplaceFEM.cpp
@@ -12,15 +12,7 @@
  * ------------------------------------------------------------------------------------------------------------
  */
 
-/*
- * Copyright (c) 2015, Lawrence Livermore National Security, LLC.
- * Produced at the Lawrence Livermore National Laboratory.
- *
- * All rights reserved.
- *
- * This source code cannot be distributed without permission and
- * further review from Lawrence Livermore National Laboratory.
- */
+
 
 #ifdef __clang__
 #pragma clang diagnostic push
