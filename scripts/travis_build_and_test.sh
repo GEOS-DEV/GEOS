c978aea3dfe0f093c71ff1a14b4f93749cdc30c8
Randolph R. Settgast <settgast1@llnl.gov>
2019-09-18 11:07:44 -0700

    repopulated teh travis build script with appropriate commmands

--- scripts/travis_build_and_test.sh
+++ scripts/travis_build_and_test.sh
@@ -9,10 +9,12 @@ function or_die () {
     fi
 }
 
-cd /home/geosx/GEOSX
-python scripts/config-build.py -hc hostconfigs/environment.cmake -bt ${CMAKE_BUILD_TYPE}
-
-
+#export CMAKE_BUILD_TYPE=Debug
+or_die cd /home/geosx/GEOSX
+or_die python scripts/config-build.py -hc host-configs/environment.cmake -bt ${CMAKE_BUILD_TYPE} -DENABLE_GEOSX_PTP:BOOL=ON
+or_die cd build-environment-debug
+or_die make -j 2 VERBOSE=1
+or_die ctest -V
 
 
 exit 0
