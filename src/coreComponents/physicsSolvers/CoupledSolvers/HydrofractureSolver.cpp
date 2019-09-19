b544cd61e7734ae1ac9e38ba379139a877f9a059
Randolph R. Settgast <settgast1@llnl.gov>
2019-09-18 22:42:20 -0700

    fix merge errors

--- src/coreComponents/physicsSolvers/CoupledSolvers/HydrofractureSolver.cpp
+++ src/coreComponents/physicsSolvers/CoupledSolvers/HydrofractureSolver.cpp
@@ -242,7 +242,6 @@ void HydrofractureSolver::UpdateDeformationForCoupling( DomainPartition * const 
       arrayView1d<real64> const & volume = subRegion->getElementVolume();
       arrayView1d<real64> const & deltaVolume = subRegion->getReference<array1d<real64> >(FlowSolverBase::viewKeyStruct::deltaVolumeString);
       arrayView1d<real64> const & area = subRegion->getElementArea();
-      //array1d< array1d<localIndex> > const & elemsToNodes = subRegion->nodeList();
       arrayView2d< localIndex const > const & elemsToFaces = subRegion->faceList();
 
       for( localIndex kfe=0 ; kfe<subRegion->size() ; ++kfe )
@@ -272,163 +271,7 @@ void HydrofractureSolver::UpdateDeformationForCoupling( DomainPartition * const 
 }
 
 
-//void HydrofractureSolver::ApplyFractureFluidCoupling( DomainPartition * const domain,
-//                                                      systemSolverInterface::EpetraBlockSystem & blockSystem )
-//{
-//  MeshLevel * const mesh = domain->getMeshBodies()->GetGroup<MeshBody>(0)->getMeshLevel(0);
-//
-//  FaceManager const * const faceManager = mesh->getFaceManager();
-//  NodeManager * const nodeManager = mesh->getNodeManager();
-//  ElementRegionManager * const elemManager = mesh->getElemManager();
-//
-//  arrayView1d<R1Tensor> const & u = nodeManager->getReference< array1d<R1Tensor> >( keys::TotalDisplacement );
-//
-//  arrayView1d<real64 const>   const & faceArea   = faceManager->faceArea();
-//  arrayView1d<R1Tensor const> const & faceNormal = faceManager->faceNormal();
-//  array1d<localIndex_array> const & facesToNodes = faceManager->nodeList();
-//  arrayView1d<R1Tensor> const & fext = nodeManager->getReference< array1d<R1Tensor> >( SolidMechanicsLagrangianFEM::viewKeyStruct::forceExternal );
-//  fext = {0,0,0};
-//
-//
-//  ConstitutiveManager const * const
-//  constitutiveManager = domain->GetGroup<ConstitutiveManager>(keys::ConstitutiveManager);
-//
-//  ContactRelationBase const * const
-//  contactRelation = constitutiveManager->GetGroup<ContactRelationBase>(m_contactRelationName);
-//
-//  arrayView1d<globalIndex> const &
-//  blockLocalDofNumber =  nodeManager->getReference<globalIndex_array>(SolidMechanicsLagrangianFEM::viewKeyStruct::globalDofNumberString);
-//  Epetra_FEVector * const rhs = blockSystem.GetResidualVector( systemSolverInterface::BlockIDs::displacementBlock );
-//  Epetra_FECrsMatrix * const matrix = blockSystem.GetMatrix( systemSolverInterface::BlockIDs::displacementBlock,
-//                                                              systemSolverInterface::BlockIDs::displacementBlock );
-//
-//  elemManager->forElementSubRegions<FaceElementSubRegion>([&]( FaceElementSubRegion * const subRegion )->void
-//  {
-//    if( subRegion->hasView("pressure") )
-//    {
-//
-//      arrayView1d<real64> const & aperture = subRegion->getElementAperture();
-//
-//      arrayView1d<real64 const> const & fluidPressure = subRegion->getReference<array1d<real64> >("pressure");
-//      arrayView1d<real64 const> const & deltaFluidPressure = subRegion->getReference<array1d<real64> >("deltaPressure");
-//
-//      arrayView1d<real64> const & volume = subRegion->getElementVolume();
-//      arrayView1d<real64> const & deltaVolume = subRegion->getReference<array1d<real64> >(FlowSolverBase::viewKeyStruct::deltaVolumeString);
-//      arrayView1d<real64> const & area = subRegion->getElementArea();
-//      array1d< array1d<localIndex> > const & elemsToNodes = subRegion->nodeList();
-//      arrayView2d< localIndex const > const & elemsToFaces = subRegion->faceList();
-//
-//
-//      forall_in_range<serialPolicy>( 0,
-//                                   subRegion->size(),
-//                                   GEOSX_LAMBDA ( localIndex const kfe )
-//      {
-//
-//        R1Tensor Nbar = faceNormal[elemsToFaces[kfe][0]];
-//        Nbar -= faceNormal[elemsToFaces[kfe][1]];
-//        Nbar.Normalize();
-//
-//        localIndex const kf0 = elemsToFaces[kfe][0];
-//        localIndex const kf1 = elemsToFaces[kfe][1];
-//        localIndex const numNodesPerFace=facesToNodes[kf0].size();
-//        localIndex const * const nodelist0 = facesToNodes[kf0];
-//        localIndex const * const nodelist1 = facesToNodes[kf1];
-//        R1Tensor temp;
-//        for( localIndex a=0 ; a<numNodesPerFace ; ++a )
-//        {
-//          temp += u[nodelist0[a]];
-//          temp -= u[nodelist1[a]];
-//        }
-//        area[kfe] = 0.5 * ( faceArea[elemsToFaces[kfe][0]] + faceArea[elemsToFaces[kfe][1]] );
-//        // TODO this needs a proper contact based strategy for aperture
-//        aperture[kfe] = -Dot(temp,Nbar) / numNodesPerFace;
-//        aperture[kfe] = contactRelation->effectiveAperture( aperture[kfe] );
-//
-//        deltaVolume[kfe] = aperture[kfe] * area[kfe] - volume[kfe];
-//        std::cout<<"kfe, area, aperture, volume, dVolume = "<<kfe<<", "<<area[kfe]<<", "<<aperture[kfe]<<", "<<volume[kfe]<<", "<<deltaVolume[kfe]<<std::endl;
-//
-//
-//
-//
-//        globalIndex rowDOF[24];
-//        globalIndex colDOF[24];
-//        real64 nodeRHS[24];
-//        stackArray2d<real64, 12*12> dRdU(numNodesPerFace*3, numNodesPerFace*3);
-//
-//        real64 nodalForceMag = ( fluidPressure[kfe]+deltaFluidPressure[kfe] ) * faceArea[elemsToFaces[kfe][0]] / numNodesPerFace;
-//        std::cout<<"fluidPressure[kfe]+deltaFluidPressure[kfe] = "<<fluidPressure[kfe]<<" + "<<deltaFluidPressure[kfe]<<std::endl;
-//        R1Tensor nodalForce(Nbar);
-//        nodalForce *= nodalForceMag;
-//        for( localIndex kf=0 ; kf<2 ; ++kf )
-//        {
-//          localIndex const faceIndex = elemsToFaces[kfe][kf];
-//
-//          for( localIndex a=0 ; a<numNodesPerFace ; ++a )
-//          {
-//            for( int component=0 ; component<3 ; ++component )
-//            {
-//              rowDOF[3*a+component] = blockLocalDofNumber[facesToNodes[faceIndex][a]]+component;
-//              nodeRHS[3*a+component] = - nodalForce[component] * pow(-1,kf);
-//              fext[facesToNodes[faceIndex][a]][component] += - nodalForce[component] * pow(-1,kf);
-//            }
-//          }
-//
-//          rhs->SumIntoGlobalValues( integer_conversion<int>(numNodesPerFace*3), rowDOF, nodeRHS );
-//        }
-//
-//
-//        real64 dAper_dU[2][4][3];
-//        for( localIndex kf=0 ; kf<2 ; ++kf )
-//        {
-//          for( localIndex a=0 ; a<numNodesPerFace ; ++a )
-//          {
-//            for( int component=0 ; component<3 ; ++component )
-//            {
-//              dAper_dU[kf][a][component] = - pow(-1,kf) * Nbar[component] / numNodesPerFace;
-//            }
-//          }
-//        }
-//
-//        real64 const K = 2e9;
-//        real64 const dP_dAper = -K / aperture[kfe];
-//
-//        for( localIndex kfa=0 ; kfa<2 ; ++kfa )
-//        {
-//          localIndex const faceIndexA = elemsToFaces[kfe][kfa];
-//          for( localIndex kfb=0 ; kfb<2 ; ++kfb )
-//          {
-//            localIndex const faceIndexb = elemsToFaces[kfe][kfb];
-//            for( int a=0 ; a<numNodesPerFace ; ++a )
-//            {
-//              for( int i=0 ; i<3 ; ++i )
-//              {
-//                rowDOF[3*a+i] = blockLocalDofNumber[facesToNodes[faceIndexA][a]]+i;
-//                real64 const integrationFactor = - faceArea[elemsToNodes[kfe][0]] / numNodesPerFace * Nbar[i] * pow(-1,kfa);
-//
-//                for( int b=0 ; b<numNodesPerFace ; ++b )
-//                {
-//                  for( int j=0 ; j<3 ; ++j )
-//                  {
-//                    colDOF[3*b+j] = blockLocalDofNumber[facesToNodes[faceIndexb][b]]+j;
-//                    dRdU(3*a+i,3*b+j) = integrationFactor * dP_dAper * dAper_dU[kfb][b][j];
-//                  }
-//                }
-//              }
-//            }
-//            matrix->SumIntoGlobalValues( integer_conversion<int>( numNodesPerFace * 3),
-//                                         rowDOF,
-//                                         integer_conversion<int>( numNodesPerFace * 3),
-//                                         colDOF,
-//                                         dRdU.data() );
-//
-//          }
-//        }
-//
-//
-//      });
-//    }
-//  });
-//}
+
 
 
 real64 HydrofractureSolver::SplitOperatorStep( real64 const & GEOSX_UNUSED_ARG( time_n ),
@@ -841,7 +684,6 @@ AssembleForceResidualDerivativeWrtPressure( DomainPartition * const domain,
   NodeManager * const nodeManager = mesh->getNodeManager();
   ElementRegionManager * const elemManager = mesh->getElemManager();
 
-//  arrayView1d<real64 const>   const & faceArea   = faceManager->faceArea();
   arrayView1d<R1Tensor const> const & faceNormal = faceManager->faceNormal();
   ArrayOfArraysView< localIndex const > const & faceToNodeMap = faceManager->nodeList();
 
@@ -884,7 +726,6 @@ AssembleForceResidualDerivativeWrtPressure( DomainPartition * const domain,
           Nbar.Normalize();
 
           localIndex const kf0 = elemsToFaces[kfe][0];
-//          localIndex const kf1 = elemsToFaces[kfe][1];
           localIndex const numNodesPerFace = faceToNodeMap.sizeOfArray(kf0);
 
           globalIndex rowDOF[24];
@@ -1014,7 +855,7 @@ AssembleFluidMassResidualDerivativeWrtDisplacement( DomainPartition const * cons
           {
             for( int i=0 ; i<3 ; ++i )
             {
-              nodeDOF[ kf*3*numNodesPerFace + 3*a+i] = 3*dispDofNumber[faceToNodeMap(elemsToFaces[ei][kf],a)] +i;
+              nodeDOF[ kf*3*numNodesPerFace + 3*a+i] = dispDofNumber[faceToNodeMap(elemsToFaces[ei][kf],a)] +i;
               real64 const dGap_dU = - pow(-1,kf) * Nbar[i] / numNodesPerFace;
               real64 const dAper_dU = contactRelation->dEffectiveAperture_dAperture( aperture[ei] ) * dGap_dU;
               dRdU(kf*3*numNodesPerFace + 3*a+i) = dRdAper * dAper_dU;
