b9b8b2c66c14143397c24fd04b7bbadf93a1fa3f
Jixiang Huang <huang40@llnl.gov>
2019-10-02 11:11:27 -0700

    return maxStableDt to the event

--- src/coreComponents/physicsSolvers/FiniteVolume/SinglePhaseFlow.cpp
+++ src/coreComponents/physicsSolvers/FiniteVolume/SinglePhaseFlow.cpp
@@ -390,8 +390,7 @@ real64 SinglePhaseFlow::ExplicitStep( real64 const& time_n,
     UpdateState( subRegion );
   } );
 
-  m_maxStableDt = 1;
-  return m_maxStableDt;
+  return dt;
 }
 
 void SinglePhaseFlow::ExplicitStepSetup( real64 const & time_n,
@@ -858,8 +857,6 @@ void SinglePhaseFlow::AssembleFluxTermsExplicit( real64 const GEOSX_UNUSED_ARG( 
                         &m_mass,
                         &m_maxStableDt);
   });
-
-  m_maxStableDt *= m_cflFactor;
 
 }
 
