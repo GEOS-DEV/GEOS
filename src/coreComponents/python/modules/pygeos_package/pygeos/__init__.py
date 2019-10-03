74bb1b6c519328c429a769dd50fc0aa3e4b0a189
Christopher Sherman <sherman27@llnl.gov>
2019-10-03 15:06:54 -0700

    Updating pygeos symbolic math, testing, formatting

--- src/coreComponents/python/modules/pygeos_package/pygeos/__init__.py
+++ src/coreComponents/python/modules/pygeos_package/pygeos/__init__.py
@@ -1,11 +1,4 @@
 
-__version__ = '0.3.1'
+__version__ = '0.4.0'
 
-
-from .regex_config import regexConfig, symbolicMathRegexHandler, parameterHandler, DictRegexHandler
-from .unit_manager import unitManager 
-from .xml_processor import preprocessGEOSXML, validateXML
-from .table_generator import writeGEOSTable, readGEOSTable
-from .test_manager import runUnitTests
-from .format_xml import format_xml_file
 
