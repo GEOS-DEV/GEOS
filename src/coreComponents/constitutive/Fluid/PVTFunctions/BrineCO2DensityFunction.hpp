a0f9715e429481e948600d17a05b68f0331941a7
Yue Hao <hao1@llnl.gov>
2019-10-07 10:55:38 -0700

    Updated the headers in all the new files

--- src/coreComponents/constitutive/Fluid/PVTFunctions/BrineCO2DensityFunction.hpp
+++ src/coreComponents/constitutive/Fluid/PVTFunctions/BrineCO2DensityFunction.hpp
@@ -1,28 +1,23 @@
-
 /*
- *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Copyright (c) 2019, Lawrence Livermore National Security, LLC.
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: LGPL-2.1-only
  *
- * Produced at the Lawrence Livermore National Laboratory
+ * Copyright (c) 2018-2019 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2019 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2018-2019 Total, S.A
+ * Copyright (c) 2019-     GEOSX Contributors
+ * All right reserved
  *
- * LLNL-CODE-746361
- *
- * All rights reserved. See COPYRIGHT for details.
- *
- * This file is part of the GEOSX Simulation Framework.
- *
- * GEOSX is a free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License (as published by the
- * Free Software Foundation) version 2.1 dated February 1999.
- *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
  */
 
 /**
  * @file BrineCO2DensityFunction.hpp
  */
 
-#ifndef SRC_COMPONENTS_CORE_SRC_CONSTITUTIVE_BRINECO2DENSITYFUNCTION_HPP
-#define SRC_COMPONENTS_CORE_SRC_CONSTITUTIVE_BRINECO2DENSITYFUNCTION_HPP
+#ifndef GEOSX_CONSTITUTIVE_FLUID_PVTFUNCTIONS_BRINECO2DENSITYFUNCTION_HPP_
+#define GEOSX_CONSTITUTIVE_FLUID_PVTFUNCTIONS_BRINECO2DENSITYFUNCTION_HPP_
 
 #include "PVTFunctionBase.hpp"
 
@@ -76,4 +71,4 @@ private:
 
 }
 
-#endif
+#endif //GEOSX_CONSTITUTIVE_FLUID_PVTFUNCTIONS_BRINECO2DENSITYFUNCTION_HPP_
