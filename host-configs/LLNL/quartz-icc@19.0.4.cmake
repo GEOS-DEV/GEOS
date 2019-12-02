f2bcb313f79de169c1fbc4176ea41611a1c3402f
Benjamin Curtice Corbett <corbett5@llnl.gov>
2019-12-02 10:45:27 -0800

    Host config reorg.

---
+++ host-configs/LLNL/quartz-icc@19.0.4.cmake
@@ -0,0 +1,23 @@
+set(CONFIG_NAME "quartz-icc@19.0.4" CACHE PATH "")
+
+set(COMPILER_DIR /usr/tce/packages/intel/intel-19.0.4/compilers_and_libraries_2019.4.227/linux )
+set(CMAKE_C_COMPILER ${COMPILER_DIR}/bin/intel64/icc CACHE PATH "")
+set(CMAKE_CXX_COMPILER ${COMPILER_DIR}/bin/intel64/icpc CACHE PATH "")
+set(CMAKE_Fortran_COMPILER ${COMPILER_DIR}/bin/intel64/ifort CACHE PATH "")
+
+set(CMAKE_C_FLAGS_RELEASE "-O3 -DNDEBUG -march=native -mtune=native" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG -march=native -mtune=native" CACHE STRING "")
+set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -DNDEBUG -march=native -mtune=native" CACHE STRING "")
+
+set(MPI_HOME             /usr/tce/packages/mvapich2/mvapich2-2.3-intel-19.0.4 CACHE PATH "")
+
+set(ENABLE_MKL ON CACHE BOOL "")
+set(MKL_ROOT /usr/tce/packages/mkl/mkl-2019.0)
+set(MKL_INCLUDE_DIRS ${MKL_ROOT}/include CACHE STRING "")
+set(MKL_LIBRARIES ${MKL_ROOT}/lib/libmkl_intel_lp64.so
+                  ${MKL_ROOT}/lib/libmkl_intel_thread.so
+                  ${MKL_ROOT}/lib/libmkl_core.so
+                  ${COMPILER_DIR}/compiler/lib/intel64/libiomp5.so
+                  CACHE STRING "")
+
+include(${CMAKE_CURRENT_LIST_DIR}/../../host-configs/LLNL/quartz-base.cmake)
