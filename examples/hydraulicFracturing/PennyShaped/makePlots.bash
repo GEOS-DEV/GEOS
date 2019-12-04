107c1a631529d96c2023036dea1540dc0fc77c61
Randolph R. Settgast <settgast1@llnl.gov>
2019-12-04 15:03:37 -0800

    update makePlots.bash for penny example

--- examples/hydraulicFracturing/PennyShaped/makePlots.bash
+++ examples/hydraulicFracturing/PennyShaped/makePlots.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python ./plotPennyShapedall.py 0.001 32.0e9 0.212 3e6 1 zeroViscosity zeroToughness
+
+python ./plotPennyShapedall.py 0.001 32.0e9 0.0265 3e6 2 0.00021 zeroToughnessBaseline
