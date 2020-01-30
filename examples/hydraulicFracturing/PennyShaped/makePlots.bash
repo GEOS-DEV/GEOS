669664359f550cbdddc4d272f209251bd23c9df7
Randolph R. Settgast <settgast1@llnl.gov>
2020-01-29 20:19:18 -0800

    fix penny verification input and scripts

--- examples/hydraulicFracturing/PennyShaped/makePlots.bash
+++ examples/hydraulicFracturing/PennyShaped/makePlots.bash
@@ -2,4 +2,4 @@
 
 
 # mu E q KI x_sourc aper_cutoffi
-python ./plotPennyShapedall.py 0.001 32.0e9 0.0265 3e6 2 0.00000125 AdaptiveDt NoDt 
+python ./plotPennyShapedall.py 0.001 32.0e9 0.0265 3e6 2 0.0001 0.0 zeroViscosity
