11fe1dc09fe08aad9b57620198e9c73e9d589cea
Christopher Sherman <sherman27@llnl.gov>
2020-02-07 15:02:09 -0800

    Adding an xml validation test after the documentation/schema update step

---
+++ scripts/validateAllXMLFiles.bash
@@ -0,0 +1,13 @@
+#!bin/bash
+
+VALIDATION_RESULTS=xml_validation_results.log
+
+find .. -name "*.xml" | xargs xmllint --schema ../src/coreComponents/fileIO/schema/schema.xsd --noout &> $VALIDATION_RESULTS
+grep -v validates $VALIDATION_RESULTS >&2
+
+if grep -q -v validates $VALIDATION_RESULTS
+then
+exit 1
+fi
+
+
