bb8556f81e5fd9a056730a1d74da2285ae3e763c
Francois Hamon <fhamon@stanford.edu>
2020-02-18 09:31:59 -0800

    fixed bug related to allocation of m_derivativeFluxResidual_dAperture

--- src/coreComponents/physicsSolvers/fluidFlow/SinglePhaseBase.cpp
+++ src/coreComponents/physicsSolvers/fluidFlow/SinglePhaseBase.cpp
@@ -294,84 +294,11 @@ void SinglePhaseBase::SetupSystem( DomainPartition * const domain,
   GEOSX_MARK_FUNCTION;
   ResetViews( domain );
 
-
   SolverBase::SetupSystem( domain,
                            dofManager,
                            matrix,
                            rhs,
                            solution );
-
-
-  MeshLevel * const mesh = domain->getMeshBodies()->GetGroup<MeshBody>(0)->getMeshLevel(0);
-  ElementRegionManager * const elemManager = mesh->getElemManager();
-
-  std::unique_ptr<CRSMatrix<real64,localIndex,localIndex> > &
-  derivativeFluxResidual_dAperture = getRefDerivativeFluxResidual_dAperture();
-  {
-
-    localIndex numRows = 0;
-    localIndex numCols = 0;
-    string_array const & flowRegions = getTargetRegions();
-    elemManager->forElementSubRegions( flowRegions, [&]( ElementSubRegionBase const * const elementSubRegion )
-    {
-      numRows += elementSubRegion->size();
-      numCols += elementSubRegion->size();
-    });
-
-    derivativeFluxResidual_dAperture = std::make_unique<CRSMatrix<real64,localIndex,localIndex>>( numRows, numCols );
-
-    derivativeFluxResidual_dAperture->reserveNonZeros( matrix.localNonzeros() );
-    localIndex maxRowSize = -1;
-    for( localIndex row=0 ; row<matrix.localRows() ; ++row )
-    {
-      localIndex const rowSize = matrix.localRowLength( row );
-      maxRowSize = maxRowSize > rowSize ? maxRowSize : rowSize;
-
-      derivativeFluxResidual_dAperture->reserveNonZeros( row,
-                                                         rowSize );
-    }
-    for( localIndex row=matrix.localRows() ; row<numRows ; ++row )
-    {
-      derivativeFluxResidual_dAperture->reserveNonZeros( row,
-                                                         maxRowSize );
-    }
-
-
-  }
-
-
-  string const presDofKey = dofManager.getKey( FlowSolverBase::viewKeyStruct::pressureString );
-
-  NumericalMethodsManager const *
-  numericalMethodManager = domain->getParent()->GetGroup<NumericalMethodsManager>( keys::numericalMethodsManager );
-
-  FiniteVolumeManager const *
-  fvManager = numericalMethodManager->GetGroup<FiniteVolumeManager>( keys::finiteVolumeManager );
-
-  FluxApproximationBase const * fluxApprox = fvManager->getFluxApproximation( getDiscretization() );
-
-
-  fluxApprox->forStencils<FaceElementStencil>( [&]( FaceElementStencil const & stencil )
-  {
-    for( localIndex iconn=0 ; iconn<stencil.size() ; ++iconn)
-    {
-      localIndex const numFluxElems = stencil.stencilSize(iconn);
-//      typename FaceElementStencil::IndexContainerViewConstType const & seri = stencil.getElementRegionIndices();
-//      typename FaceElementStencil::IndexContainerViewConstType const & sesri = stencil.getElementSubRegionIndices();
-      typename FaceElementStencil::IndexContainerViewConstType const & sei = stencil.getElementIndices();
-
-//      FaceElementSubRegion const * const
-//      elementSubRegion = elemManager->GetRegion(seri[iconn][0])->GetSubRegion<FaceElementSubRegion>(sesri[iconn][0]);
-
-      for( localIndex k0=0 ; k0<numFluxElems ; ++k0 )
-      {
-        for( localIndex k1=0 ; k1<numFluxElems ; ++k1 )
-        {
-          derivativeFluxResidual_dAperture->insertNonZero( sei[iconn][k0],sei[iconn][k1], 0.0 );
-        }
-      }
-    }
-  });
 }
 
 void SinglePhaseBase::ImplicitStepSetup( real64 const & GEOSX_UNUSED_PARAM( time_n ),
@@ -494,28 +421,11 @@ void SinglePhaseBase::AssembleSystem( real64 const time_n,
 {
   GEOSX_MARK_FUNCTION;
 
-//  MeshLevel * mesh = domain->getMeshBody(0)->getMeshLevel(0);
-//  applyToSubRegions( mesh, [&] ( localIndex , localIndex ,
-//                                 ElementRegionBase * const GEOSX_UNUSED_PARAM( region ),
-//                                 ElementSubRegionBase * const subRegion )
-//  {
-//    UpdateState( subRegion );
-//  } );
-
-
-
   matrix.zero();
   rhs.zero();
 
   matrix.open();
   rhs.open();
-
-  if( m_derivativeFluxResidual_dAperture==nullptr )
-  {
-    m_derivativeFluxResidual_dAperture = std::make_unique<CRSMatrix<real64,localIndex,localIndex>>( matrix.localRows(),
-                                                                                                    matrix.localCols() );
-  }
-  m_derivativeFluxResidual_dAperture->setValues(0.0);
 
   if (m_poroElasticFlag)
   {
