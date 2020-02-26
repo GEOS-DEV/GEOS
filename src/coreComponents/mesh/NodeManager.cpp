63eba314f9f929db2478589c28c79021f6a04be9
Randolph R. Settgast <settgast1@llnl.gov>
2020-02-26 07:56:07 -0800

    added smarter pre-allocation of nodeToElement arrays

--- src/coreComponents/mesh/NodeManager.cpp
+++ src/coreComponents/mesh/NodeManager.cpp
@@ -173,16 +173,31 @@ void NodeManager::SetElementMaps( ElementRegionManager const * const elementRegi
   ArrayOfArrays<localIndex> & toElementSubRegionList = m_toElements.m_toElementSubRegion;
   ArrayOfArrays<localIndex> & toElementList = m_toElements.m_toElementIndex;
 
+  array1d<localIndex> sizeOfArrays(size());
+
+  elementRegionManager->
+  forElementSubRegions<CellElementSubRegion>( [&]( CellElementSubRegion const * const subRegion )
+  {
+    arrayView2d< localIndex const, cells::NODE_MAP_USD > const & elemToNodeMap = subRegion->nodeList();
+    for( localIndex k=0 ; k<subRegion->size() ; ++k )
+    {
+      for( localIndex a=0 ; a<subRegion->numIndependentNodesPerElement() ; ++a )
+      {
+        localIndex nodeIndex = elemToNodeMap( k, a );
+        ++sizeOfArrays[nodeIndex];
+      }
+    }
+  });
+  localIndex const arrayCapacity = *(std::max_element( sizeOfArrays.begin(), sizeOfArrays.end() ));
+
   // This sets the capacity of each sub-array to 10. If this is using a bunch of memory
   // add a compress + shrink method to ArrayOfArrays that we can call afterwards.
   toElementRegionList.resize(0);
-  toElementRegionList.resize(size(), 44 );
-
+  toElementRegionList.resize(size(), arrayCapacity );
   toElementSubRegionList.resize(0);
-  toElementSubRegionList.resize(size(), 44 );
-
+  toElementSubRegionList.resize(size(), arrayCapacity );
   toElementList.resize(0);
-  toElementList.resize(size(), 44 );
+  toElementList.resize(size(), arrayCapacity );
 
   for( typename dataRepository::indexType kReg=0 ; kReg<elementRegionManager->numRegions() ; ++kReg )
   {
