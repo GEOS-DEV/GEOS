7d18d347be3d0ed454098fddac369edbdb844b0f
Benjamin Curtice Corbett <corbett5@llnl.gov>
2020-03-04 11:47:12 -0800

    Added NeighborData object.

--- src/coreComponents/mpiCommunications/CommunicationTools.cpp
+++ src/coreComponents/mpiCommunications/CommunicationTools.cpp
@@ -438,14 +438,14 @@ CommunicationTools::
 
 void
 CommunicationTools::
-  FindMatchedPartitionBoundaryObjects( ObjectManagerBase * const group,
+  FindMatchedPartitionBoundaryObjects( ObjectManagerBase * const objectManager,
                                        std::vector< NeighborCommunicator > & allNeighbors )
 {
   GEOSX_MARK_FUNCTION;
-  integer_array & domainBoundaryIndicator = group->getReference< integer_array >( group->m_ObjectManagerBaseViewKeys.domainBoundaryIndicator );
+  integer_array & domainBoundaryIndicator = objectManager->getReference< integer_array >( objectManager->m_ObjectManagerBaseViewKeys.domainBoundaryIndicator );
 
   array1d< globalIndex > globalPartitionBoundaryObjectsIndices;
-  group->ConstructGlobalListOfBoundaryObjects( globalPartitionBoundaryObjectsIndices );
+  objectManager->ConstructGlobalListOfBoundaryObjects( globalPartitionBoundaryObjectsIndices );
 
 
   // send the size of the partitionBoundaryObjects to neighbors
@@ -464,10 +464,7 @@ CommunicationTools::
     for( std::size_t i=0; i<allNeighbors.size(); ++i )
     {
       NeighborCommunicator & neighbor = allNeighbors[i];
-      localIndex_array &
-      matchedPartitionBoundaryObjects = group->GetGroup( group->m_ObjectManagerBaseGroupKeys.neighborData )->
-                                          GetGroup( std::to_string( neighbor.NeighborRank()))->
-                                          getReference< localIndex_array >( group->m_ObjectManagerBaseViewKeys.matchedPartitionBoundaryObjects );
+      localIndex_array & matchedPartitionBoundaryObjects = objectManager->getNeighborData( neighbor.NeighborRank() ).matchedPartitionBoundary();
 
       neighbor.MPI_WaitAll( commID );
       localIndex localCounter = 0;
@@ -477,7 +474,7 @@ CommunicationTools::
       {
         if( globalPartitionBoundaryObjectsIndices[localCounter] == neighborPartitionBoundaryObjects[i][neighborCounter] )
         {
-          localIndex const localMatchedIndex = group->m_globalToLocalMap.at( globalPartitionBoundaryObjectsIndices[localCounter] );
+          localIndex const localMatchedIndex = objectManager->m_globalToLocalMap.at( globalPartitionBoundaryObjectsIndices[localCounter] );
           matchedPartitionBoundaryObjects.push_back( localMatchedIndex );
           domainBoundaryIndicator[ localMatchedIndex ] = 2;
           ++localCounter;
@@ -508,7 +505,6 @@ void verifyGhostingConsistency( ObjectManagerBase const & objectManager,
 {
   GEOSX_MARK_FUNCTION;
 
-  Group const & neighborGroup = *( objectManager.GetGroup( objectManager.groupKeys().neighborData ) );
   arrayView1d< integer const > const & ghostRank = objectManager.GhostRank();
 
   /// Variable to track if an error has occurred.
@@ -518,9 +514,9 @@ void verifyGhostingConsistency( ObjectManagerBase const & objectManager,
   for( NeighborCommunicator const & neighbor : neighbors )
   {
     int const neighborRank = neighbor.NeighborRank();
-    Group const & neighborData = *( neighborGroup.GetGroup( std::to_string( neighborRank ) ) );
+    NeighborData const & neighborData = objectManager.getNeighborData( neighborRank );
 
-    arrayView1d< localIndex const > const & recvList = neighborData.getReference< array1d< localIndex > >( objectManager.viewKeys().ghostsToReceive );
+    arrayView1d< localIndex const > const & recvList = neighborData.ghostsToReceive();
     for( localIndex const recvIdx : recvList )
     {
       if( ghostRank[ recvIdx ] != neighborRank )
@@ -531,7 +527,7 @@ void verifyGhostingConsistency( ObjectManagerBase const & objectManager,
       }
     }
 
-    arrayView1d< localIndex const > const & sendList = neighborData.getReference< array1d< localIndex > >( objectManager.viewKeys().ghostsToSend );
+    arrayView1d< localIndex const > const & sendList = neighborData.ghostsToSend();
     for( localIndex const sendIdx : sendList )
     {
       if( ghostRank[ sendIdx ] != -1 )
@@ -540,6 +536,14 @@ void verifyGhostingConsistency( ObjectManagerBase const & objectManager,
         GEOSX_LOG_RANK( "Sending " << sendIdx << " to " << neighborRank <<
                         " but ghostRank[ " << sendIdx << " ] is " << ghostRank[ sendIdx ] );
       }
+    }
+
+    arrayView1d< std::pair< globalIndex, int > const > const & nonLocalGhosts = neighborData.nonLocalGhosts();
+    if( nonLocalGhosts.size() != 0 )
+    {
+      error = true;
+      GEOSX_LOG_RANK( "Expected to send 0 non local ghosts to rank " << neighborRank <<
+                      " but sending " << nonLocalGhosts.size() );
     }
   }
 
@@ -575,19 +579,14 @@ void fixReceiveLists( ObjectManagerBase & objectManager,
 
   constexpr int nonLocalGhostsTag = 54673246;
 
-  Group & neighborGroup = *( objectManager.GetGroup( objectManager.groupKeys().neighborData ) );
   std::vector< MPI_Request > nonLocalGhostsRequests( neighbors.size() );
 
   /// For each neighbor send them the indices of their ghosts that they mistakenly believe are owned by this rank.
   for( std::size_t i = 0; i < neighbors.size(); ++i )
   {
     int const neighborRank = neighbors[ i ].NeighborRank();
-    Group & neighborData = *( neighborGroup.GetGroup( std::to_string( neighborRank ) ) );
 
-    arrayView1d< std::pair< globalIndex, int > const > const & nonLocalGhosts =
-      neighborData.getReference< array1d< std::pair< globalIndex, int > > >( "nonLocalGhosts" );
-
-    MpiWrapper::iSend( nonLocalGhosts,
+    MpiWrapper::iSend( objectManager.getNeighborData( neighborRank ).nonLocalGhosts().toViewConst(),
                        neighborRank,
                        nonLocalGhostsTag,
                        MPI_COMM_GEOSX,
@@ -624,15 +623,12 @@ void fixReceiveLists( ObjectManagerBase & objectManager,
     }
 
     /// Remove the ghosts to fix from the neighbor's receive list.
-    removeFromCommList( ghostsToFix,
-                        neighborGroup.GetGroup( std::to_string( neighborRank ) )->getReference< array1d< localIndex > >( objectManager.viewKeys().
-                                                                                                                           ghostsToReceive ) );
+    removeFromCommList( ghostsToFix, objectManager.getNeighborData( neighborRank ).ghostsToReceive() );
 
     /// Iterate over the ranks that own the objects. For each rank add the new objects to the receive list.
     for( std::pair< int const, std::vector< localIndex > > const & pair : ghostsBySecondNeighbor )
     {
-      Group & trueOwnerData = *( neighborGroup.GetGroup( std::to_string( pair.first ) ) );
-      array1d< localIndex > & trueOwnerRecvList = trueOwnerData.getReference< array1d< localIndex > >( objectManager.viewKeys().ghostsToReceive );
+      array1d< localIndex > & trueOwnerRecvList = objectManager.getNeighborData( pair.first ).ghostsToReceive();
       trueOwnerRecvList.insert( trueOwnerRecvList.size(), pair.second.data(), pair.second.size() );
     }
   }
@@ -642,62 +638,34 @@ void fixReceiveLists( ObjectManagerBase & objectManager,
 }
 
 /**
- * @brief Return true iff the objects have no communication with the given neighbor.
- * @param neighborString string representing the neighbor rank.
- * @param objectManager owner of the objects to check.
- */
-bool usesNeighbor( std::string const & neighborString, ObjectManagerBase const & objectManager )
-{
-  Group const & neighborData = *( objectManager.GetGroup( objectManager.groupKeys().neighborData )->GetGroup( neighborString ) );
-  return neighborData.getReference< array1d< localIndex > >( objectManager.viewKeys().ghostsToSend ).size() &&
-         neighborData.getReference< array1d< localIndex > >( objectManager.viewKeys().ghostsToReceive ).size();
-}
-
-/**
- * @brief Remove the nonLocalGhosts view from the neighbor of the given object.
- * @param neighborString string representing the neighbor rank.
- * @param objectManager the object to remove nonLocalghosts from.
- */
-void removeNonLocalGhosts( std::string const & neighborString, ObjectManagerBase & objectManager )
-{
-  Group & neighborData = *( objectManager.GetGroup( objectManager.groupKeys().neighborData )->GetGroup( neighborString ) );
-  neighborData.deregisterWrapper( "nonLocalGhosts" );
-}
-
-/**
- * @brief Remove neighbors if there is no communication between them. Also remove all the nonLocalGhosts views.
+ * @brief Remove neighbors if there is no communication between them
  * @param nodeManager the NodeManager.
  * @param edgeManager the EdgeManager.
  * @param faceManager the FaceManager.
  * @param elemManager the ElementRegionManager.
  * @param neighbors the list of NeighborCommunicators, may be modified.
  */
-void removeUnusedNeighborsAndNonLocalGhosts( NodeManager & nodeManager,
-                                             EdgeManager & edgeManager,
-                                             FaceManager & faceManager,
-                                             ElementRegionManager & elemManager,
-                                             std::vector< NeighborCommunicator > & neighbors )
+void removeUnusedNeighbors( NodeManager & nodeManager,
+                            EdgeManager & edgeManager,
+                            FaceManager & faceManager,
+                            ElementRegionManager & elemManager,
+                            std::vector< NeighborCommunicator > & neighbors )
 {
   for( std::size_t i = 0; i < neighbors.size(); )
   {
     int const neighborRank = neighbors[ i ].NeighborRank();
-    std::string const neighborString = std::to_string( neighborRank );
 
     bool used = false;
 
-    used = used || usesNeighbor( neighborString, nodeManager );
-    removeNonLocalGhosts( neighborString, nodeManager );
+    used = used || nodeManager.getNeighborData( neighborRank ).communicationExists();
 
-    used = used || usesNeighbor( neighborString, edgeManager );
-    removeNonLocalGhosts( neighborString, edgeManager );
+    used = used || edgeManager.getNeighborData( neighborRank ).communicationExists();
 
-    used = used || usesNeighbor( neighborString, faceManager );
-    removeNonLocalGhosts( neighborString, faceManager );
+    used = used || faceManager.getNeighborData( neighborRank ).communicationExists();
 
     elemManager.forElementSubRegions< ElementSubRegionBase >( [&]( ElementSubRegionBase * const subRegion )
     {
-      used = used || usesNeighbor( neighborString, *subRegion );
-      removeNonLocalGhosts( neighborString, *subRegion );
+      used = used || subRegion->getNeighborData( neighborRank ).communicationExists();
     } );
 
     if( used )
@@ -706,13 +674,13 @@ void removeUnusedNeighborsAndNonLocalGhosts( NodeManager & nodeManager,
     }
     else
     {
-      nodeManager.GetGroup( nodeManager.groupKeys.neighborData )->deregisterGroup( neighborString );
-      edgeManager.GetGroup( edgeManager.groupKeys.neighborData )->deregisterGroup( neighborString );
-      faceManager.GetGroup( faceManager.groupKeys.neighborData )->deregisterGroup( neighborString );
+      nodeManager.removeNeighbor( neighborRank );
+      edgeManager.removeNeighbor( neighborRank );
+      faceManager.removeNeighbor( neighborRank );
 
-      elemManager.forElementSubRegions< ElementSubRegionBase >( [&]( ElementSubRegionBase * const subRegion )
+      elemManager.forElementSubRegions< ElementSubRegionBase >( [&]( ElementSubRegionBase * subRegion )
       {
-        subRegion->GetGroup( subRegion->groupKeys().neighborData )->deregisterGroup( neighborString );
+        subRegion->removeNeighbor( neighborRank );
       } );
 
       neighbors.erase( neighbors.begin() + i );
@@ -804,24 +772,23 @@ void CommunicationTools::FindGhosts( MeshLevel & meshLevel,
   waitOrderedOrWaitAll( neighbors.size(), { sendSyncLists, postRecv, rebuildSyncLists }, unorderedComms );
 
   nodeManager.FixUpDownMaps( false );
+  verifyGhostingConsistency( nodeManager, neighbors );
   edgeManager.FixUpDownMaps( false );
+  verifyGhostingConsistency( edgeManager, neighbors );
   faceManager.FixUpDownMaps( false );
-  elemManager.forElementSubRegions< ElementSubRegionBase >( [&]( ElementSubRegionBase * const subRegion )
+  verifyGhostingConsistency( faceManager, neighbors );
+  elemManager.forElementSubRegions< ElementSubRegionBase >( [&]( ElementSubRegionBase * subRegion )
   {
     subRegion->FixUpDownMaps( false );
+    verifyGhostingConsistency( *subRegion, neighbors );
   } );
 
-  verifyGhostingConsistency( nodeManager, neighbors );
-  verifyGhostingConsistency( edgeManager, neighbors );
-  verifyGhostingConsistency( faceManager, neighbors );
-
-  removeUnusedNeighborsAndNonLocalGhosts( nodeManager, edgeManager, faceManager, elemManager, neighbors );
+  removeUnusedNeighbors( nodeManager, edgeManager, faceManager, elemManager, neighbors );
 
   nodeManager.CompressRelationMaps();
   edgeManager.CompressRelationMaps();
   CommunicationTools::releaseCommID( commID );
 }
-
 
 void CommunicationTools::SynchronizePackSendRecvSizes( const std::map< string, string_array > & fieldNames,
                                                        MeshLevel * const mesh,
@@ -836,7 +803,7 @@ void CommunicationTools::SynchronizePackSendRecvSizes( const std::map< string, s
   for( std::size_t neighborIndex=0; neighborIndex<neighbors.size(); ++neighborIndex )
   {
     NeighborCommunicator & neighbor = neighbors[neighborIndex];
-    int const bufferSize = neighbor.PackCommSizeForSync( fieldNames, mesh, icomm.commID, on_device );
+    int const bufferSize = neighbor.PackCommSizeForSync( fieldNames, *mesh, icomm.commID, on_device );
 
     neighbor.MPI_iSendReceiveBufferSizes( icomm.commID,
                                           icomm.mpiSizeSendBufferRequest[neighborIndex],
@@ -859,7 +826,7 @@ void CommunicationTools::SynchronizePackSendRecv( const std::map< string, string
   MPI_iCommData sizeComm;
   for( NeighborCommunicator & neighbor : neighbors )
   {
-    neighbor.PackCommBufferForSync( fieldNames, mesh, icomm.commID, on_device );
+    neighbor.PackCommBufferForSync( fieldNames, *mesh, icomm.commID, on_device );
   }
 
   for( std::size_t count=0; count<neighbors.size(); ++count )
@@ -888,23 +855,6 @@ void CommunicationTools::SynchronizeUnpack( MeshLevel * const mesh,
 {
   GEOSX_MARK_FUNCTION;
 
-#if 0
-//  forall_in_range<RAJA::omp_parallel_for_exec>( 0, neighbors.size() ,
-//                                                   [&] ( int neighborIndex )->void
-
-#pragma omp parallel for
-  for( size_t neighborIndex=0; neighborIndex<neighbors.size(); ++neighborIndex )
-  {
-    std::cout<<"thread "<<omp_get_thread_num()<<" of "<<omp_get_num_threads()<<std::endl;
-    NeighborCommunicator & neighbor = neighbors[neighborIndex];
-    MPI_Wait( &( icomm.mpiRecvBufferRequest[neighborIndex] ),
-              &( icomm.mpiRecvBufferStatus[neighborIndex] ) );
-
-    neighbor.UnpackBufferForSync( icomm.fieldNames, mesh, icomm.commID );
-
-  }
-#else
-
   // unpack the buffers
   for( std::size_t count=0; count<neighbors.size(); ++count )
   {
@@ -918,7 +868,6 @@ void CommunicationTools::SynchronizeUnpack( MeshLevel * const mesh,
     neighbor.UnpackBufferForSync( icomm.fieldNames, mesh, icomm.commID, on_device );
   }
 
-#endif
   MpiWrapper::Waitall( icomm.size,
                        icomm.mpiSizeSendBufferRequest.data(),
                        icomm.mpiSizeSendBufferStatus.data() );
