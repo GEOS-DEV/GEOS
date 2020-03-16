464c3c0e56a28f00c96c4fd73c00bf4e3ed34a81
Randolph R. Settgast <settgast1@llnl.gov>
2020-03-16 15:02:36 -0700

    reverted to using system blas/lapack instead of ESSL due to underscore isues with trilinos

--- host-configs/LLNL/lassen-gcc@8.3.1.cmake
+++ host-configs/LLNL/lassen-gcc@8.3.1.cmake
@@ -15,20 +15,25 @@ set(ENABLE_PAPI OFF CACHE BOOL "")
 
 set(ENABLE_UNCRUSTIFY OFF CACHE BOOL "")
 
-set(ENABLE_ESSL ON CACHE BOOL "")
-set(ESSL_ROOT_DIR /usr/tcetmp/packages/essl/essl-6.2/ CACHE PATH "" FORCE )
-set(ESSL_INCLUDE_DIRS ${ESSL_ROOT_DIR}/include CACHE PATH "" FORCE)
-set(ESSL_LIB_DIRS ${ESSL_ROOT_DIR}/lib64 CACHE PATH "" FORCE)
-set(ESSL_LIBRARIES /usr/tcetmp/packages/essl/essl-6.2/lib64/libesslsmpcuda.so
-                   /usr/tce/packages/xl/xl-beta-2019.06.20/alllibs/libxlsmp.so
-                   /usr/tce/packages/xl/xl-beta-2019.06.20/alllibs/libxlfmath.so
-                   /usr/tce/packages/xl/xl-beta-2019.06.20/alllibs/libxlf90_r.so
-                   ${CUDA_TOOLKIT_ROOT_DIR}/lib64/libcublas.so
-                   ${CUDA_TOOLKIT_ROOT_DIR}/lib64/libcudart.so
-                   ${ESSL_LIB_DIRS}/liblapackforessl.a
-                   /usr/tce/packages/xl/xl-beta-2019.06.20/alllibs/libxl.a
-                   CACHE PATH "")
+set(ENABLE_ESSL OFF CACHE BOOL "")
 
+if( ENABLE_ESSL )
+    set(ESSL_ROOT_DIR /usr/tcetmp/packages/essl/essl-6.2/ CACHE PATH "" FORCE )
+    set(ESSL_INCLUDE_DIRS ${ESSL_ROOT_DIR}/include CACHE PATH "" FORCE)
+    set(ESSL_LIB_DIRS ${ESSL_ROOT_DIR}/lib64 CACHE PATH "" FORCE)
+    set(ESSL_LIBRARIES /usr/tcetmp/packages/essl/essl-6.2/lib64/libesslsmpcuda.so
+                       /usr/tce/packages/xl/xl-beta-2019.06.20/alllibs/libxlsmp.so
+                       /usr/tce/packages/xl/xl-beta-2019.06.20/alllibs/libxlfmath.so
+                       /usr/tce/packages/xl/xl-beta-2019.06.20/alllibs/libxlf90_r.so
+                       ${CUDA_TOOLKIT_ROOT_DIR}/lib64/libcublas.so
+                       ${CUDA_TOOLKIT_ROOT_DIR}/lib64/libcudart.so
+                       ${ESSL_LIB_DIRS}/liblapackforessl.a
+                       /usr/tce/packages/xl/xl-beta-2019.06.20/alllibs/libxl.a
+                       CACHE PATH "")
+else()
+    set( BLAS_LIBRARIES /usr/lib64/libblas.so CACHE PATH "")
+    set( LAPACK_LIBRARIES /usr/lib64/liblapack.so CACHE PATH "")
+endif()
 set(DOXYGEN_EXECUTABLE /usr/bin/doxygen CACHE PATH "")
 
 set(PETSC_OMP_DIR ${GEOSX_TPL_ROOT_DIR}/omp-links-for-petsc CACHE STRING "")
