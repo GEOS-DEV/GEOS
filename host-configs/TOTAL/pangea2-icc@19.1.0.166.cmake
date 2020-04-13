4ad4264ce86f8a033224eeb8c37d4d213529221c
Randolph Settgast <settgast1@llnl.gov>
2020-04-13 16:45:19 -0700

    Update pangea2/3 hostconfigs (#793)
    * updated pangea2 hostconfigs for successful compilation and exectuion
    
    * add option to perform xml updates. turned off option for pangea builds since they fail to run
    
    * intermediate commit....don't look at me...im so ugly
    
    * working on pangea3 build
    
    * moved mpi compiler specifications into target hostconfigs
    
    * update hostconfig for p3dev module env
    
    * remove old commands from hostconfigs

--- host-configs/TOTAL/pangea2-icc@19.1.0.166.cmake
+++ host-configs/TOTAL/pangea2-icc@19.1.0.166.cmake
@@ -7,20 +7,17 @@
 #
 # Load modules in this order:
 #   1) gcc/8.3.0
-#   2) intel-compxe/19.0.3.199
-#   3) intel-mpi/2019U3
+#   2) intel-compxe/19.1.0.166
+#   3) intel-mpi/2020
 #   * This is to have the intel compiler use the gcc8 headers instead of the 
 #     gcc4.8.5 headers, and to have intel-mpi to wrap intel instead of env cc.
 #
-# For ATS, the default python needs to be python2.7 and you need mpi4py installed
-# conda create -n python2 python=2.7 anaconda
-# source activate python2
 ########################################
 
 
-set(CONFIG_NAME "pangea2-icc@19.0.3.199" CACHE PATH "" ) 
+set(CONFIG_NAME "pangea2-icc@19.1.0.166" CACHE PATH "" ) 
 
-set(INTEL_COMPILER_ROOT "/data_local/sw/intel/RHEL7/compilers_and_libraries_2019.3.199/linux" )
+set(INTEL_COMPILER_ROOT "/data_local/sw/intel/RHEL7/compilers_and_libraries_2020.0.166/linux" )
 set(CMAKE_C_COMPILER "${INTEL_COMPILER_ROOT}/bin/intel64/icc" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "${INTEL_COMPILER_ROOT}/bin/intel64/icpc" CACHE PATH "")
 set(CMAKE_Fortran_COMPILER "${INTEL_COMPILER_ROOT}/bin/intel64/ifort" CACHE PATH "")
@@ -29,17 +26,11 @@ set(CMAKE_C_FLAGS_RELEASE "-O3 -DNDEBUG -march=native -mtune=native" CACHE STRIN
 set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG -march=native -mtune=native" CACHE STRING "")
 set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -DNDEBUG -march=native -mtune=native" CACHE STRING "")
 
-set(ENABLE_FORTRAN OFF CACHE BOOL "" FORCE)
-set(ENABLE_MPI ON CACHE BOOL "" FORCE)
-
 set(MPI_HOME "${INTEL_COMPILER_ROOT}/mpi/intel64" CACHE PATH "")
-set(MPI_C_COMPILER       "${MPI_HOME}/bin/mpiicc"   CACHE PATH "")
-set(MPI_CXX_COMPILER     "${MPI_HOME}/bin/mpiicpc"  CACHE PATH "")
-set(MPI_Fortran_COMPILER "${MPI_HOME}/bin/mpiifort" CACHE PATH "")
-set(MPIEXEC              "${MPI_HOME}/bin/mpirun" CACHE PATH "")
-set(MPIEXEC_NUMPROC_FLAG "-n" CACHE PATH "")
 
-set(ENABLE_MKL ON CACHE BOOL "")
+#set( BLT_MPI_LINK_FLAGS "-Wl,--enable-new-dtags -Wl,-rpath,/data_local/sw/intel/RHEL7/compilers_and_libraries_2019.3.199/linux/mpi/intel64/lib/release -Wl,-rpath,/data_local/sw/intel/RHEL7/compilers_and_libraries_2019.3.199/linux/mpi/intel64/lib" CACHE PATH "" FORCE )
+#set( BLT_MPI_LINK_FLAGS "-Wl,--enable-new-dtags -Wl,-rpath,${MPI_HOME}/lib/release -Wl,-rpath,${MPI_HOME}/lib" CACHE PATH "" FORCE )
+
 set(MKL_ROOT ${INTEL_COMPILER_ROOT}/mkl )
 set(MKL_INCLUDE_DIRS ${MKL_ROOT}/include CACHE STRING "")
 set(MKL_LIBRARIES ${MKL_ROOT}/lib/intel64/libmkl_intel_lp64.so
