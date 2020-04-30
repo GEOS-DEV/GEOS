0cc21fcc52f9d185a01e888bae6004f2ab28fbfe
Joshua White <white230@llnl.gov>
2020-04-30 14:27:37 -0700

    Doxygen for common.hpp

--- src/coreComponents/linearAlgebra/common.hpp
+++ src/coreComponents/linearAlgebra/common.hpp
@@ -20,32 +20,110 @@
 
 #include "common/DataTypes.hpp"
 
-/// Whether to check preconditions at runtime in LAI functions
+/**
+ * Whether to check preconditions at runtime in LAI functions
+ */
 #define GEOSX_LAI_RUNTIME_ASSERT 1
-
 #if GEOSX_LAI_RUNTIME_ASSERT
+
+/**
+ * Assert expression is true
+ * @param expr expression
+ */
 #define GEOSX_LAI_ASSERT( expr ) GEOSX_ERROR_IF( !(expr), "" )
+
+/**
+ * Assert expression and output message if false
+ * @param expr expression
+ * @param msg message
+ */
 #define GEOSX_LAI_ASSERT_MSG( expr, msg ) GEOSX_ERROR_IF( !(expr), msg )
+
+/**
+ * Assert lhs equals rhs
+ * @param lhs left hand side
+ * @param rhs right hand side
+ */
 #define GEOSX_LAI_ASSERT_EQ( lhs, rhs ) GEOSX_ERROR_IF_NE( lhs, rhs )
+
+/**
+ * Assert lhs not equal to rhs
+ * @param lhs left hand side
+ * @param rhs right hand side
+ */
 #define GEOSX_LAI_ASSERT_NE( lhs, rhs ) GEOSX_ERROR_IF_EQ( lhs, rhs )
+
+/**
+ * Assert lhs greater than rhs
+ * @param lhs left hand side
+ * @param rhs right hand side
+ */
 #define GEOSX_LAI_ASSERT_GT( lhs, rhs ) GEOSX_ERROR_IF_GE( rhs, lhs )
+
+/**
+ * Assert lhs greater than or equal to rhs
+ * @param lhs left hand side
+ * @param rhs right hand side
+ */
 #define GEOSX_LAI_ASSERT_GE( lhs, rhs ) GEOSX_ERROR_IF_GT( rhs, lhs )
 #else
+
+/**
+ * Assert expression is true
+ * @param expr expression
+ */
 #define GEOSX_LAI_ASSERT( expr ) GEOSX_ASSERT( expr )
+
+/**
+ * Assert expression and output message if false
+ * @param expr expression
+ * @param msg message
+ */
 #define GEOSX_LAI_ASSERT_MSG( expr, msg ) GEOSX_ASSERT_MSG( expr, msg )
+
+/**
+ * Assert lhs equals rhs
+ * @param lhs left hand side
+ * @param rhs right hand side
+ */
 #define GEOSX_LAI_ASSERT_EQ( lhs, rhs ) GEOSX_ASSERT_EQ( lhs, rhs )
+
+/**
+ * Assert lhs not equal to rhs
+ * @param lhs left hand side
+ * @param rhs right hand side
+ */
 #define GEOSX_LAI_ASSERT_NE( lhs, rhs ) GEOSX_ASSERT_NE( lhs, rhs )
+
+/**
+ * Assert lhs greater rhs
+ * @param lhs left hand side
+ * @param rhs right hand side
+ */
 #define GEOSX_LAI_ASSERT_GT( lhs, rhs ) GEOSX_ASSERT_GT( lhs, rhs )
+
+/**
+ * Assert lhs greater than or equal to rhs
+ * @param lhs left hand side
+ * @param rhs right hand side
+ */
 #define GEOSX_LAI_ASSERT_GE( lhs, rhs ) GEOSX_ASSERT_GE( lhs, rhs )
 #endif
 
-/// Macro for checking and reporting error codes from TPL packages
+/**
+ * Macro for checking and reporting error codes from TPL packages
+ * @param call call to check function
+ */
 #define GEOSX_LAI_CHECK_ERROR( call ) \
   do { \
     auto const ierr = call; \
     GEOSX_ERROR_IF_NE_MSG( ierr, 0, "Error in call to " << #call ); \
   } while( false )
 
+/**
+ * Macro for checking and reporting non-negative error codes from TPL packages
+ * @param call call to check function
+ */
 #define GEOSX_LAI_CHECK_ERROR_NNEG( call ) \
   do { \
     auto const ierr = call; \
@@ -55,6 +133,9 @@
 namespace geosx
 {
 
+/**
+ *  Enumeration of available output formats for LAI objects
+ */
 enum class LAIOutputFormat
 {
   NATIVE_ASCII,
@@ -64,13 +145,16 @@ enum class LAIOutputFormat
   MATRIX_MARKET
 };
 
+/**
+ * Permutation info for row major and column major layouts
+ */
 struct MatrixLayout
 {
-  using ROW_MAJOR_PERM = RAJA::PERM_IJ;
-  using COL_MAJOR_PERM = RAJA::PERM_JI;
+  using ROW_MAJOR_PERM = RAJA::PERM_IJ; //!< typedef LAI row major permutation consistent with RAJA order
+  using COL_MAJOR_PERM = RAJA::PERM_JI; //!< typedef LAI col major permutation consistent with RAJA order
 
-  constexpr static int const ROW_MAJOR = LvArray::getStrideOneDimension( ROW_MAJOR_PERM {} );
-  constexpr static int const COL_MAJOR = LvArray::getStrideOneDimension( COL_MAJOR_PERM {} );
+  constexpr static int const ROW_MAJOR = LvArray::getStrideOneDimension( ROW_MAJOR_PERM {} );  //!< row major data layout
+  constexpr static int const COL_MAJOR = LvArray::getStrideOneDimension( COL_MAJOR_PERM {} );  //!< column major data layout
 };
 
 }
