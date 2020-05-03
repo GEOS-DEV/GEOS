360265deddb3b1fbb4678caa7f654de95ea12456
Randolph Settgast <settgast1@llnl.gov>
2020-05-02 21:51:44 -0700

    doxygen for DataTypes.hpp, xmlWrapper.hpp, NumericalMethodsManager.hpp, and MeshForLoopInterface.hpp. Collatoral damage changes for brining NumericalMethodsManager up to standards (#914)

--- src/coreComponents/managers/NumericalMethodsManager.hpp
+++ src/coreComponents/managers/NumericalMethodsManager.hpp
@@ -20,7 +20,10 @@
 #define GEOSX_MANAGERS_NUMERICALMETHODSMANAGER_HPP_
 
 #include "dataRepository/Group.hpp"
-#include "finiteElement/FiniteElementDiscretization.hpp"
+#include "finiteElement/basis/BasisFunctionManager.hpp"
+#include "finiteElement/quadrature/QuadratureRuleManager.hpp"
+#include "finiteElement/FiniteElementDiscretizationManager.hpp"
+#include "finiteVolume/FiniteVolumeManager.hpp"
 
 namespace geosx
 {
@@ -28,26 +31,98 @@ namespace dataRepository
 {
 namespace keys
 {
-string const numericalMethodsManager = "NumericalMethods";
-string const basisFunctions = "BasisFunctions";
-string const quadratureRules = "QuadratureRules";
-string const finiteElementDiscretizations = "FiniteElements";
-string const finiteVolumeManager = "FiniteVolume";
 }
 }
 
-
+/**
+ * @class NumericalMethodsManager
+ *
+ * Contains the discretization methods and their components for application to the mesh.
+ */
 class NumericalMethodsManager : public dataRepository::Group
 {
 public:
+  /// Deleted default constructor.
   NumericalMethodsManager() = delete;
+
+  /**
+   * @copydoc geosx::dataRepository::Group::Group( string const &, Group * const )
+   */
   NumericalMethodsManager( string const & name, Group * const parent );
+
   virtual ~NumericalMethodsManager() override;
 
   virtual Group * CreateChild( string const & childKey, string const & childName ) override;
 
-  dataRepository::Group const * FindNumericalMethodByName( string const & name ) const;
+  /// Contains the keys for the object names in the data repository.
+  struct groupKeysStruct
+  {
+    /// Name for the basis function manager.
+    static constexpr auto basisFunctions = "BasisFunctions";
 
+    /// Name for the quadrature rule manager.
+    static constexpr auto quadratureRules = "QuadratureRules";
+
+    /// Name for the finite element discretization manager.
+    static constexpr auto finiteElementDiscretizations = "FiniteElements";
+
+    /// Name for the finite volume manager.
+    static constexpr auto finiteVolumeManager = "FiniteVolume";
+
+  };
+
+  /**
+   * @brief @return Returns reference to const BasisFunctionManager m_basisFunctions.
+   */
+  BasisFunctionManager const & getBasisFunctions() const { return m_basisFunctions; }
+
+  /**
+   * @brief @return Returns reference to BasisFunctionManager m_basisFunctions.
+   */
+  BasisFunctionManager       & getBasisFunctions()       { return m_basisFunctions; }
+
+  /**
+   * @brief @return Returns reference to const QuadratureRuleManager m_quadratureRules.
+   */
+  QuadratureRuleManager const & getQuadratureRules() const { return m_quadratureRules; }
+
+  /**
+   * @brief @return Returns reference to QuadratureRuleManager m_quadratureRules.
+   */
+  QuadratureRuleManager       & getQuadratureRules()       { return m_quadratureRules; }
+
+  /**
+   * @brief @return Returns reference to const FiniteElementDiscretizationManager m_finiteElementDiscretizationManager.
+   */
+  FiniteElementDiscretizationManager const & getFiniteElementDiscretizationManager() const { return m_finiteElementDiscretizationManager; }
+
+  /**
+   * @brief @return Returns reference to FiniteElementDiscretizationManager m_finiteElementDiscretizationManager.
+   */
+  FiniteElementDiscretizationManager       & getFiniteElementDiscretizationManager()       { return m_finiteElementDiscretizationManager; }
+
+  /**
+   * @brief @return Returns reference to FiniteVolumeManager m_finiteVolumeManager.
+   */
+  FiniteVolumeManager       & getFiniteVolumeManager()       { return m_finiteVolumeManager; }
+
+  /**
+   * @brief @return Returns reference to const FiniteVolumeManager m_finiteVolumeManager.
+   */
+  FiniteVolumeManager const & getFiniteVolumeManager() const { return m_finiteVolumeManager; }
+
+private:
+  /// Contains basis functions available for use in the various discretization packages.
+  BasisFunctionManager m_basisFunctions;
+
+  /// Contains quadrature rules available for use in the various discretization packages.
+  QuadratureRuleManager m_quadratureRules;
+
+  /// Contains the finite element discretizations
+  FiniteElementDiscretizationManager m_finiteElementDiscretizationManager;
+
+  /// Contains the finite volume discretizations.
+  FiniteVolumeManager m_finiteVolumeManager;
 
 };
 
