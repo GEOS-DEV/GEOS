8870bf897f29e392e2125a95e1aeb1726e3a972b
Antoine Mazuyer <32493098+AntoineMazuyer@users.noreply.github.com>
2020-05-07 23:09:25 +0200

    Using VTK in the TPL
    This new code base does not build the data by hand anymore, but relies on the vtk library to do the job.
    The outputs are now much more extended and complete.
    The counterpart is that from now on we need vtk as a TPL.
    This option can be deactivated using to the `ENABLE_VTK` cmake variable.

---
+++ src/coreComponents/fileIO/vtk/VTKGEOSXData.hpp
@@ -0,0 +1,57 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ * Copyright (c) 2018-2019 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2019 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2018-2019 Total, S.A
+ * Copyright (c) 2019-     GEOSX Contributors
+ * All right reserved
+ *
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
+#ifndef GEOSX_FILEIO_VTK_VTKGEOSXDATA_HPP_
+#define GEOSX_FILEIO_VTK_VTKGEOSXDATA_HPP_
+
+#include "common/DataTypes.hpp"
+
+#include <vtkAOSDataArrayTemplate.h>
+
+namespace geosx
+{
+namespace vtk
+{
+
+class VTKGEOSXData : public vtkAOSDataArrayTemplate< real64 >
+{
+public:
+  static VTKGEOSXData *New()
+  {
+    VTK_STANDARD_NEW_BODY( VTKGEOSXData );
+  }
+  /*!
+   * @brief insert Value in a custom way for R1Tensors
+   * @param[in] index index where the value \p val will be inserted
+   * @param[in] val value to be inserted
+   */
+  template< typename T >
+  void CustomInsertValue( localIndex index, T const & val )
+  {
+    this->InsertValue( index, val );
+  }
+};
+
+template<>
+void VTKGEOSXData::CustomInsertValue< R1Tensor >( localIndex index, R1Tensor const & val );
+
+template<>
+void VTKGEOSXData::CustomInsertValue< R2Tensor >( localIndex index, R2Tensor const & val );
+
+template<>
+void VTKGEOSXData::CustomInsertValue< R2SymTensor >( localIndex index, R2SymTensor const & val );
+}
+}
+
+#endif
