56a6d58ca799d4da060ce9084aec7ab0263853a0
Christopher Sherman <sherman27@llnl.gov>
2020-05-12 11:51:23 -0700

    Adding a simple xml formatter to pygeos

--- src/coreComponents/python/modules/pygeos_package/setup.py
+++ src/coreComponents/python/modules/pygeos_package/setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
 setup(name='pygeos',
-      version='0.4.1',
+      version='0.4.2',
       description='GEOS front-end preprocessing package',
       author='Chris Sherman',
       author_email='sherman27@llnl.gov',
       packages=['pygeos', 'pygeos.tests'],
       entry_points={'console_scripts': ['pygeos = pygeos.__main__:main',
+                                        'format_xml = pygeos.__main__:format_xml',
                                         'test_pygeos = pygeos.tests.test_manager:run_unit_tests']},
-      install_requires=['lxml', 'numpy'])
+      install_requires=['lxml>=4.5.0', 'numpy'])
 
