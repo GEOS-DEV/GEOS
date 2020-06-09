ab1390d01bb8e8213743b78832136b121f20dd2c
Christopher Sherman <sherman27@llnl.gov>
2020-06-09 10:28:41 -0700

    Handling optional namespace information in the xml formatter

--- src/coreComponents/python/modules/pygeos_package/pygeos/xml_formatter.py
+++ src/coreComponents/python/modules/pygeos_package/pygeos/xml_formatter.py
@@ -10,7 +10,8 @@ def format_xml_level(output,
                      block_separation_max_depth=2,
                      modify_attribute_indent=False,
                      sort_attributes=False,
-                     close_tag_newline=False):
+                     close_tag_newline=False,
+                     include_namespace=False):
   # Handle comments
   if node.tag is ElementTree.Comment:
     output.write('\n%s<!--%s-->' % (indent*level, node.text))
@@ -27,24 +28,38 @@ def format_xml_level(output,
       if modify_attribute_indent:
         attribute_indent = ' ' * (len(opening_line))
 
+      # Get a copy of the attributes
+      attribute_dict = {}
+      if ((level == 0) & include_namespace):
+        # Handle the optional namespace information at the root level
+        attribute_dict['xmlns:xsi'] = 'http://www.w3.org/2001/XMLSchema-instance'
+        attribute_dict['xsi:noNamespaceSchemaLocation'] = '/usr/gapps/GEOS/schema/schema.xsd'
+      elif (level > 0):
+        attribute_dict = node.attrib
+
       # Sort attribute names
-      akeys = list(node.attrib.keys())
+      akeys = list(attribute_dict.keys())
       if sort_attributes:
         akeys = sorted(akeys)
 
       for ii in range(0, len(akeys)):
         k = akeys[ii]
         if ((ii == 0) & modify_attribute_indent):
-          output.write(' %s=\"%s\"' % (k, node.attrib[k]))
+          output.write(' %s=\"%s\"' % (k, attribute_dict[k]))
         else:
-          output.write('\n%s%s=\"%s\"' % (attribute_indent, k, node.attrib[k]))
+          output.write('\n%s%s=\"%s\"' % (attribute_indent, k, attribute_dict[k]))
 
     # Write children
     if len(node):
       output.write('>')
       Nc = len(node)
       for ii, child in zip(range(Nc), node):
-        format_xml_level(output, child, level+1)
+        format_xml_level(output, child, level+1, indent,
+                         block_separation_max_depth,
+                         modify_attribute_indent,
+                         sort_attributes,
+                         close_tag_newline,
+                         include_namespace)
 
         # Add space between blocks
         if ((level < block_separation_max_depth) & (ii < Nc-1)):
@@ -61,10 +76,10 @@ def format_xml_level(output,
 
 # Class to handle commented xml structure
 class CommentedTreeBuilder(ElementTree.TreeBuilder):
-    def comment(self, data):
-        self.start(ElementTree.Comment, {})
-        self.data(data)
-        self.end(ElementTree.Comment)
+  def comment(self, data):
+    self.start(ElementTree.Comment, {})
+    self.data(data)
+    self.end(ElementTree.Comment)
 
 
 def main():
@@ -76,21 +91,35 @@ def main():
   # Parse the user arguments
   parser = argparse.ArgumentParser()
   parser.add_argument('input', type=str, help='Input file name')
+  parser.add_argument('-i', '--indent', type=int, help='Indent size', default=2)
+  parser.add_argument('-s', '--style', type=int, help='Indent style', default=0)
+  parser.add_argument('-d', '--depth', type=int, help='Block separation depth', default=2)
+  parser.add_argument('-a', '--alphebitize', type=int, help='Alphebetize attributes', default=0)
+  parser.add_argument('-c', '--close', type=int, help='Close tag style', default=0)
+  parser.add_argument('-n', '--namespace', type=int, help='Include namespace', default=0)
   args = parser.parse_args()
 
   # Process the xml file
   fname = os.path.expanduser(args.input)
   try:
-    xml_parser = parser = ElementTree.XMLParser(target=CommentedTreeBuilder())
+    xml_parser = ElementTree.XMLParser(target=CommentedTreeBuilder())
     tree = ElementTree.parse(fname, xml_parser)
     root = tree.getroot()
 
     with open(fname, 'w') as f:
       f.write('<?xml version=\"1.0\" ?>\n')
-      format_xml_level(f, root, 0)
+      format_xml_level(f,
+                       root,
+                       0,
+                       indent=' '*args.indent,
+                       block_separation_max_depth=args.depth,
+                       modify_attribute_indent=args.style,
+                       sort_attributes=args.alphebitize,
+                       close_tag_newline=args.close,
+                       include_namespace=args.namespace)
       f.write('\n')
 
-  except ElementTree.XMLSyntaxError as err:
+  except ElementTree.ParseError as err:
     print('\nCould not load file: %s' % (fname))
     print(err.msg)
     raise Exception('\nCheck input file!')
