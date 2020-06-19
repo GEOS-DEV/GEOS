0258e3938c76556d31b24794d57c18cad08367fe
Matteo Cusini <49037133+CusiniM@users.noreply.github.com>
2020-06-19 11:59:57 -0700

    Fix doxygen warnings in mesh folder (#962)
    * Pointing to a git documentation.
    
    * Fix display problem with some code blocks that were badly indented
    
    * First contribution to the tutorial with a more complex field case
    
    * remove bad file
    
    * Progress on the field case tutorial
    
    * update Doxyfile.in with specific filename (change to directory once merged)
    
    * doxigen comment for EdgeManager.hpp
    
    * Doxygen for NodeManager
    
    * first commit doxygen of FaceManager class
    
    * small fixes and added cpp files in Doxyfile.in
    
    * Doxygen documentation for edge manager
    
    * Doxygen quick proofreading
    
    * displaced deleted ctor in NodeManager.hpp/ consistent defgroup in NodeManager FaceManager /minor changes
    
    * Doxygen Docs rapide review
    
    * correct typo, add details and correct some mistakes/imprecision and bad copy pastes
    
    * Doxygen comment for AggregateElementSubRegion
    
    * added Doxygen documentation in MeshLevel
    
    * Should be all fine for the mesh doxygen. overrriden functions wiell need to be fixed in the base classes.
    
    * fixed submodules commits
    
    * Fixed last warnings in mesh folder.
    
    * Apply suggestions from code review
    
    Fixed ACKNOWLEDGEMENTS
    
    * uncrustify
    
    * try not to use copydoc
    
    * should fix travis doxygen warning
    
    * uncrustified.
    
    Co-authored-by: Thomas Gazzola <thomas.gazzola@total.com>
    Co-authored-by: Antoine Mazuyer <amazuyer@stanford.edu>
    Co-authored-by: jacques <j>
    Co-authored-by: mndiaye24 <mndiaye@stanford.edu>
    Co-authored-by: Herve Gross <herve.gross@total.com>
    Co-authored-by: Francois Hamon <fhamon@stanford.edu>

--- src/coreComponents/mesh/AggregateElementSubRegion.hpp
+++ src/coreComponents/mesh/AggregateElementSubRegion.hpp
@@ -23,20 +23,47 @@
 namespace geosx
 {
 
+
+/**
+ * @class AggregateElementSubRegion
+ * @brief The AggregateElementSubRegion class provides an interface to aggregate fine cells into coarse cells.
+ */
+
 class AggregateElementSubRegion : public ElementSubRegionBase
 {
 public:
 
+  /// AggregateToNode map type
   using NodeMapType=FixedOneToManyRelation;
 
+  /**
+   * @name Static Factory Catalog Functions
+   */
+  ///@{
+
+  /**
+   * @brief Return the name of the aggregate element sub-region in the object catalog.
+   * @return string that contains the AggregateElementSubRegion catalog name
+   */
   static const string CatalogName()
   { return "AggregateCell"; }
 
+  /**
+   * @brief Provide a virtual access to CatalogName().
+   * @return string that contains the AggregateElementSubRegion catalog name
+   */
   virtual const string getCatalogName() const override
   {
     return AggregateElementSubRegion::CatalogName();
   }
+  ///@}
 
+  /**
+   * @brief Find all the fine cell in a given aggregate coarse cell.
+   * @tparam LAMBDA template argument
+   * @param[in] aggregateIndex index of the aggregate
+   * @param[in,out] lambda all the fine cells in the aggregate
+   */
   template< typename LAMBDA >
   void forFineCellsInAggregate( localIndex aggregateIndex, LAMBDA lambda )
   {
@@ -47,41 +74,85 @@ public:
     }
   }
 
+
+  /**
+   * @brief Gives the number of fine cells of an aggregate coarse cell.
+   * @param[in] aggregateIndex index of the aggregate coarse cell
+   * @return the number of fine cell in the aggregate
+   */
   localIndex GetNbCellsPerAggregate( localIndex aggregateIndex ) const
   {
     return m_nbFineCellsPerCoarseCell[aggregateIndex + 1] - m_nbFineCellsPerCoarseCell[aggregateIndex];
   }
 
+  /**
+   * @name Constructor / Destructor
+   */
+  ///@{
+
+  /**
+   * @brief Constructor for this class.
+   * @param[in] name the name of this object manager
+   * @param[in] parent the parent Group
+   */
   AggregateElementSubRegion( string const & name,
                              dataRepository::Group * const parent );
-
+  /**
+   * @brief Destructor.
+   */
   virtual ~AggregateElementSubRegion() override;
+  ///@}
 
+  /**
+   * @brief Construct the relation map between fine and coarse cells and order by aggregates.
+   * @param[in] nbAggregates number of aggregate
+   * @param[in] fineToCoarse index array of fine cells to be aggregated to form coarse cells
+   * @param[in] barycenters coordinates of the elements center
+   */
   void CreateFromFineToCoarseMap( localIndex nbAggregates,
                                   array1d< localIndex > const & fineToCoarse,
                                   array1d< R1Tensor > const & barycenters );
 
+  /**
+   * @brief Accessor to the relation array between fine and coarse elements.
+   * @return the relation array between fine and coarse elements ordered by aggregates
+   */
   const array1d< localIndex > & GetFineToCoarseMap()
   {
     return m_fineToCoarse;
   }
 
-  virtual void CalculateElementGeometricQuantities( NodeManager const & GEOSX_UNUSED_PARAM( nodeManager ),
-                                                    FaceManager const & GEOSX_UNUSED_PARAM( faceManager ) ) override
+  virtual void CalculateElementGeometricQuantities( NodeManager const & nodeManager,
+                                                    FaceManager const & faceManager ) override
   {
+    GEOSX_UNUSED_VAR( nodeManager );
+    GEOSX_UNUSED_VAR( faceManager );
     //TODO ?
   }
 
-  virtual void setupRelatedObjectsInRelations( MeshLevel const * const GEOSX_UNUSED_PARAM( mesh ) ) override
+  virtual void setupRelatedObjectsInRelations( MeshLevel const * const mesh ) override
   {
+    GEOSX_UNUSED_VAR( mesh );
     //TODO ?
   }
 
+  /**
+   * @name viewKeyStruct
+   */
+  ///@{
+
+  /**
+   *  @struct viewKeyStruct
+   *  @brief Contains added view access key to be bound with class data member.
+   */
   struct viewKeyStruct : ObjectManagerBase::viewKeyStruct
   {
+    /// @cond DO_NOT_DOCUMENT
     static constexpr auto elementVolumeString          = "elementVolume";
     static constexpr auto fineElementsListString       = "fineElements";
+    /// @endcond
   };
+  ///@}
 
 private:
   /// The elements to nodes relation is one to one relation.
