a3f4e54bdd4e0e83ed1447dee268abc77b033b73
Christopher Sherman <sherman27@llnl.gov>
2020-06-23 17:43:10 -0700

    Add python XML formatter (#947)
    * Adding a simple xml formatter to pygeos
    
    * Adding gitignore
    
    * Updating the xml formatter
    
    * Adding missing line at end of pygeos gitignore
    
    * Updating xml formatter
    
    * Handling optional namespace information in the xml formatter
    
    * Pluggin the xml formatter into the cmake structure
    
    * Changing comment spacing in xml formatter

--- src/coreComponents/python/modules/pygeos_package/setup.py
+++ src/coreComponents/python/modules/pygeos_package/setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
 setup(name='pygeos',
-      version='0.4.1',
+      version='0.5.0',
       description='GEOS front-end preprocessing package',
       author='Chris Sherman',
       author_email='sherman27@llnl.gov',
       packages=['pygeos', 'pygeos.tests'],
       entry_points={'console_scripts': ['pygeos = pygeos.__main__:main',
+                                        'format_xml = pygeos.xml_formatter:main',
                                         'test_pygeos = pygeos.tests.test_manager:run_unit_tests']},
       install_requires=['lxml', 'numpy'])
 
