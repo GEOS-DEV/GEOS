37b4148619681383f23c7e550d4899094b2eb950
wrtobin <tobin6@llnl.gov>
2020-07-08 14:04:11 -0700

    moving time history collector

--- src/coreComponents/managers/TimeHistory/TimeHistoryCollector.cpp
+++ src/coreComponents/managers/TimeHistory/TimeHistoryCollector.cpp
@@ -5,4 +5,7 @@ namespace geosx
 {
   REGISTER_CATALOG_ENTRY( TaskBase, HistoryCollection, std::string const &, Group * const );
   REGISTER_CATALOG_ENTRY( TaskBase, PackCollection, std::string const &, Group * const );
+
+
+
 }
