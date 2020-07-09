22ca8889cfc1d2169cb4d988d37e1f5dd68f25bd
wrtobin <tobin6@llnl.gov>
2020-07-09 11:36:45 -0700

    removing uneeded file

--- src/coreComponents/managers/TimeHistory/TimeHistoryCollection.cpp
+++
@@ -1,7 +0,0 @@
-#include "TimeHistoryCollection.hpp"
-#include "managers/Tasks/TaskBase.hpp"
-
-namespace geosx
-{
-//REGISTER_CATALOG_ENTRY( TaskBase, HistoryCollection, std::string const &, Group * const )
-}
