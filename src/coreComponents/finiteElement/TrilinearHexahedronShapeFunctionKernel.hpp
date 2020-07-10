f5321077351f40d06414e7492d6c3cad69ff02c3
Randolph R. Settgast <settgast1@llnl.gov>
2020-07-10 07:22:38 -0700

    1) Added alternate method to calc dNdX reduction in register count. Simplified Jacobian Inverse

--- src/coreComponents/finiteElement/TrilinearHexahedronShapeFunctionKernel.hpp
+++ src/coreComponents/finiteElement/TrilinearHexahedronShapeFunctionKernel.hpp
@@ -75,6 +75,28 @@ private:
 
   GEOSX_HOST_DEVICE
   GEOSX_FORCE_INLINE
+  constexpr static int basisIndex0( localIndex const a )
+  {
+    return (a & 1);
+  }
+
+  GEOSX_HOST_DEVICE
+  GEOSX_FORCE_INLINE
+  constexpr static int basisIndex1( localIndex const a )
+  {
+    return ( a & 2 ) >> 1;
+  }
+
+  GEOSX_HOST_DEVICE
+  GEOSX_FORCE_INLINE
+  constexpr static int basisIndex2( localIndex const a )
+  {
+    return ( a & 4 ) >> 2;
+  }
+
+
+  GEOSX_HOST_DEVICE
+  GEOSX_FORCE_INLINE
   constexpr static real64 parentCoords0( localIndex const a )
   {
     return -1.0 + 2.0 * (a & 1);
@@ -124,6 +146,9 @@ real64 TrilinearHexahedronShapeFunctionKernel::shapeFunctionDerivatives( localIn
 {
   real64 J[3][3] = {{0}};
 
+#define PD 3
+
+#if PD==1
   real64 const quadratureCoords[3] = { quadratureFactor *parentCoords0( q ),
                                        quadratureFactor *parentCoords1( q ),
                                        quadratureFactor *parentCoords2( q ) };
@@ -136,15 +161,54 @@ real64 TrilinearHexahedronShapeFunctionKernel::shapeFunctionDerivatives( localIn
                            oneDimensionalShapeHalf( 0.5, quadratureCoords[2] ) };
   constexpr real64 dpsi[2] = { -0.5, 0.5 };
 
-  for( localIndex a=0; a<2; ++a )
+#elif PD==2
+  constexpr static real64 linearBasisAtQuadrature[2] = { 0.5 * ( 1 + quadratureFactor ),
+                                                         0.5 * ( 1 - quadratureFactor ) };
+  real64 const psi0[2] = { linearBasisAtQuadrature[basisIndex0(q)], linearBasisAtQuadrature[!basisIndex0(q)] };
+  real64 const psi1[2] = { linearBasisAtQuadrature[basisIndex1(q)], linearBasisAtQuadrature[!basisIndex1(q)] };
+  real64 const psi2[2] = { linearBasisAtQuadrature[basisIndex2(q)], linearBasisAtQuadrature[!basisIndex2(q)] };
+  constexpr real64 dpsi[2] = { -0.5, 0.5 };
+
+#elif PD==3
+  int const qa = basisIndex0(q);
+  int const qb = basisIndex1(q);
+  int const qc = basisIndex2(q);
+
+  constexpr static real64 linearBasisAtQuadrature[2] = { 0.5 + 0.5 * quadratureFactor,
+                                                         0.5 - 0.5 * quadratureFactor };
+  constexpr static real64 psiProduct[3] = { 0.5 * linearBasisAtQuadrature[0]*linearBasisAtQuadrature[0],
+                                            0.5 * linearBasisAtQuadrature[0]*linearBasisAtQuadrature[1],
+                                            0.5 * linearBasisAtQuadrature[1]*linearBasisAtQuadrature[1] };
+
+  constexpr static int dpsi[2] = { -1, 1 };
+
+#endif
+
+
+  for( int a=0; a<2; ++a )
   {
-    for( localIndex b=0; b<2; ++b )
+#if PD==3
+    int const qaa = abs(a-qa);
+#endif
+    for( int b=0; b<2; ++b )
     {
-      for( localIndex c=0; c<2; ++c )
+#if PD==3
+      int const qbb = abs(b-qb);
+#endif
+      for( int c=0; c<2; ++c )
       {
+#if PD<3
         real64 const dNdXi[3] = { dpsi[a] * psi1[b] * psi2[c],
                                   psi0[a] * dpsi[b] * psi2[c],
                                   psi0[a] * psi1[b] * dpsi[c] };
+#else
+        int const qcc = abs(c-qc);
+        real64 const dNdXi[3] = { dpsi[a] * psiProduct[ qbb + qcc ],
+                                   dpsi[b] * psiProduct[ qaa + qcc ],
+                                   dpsi[c] * psiProduct[ qaa + qbb ] };
+
+#endif
+
         localIndex const nodeIndex = linearMap( a, b, c );
 
         for( int i = 0; i < 3; ++i )
@@ -158,18 +222,31 @@ real64 TrilinearHexahedronShapeFunctionKernel::shapeFunctionDerivatives( localIn
     }
   }
 
-  real64 const detJ = inverse( J, &(dNdX[0][0]) );
+  real64 const detJ = inverse( J );
 
 
-  for( localIndex a=0; a<2; ++a )
+  for( int a=0; a<2; ++a )
   {
-    for( localIndex b=0; b<2; ++b )
+#if PD==3
+    int const qaa = abs(a-qa);
+#endif
+    for( int b=0; b<2; ++b )
     {
-      for( localIndex c=0; c<2; ++c )
+#if PD==3
+      int const qbb = abs(b-qb);
+#endif
+      for( int c=0; c<2; ++c )
       {
+#if PD<3
         real64 const dNdXi[3] = { dpsi[a] * psi1[b] * psi2[c],
                                   psi0[a] * dpsi[b] * psi2[c],
                                   psi0[a] * psi1[b] * dpsi[c] };
+#else
+        int const qcc = abs(c-qc);
+        real64 const dNdXi[3] = { dpsi[a] * psiProduct[ qbb + qcc ],
+                                  dpsi[b] * psiProduct[ qaa + qcc ],
+                                  dpsi[c] * psiProduct[ qaa + qbb ] };
+#endif
         localIndex const nodeIndex = linearMap( a, b, c );
         for( int i = 0; i < 3; ++i )
         {
