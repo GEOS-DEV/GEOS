149242dabab3e5fba59d21a1c5bb6ced32f38d69
Nicola Castelletto <castelletto1@llnl.gov>
2020-07-16 16:11:32 -0700

    Adding quadrilateral face

--- src/coreComponents/finiteElement/BiLinearWedgeShapeFunctionKernel.hpp
+++ src/coreComponents/finiteElement/BiLinearWedgeShapeFunctionKernel.hpp
@@ -1,4 +1,20 @@
-
+/*
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Copyright (c) 2018, Lawrence Livermore National Security, LLC.
+ *
+ * Produced at the Lawrence Livermore National Laboratory
+ *
+ * LLNL-CODE-746361
+ *
+ * All rights reserved. See COPYRIGHT for details.
+ *
+ * This file is part of the GEOSX Simulation Framework.
+ *
+ * GEOSX is a free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License (as published by the
+ * Free Software Foundation) version 2.1 dated February 1999.
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
 
 #ifndef GEOSX_CORE_FINITEELEMENT_BILINEARWEDGESHAPEFUNCTIONKERNEL
 #define GEOSX_CORE_FINITEELEMENT_BILINEARWEDGESHAPEFUNCTIONKERNEL
@@ -165,7 +181,6 @@ real64 BiLinearWedgeShapeFunctionKernel::shapeFunctionDerivatives( localIndex co
   // Return determinant times the weight (i.e. for 1-point formula the volume of the tetrahedron)
   return detJ * weight;
 }
-
 
 }
 
