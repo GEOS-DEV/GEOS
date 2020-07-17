c53c98752781d52a6a1e85a71b01ec6bb5629966
Nicola Castelletto <castelletto1@llnl.gov>
2020-07-16 23:15:39 -0700

    Adding triangular face

--- src/coreComponents/finiteElement/BiLinearQuadrilateralFaceShapeFunctionKernel.hpp
+++ src/coreComponents/finiteElement/BiLinearQuadrilateralFaceShapeFunctionKernel.hpp
@@ -1,19 +1,19 @@
 /*
- *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Copyright (c) 2018, Lawrence Livermore National Security, LLC.
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: LGPL-2.1-only
  *
- * Produced at the Lawrence Livermore National Laboratory
+ * Copyright (c) 2018-2019 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2019 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2018-2019 Total, S.A
+ * Copyright (c) 2019-     GEOSX Contributors
+ * All right reserved
  *
- * LLNL-CODE-746361
- *
- * All rights reserved. See COPYRIGHT for details.
- *
- * This file is part of the GEOSX Simulation Framework.
- *
- * GEOSX is a free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License (as published by the
- * Free Software Foundation) version 2.1 dated February 1999.
- *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
+/**
+ * @file BiLinearQuadrilateralShapeShapeFunctionKernel.hpp
  */
 
 #ifndef GEOSX_CORE_FINITEELEMENT_BILINEARQUADRILATERALFACE
@@ -34,8 +34,8 @@ public:
 
   using BaseClass::numNodes;
   using BaseClass::numQuadraturePoints;
-  constexpr static real64 parentVolume = 4.0;
-  constexpr static real64 weight = parentVolume / numQuadraturePoints;
+  constexpr static real64 parentArea = 4.0;
+  constexpr static real64 weight = parentArea / numQuadraturePoints;
   constexpr static real64 quadratureFactor = 1.0 / 1.732050807568877293528;
 
   GEOSX_HOST_DEVICE
