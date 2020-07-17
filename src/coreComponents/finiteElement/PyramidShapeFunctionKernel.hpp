d365c3ab08b03479be2a84f7ba795d59271be258
Nicola Castelletto <castelletto1@llnl.gov>
2020-07-16 23:15:39 -0700

    Adding triangular face

--- src/coreComponents/finiteElement/PyramidShapeFunctionKernel.hpp
+++ src/coreComponents/finiteElement/PyramidShapeFunctionKernel.hpp
@@ -1,4 +1,20 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ * Copyright (c) 2018-2019 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2019 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2018-2019 Total, S.A
+ * Copyright (c) 2019-     GEOSX Contributors
+ * All right reserved
+ *
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
 
+/**
+ * @file PyramidShapeFunctionKernel.hpp
+ */
 
 #ifndef GEOSX_CORE_FINITEELEMENT_PYRAMIDSHAPEFUNCTIONKERNEL
 #define GEOSX_CORE_FINITEELEMENT_PYRAMIDSHAPEFUNCTIONKERNEL
