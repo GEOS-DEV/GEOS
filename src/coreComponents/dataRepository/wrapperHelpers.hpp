299bc1c008fc7e161b8ecfe78365a022169ecb7e
Benjamin Curtice Corbett <corbett5@llnl.gov>
2020-07-22 15:21:23 -0700

    Python framework in place.

--- src/coreComponents/dataRepository/wrapperHelpers.hpp
+++ src/coreComponents/dataRepository/wrapperHelpers.hpp
@@ -741,8 +741,8 @@ UnpackDataByIndexDevice( buffer_unit_type const * &, T const &, IDX & )
 
 template< typename T >
 inline std::enable_if_t< LvArray::python::CanCreate< T >, PyObject * >
-createPythonObject( T & object )
-{ return LvArray::python::create( object ); }
+createPythonObject( T & object, bool const modify )
+{ return LvArray::python::create( object, modify ); }
 
 template< typename T >
 inline std::enable_if_t< !LvArray::python::CanCreate< T >, PyObject * >
