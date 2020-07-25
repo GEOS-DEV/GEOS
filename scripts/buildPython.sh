2396a957b41ba8604f35ae873e5ee4b2e181426f
Benjamin Curtice Corbett <corbett5@llnl.gov>
2020-07-25 10:53:54 -0700

    Squash

--- scripts/buildPython.sh
+++ scripts/buildPython.sh
@@ -1,6 +1,7 @@
 # Delete me
 
 # wget https://www.python.org/ftp/python/3.8.5/Python-3.8.5.tgz
+# https://bugs.python.org/issue39697
 
 mkdir -p /usr/WS1/GEOS/GEOSX/python/quartz-clang@10.0.0/lib
 ./configure --prefix=/usr/WS1/GEOS/GEOSX/python/quartz-clang@10.0.0 CXX=/usr/tce/packages/clang/clang-10.0.0/bin/clang++ CC=/usr/tce/packages/clang/clang-10.0.0/bin/clang --enable-shared LDFLAGS="-Wl,-rpath /usr/WS1/GEOS/GEOSX/python/quartz-clang@10.0.0/lib"
