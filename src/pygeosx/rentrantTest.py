0f81f1e956353ddc6ebc51358540698f9146140b
Benjamin Curtice Corbett <corbett5@llnl.gov>
2020-07-30 17:01:17 -0700

    No longer cd-ing to the output directory, vtk output now in the wrong spot but all tests pass rentrant.

--- src/pygeosx/rentrantTest.py
+++ src/pygeosx/rentrantTest.py
@@ -10,17 +10,31 @@ if rank == 0:
     print( "In python" )
     sys.stdout.flush()
 
-problem = pygeosx.initialize( rank, sys.argv )
+# If not a restart run then we'll run the SSLE-sedov problem first. 
+if "-r" not in sys.argv:
+  argCopy = list( sys.argv )
+
+  xmlIndex = argCopy.index( "-i" ) + 1
+  argCopy[ xmlIndex ] = "/usr/WS2/corbett5/geosx/integratedTests/update/run/solidMechanicsSSLE/SSLE-sedov.xml"
+
+  outputIndex = argCopy.index( "-o" ) + 1
+  argCopy[ outputIndex ] = sys.argv[ outputIndex ] + "_ssle_dummy"
+
+  problem = pygeosx.initialize( rank, argCopy )
+  pygeosx.applyInitialConditions()
+
+  while pygeosx.run() != pygeosx.COMPLETED:
+      pass
+
+  if rank == 0:
+    print("\n\n In python second time around")
+    sys.stdout.flush()
+
+  problem = pygeosx.reinit( sys.argv )
+else:
+  problem = pygeosx.initialize( rank, sys.argv )
+
 pygeosx.applyInitialConditions()
 
 while pygeosx.run() != pygeosx.COMPLETED:
     pass
-
-# if rank == 0:
-#   print("\n\n In python second time around")
-
-# problem = pygeosx.reinit( sys.argv )
-# pygeosx.applyInitialConditions()
-
-# while pygeosx.run() != pygeosx.COMPLETED:
-#     pass
