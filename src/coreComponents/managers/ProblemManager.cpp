9b8d9dfe556a7aacd48cdcb27e67b4a16eac537c
Herve Gross <herve.gross@total.com>
2020-08-27 15:52:33 -0700

    GEOSX instead of GEOS

--- src/coreComponents/managers/ProblemManager.cpp
+++ src/coreComponents/managers/ProblemManager.cpp
@@ -458,7 +458,7 @@ void ProblemManager::ParseInputFile()
   Py_DECREF( pModule );
 
 #else
-  GEOSX_LOG_RANK_0( "GEOS must be configured to use Python to use parameters, symbolic math, etc. in input files" );
+  GEOSX_LOG_RANK_0( "GEOSX must be configured to use Python to use parameters, symbolic math, etc. in input files" );
 #endif
 
 
