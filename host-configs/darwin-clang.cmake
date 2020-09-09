a39b68bb5873ec6bbcdaef72b89a10339ad08905
Shabnam <ssemnani@ucsd.edu>
2020-09-09 10:20:37 -0700

    modified host config file for darwin

--- host-configs/darwin-clang.cmake
+++ host-configs/darwin-clang.cmake
@@ -1,10 +1,11 @@
 site_name(HOST_NAME)
-set(CONFIG_NAME "${HOST_NAME}-darwin-x86_64-clang@apple-mp" CACHE PATH "") 
+set(CONFIG_NAME "${HOST_NAME}-darwin-x86_64-apple-darwin19.6.0" CACHE PATH "") 
 message( "CONFIG_NAME = ${CONFIG_NAME}" )
 
 set(CMAKE_C_COMPILER "/usr/bin/clang" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "/usr/bin/clang++" CACHE PATH "")
-set(ENABLE_FORTRAN OFF CACHE BOOL "" FORCE)
+set(CMAKE_Fortran_COMPILER "/Applications/deal.II.app/Contents/Resources/opt/gfortran-5.0/bin/gfortran" CACHE PATH "")
+set(ENABLE_FORTRAN ON CACHE BOOL "" FORCE)
 
 set(ENABLE_MPI ON CACHE PATH "")
 set(MPI_C_COMPILER "/usr/local/bin/mpicc" CACHE PATH "")
@@ -27,4 +28,5 @@ set( LAPACK_LIBRARIES /usr/lib/liblapack.dylib CACHE PATH "" FORCE )
 
 set( ENABLE_DOXYGEN OFF CACHE BOOL "" FORCE )
 #set( DOXYGEN_EXECUTABLE /usr/local/bin/doxygen CACHE PATH "" FORCE )
-#set( SPHINX_EXECUTABLE /usr/local/bin/sphinx-build CACHE PATH "" FORCE )
+set( SPHINX_EXECUTABLE /usr/local/Cellar/sphinx-doc/3.1.2/bin/sphinx-build CACHE PATH "" FORCE )
+set(ENABLE_UNCRUSTIFY OFF CACHE PATH "" FORCE) 
