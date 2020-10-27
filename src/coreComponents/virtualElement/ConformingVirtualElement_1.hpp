6dbee70fa37c78d1c9f85cb190cd60dd13547498
Andrea Borio <andrea.borio@polito.it>
2020-10-27 17:03:53 +0100

    First hypothetical structure for VEM classes.
    Also added a test to check build.

---
+++ src/coreComponents/virtualElement/ConformingVirtualElement_1.hpp
@@ -0,0 +1,35 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ * Copyright (c) 2018-2019 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2019 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2018-2019 Total, S.A
+ * Copyright (c) 2019-     GEOSX Contributors
+ * All right reserved
+ *
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
+/**
+ * @file ConformingVirtualElement_1.hpp
+ */
+
+#ifndef GEOSX_VIRTUALELEMENT_CONFORMINGVIRTUALELEMENT1_HPP_
+#define GEOSX_VIRTUALELEMENT_CONFORMINGVIRTUALELEMENT1_HPP_
+
+#include "VirtualElementBase.hpp"
+
+namespace geosx
+{
+  namespace virtualElement
+  {
+    class ConformingVirtualElement_1 final : public VirtualElementBase
+    {
+      
+    };
+  }
+}
+
+#endif // GEOSX_VIRTUALELEMENT_CONFORMINGVIRTUALELEMENT1_HPP_
---
+++ src/coreComponents/virtualElement/unitTests/testVirtualElementBase.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "virtualElement/VirtualElementBase.hpp"
+#include "virtualElement/ConformingVirtualElement_1.hpp"
+
+TEST( VirtualElementBase, compilation )
+{
+
+}
