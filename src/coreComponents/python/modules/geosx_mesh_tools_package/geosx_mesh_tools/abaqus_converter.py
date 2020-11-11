23ff966881344d199a0200dad73686e9fcfa41ee
Christopher Sherman <sherman27@llnl.gov>
2020-11-11 10:55:51 -0800

    Adding exit warning message, multi-block support to abaqus converter

--- src/coreComponents/python/modules/geosx_mesh_tools_package/geosx_mesh_tools/abaqus_converter.py
+++ src/coreComponents/python/modules/geosx_mesh_tools_package/geosx_mesh_tools/abaqus_converter.py
@@ -4,6 +4,7 @@ from meshio._mesh import CellBlock
 import numpy as np
 import argparse
 import logging
+import sys
 
 
 def convert_abaqus_to_gmsh(input_mesh, output_mesh, logger=None):
@@ -21,25 +22,35 @@ def convert_abaqus_to_gmsh(input_mesh, output_mesh, logger=None):
       logging.basicConfig(level=logging.WARNING)
       logger = logging.getLogger(__name__)
 
+    # Keep track of the number of warnings
+    n_warnings = 0
+
     # Load the mesh
     logger.info('Reading abaqus mesh...')
-    mesh = meshio.Mesh.read(input_mesh, "abaqus")
+    mesh = meshio.read(input_mesh, file_format="abaqus")
 
     # Convert the element regions to tags
     logger.info('Converting region tags...')
     region_list = list(mesh.cell_sets.keys())
     n_regions = len(region_list)
-    cell_ids = np.zeros(len(mesh.cells[0][1]), dtype=int) - 1
-    for ii, region in enumerate(region_list):
-        mesh.field_data[region] = [ii + 1, 3]
-        cell_ids[mesh.cell_sets[region][0]] = ii + 1
+    cell_ids = []
+    for block_id, block in enumerate(mesh.cells):
+        cell_ids.append(np.zeros(len(block[1]), dtype=int) - 1)
+        for region_id, region in enumerate(region_list):
+            mesh.field_data[region] = [region_id + 1, 3]
+            cell_ids[block_id][mesh.cell_sets[region][block_id]] = region_id + 1
+
+        # Check for bad element region conversions
+        if (-1 in cell_ids[-1]):
+            logger.warning('Some element regions in block %i did not convert correctly to tags!' % (block_id))
+            logger.warning('Note: These will be indicated by a -1 in the output file.')
+            n_warnings += 1
 
     # Add to the meshio datastructure
-    mesh.cell_data['gmsh:physical'] = [cell_ids]
-    mesh.cell_data['gmsh:geometrical'] = [cell_ids]
-    if (-1 in cell_ids):
-        logger.warning('Some element regions did not convert correctly to tags!')
-        logger.warning('Note: These will be indicated by a -1 in the output file.')
+    # Note: the slice here is required, so that later appends
+    #       do not break these dicts
+    mesh.cell_data['gmsh:physical'] = cell_ids[:]
+    mesh.cell_data['gmsh:geometrical'] = cell_ids[:]
 
     # Build the face elements
     logger.info('Converting nodesets to face elements, tags...')
@@ -51,38 +62,37 @@ def convert_abaqus_to_gmsh(input_mesh, output_mesh, logger=None):
         mesh.field_data[nodeset_name] = [nodeset_id + n_regions + 1, 2]
         nodeset = mesh.point_sets[nodeset_name]
 
-        # Search the elements
-        for element_id in range(len(mesh.cells[0][1])):
-            # Find any matching nodes
-            matching_nodes = []
-            for node_id in mesh.cells[0][1][element_id]:
-                if node_id in nodeset:
-                    matching_nodes.append(node_id)
+        # Search by block, then element
+        for block_id, block in enumerate(mesh.cells):
+            for element_id, element in enumerate(block[1]):
+                # Find any matching nodes
+                matching_nodes = [x for x in element if x in nodeset]
 
-            # Add a new face element if there are enough nodes
-            n_matching = len(matching_nodes)
-            if (n_matching >= 3):
-                # Find the region
-                region_id = -1
-                for region in region_list:
-                    if (element_id in mesh.cell_sets[region][0]):
-                        region_id = mesh.field_data[region][0]
+                # Add a new face element if there are enough nodes
+                n_matching = len(matching_nodes)
+                if (n_matching >= 3):
+                    # Find the region
+                    region_id = -1
+                    for region in region_list:
+                        if (element_id in mesh.cell_sets[region][block_id]):
+                            region_id = mesh.field_data[region][block_id]
 
-                # Test to see if the element is a quad or triangle
-                tag_id = mesh.field_data[nodeset_name][0]
-                if (n_matching == 3):
-                    new_tris.append(matching_nodes)
-                    tri_nodeset.append(tag_id)
-                    tri_region.append(region_id)
+                    # Test to see if the element is a quad or triangle
+                    tag_id = mesh.field_data[nodeset_name][0]
+                    if (n_matching == 3):
+                        new_tris.append(matching_nodes)
+                        tri_nodeset.append(tag_id)
+                        tri_region.append(region_id)
 
-                elif (n_matching == 4):
-                    new_quads.append(matching_nodes)
-                    quad_nodeset.append(tag_id)
-                    quad_region.append(region_id)
+                    elif (n_matching == 4):
+                        new_quads.append(matching_nodes)
+                        quad_nodeset.append(tag_id)
+                        quad_region.append(region_id)
 
-                else:
-                    logger.warning('  Discarding an element with an unexpected number of nodes')
-                    logger.warning('    n_nodes=%i, element=%i, set=%s' % (n_matching, element_id, nodeset_name))
+                    else:
+                        logger.warning('  Discarding an element with an unexpected number of nodes')
+                        logger.warning('    n_nodes=%i, element=%i, set=%s' % (n_matching, element_id, nodeset_name))
+                        n_warnings += 1
 
     # Add new tris
     if new_tris:
@@ -90,16 +100,18 @@ def convert_abaqus_to_gmsh(input_mesh, output_mesh, logger=None):
         if (-1 in tri_region):
             logger.warning('Traingles with empty region information found!')
             logger.warning('Note: These will be indicated by a -1 in the output file.')
+            n_warnings += 1
         mesh.cells.append(CellBlock('triangle', np.array(new_tris)))
         mesh.cell_data['gmsh:geometrical'].append(np.array(tri_region))
         mesh.cell_data['gmsh:physical'].append(np.array(tri_nodeset))
 
-    # Add  new quads
+    # Add new quads
     if new_quads:
         logger.info('  Adding %i new quads...' % (len(new_quads)))
         if (-1 in quad_region):
             logger.warning('Quads with empty region information found!')
             logger.warning('Note: These will be indicated by a -1 in the output file.')
+            n_warnings += 1
         mesh.cells.append(CellBlock('quad', np.array(new_quads)))
         mesh.cell_data['gmsh:geometrical'].append(np.array(quad_region))
         mesh.cell_data['gmsh:physical'].append(np.array(quad_nodeset))
@@ -107,8 +119,9 @@ def convert_abaqus_to_gmsh(input_mesh, output_mesh, logger=None):
     # Write the final mesh
     logger.info('Writing gmsh mesh...')
     meshio.write(output_mesh, mesh, file_format="gmsh22", binary=False)
-
     logger.info('Done!')
+
+    return (n_warnings > 0)
 
 
 def main():
@@ -131,6 +144,8 @@ def main():
     if args.verbose:
       logger.setLevel(logging.INFO)
 
-    convert_abaqus_to_gmsh(args.input, args.output, logger)
-
+    # Call the converter
+    err = convert_abaqus_to_gmsh(args.input, args.output, logger)
+    if err:
+        sys.exit('Warnings detected: check the output file for potential errors!')
 
