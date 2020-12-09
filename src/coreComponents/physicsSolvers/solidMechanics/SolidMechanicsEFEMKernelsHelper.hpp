df4f118649eb29429d7848a79a15399d030dcee4
Matteo Cusini <matteo.cusini@gmail.com>
2020-12-09 10:02:56 -0800

    update code to use new tensorOps

--- src/coreComponents/physicsSolvers/solidMechanics/SolidMechanicsEFEMKernelsHelper.hpp
+++ src/coreComponents/physicsSolvers/solidMechanics/SolidMechanicsEFEMKernelsHelper.hpp
@@ -85,7 +85,7 @@ void assembleStrainOperator( real64 ( & strainMatrix )[I_SIZE][J_SIZE],
 template< int NUM_NODES >
 GEOSX_HOST_DEVICE
 GEOSX_FORCE_INLINE
-void assembleCompatibilityOperator( real64 ( & compMatrix )[6][3],
+void assembleCompatibilityOperator( real64 ( & compMatrix )[3][6],
                                     arraySlice1d< real64 const > const & nVec,
                                     arraySlice1d< real64 const > const & tVec1,
                                     arraySlice1d< real64 const > const & tVec2,
@@ -97,7 +97,6 @@ void assembleCompatibilityOperator( real64 ( & compMatrix )[6][3],
   // Fill in compatibility operator
 
   // 1. construct mvector sum(dNdX(a) * H(a)) value for each Gauss point
-
   real64 mVec[3];
   LvArray::tensorOps::fill< 3 >( mVec, 0 );
   for( integer a=0; a<NUM_NODES; ++a )
@@ -107,40 +106,21 @@ void assembleCompatibilityOperator( real64 ( & compMatrix )[6][3],
     mVec[2] -= dNdX[a][2] * heavisideFun[a];
   }
 
-
   // 2. fill in the operator itself
+  // sym(n dyadic m), sym(m dyadic t1) and sym (m dyadic t2)
+  LvArray::tensorOps::Rij_eq_AiBj_plus_AjBi< 3 >( compMatrix[0],  mVec, nVec );
+  LvArray::tensorOps::Rij_eq_AiBj_plus_AjBi< 3 >( compMatrix[1], mVec, tVec1 );
+  LvArray::tensorOps::Rij_eq_AiBj_plus_AjBi< 3 >( compMatrix[2], mVec, tVec2 );
 
-  real64 nDmSym[6], t1DmSym[6], t2DmSym[6];
+ // scale by 0.5 the diagonal entries (it's like a strain)
+  for (int ii = 0; ii<3; ii++)
+  {
+    for (int jj=0; jj<3; jj++)
+    {
+      compMatrix[ii][jj] *= 0.5;
+    }
+  }
 
-  // sym(n dyadic m), sym(n dyadic t1) and sym (n dyadic t2)
-  LvArray::tensorOps::Rij_eq_AiBj_plus_AjBi< 3 >( nDmSym, mVec, nVec );
-  LvArray::tensorOps::Rij_eq_AiBj_plus_AjBi< 3 >( t1DmSym, mVec, tVec1 );
-  LvArray::tensorOps::Rij_eq_AiBj_plus_AjBi< 3 >( t2DmSym, mVec, tVec2 );
-
-  // if compMatrix was real64[3][6], then you could just pass
-  // compMatrix[0], compMatrix[1], compMatrix[2] instead of creating
-  // nDmSum, t1DmSum, t2DmSum.
-
-  compMatrix[0][0] = 0.5 * nDmSym[0];
-  compMatrix[1][0] = 0.5 * nDmSym[1];
-  compMatrix[2][0] = 0.5 * nDmSym[2];
-  compMatrix[3][0] = nDmSym[3];
-  compMatrix[4][0] = nDmSym[4];
-  compMatrix[5][0] = nDmSym[5];
-
-  compMatrix[0][1] = 0.5 * t1DmSym[0];
-  compMatrix[1][1] = 0.5 * t1DmSym[1];
-  compMatrix[2][1] = 0.5 * t1DmSym[2];
-  compMatrix[3][1] = t1DmSym[3];
-  compMatrix[4][1] = t1DmSym[4];
-  compMatrix[5][1] = t1DmSym[5];
-
-  compMatrix[0][2] = 0.5 * t2DmSym[0];
-  compMatrix[1][2] = 0.5 * t2DmSym[1];
-  compMatrix[2][2] = 0.5 * t2DmSym[2];
-  compMatrix[3][2] = t2DmSym[3];
-  compMatrix[4][2] = t2DmSym[4];
-  compMatrix[5][2] = t2DmSym[5];
 }
 
 GEOSX_HOST_DEVICE
@@ -151,43 +131,16 @@ void assembleEquilibriumOperator( real64 ( & eqMatrix )[3][6],
                                   arraySlice1d< real64 const > const tVec2,
                                   real64 const hInv )
 {
-  LvArray::tensorOps::fill< 3, 6 >( eqMatrix, 0 );
+  // (n dyadic n), sym(n dyadic t1) and sym (n dyadic t2)
+  LvArray::tensorOps::Rij_eq_AiBj_plus_AjBi< 3 >( eqMatrix[0], nVec, nVec  );
+  LvArray::tensorOps::Rij_eq_AiBj_plus_AjBi< 3 >( eqMatrix[1], nVec, tVec1 );
+  LvArray::tensorOps::Rij_eq_AiBj_plus_AjBi< 3 >( eqMatrix[2], nVec, tVec2 );
 
-  // Define the dyadic producs
-  // nDn := nVec_dyadic_nVec,
-  // t1DnSym:= sym(tVec1_dyadic_nVec)
-  // t2DnSym:= sym(tVec2_dyadic_nVec)
-  real64 nDn[3][3], t1DnSym[3][3], t2DnSym[3][3];
-
-  // n dyadic n
-  LvArray::tensorOps::Rij_eq_AiBj< 3, 3 >( nDn, nVec, nVec );
-
-  // sym(n dyadic t1) and sym (n dyadic t2)
-  LvArray::tensorOps::Rij_eq_AiBj< 3, 3 >( t1DnSym, nVec, tVec1 );
-  LvArray::tensorOps::Rij_add_AiBj< 3, 3 >( t1DnSym, tVec1, nVec );
-  LvArray::tensorOps::scale< 3, 3 >( t1DnSym, 0.5 );
-
-  LvArray::tensorOps::Rij_eq_AiBj< 3, 3 >( t2DnSym, nVec, tVec2 );
-  LvArray::tensorOps::Rij_add_AiBj< 3, 3 >( t2DnSym, tVec2, nVec );
-  LvArray::tensorOps::scale< 3, 3 >( t2DnSym, 0.5 );
-
-  int VoigtIndex;
-
-  for( int i=0; i < 3; ++i )
+  for (int ii = 0; ii<3; ii++)
   {
-    for( int j=0; j < 3; ++j )
+    for (int jj=0; jj<3; jj++)
     {
-      if( i == j )
-      {
-        VoigtIndex = 1;
-      }
-      else
-      {
-        VoigtIndex = 6 - i - j;
-      }
-      eqMatrix[0][VoigtIndex] += nDn     [i][j];
-      eqMatrix[1][VoigtIndex] += t1DnSym [i][j];
-      eqMatrix[2][VoigtIndex] += t2DnSym [i][j];
+      eqMatrix[ii][jj] *= 0.5;
     }
   }
 
