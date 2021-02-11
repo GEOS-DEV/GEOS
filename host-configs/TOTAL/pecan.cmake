268d46b12d2ed22578ca57a32309bc3b7c0bc88d
Thomas Gazzola <thomas.gazzola@total.com>
2021-02-10 20:44:30 -0800

    Test pecan GPU build

--- host-configs/TOTAL/pecan.cmake
+++ host-configs/TOTAL/pecan.cmake
@@ -1,26 +1,25 @@
-site_name(HOST_NAME)
-
-#set(COMPILER_HOME "/apps/gcc/8.2.0/x86_64")
-set(COMPILER_HOME "/data/gpfs/Users/j0436735/deployments/local")
-#set(MPI_HOME "/hrtc/apps/mpi/openmpi/4.0.1/RDHPC/gcc/8.2.0")
-set(MPI_HOME "/data/gpfs/Users/j0436735/deployments/local")
+set(COMPILER_HOME "/apps/gcc/8.2.0/x86_64")
+set(MPI_HOME "/hrtc/apps/mpi/openmpi/4.0.1/RDHPC/gcc/8.2.0")
 
 set(CMAKE_C_COMPILER ${COMPILER_HOME}/bin/gcc CACHE PATH "" FORCE)
 set(CMAKE_CXX_COMPILER ${COMPILER_HOME}/bin/g++ CACHE PATH "" FORCE)
-set(CMAKE_Fortran_COMPILER ${COMPILER_HOME}/bin/gfortran CACHE PATH "" FORCE)
-#set(ENABLE_FORTRAN OFF CACHE BOOL "" FORCE)
+#set(CMAKE_Fortran_COMPILER ${COMPILER_HOME}/bin/gfortran CACHE PATH "" FORCE)
+set(ENABLE_FORTRAN OFF CACHE BOOL "" FORCE)
 
 set(ENABLE_MPI ON CACHE PATH "" FORCE)
 set(ENABLE_OPENMP ON CACHE PATH "" FORCE)
+
 set(MPI_C_COMPILER "${MPI_HOME}/bin/mpicc" CACHE PATH "" FORCE)
 set(MPI_CXX_COMPILER "${MPI_HOME}/bin/mpicxx" CACHE PATH "" FORCE)
-
+#set(MPI_Fortran_COMPILER "${MPI_HOME}/bin/mpifort" CACHE PATH "" FORCE)
 set(MPIEXEC_EXECUTABLE "${MPI_HOME}/bin/mpirun" CACHE PATH "" FORCE)
+#set(MPIEXEC_EXECUTABLE /apps/slurm/x86/20.02.0/bin/srun CACHE PATH "" FORCE)
+#set(MPIEXEC_NUMPROC_FLAG   "-p pecan -n" CACHE STRING "")
+set(ENABLE_WRAP_ALL_TESTS_WITH_MPIEXEC ON CACHE BOOL "")
 
-set(ENABLE_CUDA OFF CACHE PATH "" FORCE)
-
+set(ENABLE_CUDA ON CACHE PATH "" FORCE)
 if(ENABLE_CUDA)
-  set(CUDA_TOOLKIT_ROOT_DIR /hrtc/apps/cuda/10.1/x86_64 CACHE PATH "")
+  set(CUDA_TOOLKIT_ROOT_DIR /hrtc/apps/cuda/10.2.89/x86_64 CACHE PATH "")
   set(CMAKE_CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER} CACHE STRING "")
   set(CMAKE_CUDA_COMPILER ${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc CACHE STRING "")
   set(CUDA_ARCH sm_75 CACHE STRING "")
@@ -31,7 +30,6 @@ if(ENABLE_CUDA)
   set(CMAKE_CUDA_FLAGS_RELWITHDEBINFO "-g -lineinfo ${CMAKE_CUDA_FLAGS_RELEASE}" CACHE STRING "")
   set(CMAKE_CUDA_FLAGS_DEBUG "-g -G -O0 -Xcompiler -O0" CACHE STRING "")
 endif()
-
 
 set(ENABLE_GTEST_DEATH_TESTS ON CACHE BOOL "" FORCE)
 set(ENABLE_CALIPER ON CACHE BOOL "")
@@ -48,6 +46,5 @@ set(MKL_LIBRARIES ${MKL_ROOT}/lib/intel64/libmkl_intel_lp64.so
                   ${INTEL_ROOT}/compiler/lib/intel64_lin/libiomp5.so
                   CACHE STRING "")
 
-
-set(GEOSX_TPL_DIR "/data/gpfs/Users/j0436735/deployments/TPL" CACHE PATH "" FORCE)
+#set(GEOSX_TPL_DIR "/data/gpfs/Users/j0436735/deployments/TPL-GPU" CACHE PATH "" FORCE)
 include(${CMAKE_CURRENT_LIST_DIR}/../tpls.cmake)
