5b147944b3b6ffcc25eff1769f4c686544f47429
sytuannguyen <stuan.nguyen@gmail.com>
2021-02-11 16:36:50 +0100

    use the elastoplastic base for DPx

--- src/coreComponents/constitutive/solid/DruckerPragerExtended.hpp
+++ src/coreComponents/constitutive/solid/DruckerPragerExtended.hpp
@@ -19,6 +19,7 @@
 #ifndef GEOSX_CONSTITUTIVE_SOLID_DRUCKERPRAGEREXTENDED_HPP_
 #define GEOSX_CONSTITUTIVE_SOLID_DRUCKERPRAGEREXTENDED_HPP_
 
+#include "ElastoPlasticUpdates.hpp"
 #include "ElasticIsotropic.hpp"
 #include "InvariantDecompositions.hpp"
 #include "PropertyConversions.hpp"
@@ -36,7 +37,7 @@ namespace constitutive
  *
  * Class to provide material updates that may be called from a kernel function.
  */
-class DruckerPragerExtendedUpdates : public ElasticIsotropicUpdates
+class DruckerPragerExtendedUpdates : public ElastoPlasticUpdates
 {
 public:
 
@@ -62,7 +63,7 @@ public:
                                 arrayView1d< real64 const > const & shearModulus,
                                 arrayView3d< real64, solid::STRESS_USD > const & newStress,
                                 arrayView3d< real64, solid::STRESS_USD > const & oldStress ):
-    ElasticIsotropicUpdates( bulkModulus, shearModulus, newStress, oldStress ),
+    ElastoPlasticUpdates( bulkModulus, shearModulus, newStress, oldStress ),
     m_initialFriction( initialFriction ),
     m_residualFriction( residualFriction ),
     m_dilationRatio( dilationRatio ),
@@ -86,41 +87,23 @@ public:
   /// Deleted move assignment operator
   DruckerPragerExtendedUpdates & operator=( DruckerPragerExtendedUpdates && ) =  delete;
 
-  /// Use the uncompressed version of the stiffness bilinear form
-  using DiscretizationOps = SolidModelDiscretizationOpsFullyAnisotroipic; 
+private:
 
-  // Bring in base implementations to prevent hiding warnings
-  using ElasticIsotropicUpdates::smallStrainUpdate;
-
-  GEOSX_HOST_DEVICE
-  virtual void smallStrainUpdate( localIndex const k,
-                                  localIndex const q,
-                                  real64 const ( &strainIncrement )[6],
-                                  real64 ( &stress )[6],
-                                  real64 ( &stiffness )[6][6] ) const override final;
-
-  GEOSX_HOST_DEVICE
-  virtual void smallStrainUpdate( localIndex const k,
-                                  localIndex const q,
-                                  real64 const ( &strainIncrement )[6],
-                                  real64 ( &stress )[6],
-                                  DiscretizationOps & stiffness ) const final;
-
-  real64 yield( localIndex const k,
-                localIndex const GEOSX_UNUSED_PARAM( q ),
-                real64 const invP,
-                real64 const invQ,
-                real64 const friction ) const
+  virtual real64 yield( localIndex const k,
+                        localIndex const GEOSX_UNUSED_PARAM( q ),
+                        real64 const invP,
+                        real64 const invQ,
+                        real64 const friction ) const override final
   {
     return  invQ + friction * ( invP - m_pressureIntercept[k] );
   }
 
-  void yieldDerivatives( localIndex const k,
-                         localIndex const GEOSX_UNUSED_PARAM( q ),
-                         real64 const invP,
-                         real64 const GEOSX_UNUSED_PARAM( invQ ),
-                         real64 const friction,
-                         real64 (& dF)[3] ) const
+  virtual void yieldDerivatives( localIndex const k,
+                                 localIndex const GEOSX_UNUSED_PARAM( q ),
+                                 real64 const invP,
+                                 real64 const GEOSX_UNUSED_PARAM( invQ ),
+                                 real64 const friction,
+                                 real64 (& dF)[3] ) const override final
   {
 
     // The yield function is: invQ + friction * ( invP - m_pressureIntercept[k] )
@@ -134,13 +117,13 @@ public:
     dF[2] = dF_dFriction;
   }
 
-  void potentialDerivatives( localIndex const k,
-                             localIndex const GEOSX_UNUSED_PARAM( q ),
-                             real64 const GEOSX_UNUSED_PARAM( invP ),
-                             real64 const GEOSX_UNUSED_PARAM( invQ ),
-                             real64 const friction,
-                             real64 (& dG)[8] ) const
-  {
+  virtual void potentialDerivatives( localIndex const k,
+                                     localIndex const GEOSX_UNUSED_PARAM( q ),
+                                     real64 const GEOSX_UNUSED_PARAM( invP ),
+                                     real64 const GEOSX_UNUSED_PARAM( invQ ),
+                                     real64 const friction,
+                                     real64 (& dG)[8] ) const override final
+  { 
 
     // The plastic potential function is: G = invQ + invP * m_dilationRatio[k] * friction
 
@@ -167,9 +150,9 @@ public:
     dG[7] = dG_dQ_dH;
   }
 
-  real64 hardening( localIndex const k,
-                    localIndex const GEOSX_UNUSED_PARAM( q ),
-                    real64 const state ) const
+  virtual real64 hardening( localIndex const k,
+                            localIndex const GEOSX_UNUSED_PARAM( q ),
+                            real64 const state ) const override final
   {
     if( state<1e-9 )
     {
@@ -181,9 +164,9 @@ public:
     }
   }
 
-  real64 hardeningDerivatives( localIndex const k,
-                               localIndex const GEOSX_UNUSED_PARAM( q ),
-                               real64 const state ) const
+  virtual real64 hardeningDerivatives( localIndex const k,
+                                       localIndex const GEOSX_UNUSED_PARAM( q ),
+                                       real64 const state ) const override final
   {
     if( state<1e-9 )
     {
@@ -195,11 +178,11 @@ public:
     }
   }
 
-  real64 tmpState( localIndex const k,
-                   localIndex const q,
-                   real64 const GEOSX_UNUSED_PARAM( invP ),
-                   real64 const GEOSX_UNUSED_PARAM( invQ ),
-                   real64 const plasticMultiplier ) const
+  virtual real64 tmpState( localIndex const k,
+                           localIndex const q,
+                           real64 const GEOSX_UNUSED_PARAM( invP ),
+                           real64 const GEOSX_UNUSED_PARAM( invQ ),
+                           real64 const plasticMultiplier ) const override final
   {
 
     // The temporal state variable is updated inside the Newton loops by: state += plasticMultiplier
@@ -208,29 +191,27 @@ public:
     return m_state[k][q] + plasticMultiplier;
   }
 
-  real64 stateDerivatives( localIndex const GEOSX_UNUSED_PARAM( k ),
-                           localIndex const GEOSX_UNUSED_PARAM( q ),
-                           real64 const GEOSX_UNUSED_PARAM( invP ),
-                           real64 const GEOSX_UNUSED_PARAM( invQ ),
-                           real64 const GEOSX_UNUSED_PARAM( plasticMultiplier ) ) const
+  virtual real64 stateDerivatives( localIndex const GEOSX_UNUSED_PARAM( k ),
+                                   localIndex const GEOSX_UNUSED_PARAM( q ),
+                                   real64 const GEOSX_UNUSED_PARAM( invP ),
+                                   real64 const GEOSX_UNUSED_PARAM( invQ ),
+                                   real64 const GEOSX_UNUSED_PARAM( plasticMultiplier ) ) const override final
   {
     return 1.0;
   }
 
-  real64 getStateVariable( localIndex const k,
-                           localIndex const q ) const
+  virtual real64 getStateVariable( localIndex const k,
+                                   localIndex const q ) const override final
   {
     return m_state[k][q];
   }
 
- void saveStateVariable( localIndex const k,
-                         localIndex const q,
-                         real64 const state ) const
+  virtual void saveStateVariable( localIndex const k,
+                                  localIndex const q,
+                                  real64 const state ) const override final
   {
     m_state[k][q] = state;
   }
-
-private:
 
   /// A reference to the ArrayView holding the initial friction coefficient for each element.
   arrayView1d< real64 const > const m_initialFriction;
@@ -250,187 +231,6 @@ private:
   /// A reference to the ArrayView holding the state variable for each integration point
   arrayView2d< real64 > const m_state;
 };
-
-
-GEOSX_HOST_DEVICE
-GEOSX_FORCE_INLINE
-void DruckerPragerExtendedUpdates::smallStrainUpdate( localIndex const k,
-                                                      localIndex const q,
-                                                      real64 const ( &strainIncrement )[6],
-                                                      real64 ( & stress )[6],
-                                                      real64 ( & stiffness )[6][6] ) const
-{
-  // elastic predictor (assume strainIncrement is all elastic)
-
-  ElasticIsotropicUpdates::smallStrainUpdate( k, q, strainIncrement, stress, stiffness );
-
-  // decompose into mean (P) and von mises (Q) stress invariants
-
-  real64 trialP;
-  real64 trialQ;
-  real64 deviator[6];
-
-  twoInvariant::stressDecomposition( stress,
-                                     trialP,
-                                     trialQ,
-                                     deviator );
-
-  // Plastic functions and their derivatives
-
-  real64 dF[3], dG[8], hardeningParam, stateVariable, dHardeningParam_dLambda;
-
-  // check yield function F <= 0, using old state
-
-  stateVariable = getStateVariable( k, q );
-
-  hardeningParam = hardening( k, q, stateVariable );
-
-  if( yield( k, q, trialP, trialQ, hardeningParam ) < 1e-9 ) // elasticity
-  {
-    return;
-  }
-
-  // else, plasticity (trial stress point lies outside yield surface)
-  // the return mapping can in general be written as a newton iteration.
-
-  real64 solution[3], residual[3], delta[3];
-  real64 jacobian[3][3] = {{}}, jacobianInv[3][3] = {{}};
-
-  solution[0] = trialP; // initial guess for newP
-  solution[1] = trialQ; // initial guess for newQ
-  solution[2] = 1e-5;   // initial guess for plastic multiplier
-
-  real64 norm, normZero = 1e30;
-
-  // begin newton loop
-
-  for( localIndex iter=0; iter<20; ++iter )
-  {
-
-    // Derivatives of the yield function
-    // dF_dP = dF[0], dF_dQ = dF[1], dF_dHardeningParam = dF[2]
-
-    yieldDerivatives( k, q, solution[0], solution[1], hardeningParam, dF );
-
-    // Derivatives of the plastic potential function
-    // dG_dP = dG[0], dG_dQ = dG[1]
-    // dG_dP_dP = dG[2], dG_dP_dQ = dG[3], dG_dP_dHardeningParam = dG[4] 
-    // dG_dQ_dP = dG[5], dG_dQ_dQ = dG[6], dG_dQ_dHardeningParam = dG[7]
-
-    potentialDerivatives( k, q, solution[0], solution[1], hardeningParam, dG );
- 
-    // Hardening parameter and its derivative to the plastic multiplier
-     
-    stateVariable = tmpState( k, q, solution[0], solution[1], solution[2] );
-   
-    hardeningParam = hardening( k, q, stateVariable );
-
-    dHardeningParam_dLambda  = hardeningDerivatives( k, q, stateVariable );
-    dHardeningParam_dLambda *= stateDerivatives( k, q, solution[0], solution[1], solution[2] );
-
-    // assemble residual system
-    // resid1 = P - trialP + dlambda*bulkMod*dG/dP = 0
-    // resid2 = Q - trialQ + dlambda*3*shearMod*dG/dQ = 0
-    // resid3 = F = 0
-
-    residual[0] = solution[0] - trialP + solution[2] * m_bulkModulus[k] * dG[0];
-    residual[1] = solution[1] - trialQ + solution[2] * 3.0 * m_shearModulus[k] * dG[1];
-    residual[2] = yield( k, q, solution[0], solution[1], hardeningParam );
-
-    // check for convergence
-
-    norm = LvArray::tensorOps::l2Norm< 3 >( residual );
-
-    if( iter==0 )
-    {
-      normZero = norm;
-    }
-
-    if( norm < 1e-8*(normZero+1))
-    {
-      break;
-    }
-
-    // solve Newton system
-
-    jacobian[0][0] = 1.0 + solution[2] * m_bulkModulus[k] * dG[2];
-    jacobian[0][1] = solution[2] * m_bulkModulus[k] * dG[3];
-    jacobian[0][2] = m_bulkModulus[k] *dG[0] + solution[2] * m_bulkModulus[k] * dG[4] * dHardeningParam_dLambda;
-    jacobian[1][0] = solution[2] * 3.0 * m_shearModulus[k] * dG[5];
-    jacobian[1][1] = 1.0 + solution[2] * 3.0 * m_shearModulus[k] * dG[6];
-    jacobian[1][2] = 3.0 * m_shearModulus[k] * dG[1] + solution[2] * 3.0 * m_shearModulus[k] * dG[7] * dHardeningParam_dLambda;
-    jacobian[2][0] = dF[0];
-    jacobian[2][1] = dF[1];
-    jacobian[2][2] = dF[2] * dHardeningParam_dLambda;
-
-    LvArray::tensorOps::invert< 3 >( jacobianInv, jacobian );
-    LvArray::tensorOps::Ri_eq_AijBj< 3, 3 >( delta, jacobianInv, residual );
-
-    for( localIndex i=0; i<3; ++i )
-    {
-      solution[i] -= delta[i];
-    }
-  }
-
-  // re-construct stress = P*eye + sqrt(2/3)*Q*nhat
-
-  twoInvariant::stressRecomposition( solution[0],
-                                     solution[1],
-                                     deviator,
-                                     stress );
-
-  // construct consistent tangent stiffness
-  // note: if trialQ = 0, we will get a divide by zero error below,
-  // but this is an unphysical (zero-strength) state anyway
-
-  LvArray::tensorOps::fill< 6, 6 >( stiffness, 0 );
-
-  real64 c1 = 2 * m_shearModulus[k] * solution[1] / trialQ;
-  real64 c2 = jacobianInv[0][0] * m_bulkModulus[k] - c1 / 3;
-  real64 c3 = sqrt( 2./3 ) * 3 * m_shearModulus[k] * jacobianInv[0][1];
-  real64 c4 = sqrt( 2./3 ) * m_bulkModulus[k] * jacobianInv[1][0];
-  real64 c5 = 2 * jacobianInv[1][1] * m_shearModulus[k] - c1;
-
-  real64 identity[6];
-
-  for( localIndex i=0; i<3; ++i )
-  {
-    stiffness[i][i] = c1;
-    stiffness[i+3][i+3] = 0.5 * c1;
-    identity[i] = 1.0;
-    identity[i+3] = 0.0;
-  }
-
-  for( localIndex i=0; i<6; ++i )
-  {
-    for( localIndex j=0; j<6; ++j )
-    {
-      stiffness[i][j] +=   c2 * identity[i] * identity[j]
-                         + c3 * identity[i] * deviator[j]
-                         + c4 * deviator[i] * identity[j]
-                         + c5 * deviator[i] * deviator[j];
-    }
-  }
-
-  // save stress, state and return
-  saveStateVariable( k, q, stateVariable );
-  saveStress( k, q, stress );
-  return;
-}
-
-
-GEOSX_HOST_DEVICE
-GEOSX_FORCE_INLINE
-void DruckerPragerExtendedUpdates::smallStrainUpdate( localIndex const k,
-                                                      localIndex const q,
-                                                      real64 const ( &strainIncrement )[6],
-                                                      real64 ( & stress )[6],
-                                                      DiscretizationOps & stiffness ) const
-{
-  smallStrainUpdate( k, q, strainIncrement, stress, stiffness.m_c );
-}
-
-
 
 /**
  * @class DruckerPragerExtended
