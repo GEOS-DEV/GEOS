7d7581e66b7750114527433c34e26c611d700630
wrtobin <tobin6@llnl.gov>
2021-02-11 12:18:17 -0800

    add max high water in addition to global total for umpire memory reporting

--- src/coreComponents/managers/initialization.cpp
+++ src/coreComponents/managers/initialization.cpp
@@ -110,10 +110,14 @@ void addUmpireHighWaterMarks()
     // This is a little redundant since
     std::size_t const mark = rm.getAllocator( allocatorName ).getHighWatermark();
     std::size_t const totalMark = MpiWrapper::sum( mark );
-    GEOSX_LOG_RANK_0( "Umpire " << std::setw( 15 ) << allocatorName << " high water mark: " <<
+    std::size_t const maxMark = MpiWrapper::max( mark );
+    GEOSX_LOG_RANK_0( "Umpire " << std::setw( 15 ) << allocatorName << " total high water mark: " <<
                       std::setw( 9 ) << LvArray::system::calculateSize( totalMark ) );
+    GEOSX_LOG_RANK_0( "Umpire " << std::setw( 15 ) << allocatorName << " max high water mark: " <<
+                      std::setw( 9 ) << LvArray::system::calculateSize( maxMark ) );
 
-    pushStatsIntoAdiak( allocatorName + " high water mark", mark );
+    pushStatsIntoAdiak( allocatorName + " total high water mark", mark );
+    pushStatsIntoAdiak( allocatorName + " max high water mark", mark );
   }
 }
 
