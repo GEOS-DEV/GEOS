f879524f151fd2970589eef23b9699abd811361d
Francois Hamon <fhamon@stanford.edu>
2021-02-19 10:16:17 -0800

    Implement relative permeabilities with tables on device (#1277)
    * completed initial implementation of table relperms on device
    
    * addressed Sergey's comments; add unit test and three-phase interpolation
    
    * add integrated tests
    
    * defined KernelWrapper as nested class of TableFunction
    
    * addressed Thomas' comments
    
    * added check on the axis monotonicity
    
    * updated integratedTests submodule
    
    * updated integratedTests submodule

--- src/coreComponents/constitutive/ConstitutiveBase.cpp
+++ src/coreComponents/constitutive/ConstitutiveBase.cpp
@@ -12,23 +12,6 @@
  * ------------------------------------------------------------------------------------------------------------
  */
 
-/*
- *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Copyright (c) 2018, Lawrence Livermore National Security, LLC.
- *
- * Produced at the Lawrence Livermore National Laboratory
- *
- * LLNL-CODE-746361
- *
- * All rights reserved. See COPYRIGHT for details.
- *
- * This file is part of the GEOSX Simulation Framework.
- *
- * GEOSX is a free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License (as published by the
- * Free Software Foundation) version 2.1 dated February 1999.
- *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
 /**
  * @file ConstitutiveBase.cpp
  */
