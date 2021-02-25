41e89e878ae67f01993ca9ec03a248e1017fe4a5
William R Tobin <4522899+wrtobin@users.noreply.github.com>
2021-02-25 12:24:08 -0800

    Fix Debug-build compile fail on Lassen (#1337)
    * remove host-device lambda calling host-only (serialReduce) functions
    
    * Update src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseHybridFVMKernels.hpp
    
    Co-authored-by: Ben Corbett <32752943+corbett5@users.noreply.github.com>
    
    Co-authored-by: Ben Corbett <32752943+corbett5@users.noreply.github.com>

--- src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseHybridFVMKernels.hpp
+++ src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseHybridFVMKernels.hpp
@@ -860,7 +860,7 @@ struct PrecomputeKernel
       }
     } );
 
-    forAll< serialPolicy >( faceManagerSize, [=] GEOSX_HOST_DEVICE ( localIndex const iface )
+    forAll< serialPolicy >( faceManagerSize, [=] ( localIndex const iface )
     {
       if( !isZero( mimFaceGravCoefDenominator[iface].get() ) )
       {
