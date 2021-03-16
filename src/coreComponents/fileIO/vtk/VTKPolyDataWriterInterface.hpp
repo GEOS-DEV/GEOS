geofiber/GEOSX

WIP mock for VTK

src/coreComponents/fileIO/vtk/VTKPolyDataWriterInterface.hpp
@@ -112,6 +112,11 @@ public:
    */
   void Write( real64 time, integer cycle, DomainPartition const & domain );
 
+  void Write( real64 time,
+              integer cycle,
+              ElementRegionManagerABC const & elemManager,
+              NodeManagerABC const & nodeManager );
+
 private:
   /*!
    * @brief Ask rank \p rank for the number of elements in its ElementRegionBase \p er.
@@ -119,7 +124,7 @@ private:
    * @param[in] rank the rank we want to ask
    * @return the number of elements in the region for the asked rank
    */
-  void gatherNbElementsInRegion( ElementRegionBase const & er,
+  void gatherNbElementsInRegion( ElementRegionBaseABC const & er,
                                  array1d< localIndex > & nbElemsInRegion ) const;
 
   /*!
@@ -146,7 +151,7 @@ private:
    * @param[in] elemManager the ElementRegionManager containing the CellElementRegions to be output
    * @param[in] nodeManager the NodeManager containing the nodes of the domain to be output
    */
-  void WriteCellElementRegions( real64 time, ElementRegionManager const & elemManager, NodeManager const & nodeManager ) const;
+  void WriteCellElementRegions( real64 time, ElementRegionManagerABC const & elemManager, NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Gets the cell connectivities as
@@ -156,14 +161,14 @@ private:
    * containg the type of the cells.
    * the second value is a VTK object containing the connectivity information
    * */
-  std::pair< std::vector< int >, vtkSmartPointer< vtkCellArray > > GetVTKCells( CellElementRegion const & er ) const;
+  std::pair< std::vector< int >, vtkSmartPointer< vtkCellArray > > GetVTKCells( CellElementRegionABC const & er ) const;
 
   /*!
    * @brief Gets the vertices coordinates
    * as a VTK Object for \p nodeManager
    * @param[in] nodeManager the NodeManager associated with the domain being written
    */
-  vtkSmartPointer< vtkPoints > GetVTKPoints( NodeManager const & nodeManager ) const;
+  vtkSmartPointer< vtkPoints > GetVTKPoints( NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Writes the files containing the well representation
@@ -172,7 +177,7 @@ private:
    * @param[in] elemManager the ElementRegionManager containing the WellElementRegions to be output
    * @param[in] nodeManager the NodeManager containing the nodes of the domain to be output
    */
-  void WriteWellElementRegions( real64 time, ElementRegionManager const & elemManager, NodeManager const & nodeManager ) const;
+  void WriteWellElementRegions( real64 time, ElementRegionManagerABC const & elemManager, NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Gets the cell connectivities and the vertices coordinates
@@ -182,7 +187,7 @@ private:
    * @return a pair containing a VTKPoints (with the information on the vertices and their coordinates)
    * and a VTKCellArray (with the cell connectivities).
    */
-  std::pair< vtkSmartPointer< vtkPoints >, vtkSmartPointer< vtkCellArray > >GetWell( WellElementSubRegion const & esr, NodeManager const & nodeManager ) const;
+  std::pair< vtkSmartPointer< vtkPoints >, vtkSmartPointer< vtkCellArray > >GetWell( WellElementSubRegionABC const & esr, NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Gets the cell connectivities and the vertices coordinates
@@ -192,8 +197,8 @@ private:
    * @return a pair containing a VTKPoints (with the information on the vertices and their coordinates)
    * and a VTKCellArray (with the cell connectivities).
    */
-  std::pair< vtkSmartPointer< vtkPoints >, vtkSmartPointer< vtkCellArray > >GetSurface( FaceElementSubRegion const & esr,
-                                                                                        NodeManager const & nodeManager ) const;
+  std::pair< vtkSmartPointer< vtkPoints >, vtkSmartPointer< vtkCellArray > >GetSurface( FaceElementSubRegionABC const & esr,
+                                                                                        NodeManagerANC const & nodeManager ) const;
 
   /*!
    * @brief Gets the cell connectivities and the vertices coordinates
@@ -205,8 +210,8 @@ private:
    * @return a pair containing a VTKPoints (with the information on the vertices and their coordinates)
    * and a VTKCellArray (with the cell connectivities).
    */
-  std::pair< vtkSmartPointer< vtkPoints >, vtkSmartPointer< vtkCellArray > >GetEmbeddedSurface( EmbeddedSurfaceSubRegion const & esr,
-                                                                                                NodeManager const & nodeManager ) const;
+  std::pair< vtkSmartPointer< vtkPoints >, vtkSmartPointer< vtkCellArray > >GetEmbeddedSurface( EmbeddedSurfaceSubRegionABC const & esr,
+                                                                                                NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Writes the files containing the faces elements
@@ -216,8 +221,8 @@ private:
    * @param[in] nodeManager the NodeManager containing the nodes of the domain to be output
    */
   void WriteSurfaceElementRegions( real64 time,
-                                   ElementRegionManager const & elemManager,
-                                   NodeManager const & nodeManager ) const;
+                                   ElementRegionManagerABC const & elemManager,
+                                   NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Writes a VTM file for the time-step \p time.
@@ -226,22 +231,28 @@ private:
    * @param[in] elemManager the ElementRegionManager containing all the regions to be output and refered in the VTM file
    * @param[in] vtmWrite a writer specialized for the VTM file format
    */
-  void WriteVTMFile( real64 time, ElementRegionManager const & elemManager, VTKVTMWriter const & vtmWriter ) const;
+  void WriteVTMFile( real64 time, ElementRegionManagerABC const & elemManager, VTKVTMWriter const & vtmWriter ) const;
 
   /*!
    * @brief Write all the fields associated to the nodes of \p nodeManager if their plotlevel is <= m_plotLevel
    * @param[in] pointdata a VTK object containing all the fields associated with the nodes
    * @param[in] nodeManager the NodeManager associated with the domain being written
    */
-  void WriteNodeFields( vtkSmartPointer< vtkPointData > const pointdata, NodeManager const & nodeManager ) const;
+  void WriteNodeFields( vtkSmartPointer< vtkPointData > const pointdata, NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Writes all the fields associated to the elements of \p er if their plotlevel is <= m_plotLevel
    * @param[in] celldata a VTK object containing all the fields associated with the elements
    * @param[in] er ElementRegion being written
    */
-  template< class SUBREGION >
-  void WriteElementFields( vtkSmartPointer< vtkCellData > const celldata, ElementRegionBase const & er ) const;
+//  template< class SUBREGION >
+//  void WriteElementFields( vtkSmartPointer< vtkCellData > const celldata, ElementRegionBaseABC const & er ) const;
+//
+//  template< class ELEMENT_REGION >
+//  void MyWriteElementFields( vtkSmartPointer< vtkCellData > const celldata, ELEMENT_REGION const & er ) const;
+
+  template< class PREDICATE >
+  void WriteElementFieldsPredicate( vtkSmartPointer< vtkCellData > const celldata, ElementRegionBaseABC const & er, PREDICATE p ) const;
 
   /*!
    * @brief Writes a field from \p wrapperBase

src/coreComponents/fileIO/vtk/unitTests/testVTK.cpp
@@ -1,0 +1,97 @@
+/*	
+ * ------------------------------------------------------------------------------------------------------------	
+ * SPDX-License-Identifier: LGPL-2.1-only	
+ *	
+ * Copyright (c) 2018-2020 Lawrence Livermore National Security LLC	
+ * Copyright (c) 2018-2020 The Board of Trustees of the Leland Stanford Junior University	
+ * Copyright (c) 2018-2020 Total, S.A	
+ * Copyright (c) 2020-     GEOSX Contributors	
+ * All right reserved	
+ *	
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.	
+ * ------------------------------------------------------------------------------------------------------------	
+ */
+
+#include "../VTKPolyDataWriterInterface.hpp"
+
+#include "mesh/ElementRegionManager.hpp"
+#include "mesh/NodeManager.hpp"
+
+#include "managers/initialization.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace geosx;
+
+// TODO implement the ElementRegionManagerABC, NodeManagerABC
+// TODO VTKPolyDataWriterInterface must compile with these...
+class MockElementRegionManager: public ElementRegionManager
+{
+public:
+  MOCK_METHOD(
+    (std::list< std::reference_wrapper< const CellElementRegion > >),
+    getCellElementRegions,
+    (),
+    (const, override)
+  );
+};
+
+class MockNodeManager: public NodeManager
+{
+public:
+
+};
+
+TEST( testVTK, dieIfFileAlreadyExists )
+{
+//  geosx::vtk::VTKPolyDataWriterInterface writer( "/I/cannot/be/created/simply" );
+
+//  auto instanciate = []() {
+//    geosx::vtk::VTKPolyDataWriterInterface writer( "/I/cannot/be/created/simply" );
+//  };
+
+  testing::FLAGS_gtest_death_test_style="fast";
+//  testing::FLAGS_gtest_death_test_style="threadsafe";
+
+  auto instanciate2 = []() {
+//    std::cerr << "/I/cannot/be/created/simply" << std::endl ;
+//    exit(1);
+    geosx::vtk::VTKPolyDataWriterInterface writer( "/I/cannot/be/created/simply" );
+  };
+
+//  const real64 time = 0;
+//  const integer cycle = 0;
+  const MockElementRegionManager elementRegionManager;
+  const MockNodeManager nodeManager;
+  using ::testing::HasSubstr;
+//  writer.Write( time, cycle, elementRegionManager, nodeManager );
+  // I cannot use the msg match since our error logs sends on stdout and not stderr.
+  ASSERT_DEATH( instanciate2(), HasSubstr("/I/cannot/be/created/simply"));
+//  ASSERT_DEATH( writer.Write( time, cycle, elementRegionManager, nodeManager ), "HasSubstr(already)");
+}
+
+//TEST( testVTK, doIt )
+//{
+//  EXPECT_TRUE(true);
+//
+//  geosx::vtk::VTKPolyDataWriterInterface writer("/tmp/test");
+//
+//
+//  const real64 time = 0;
+//  const integer cycle = 0;
+//
+//  const MockElementRegionManager elementRegionManager;
+//  const MockNodeManager nodeManager;
+//
+//  writer.Write(time, cycle, elementRegionManager, nodeManager);
+//}
+
+int main( int ac, char * av[] )
+{
+  ::testing::InitGoogleTest( &ac, av );
+  geosx::basicSetup( ac, av );
+  int const result = RUN_ALL_TESTS();
+  geosx::basicCleanup();
+  return result;
+}

src/coreComponents/mesh/EmbeddedSurfaceSubRegion.hpp
@@ -29,13 +29,19 @@
 namespace geosx
 {
 
+class EmbeddedSurfaceSubRegionABC: public ElementSubRegionBaseABC
+{
+public:
+
+};
+
 /**
  * @class EmbeddedSurfaceSubRegion
  *
  * The EmbeddedSurfaceSubRegion class contains the functionality to support the concept of an embedded
  * surface element. It consists of a 2D surface that cuts a 3D matrix cell.
  */
-class EmbeddedSurfaceSubRegion : public SurfaceElementSubRegion
+class EmbeddedSurfaceSubRegion : public SurfaceElementSubRegion, public EmbeddedSurfaceSubRegionABC
 {
 public:
 

src/coreComponents/mesh/SurfaceElementRegion.hpp
@@ -29,6 +29,20 @@ namespace geosx
 
 class EdgeManager;
 
+class SurfaceElementRegionABC: public ElementRegionBaseABC
+{
+public:
+  enum class SurfaceSubRegionType : integer
+  {
+    faceElement,
+    embeddedElement
+  };
+
+  virtual EmbeddedSurfaceSubRegionABC const * getEmbeddedSurfaceSubRegion() const = 0;
+  virtual FaceElementSubRegionABC const * getFaceElementSubRegion() const = 0;
+  virtual SurfaceSubRegionType subRegionType() const = 0;
+};
+
 /**
  * @class SurfaceElementRegion
  *
@@ -36,7 +50,7 @@ class EdgeManager;
  * element hierarchy. SurfaceElementRegion derives from ElementRegion and has an entry in the ObjectManagerBase
  * catalog.
  */
-class SurfaceElementRegion : public ElementRegionBase
+class SurfaceElementRegion : public ElementRegionBase, public SurfaceElementRegionABC
 {
 public:
 
@@ -45,11 +59,11 @@ public:
    *
    * The options for the surface subregion type
    */
-  enum class SurfaceSubRegionType : integer
-  {
-    faceElement,
-    embeddedElement
-  };
+//  enum class SurfaceSubRegionType : integer
+//  {
+//    faceElement,
+//    embeddedElement
+//  };
 
   /**
    * @name Constructor / Destructor
@@ -135,9 +149,20 @@ public:
    * @brief Get subRegion type.
    * @return subRegion type
    */
-  SurfaceSubRegionType subRegionType() const { return m_subRegionType; }
+  SurfaceSubRegionType subRegionType() const override
+  {
+    return m_subRegionType;
+  }
 
+  EmbeddedSurfaceSubRegion const * getEmbeddedSurfaceSubRegion() const override
+  {
+    return this->GetSubRegion( 0 )->group_cast< EmbeddedSurfaceSubRegion const * >();
+  }
 
+  FaceElementSubRegion const * getFaceElementSubRegion() const override
+  {
+    return this->GetSubRegion( 0 )->group_cast< FaceElementSubRegion const * >();
+  }
   ///@}
 
   /**

src/coreComponents/mesh/WellElementRegion.hpp
@@ -28,13 +28,20 @@ namespace geosx
 
 class MeshLevel;
 
+class WellElementRegionABC: public ElementRegionBaseABC
+{
+public:
+  virtual WellElementSubRegion const * getWellElementSubRegion() const = 0;
+  virtual std::list< std::reference_wrapper< const WellElementSubRegionABC > > getElementSubRegionsSpec() const = 0;
+};
+
 /**
  * @class WellElementRegion
  * @brief This class specializes the element region for the case
  *        of a well. This class is also in charge of starting the
  *        construction of the well data structure in GenerateWell.
  */
-class WellElementRegion : public ElementRegionBase
+class WellElementRegion : public ElementRegionBase, public WellElementRegionABC
 {
 public:
 
@@ -110,6 +117,12 @@ public:
    * @return the name of the subRegion object
    */
   string const & GetSubRegionName() const { return m_subRegionName; }
+
+
+  virtual WellElementSubRegion const * getWellElementSubRegion() const override
+  {
+    return GetSubRegion( 0 )->group_cast< WellElementSubRegion const * >();
+  }
 
   ///@}
 

GEOS-DEV/GEOS

src/coreComponents/fileIO/vtk/VTKPolyDataWriterInterface.hpp
@@ -124,8 +124,12 @@ public:
    */
   void write( real64 time, integer cycle, DomainPartition const & domain );
 
-private:
+  void write( real64 time,
+              integer cycle,
+              ElementRegionManagerABC const & elemManager,
+              NodeManagerABC const & nodeManager );
 
+private:
   /*!
    * @brief Given a time-step \p time, returns the relative path
    * to the subfolder containing the files concerning this time-step
@@ -142,8 +146,8 @@ private:
    * @param[in] nodeManager the NodeManager containing the nodes of the domain to be output
    */
   void writeCellElementRegions( real64 time,
-                                ElementRegionManager const & elemManager,
-                                NodeManager const & nodeManager ) const;
+                                ElementRegionManagerABC const & elemManager,
+                                NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Gets the cell connectivities as
@@ -154,14 +158,14 @@ private:
    * the second value is a VTK object containing the connectivity information
    * */
   std::pair< std::vector< int >, vtkSmartPointer< vtkCellArray > >
-  getVtkCells( CellElementRegion const & er ) const;
+  getVtkCells( CellElementRegionABC const & er ) const;
 
   /*!
    * @brief Gets the vertices coordinates
    * as a VTK Object for \p nodeManager
    * @param[in] nodeManager the NodeManager associated with the domain being written
    */
-  vtkSmartPointer< vtkPoints > getVtkPoints( NodeManager const & nodeManager ) const;
+  vtkSmartPointer< vtkPoints > getVtkPoints( NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Writes the files containing the well representation
@@ -170,8 +174,8 @@ private:
    * @param[in] elemManager the ElementRegionManager containing the WellElementRegions to be output
    * @param[in] nodeManager the NodeManager containing the nodes of the domain to be output
    */
-  void writeWellElementRegions( real64 time, ElementRegionManager const & elemManager,
-                                NodeManager const & nodeManager ) const;
+  void writeWellElementRegions( real64 time, ElementRegionManagerABC const & elemManager,
+                                NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Gets the cell connectivities and the vertices coordinates
@@ -181,9 +185,8 @@ private:
    * @return a pair containing a VTKPoints (with the information on the vertices and their coordinates)
    * and a VTKCellArray (with the cell connectivities).
    */
-  std::pair< vtkSmartPointer< vtkPoints >, vtkSmartPointer< vtkCellArray > >
-  getWell( WellElementSubRegion const & esr,
-           NodeManager const & nodeManager ) const;
+  std::pair< vtkSmartPointer< vtkPoints >, vtkSmartPointer< vtkCellArray > > getWell( WellElementSubRegionABC const & esr,
+           NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Gets the cell connectivities and the vertices coordinates
@@ -193,9 +196,8 @@ private:
    * @return a pair containing a VTKPoints (with the information on the vertices and their coordinates)
    * and a VTKCellArray (with the cell connectivities).
    */
-  std::pair< vtkSmartPointer< vtkPoints >, vtkSmartPointer< vtkCellArray > >
-  getSurface( FaceElementSubRegion const & esr,
-              NodeManager const & nodeManager ) const;
+  std::pair< vtkSmartPointer< vtkPoints >, vtkSmartPointer< vtkCellArray > > getSurface( FaceElementSubRegionABC const & esr,
+                                                                                         NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Gets the cell connectivities and the vertices coordinates
@@ -208,8 +210,8 @@ private:
    * and a VTKCellArray (with the cell connectivities).
    */
   std::pair< vtkSmartPointer< vtkPoints >, vtkSmartPointer< vtkCellArray > >
-  getEmbeddedSurface( EmbeddedSurfaceSubRegion const & esr,
-                      NodeManager const & nodeManager ) const;
+  getEmbeddedSurface( EmbeddedSurfaceSubRegionABC const & esr,
+                      NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Writes the files containing the faces elements
@@ -219,8 +221,8 @@ private:
    * @param[in] nodeManager the NodeManager containing the nodes of the domain to be output
    */
   void writeSurfaceElementRegions( real64 time,
-                                   ElementRegionManager const & elemManager,
-                                   NodeManager const & nodeManager ) const;
+                                   ElementRegionManagerABC const & elemManager,
+                                   NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Writes a VTM file for the time-step \p time.
@@ -230,7 +232,7 @@ private:
    * @param[in] vtmWrite a writer specialized for the VTM file format
    */
   void writeVtmFile( real64 time,
-                     ElementRegionManager const & elemManager,
+                     ElementRegionManagerABC const & elemManager,
                      VTKVTMWriter const & vtmWriter ) const;
 
   /*!
@@ -239,16 +241,22 @@ private:
    * @param[in] nodeManager the NodeManager associated with the domain being written
    */
   void writeNodeFields( vtkSmartPointer< vtkPointData > pointdata,
-                        NodeManager const & nodeManager ) const;
+                        NodeManagerABC const & nodeManager ) const;
 
   /*!
    * @brief Writes all the fields associated to the elements of \p er if their plotlevel is <= m_plotLevel
    * @param[in] celldata a VTK object containing all the fields associated with the elements
    * @param[in] er ElementRegion being written
    */
-  template< class SUBREGION >
-  void writeElementFields( vtkSmartPointer< vtkCellData > celldata,
-                           ElementRegionBase const & er ) const;
+//  template< class SUBREGION >
+//  void writeElementFields( vtkSmartPointer< vtkCellData > celldata,
+                           ElementRegionBaseABC const & er ) const;
+//
+//  template< class ELEMENT_REGION >
+//  void MyWriteElementFields( vtkSmartPointer< vtkCellData > const celldata, ELEMENT_REGION const & er ) const;
+
+  template< class PREDICATE >
+  void writeElementFieldsPredicate( vtkSmartPointer< vtkCellData > const celldata, ElementRegionBaseABC const & er, PREDICATE p ) const;
 
   /*!
    * @brief Writes a field from \p wrapperBase
@@ -260,10 +268,11 @@ private:
    * @param[in,out] a counter that is incremented each time a value is written. This is useful
    * for CellElementSubRegion.
    */
-  void writeField( dataRepository::WrapperBase const & wrapperBase,
+//  void writeField( dataRepository::WrapperBase const & wrapperBase,
                    vtkSmartPointer< VTKGEOSXData > data,
                    localIndex size,
                    localIndex & count ) const;
+  void writeField( FieldABC const & field, vtkSmartPointer< VTKGEOSXData > data, localIndex size, localIndex & count ) const;
 
   /*!
    * @brief Writes an unstructured grid

src/coreComponents/fileIO/vtk/unitTests/testVTK.cpp
@@ -1,0 +1,97 @@
+/*	
+ * ------------------------------------------------------------------------------------------------------------	
+ * SPDX-License-Identifier: LGPL-2.1-only	
+ *	
+ * Copyright (c) 2018-2020 Lawrence Livermore National Security LLC	
+ * Copyright (c) 2018-2020 The Board of Trustees of the Leland Stanford Junior University	
+ * Copyright (c) 2018-2020 Total, S.A	
+ * Copyright (c) 2020-     GEOSX Contributors	
+ * All right reserved	
+ *	
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.	
+ * ------------------------------------------------------------------------------------------------------------	
+ */
+
+#include "../VTKPolyDataWriterInterface.hpp"
+
+#include "mesh/ElementRegionManager.hpp"
+#include "mesh/NodeManager.hpp"
+
+#include "managers/initialization.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace geosx;
+
+// TODO implement the ElementRegionManagerABC, NodeManagerABC
+// TODO VTKPolyDataWriterInterface must compile with these...
+class MockElementRegionManager: public ElementRegionManager
+{
+public:
+  MOCK_METHOD(
+    (std::list< std::reference_wrapper< const CellElementRegionABC > >),
+    getCellElementRegions,
+    (),
+    (const, override)
+  );
+};
+
+class MockNodeManager: public NodeManager
+{
+public:
+
+};
+
+TEST( testVTK, dieIfFileAlreadyExists )
+{
+//  geosx::vtk::VTKPolyDataWriterInterface writer( "/I/cannot/be/created/simply" );
+
+//  auto instanciate = []() {
+//    geosx::vtk::VTKPolyDataWriterInterface writer( "/I/cannot/be/created/simply" );
+//  };
+
+  testing::FLAGS_gtest_death_test_style="fast";
+//  testing::FLAGS_gtest_death_test_style="threadsafe";
+
+  auto instanciate2 = []() {
+//    std::cerr << "/I/cannot/be/created/simply" << std::endl ;
+//    exit(1);
+    geosx::vtk::VTKPolyDataWriterInterface writer( "/I/cannot/be/created/simply" );
+  };
+
+//  const real64 time = 0;
+//  const integer cycle = 0;
+  const MockElementRegionManager elementRegionManager;
+  const MockNodeManager nodeManager;
+  using ::testing::HasSubstr;
+//  writer.Write( time, cycle, elementRegionManager, nodeManager );
+  // I cannot use the msg match since our error logs sends on stdout and not stderr.
+  ASSERT_DEATH( instanciate2(), HasSubstr("/I/cannot/be/created/simply"));
+//  ASSERT_DEATH( writer.Write( time, cycle, elementRegionManager, nodeManager ), "HasSubstr(already)");
+}
+
+//TEST( testVTK, doIt )
+//{
+//  EXPECT_TRUE(true);
+//
+//  geosx::vtk::VTKPolyDataWriterInterface writer("/tmp/test");
+//
+//
+//  const real64 time = 0;
+//  const integer cycle = 0;
+//
+//  const MockElementRegionManager elementRegionManager;
+//  const MockNodeManager nodeManager;
+//
+//  writer.Write(time, cycle, elementRegionManager, nodeManager);
+//}
+
+int main( int ac, char * av[] )
+{
+  ::testing::InitGoogleTest( &ac, av );
+  geosx::basicSetup( ac, av );
+  int const result = RUN_ALL_TESTS();
+  geosx::basicCleanup();
+  return result;
+}

src/coreComponents/mesh/EmbeddedSurfaceSubRegion.hpp
@@ -29,13 +29,19 @@
 namespace geosx
 {
 
+class EmbeddedSurfaceSubRegionABC: public SurfaceElementSubRegionABC
+{
+public:
+
+};
+
 /**
  * @class EmbeddedSurfaceSubRegion
  *
  * The EmbeddedSurfaceSubRegion class contains the functionality to support the concept of an embedded
  * surface element. It consists of a 2D surface that cuts a 3D matrix cell.
  */
-class EmbeddedSurfaceSubRegion : public SurfaceElementSubRegion
+class EmbeddedSurfaceSubRegion : public SurfaceElementSubRegion, public EmbeddedSurfaceSubRegionABC
 {
 public:
 

src/coreComponents/mesh/SurfaceElementRegion.hpp
@@ -29,6 +29,20 @@ namespace geosx
 
 class EdgeManager;
 
+class SurfaceElementRegionABC: public ElementRegionBaseABC
+{
+public:
+  enum class SurfaceSubRegionType : integer
+  {
+    faceElement,
+    embeddedElement
+  };
+
+  virtual EmbeddedSurfaceSubRegionABC const * getEmbeddedSurfaceSubRegion() const = 0;
+  virtual FaceElementSubRegionABC const * getFaceElementSubRegion() const = 0;
+  virtual SurfaceSubRegionType subRegionType() const = 0;
+};
+
 /**
  * @class SurfaceElementRegion
  *
@@ -36,7 +50,7 @@ class EdgeManager;
  * element hierarchy. SurfaceElementRegion derives from ElementRegion and has an entry in the ObjectManagerBase
  * catalog.
  */
-class SurfaceElementRegion : public ElementRegionBase
+class SurfaceElementRegion : public ElementRegionBase, public SurfaceElementRegionABC
 {
 public:
 
@@ -45,11 +59,11 @@ public:
    *
    * The options for the surface subregion type
    */
-  enum class SurfaceSubRegionType : integer
-  {
-    faceElement,
-    embeddedElement
-  };
+//  enum class SurfaceSubRegionType : integer
+//  {
+//    faceElement,
+//    embeddedElement
+//  };
 
   /**
    * @name Constructor / Destructor
@@ -135,9 +149,20 @@ public:
    * @brief Get subRegion type.
    * @return subRegion type
    */
-  SurfaceSubRegionType subRegionType() const { return m_subRegionType; }
+  SurfaceSubRegionType subRegionType() const override
+  {
+    return m_subRegionType;
+  }
 
+  EmbeddedSurfaceSubRegion const * getEmbeddedSurfaceSubRegion() const override
+  {
+    return this->getSubRegion( 0 )->groupCast< EmbeddedSurfaceSubRegion const * >();
+  }
 
+  FaceElementSubRegion const * getFaceElementSubRegion() const override
+  {
+    return this->getSubRegion( 0 )->groupCast< FaceElementSubRegion const * >();
+  }
   ///@}
 
   /**

src/coreComponents/mesh/WellElementRegion.hpp
@@ -28,13 +28,20 @@ namespace geosx
 
 class MeshLevel;
 
+class WellElementRegionABC: public ElementRegionBaseABC
+{
+public:
+  virtual WellElementSubRegion const * getWellElementSubRegion() const = 0;
+  virtual std::list< std::reference_wrapper< const WellElementSubRegionABC > > getElementSubRegionsSpec() const = 0;
+};
+
 /**
  * @class WellElementRegion
  * @brief This class specializes the element region for the case
  *        of a well. This class is also in charge of starting the
  *        construction of the well data structure in GenerateWell.
  */
-class WellElementRegion : public ElementRegionBase
+class WellElementRegion : public ElementRegionBase, public WellElementRegionABC
 {
 public:
 
@@ -110,6 +117,12 @@ public:
    * @return the name of the subRegion object
    */
   string const & getSubRegionName() const { return m_subRegionName; }
+
+
+  virtual WellElementSubRegion const * getWellElementSubRegion() const override
+  {
+    return getSubRegion( 0 )->groupCast< WellElementSubRegion const * >();
+  }
 
   ///@}
 