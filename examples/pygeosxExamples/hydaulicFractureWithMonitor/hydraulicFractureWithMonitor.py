b41ef52c80cecdbdd6e1e5c7e7da6adf7e2a5795
Christopher Sherman <sherman27@llnl.gov>
2021-03-18 14:27:35 -0700

    Fixing formatting for new pygeosx tools examples, docstrings

--- examples/pygeosxExamples/hydaulicFractureWithMonitor/hydraulicFractureWithMonitor.py
+++ examples/pygeosxExamples/hydaulicFractureWithMonitor/hydraulicFractureWithMonitor.py
@@ -12,46 +12,46 @@ rank = comm.Get_rank()
 
 
 def run_problem():
-  """
-  @brief Run the GEOSX problem
-  """
-  # Initialize the code
-  xml.apply_xml_preprocessor()
-  problem = pygeosx.initialize(rank, sys.argv)
+    """
+    Run the GEOSX problem
+    """
+    # Initialize the code
+    xml.apply_xml_preprocessor()
+    problem = pygeosx.initialize(rank, sys.argv)
 
-  # Apply initial conditions
-  pygeosx.apply_initial_conditions()
+    # Apply initial conditions
+    pygeosx.apply_initial_conditions()
 
-  # Rather than specifying the wrapper paths explicitly,
-  # search for them using a set of filters
-  fracture_location_key = wrapper.get_matching_wrapper_path(problem, ['Fracture', 'elementCenter'])
-  fracture_aperture_key = wrapper.get_matching_wrapper_path(problem, ['Fracture', 'effectiveAperture'])
+    # Rather than specifying the wrapper paths explicitly,
+    # search for them using a set of filters
+    fracture_location_key = wrapper.get_matching_wrapper_path(problem, ['Fracture', 'elementCenter'])
+    fracture_aperture_key = wrapper.get_matching_wrapper_path(problem, ['Fracture', 'effectiveAperture'])
 
-  # Note: we will be plotting our results.
-  # This will modify the fonts so that they a bit easier to read
-  plot_font = {'weight': 'normal', 'size': 8}
-  plt.rc('font', **plot_font)
+    # Note: we will be plotting our results.
+    # This will modify the fonts so that they a bit easier to read
+    plot_font = {'weight': 'normal', 'size': 8}
+    plt.rc('font', **plot_font)
 
-  # Setup values to record
-  records = {fracture_location_key: {'label': 'Fracture Extents (m)',
-                                     'scale': 1.0,
-                                     'history': [],
-                                     'fhandle': plt.figure()},
-             fracture_aperture_key: {'label': 'Aperture (mm)',
-                                     'scale': 1e3,
-                                     'history': [],
-                                     'fhandle': plt.figure()},
-             'time': {'label': 'Time (min)',
-                      'scale': 1.0 / 60.0,
-                      'history': []}}
+    # Setup values to record
+    records = {fracture_location_key: {'label': 'Fracture Extents (m)',
+                                       'scale': 1.0,
+                                       'history': [],
+                                       'fhandle': plt.figure()},
+               fracture_aperture_key: {'label': 'Aperture (mm)',
+                                       'scale': 1e3,
+                                       'history': [],
+                                       'fhandle': plt.figure()},
+               'time': {'label': 'Time (min)',
+                        'scale': 1.0 / 60.0,
+                        'history': []}}
 
-  # Run the code
-  while pygeosx.run() != pygeosx.COMPLETED:
-    wrapper.run_queries(problem, records)
-    wrapper.plot_history(records)
+    # Run the code
+    while pygeosx.run() != pygeosx.COMPLETED:
+        wrapper.run_queries(problem, records)
+        wrapper.plot_history(records)
 
 
 if __name__ == '__main__':
-  run_problem()
+    run_problem()
 
 
--- src/coreComponents/python/modules/pygeosx_tools_package/pygeosx_tools/well_log.py
+++ src/coreComponents/python/modules/pygeosx_tools_package/pygeosx_tools/well_log.py
@@ -4,11 +4,15 @@ import numpy as np
 
 def parse_las(fname, variable_start='~CURVE INFORMATION', body_start='~A'):
     """
-    @brief parse an las format log file
-    @param fname the path to the log file
-    @param variable_start a string that indicates the start of variable header information (default = '~CURVE INFORMATION')
-    @param variable_start a string that indicates the start of the log body (default = '~A')
-    @return a dict containing the values and unit definitions for each variable in the log
+    Parse an las format log file
+
+    Args:
+        fname (str): Path to the log file
+        variable_start (str): A string that indicates the start of variable header information (default = '~CURVE INFORMATION')
+        body_start (str): a string that indicates the start of the log body (default = '~A')
+
+    Returns:
+        np.ndarray: a dict containing the values and unit definitions for each variable in the log
     """
     variable_names = []
     variable_units = []
@@ -49,12 +53,14 @@ def parse_las(fname, variable_start='~CURVE INFORMATION', body_start='~A'):
 
 def convert_E_nu_to_K_G(E, nu):
     """
-    @brief convert young's modulus and poisson's ratio to bulk and shear modulus
-                 the input/output parameters can be either scalars or arrays
-    @param E the young's modulus
-    @param nu the poisson's ratio
-    @return K the bulk modulus
-    @return G the shear modulus
+    Convert young's modulus and poisson's ratio to bulk and shear modulus
+
+    Args:
+        E (float, np.ndarray): Young's modulus
+        nu (float, np.ndarray): Poisson's ratio
+
+    Returns:
+        tuple: bulk modulus, shear modulus with same size as inputs
     """
     K = E / (3.0 * (1 - 2.0 * nu))
     G = E / (2.0 * (1 + nu))
@@ -63,12 +69,15 @@ def convert_E_nu_to_K_G(E, nu):
 
 def estimate_shmin(z, rho, nu):
     """
-    @brief estimate the minimum horizontal stress using the poisson's ratio
-                 the input/output parameters can be either scalars or arrays
-    @param z the depth
-    @param rho the density
-    @return nu the poisson's ratio
-    @return sigma_h the minimum horizontal stress
+    Estimate the minimum horizontal stress using the poisson's ratio
+
+    Args:
+        z (float, np.ndarray): Depth
+        rho (float, np.ndarray): Density
+        nu (float, np.ndarray): Poisson's ratio
+
+    Returns:
+        float: minimum horizontal stress
     """
     k = nu / (1.0 - nu)
     sigma_h = k * rho * 9.81 * z
