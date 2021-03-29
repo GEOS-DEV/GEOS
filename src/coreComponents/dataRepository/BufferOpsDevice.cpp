1b90f6833e3c10a6b2ba29d5357a0b95b20ee83c
wrtobin <tobin6@llnl.gov>
2021-03-29 13:43:47 -0700

    remove memcpy usage and ensure (un) packing uses the correct memory alignment for assignment packing

--- src/coreComponents/dataRepository/BufferOpsDevice.cpp
+++ src/coreComponents/dataRepository/BufferOpsDevice.cpp
@@ -138,32 +138,43 @@ UnpackDevice( buffer_unit_type const * & buffer,
 }
 
 template< bool DO_PACKING, typename T, int NDIM, int USD, typename T_INDICES >
-typename std::enable_if< can_memcpy< T >, localIndex >::type
+typename std::enable_if< ( can_memcpy< T > ), localIndex >::type
 PackDataByIndexDevice ( buffer_unit_type * & buffer,
                         ArrayView< T const, NDIM, USD > const & var,
                         const T_INDICES & indices,
                         parallelDeviceEvents & events )
 {
   localIndex const numIndices = indices.size();
-  localIndex const unitSize = var.size() / var.size( 0 ) * sizeof( T );
-
-  buffer_unit_type * const devBuffer = buffer;
+  localIndex const sliceSize = var.size() / var.size( 0 );
+  size_t typeSize = sizeof( T );
+  
+  localIndex packedSize = numIndices * sliceSize * typeSize;
+  packedSize += typeSize - 1; // provide enough padding when calculating size to be able to do any offset to get correct alignment when packing
+  
   if( DO_PACKING )
   {
-    parallelDeviceStream stream;
-    events.emplace_back( forAll< parallelDeviceAsyncPolicy<> >( stream, numIndices, [=] GEOSX_DEVICE ( localIndex const ii )
+    T * devBuffer = reinterpret_cast< T * >( &buffer[0] );
+    uintptr_t address = reinterpret_cast< uintptr_t >( devBuffer );
+    uintptr_t misalignment = address % typeSize;
+    if ( misalignment != 0 )
     {
-      buffer_unit_type * threadBuffer = devBuffer + ii * unitSize;
-      LvArray::forValuesInSlice( var[ indices[ ii ] ], [&threadBuffer] GEOSX_DEVICE ( T const & value )
+      devBuffer = reinterpret_cast< T * >( &buffer[0] + typeSize - misalignment );
+    }
+
+    parallelDeviceStream stream;
+    events.emplace_back( forAll< parallelDevicePolicy< > >( stream, numIndices, [=] GEOSX_DEVICE ( localIndex const ii )
+    {
+      T * threadBuffer = &devBuffer[ ii * sliceSize ];
+      LvArray::forValuesInSlice( var[ indices[ ii ] ], [&] GEOSX_DEVICE ( T const & value )
       {
-        memcpy( threadBuffer, &value, sizeof( T ) );
-        threadBuffer += sizeof( T );
+        *threadBuffer = value;
+        threadBuffer++;
       } );
     } ) );
 
-    buffer += numIndices * unitSize;
+    buffer += packedSize;
   }
-  localIndex packedSize = numIndices * unitSize;
+
   return packedSize;
 }
 
@@ -187,22 +198,32 @@ UnpackDataByIndexDevice ( buffer_unit_type const * & buffer,
                           parallelDeviceEvents & events )
 {
   localIndex numIndices = indices.size();
-  buffer_unit_type const * devBuffer = buffer;
-  localIndex unitSize = var.size() / var.size( 0 ) * sizeof(T);
-  parallelDeviceStream stream;
-  events.emplace_back( forAll< parallelDeviceAsyncPolicy<> >( stream, numIndices, [=] GEOSX_DEVICE ( localIndex const i )
+  localIndex sliceSize = var.size() / var.size( 0 );
+  size_t typeSize = sizeof( T );
+
+  localIndex unpackSize = numIndices * sliceSize * typeSize;
+  unpackSize += typeSize - 1; // return the correct (padded) size to the calling function
+
+  T const * devBuffer = reinterpret_cast< T const * >( &buffer[0] );
+  uintptr_t address = reinterpret_cast< uintptr_t >( devBuffer );
+  uintptr_t misalignment = address % typeSize;
+  if ( misalignment != 0 )
   {
-    buffer_unit_type const * threadBuffer = devBuffer + i * unitSize;
-    LvArray::forValuesInSlice( var[ indices[ i ] ], [&threadBuffer] GEOSX_DEVICE ( T & value )
+    devBuffer = reinterpret_cast< T const * >( &buffer[0] + typeSize - misalignment );
+  }
+  parallelDeviceStream stream;
+  events.emplace_back( forAll< parallelDeviceAsyncPolicy<> >( stream, numIndices, [=] GEOSX_DEVICE ( localIndex const ii )
+  {
+    T const * threadBuffer = &devBuffer[ ii * sliceSize ];
+    LvArray::forValuesInSlice( var[ indices[ ii ] ], [&threadBuffer] GEOSX_DEVICE ( T & value )
     {
-      memcpy( &value, threadBuffer, sizeof( T ) );
-      threadBuffer += sizeof( T );
+      value = *threadBuffer;
+      threadBuffer++;
     } );
   } ) );
-  localIndex avSize = numIndices * unitSize;
-  localIndex sizeOfPackedChars = avSize;
-  buffer += avSize;
-  return sizeOfPackedChars;
+  
+  buffer += unpackSize;
+  return unpackSize;
 }
 
 template< typename T, int NDIM, int USD, typename T_INDICES >
