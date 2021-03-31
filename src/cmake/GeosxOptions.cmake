010b230ef07e5935bb5f5361ba2e4a9750b72271
Randolph R. Settgast <settgast1@llnl.gov>
2021-03-31 16:35:33 -0700

    reenable PVT and PAMELA

--- src/cmake/GeosxOptions.cmake
+++ src/cmake/GeosxOptions.cmake
@@ -21,8 +21,8 @@ option( RAJA_ENABLE_CUDA "" OFF )
 option( RAJA_ENABLE_TESTS "" OFF )
 
 option( ENABLE_GEOSX_PTP "" ON)
-option( ENABLE_PAMELA "" OFF )
-option( ENABLE_PVTPackage "" OFF )
+option( ENABLE_PAMELA "" ON )
+option( ENABLE_PVTPackage "" ON )
 
 option( ENABLE_UNCRUSTIFY "" ON )
 
