39c41168fbe6bc3657c87438cac32c505efcf465
jafranc <jfranc@stanford.edu>
2021-04-05 00:20:23 -0700

    Restoring mistakenly staged

--- src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseBaseKernels.hpp
+++ src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseBaseKernels.hpp
@@ -23,7 +23,6 @@
 #include "mesh/ElementRegionManager.hpp"
 #include "rajaInterface/GEOS_RAJA_Interface.hpp"
 
-
 namespace geosx
 {
 
@@ -135,7 +134,7 @@ struct FluidUpdateKernel
           real64 const temp,
           arrayView2d< real64 const > const & compFrac )
   {
-    forAll< POLICY >( size, [=] ( localIndex const k )
+    forAll< POLICY >( size, [=] GEOSX_HOST_DEVICE ( localIndex const k )
     {
       for( localIndex q = 0; q < fluidWrapper.numGauss(); ++q )
       {
@@ -153,7 +152,7 @@ struct FluidUpdateKernel
           real64 const temp,
           arrayView2d< real64 const > const & compFrac )
   {
-    forAll< POLICY >( size, [=] ( localIndex const k )
+    forAll< POLICY >( size, [=] GEOSX_HOST_DEVICE ( localIndex const k )
     {
       for( localIndex q = 0; q < fluidWrapper.numGauss(); ++q )
       {
@@ -171,7 +170,7 @@ struct FluidUpdateKernel
           real64 const temp,
           arrayView2d< real64 const > const & compFrac )
   {
-    forAll< POLICY >( targetSet.size(), [=] ( localIndex const a )
+    forAll< POLICY >( targetSet.size(), [=] GEOSX_HOST_DEVICE ( localIndex const a )
     {
       localIndex const k = targetSet[a];
       for( localIndex q = 0; q < fluidWrapper.numGauss(); ++q )
@@ -189,7 +188,7 @@ struct FluidUpdateKernel
           real64 const temp,
           arrayView2d< real64 const > const & compFrac )
   {
-    forAll< POLICY >( targetSet.size(), [=] ( localIndex const a )
+    forAll< POLICY >( targetSet.size(), [=] GEOSX_HOST_DEVICE ( localIndex const a )
     {
       localIndex const k = targetSet[a];
       for( localIndex q = 0; q < fluidWrapper.numGauss(); ++q )
