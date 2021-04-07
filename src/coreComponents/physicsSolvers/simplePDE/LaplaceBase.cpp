c3b68acf3163bd9a986298e066a400de536ade79
Andrea Borio <andrea.borio@polito.it>
2021-04-07 18:06:04 +0200

    Add empty LaplaceBase class

---
+++ src/coreComponents/physicsSolvers/simplePDE/LaplaceBase.cpp
@@ -0,0 +1,1 @@
+#include "LaplaceBase.hpp"
---
+++ src/coreComponents/physicsSolvers/simplePDE/LaplaceBase.hpp
@@ -0,0 +1,26 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ * Copyright (c) 2018-2020 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2020 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2018-2020 Total, S.A
+ * Copyright (c) 2019-     GEOSX Contributors
+ * All rights reserved
+ *
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
+#ifndef GEOSX_PHYSICESSOLVERS_SIMPLEPDE_LAPLACE_BASE_HPP
+#define GEOSX_PHYSICESSOLVERS_SIMPLEPDE_LAPLACE_BASE_HPP
+
+#include "physicsSolvers/SolverBase.hpp"  // an abstraction class shared by all physics solvers
+
+namespace geosx
+{
+class LaplaceBase : public SolverBase
+{};
+}
+
+#endif
