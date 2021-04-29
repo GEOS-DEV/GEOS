3a487c204eb82e9eb23a73a2382ed8c27a293976
Andrea Borio <58048459+andrea-borio@users.noreply.github.com>
2021-04-29 08:00:52 +0200

    Add LaplaceBase class (#1385)
    * Add empty LaplaceBase class
    
    * Prepare LaplaceBase for methods in child classes.
    
    Implement constructor and use it in LaplaceFEM and LaplaceVEM.
    Update catalog.
    
    * Implement LaplaceVEM::setupSystem().
    
    Moved code from LaplaceVEM:implicitStepSetup(). This is done for symmetry with LaplaceFEM.
    
    * Moved method registerDataOnMesh() to LaplaceBase.
    
    * Copy all methods common to Laplace{VEM,FEM} to LaplaceBase.
    
    Copied code has been commented out and will be removed in a following commit.
    Sphinx documentation to be checked and fixed.
    
    * Remove unnecessary #include in Laplace{Base,VEM,FEM}
    
    * Remove ExplicitTransient option from LaplaceBase.
    
    * Update Sphinx documentation and remove commented code.c
    
    * Uncrustify.
    
    * Fix documentation of LaplaceBase.hpp
    
    * Use more descriptive names for Sphinx tags.
    
    * Fix typos and improve documenting comments.
    
    * Cleanup
    
    * Renamed LaplaceBase to LaplaceBaseH1
    
    * Update comment to LaplaceBaseH1::implicitStepSetup
    
    * Using static methods in LaplaceBaseH1::viewKeyStruct.
    
    Co-authored-by: Randolph R. Settgast <settgast1@llnl.gov>

--- src/coreComponents/physicsSolvers/simplePDE/LaplaceFEM.cpp
+++ src/coreComponents/physicsSolvers/simplePDE/LaplaceFEM.cpp
@@ -20,13 +20,6 @@
 #include "LaplaceFEM.hpp"
 #include "LaplaceFEMKernels.hpp"
 
-#include "mesh/mpiCommunications/CommunicationTools.hpp"
-#include "common/TimingMacros.hpp"
-#include "common/DataTypes.hpp"
-#include "finiteElement/FiniteElementDiscretizationManager.hpp"
-#include "mesh/DomainPartition.hpp"
-#include "discretizationMethods/NumericalMethodsManager.hpp"
-
 namespace geosx
 {
 
@@ -80,123 +73,21 @@ using namespace dataRepository;
    The description that is set is used in auto-generated documentation and console error messages.
  */
 
-//START_SPHINX_INCLUDE_01
+//START_SPHINX_INCLUDE_CONSTRUCTOR
 LaplaceFEM::LaplaceFEM( const string & name,
                         Group * const parent ):
-  SolverBase( name, parent ),
-  m_fieldName( "primaryField" ),
-  m_timeIntegrationOption( TimeIntegrationOption::ImplicitTransient )
-{
-  registerWrapper( laplaceFEMViewKeys.timeIntegrationOption.key(), &m_timeIntegrationOption ).
-    setInputFlag( InputFlags::REQUIRED ).
-    setDescription( "Time integration method. Options are:\n* " + EnumStrings< TimeIntegrationOption >::concat( "\n* " ) );
+  LaplaceBaseH1( name, parent )
+{}
+//END_SPHINX_INCLUDE_CONSTRUCTOR
 
-  registerWrapper( laplaceFEMViewKeys.fieldVarName.key(), &m_fieldName ).
-    setInputFlag( InputFlags::REQUIRED ).
-    setDescription( "Name of field variable" );
-}
-//END_SPHINX_INCLUDE_01
-
-
-// Destructor
 LaplaceFEM::~LaplaceFEM()
 {
   // TODO Auto-generated destructor stub
 }
 
-/* REGISTER THE PDE SOLUTION DATA ON THE MESH
-   In the LaplaceFEM solver, we compute the solution of the partial differential equation "numerically".
-   This means that we are not solving this PDE everywhere,
-   we are computing the solution at specific locations in space.
-   To do that, we have to register the Laplace solver so that it works on nodes of a mesh.
-   This registration process is done here, in three steps:
-   1 - for each mesh body (if the mesh is split), we collect he "nodes" (nodes carry their location information),
-   2 - On nodes, we register a new property called m_fieldName and give it a type (here, the type is an array of real64)
-   3 - We set some information for this property on the nodes: what is their "PlotLevel"? how can they be described?
-     The PlotLevel is a flag that instructs GEOSX to export values of this property for instance so that they can be plotted.
-     All properties mounted on nodes carry a certain PlotLevel value. This way, every time GEOSX triggers an
-     output event (a request to "print out" data), all properties at or above a certain PlotLevel are automatically exported.
-     The description here is simply an additional metadata for the newly mounted property.
- */
-//START_SPHINX_INCLUDE_02
-void LaplaceFEM::registerDataOnMesh( Group & meshBodies )
-{
-  meshBodies.forSubGroups< MeshBody >( [&] ( MeshBody & meshBody )
-  {
-    NodeManager & nodes = meshBody.getMeshLevel( 0 ).getNodeManager();
-
-    nodes.registerWrapper< real64_array >( m_fieldName ).
-      setApplyDefaultValue( 0.0 ).
-      setPlotLevel( PlotLevel::LEVEL_0 ).
-      setDescription( "Primary field variable" );
-  } );
-}
-//END_SPHINX_INCLUDE_02
-
-
-/* STEPPING IN TIME
-   Here, we decide how we march in time in the resolutions based on the possible
-   three options set in the XML file (Steady state, Implicit transient, or Explicit transient).
-   Based on these options, we can either perform an Explicit Step (forward Euler),
-   or an Implicit Step (backward Euler).
-   The implementation of the Explicit or Implicit Steps are found in the SolverBase.
-   From now on, we oscillate between specific Laplace solver operations if implemented and more generic SolverBase operations.
-   The initial values of the solver step are all at time_n, and the solver attempts to advance by a time step of dt.
-   This dt time step size is specified initially by the user; and unfortunately, it can sometimes be too large for convergence.
-   The SolverStep method thus returns the time step value that is was actually capable of solving for with good convergence.
- */
-
-real64 LaplaceFEM::solverStep( real64 const & time_n,
-                               real64 const & dt,
-                               const int cycleNumber,
-                               DomainPartition & domain )
-{
-  real64 dtReturn = dt;
-  if( m_timeIntegrationOption == TimeIntegrationOption::ExplicitTransient )
-  {
-    dtReturn = explicitStep( time_n, dt, cycleNumber, domain );
-  }
-  else if( m_timeIntegrationOption == TimeIntegrationOption::ImplicitTransient ||
-           m_timeIntegrationOption == TimeIntegrationOption::SteadyState )
-  {
-    dtReturn = this->linearImplicitStep( time_n, dt, cycleNumber, domain );
-  }
-  return dtReturn;
-}
-
-/*
-   IMPLICIT STEP SETUP
-   This method uses the system setup from LaplaceFEM (see below).
-   It "deactivates" the time variables (with the GEOSX_UNUSED_PARAM macro) and does a steady state system set-up.
- */
-void LaplaceFEM::implicitStepSetup( real64 const & GEOSX_UNUSED_PARAM( time_n ),
-                                    real64 const & GEOSX_UNUSED_PARAM( dt ),
-                                    DomainPartition & domain )
-{
-  // Computation of the sparsity pattern
-  setupSystem( domain, m_dofManager, m_localMatrix, m_localRhs, m_localSolution );
-}
-
-void LaplaceFEM::implicitStepComplete( real64 const & GEOSX_UNUSED_PARAM( time_n ),
-                                       real64 const & GEOSX_UNUSED_PARAM( dt ),
-                                       DomainPartition & GEOSX_UNUSED_PARAM( domain ) )
-{}
-
-void LaplaceFEM::setupDofs( DomainPartition const & GEOSX_UNUSED_PARAM( domain ),
-                            DofManager & dofManager ) const
-{
-  dofManager.addField( m_fieldName,
-                       DofManager::Location::Node );
-
-  dofManager.addCoupling( m_fieldName,
-                          m_fieldName,
-                          DofManager::Connector::Elem );
-}
-
 /* SETUP SYSTEM
    Setting up the system using the base class method
  */
-
 void LaplaceFEM::setupSystem( DomainPartition & domain,
                               DofManager & dofManager,
                               CRSMatrix< real64, globalIndex > & localMatrix,
@@ -209,8 +100,9 @@ void LaplaceFEM::setupSystem( DomainPartition & domain,
 
   MeshLevel & mesh = domain.getMeshBody( 0 ).getMeshLevel( 0 );
   NodeManager const & nodeManager = mesh.getNodeManager();
+  string const dofKey = dofManager.getKey( m_fieldName );
   arrayView1d< globalIndex const > const &
-  dofIndex = nodeManager.getReference< globalIndex_array >( dofManager.getKey( m_fieldName ) );
+  dofIndex = nodeManager.getReference< globalIndex_array >( dofKey );
 
   SparsityPattern< globalIndex > sparsityPattern( dofManager.numLocalDofs(),
                                                   dofManager.numGlobalDofs(),
@@ -246,7 +138,7 @@ void LaplaceFEM::setupSystem( DomainPartition & domain,
    The call to the kernel is a templated call designed for performance (we will not explain the kernel here).
    See the implementation in LaplaceFEMKernel.cpp.
  */
-//START_SPHINX_INCLUDE_04
+//START_SPHINX_INCLUDE_ASSEMBLY
 void LaplaceFEM::assembleSystem( real64 const GEOSX_UNUSED_PARAM( time_n ),
                                  real64 const GEOSX_UNUSED_PARAM( dt ),
                                  DomainPartition & domain,
@@ -257,9 +149,9 @@ void LaplaceFEM::assembleSystem( real64 const GEOSX_UNUSED_PARAM( time_n ),
   MeshLevel & mesh = domain.getMeshBody( 0 ).getMeshLevel( 0 );
 
   NodeManager & nodeManager = mesh.getNodeManager();
-
+  string const dofKey = dofManager.getKey( m_fieldName );
   arrayView1d< globalIndex const > const &
-  dofIndex =  nodeManager.getReference< array1d< globalIndex > >( dofManager.getKey( m_fieldName ) );
+  dofIndex =  nodeManager.getReference< array1d< globalIndex > >( dofKey );
 
 
   finiteElement::
@@ -276,60 +168,8 @@ void LaplaceFEM::assembleSystem( real64 const GEOSX_UNUSED_PARAM( time_n ),
                                                       localRhs,
                                                       m_fieldName );
 
-
-
-  //END_SPHINX_INCLUDE_04
 }
-
-void LaplaceFEM::applySystemSolution( DofManager const & dofManager,
-                                      arrayView1d< real64 const > const & localSolution,
-                                      real64 const scalingFactor,
-                                      DomainPartition & domain )
-{
-  dofManager.addVectorToField( localSolution,
-                               m_fieldName,
-                               m_fieldName,
-                               scalingFactor );
-
-  // Synchronize ghost nodes
-  std::map< string, string_array > fieldNames;
-  fieldNames["node"].emplace_back( m_fieldName );
-
-  CommunicationTools::getInstance().synchronizeFields( fieldNames,
-                                                       domain.getMeshBody( 0 ).getMeshLevel( 0 ),
-                                                       domain.getNeighbors(),
-                                                       true );
-}
-
-/*
-   APPLY BOUNDARY CONDITIONS
-   Here, this call is the generic call from SolverBase.
-   All it does is to call a specific Dirichlet boundary condition implemented for this solver
- */
-void LaplaceFEM::applyBoundaryConditions( real64 const time_n,
-                                          real64 const dt,
-                                          DomainPartition & domain,
-                                          DofManager const & dofManager,
-                                          CRSMatrixView< real64, globalIndex const > const & localMatrix,
-                                          arrayView1d< real64 > const & localRhs )
-{
-  applyDirichletBCImplicit( time_n + dt, dofManager, domain, localMatrix, localRhs );
-}
-
-/*
-   SOLVE SYSTEM
-   This method is simply initiating the solution and right-hand side
-   and pass is to the base class solver.
- */
-void LaplaceFEM::solveSystem( DofManager const & dofManager,
-                              ParallelMatrix & matrix,
-                              ParallelVector & rhs,
-                              ParallelVector & solution )
-{
-  rhs.scale( -1.0 ); // TODO decide if we want this here
-  solution.zero();
-  SolverBase::solveSystem( dofManager, matrix, rhs, solution );
-}
+//END_SPHINX_INCLUDE_ASSEMBLY
 
 /*
    DIRICHLET BOUNDARY CONDITIONS
@@ -365,10 +205,7 @@ void LaplaceFEM::applyDirichletBCImplicit( real64 const time,
   } );
 }
 
-void LaplaceFEM::resetStateToBeginningOfStep( DomainPartition & GEOSX_UNUSED_PARAM( domain ) )
-{}
-
-//START_SPHINX_INCLUDE_00
+//START_SPHINX_INCLUDE_REGISTER
 REGISTER_CATALOG_ENTRY( SolverBase, LaplaceFEM, string const &, Group * const )
-//END_SPHINX_INCLUDE_00
-} /* namespace ANST */
+//END_SPHINX_INCLUDE_REGISTER
+} /* namespace geosx */
