95fccbd8421b5945745da9c2d764f73062872a6e
Christopher Sherman <sherman27@llnl.gov>
2021-06-10 09:56:47 -0700

    Adding method to write insar data in hdf5 format

--- src/coreComponents/python/modules/pygeosx_tools_package/pygeosx_tools/geophysics/insar.py
+++ src/coreComponents/python/modules/pygeosx_tools_package/pygeosx_tools/geophysics/insar.py
@@ -2,9 +2,11 @@
 import os
 import numpy as np
 from pygeosx_tools import wrapper, parallel_io, plot_tools
+import hdf5_wrapper
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from pyevtk.hl import gridToVTK
+from scipy import interpolate
 
 
 class InSAR_Analysis():
@@ -150,17 +152,55 @@ class InSAR_Analysis():
         global_N = np.sum(np.array(parallel_io.gather_array(local_N, concatenate=False)), axis=0)
 
         # Find final 3D displacement
+        global_displacement = np.zeros((Nx, Ny, 3))
         if (parallel_io.rank == 0):
             range_change = np.zeros((Nx, Ny))
             for ii in range(3):
-                global_displacement_sum[:, :, ii] /= global_N + 1e-10
-                range_change += global_displacement_sum[:, :, ii] * self.satellite_vector[ii]
+                d = np.squeeze(global_displacement_sum[:, :, ii]) / (global_N + 1e-10)
+                d[global_N == 0] = np.NaN
+                global_displacement[:, :, ii] = self.fill_nan_gaps(d)
+                range_change += d * self.satellite_vector[ii]
 
-            self.displacement.append(global_displacement_sum)
+            # Filter nans
+            self.displacement.append(global_displacement)
             self.range_change.append(range_change)
             self.phase.append(np.angle(np.exp(2j * np.pi * range_change / self.wavelength)))
             self.mask.append(global_N > 0)
             self.times.append(time)
+
+    def fill_nan_gaps(self, values):
+        """
+        Fill gaps in the insar data which are specified via NaN's
+        """
+        z = np.isnan(values)
+        if np.sum(z):
+            N = np.shape(values)
+            grid = np.meshgrid(self.x_grid, self.y_grid, indexing='ij')
+
+            # Filter out values in flattened arrays
+            x_flat = np.reshape(grid[0], (-1))
+            y_flat = np.reshape(grid[1], (-1))
+            v_flat = np.reshape(values, (-1))
+            t_flat = np.reshape(z, (-1))
+            I_valid = np.where(~t_flat)[0]
+            x_valid = x_flat[I_valid]
+            y_valid = y_flat[I_valid]
+            v_valid = v_flat[I_valid]
+
+            # Re-interpolate values
+            vb = interpolate.griddata((x_valid, y_valid), v_valid, tuple(grid), method='linear')
+            values = np.reshape(vb, N)
+        return values
+
+    def save_hdf5(self, header='insar', output_root='./results'):
+        os.makedirs(output_root, exist_ok=True)
+        with hdf5_wrapper.hdf5_wrapper('%s/%s.hdf5' % (output_root, header), mode='w') as data:
+            data['x'] = self.x_grid
+            data['y'] = self.y_grid
+            data['time'] = self.times
+            data['displacement'] = np.array(self.displacement)
+            data['range_change'] = np.array(self.range_change)
+            data['phase'] = np.array(self.phase)
 
     def save_csv(self, header='insar', output_root='./results'):
         os.makedirs(output_root, exist_ok=True)
@@ -171,7 +211,7 @@ class InSAR_Analysis():
                    self.y_grid,
                    delimiter=', ')
         for ii, t in enumerate(self.times):
-            comments = 'T (days), %1.4e' % (t / (60 * 60 * 24))
+            comments = 'T (days), %1.8e' % (t / (60 * 60 * 24))
             np.savetxt('%s/%s_range_change_%03d.csv' % (output_root, header, ii),
                        self.range_change[ii],
                        delimiter=', ',
