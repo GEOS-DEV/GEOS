054e7e474c667f4799216e27ddcf33761257e990
Matteo Cusini <matteo.cusini@gmail.com>
2021-07-08 14:58:52 -0700

    Modified all xml files.

--- src/coreComponents/physicsSolvers/fluidFlow/SinglePhaseHybridFVM.cpp
+++ src/coreComponents/physicsSolvers/fluidFlow/SinglePhaseHybridFVM.cpp
@@ -269,8 +269,8 @@ void SinglePhaseHybridFVM::assembleFluxTerms( real64 const GEOSX_UNUSED_PARAM( t
     SingleFluidBase const & fluid =
       getConstitutiveModel< SingleFluidBase >( subRegion, m_fluidModelNames[targetIndex] );
 
-    PermeabilityBase const & permeabilityModel =
-          getConstitutiveModel< PermeabilityBase >( subRegion, m_permeabilityModelNames[targetIndex] );
+    arrayView2d< real64 const > const & permeability =
+          getConstitutiveModel< PermeabilityBase >( subRegion, m_permeabilityModelNames[targetIndex] ).permeability();
 
     mimeticInnerProductDispatch( mimeticInnerProductBase,
                                  [&] ( auto const mimeticInnerProduct )
@@ -282,7 +282,6 @@ void SinglePhaseHybridFVM::assembleFluxTerms( real64 const GEOSX_UNUSED_PARAM( t
                                                    esr,
                                                    subRegion,
                                                    fluid,
-                                                   permeabilityModel,
                                                    m_regionFilter.toViewConst(),
                                                    nodePosition,
                                                    elemRegionList,
@@ -293,6 +292,7 @@ void SinglePhaseHybridFVM::assembleFluxTerms( real64 const GEOSX_UNUSED_PARAM( t
                                                    faceGhostRank,
                                                    facePres,
                                                    dFacePres,
+                                                   permeability,
                                                    faceGravCoef,
                                                    transMultiplier,
                                                    m_mobility.toNestedViewConst(),
