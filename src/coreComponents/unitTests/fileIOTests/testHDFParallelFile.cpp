b6edbfd97ab32c900e3e886780aff19c1bb81bbc
William R Tobin <4522899+wrtobin@users.noreply.github.com>
2021-07-15 09:39:34 -0700

    Dynamic Time History Collection  (#1360)
    * avoid invalid mem access if init size is zero
    
    * working on allowing dynamic resizing of time history data collected, needs testing
    
    * explicitly update the index sets...
    
    * collect any packable item in the datarepo, collect coordinates automatically when collecting from object managers / fields, account for dynamic set changes for the coordinates
    
    * index set bugfixes
    
    * removing serial file writer since it is now functionally obsolete, changing file row prealloc to 1 so that coords being written and never updated don't have erroneous zero rows
    
    * updating sneddon validation benchmark/tutorial for new time history output, also fixing an init collection size 0 bug
    
    * removing existing explicit coordinate collection since it is automatically output when collecting from mesh objects
    
    * make multiple writers/collectors operating on the same file more feasible
    
    * coord collection bugfix
    
    * Updating tutorial 8
    
    Co-authored-by: Nicola Castelletto <castelletto1@llnl.gov>

--- src/coreComponents/unitTests/fileIOTests/testHDFParallelFile.cpp
+++ src/coreComponents/unitTests/fileIOTests/testHDFParallelFile.cpp
@@ -24,6 +24,55 @@ TEST( testHDFIO_parallel, SingleValueHistory )
   }
 
   io.write( );
+  io.compressInFile();
+}
+
+
+TEST( testHDFIO_parallel, DynamicHistory )
+{
+  GEOSX_MARK_FUNCTION;
+  string filename( "dynamic_parallel" );
+  HistoryMetadata singleItemSpec( "some_real", 1, std::type_index( typeid(real64)));
+
+  int rnk = MpiWrapper::commRank( );
+
+  HDFHistIO io( filename, singleItemSpec );
+  io.init( true );
+  real64 val = 0.0;
+  for( localIndex tidx = 0; tidx < 4; ++tidx )
+  {
+    val += 0.5 * ( rnk + 1 );
+    if( ( rnk + tidx ) % 2 == 0 )
+    {
+      io.updateCollectingCount( 1 );
+      buffer_unit_type * buffer = io.getBufferHead( );
+      memcpy( buffer, &val, sizeof(real64));
+    }
+    else
+    {
+      io.updateCollectingCount( 0 );
+      io.getBufferHead( );
+    }
+  }
+  io.write( );
+  val = 0.0;
+  for( localIndex tidx = 0; tidx < 8; ++tidx )
+  {
+    val += 0.5 * ( rnk + 1 );
+    if( ( rnk + tidx ) % 2 == 1 )
+    {
+      io.updateCollectingCount( 1 );
+      buffer_unit_type * buffer = io.getBufferHead( );
+      memcpy( buffer, &val, sizeof(real64) );
+    }
+    else
+    {
+      io.updateCollectingCount( 0 );
+      io.getBufferHead( );
+    }
+  }
+  io.write( );
+  io.compressInFile();
 }
 
 int main( int ac, char * av[] )
