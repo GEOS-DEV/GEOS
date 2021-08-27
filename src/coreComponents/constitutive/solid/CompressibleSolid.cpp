a23824a6fc366c035a7a74f2adcd903882b8f12f
Matteo Cusini <49037133+CusiniM@users.noreply.github.com>
2021-08-27 11:31:50 -0700

    Use of CompressibleSolid - Stencils refactoring - Transmissibility update (#1311)
    * Initial commit. Adapting kernels to include fractures.
    
    * Renamed FaceElementStencil to SurfaceElementStencil
    
    * Added EDFM stencil.
    
    * FaceElementToCellStencil Working
    
    * Compositional flow in fractures.
    
    * added updateState + refactoring of hydrofractureSolver.
    
    * Design for poroelastic fluxes + added PoroelasticEFEMKernels.
    
    * Fixed all unit tests.
    
    * compiles on lassen. New permkernel.
    
    * Proper usage of CompressibleSolid + porosity and perm models.
    
    * Adding CoupledSolidBase.
    
    * Changed Poroelastic with Poromechanics
    
    * remove old rst files
    
    * setting referencePorosity instead of porosity.
    
    * Hydrofrac solver fluxes.
    
    * fixed all xmls.
    
    * Addressed Sergey's comments.
    
    * explicit capture + removed old porosity, pvMult wrappers.
    
    * fixed junction flow.
    
    * fixed all doxygen errors.
    
    * Francois's comments.
    
    * added integrated tests. Updated integratedTests tag.

--- src/coreComponents/constitutive/solid/CompressibleSolid.cpp
+++ src/coreComponents/constitutive/solid/CompressibleSolid.cpp
@@ -21,6 +21,7 @@
 #include "porosity/PressurePorosity.hpp"
 #include "constitutive/permeability/ConstantPermeability.hpp"
 #include "constitutive/permeability/CarmanKozenyPermeability.hpp"
+#include "constitutive/permeability/ParallelPlatesPermeability.hpp"
 
 namespace geosx
 {
@@ -43,9 +44,11 @@ CompressibleSolid< PORO_TYPE, PERM_TYPE >::~CompressibleSolid() = default;
 // Register all CompressibleSolid model types.
 typedef CompressibleSolid< PressurePorosity, ConstantPermeability > CompressibleRockConstant;
 typedef CompressibleSolid< PressurePorosity, CarmanKozenyPermeability > CompressibleRockCK;
+typedef CompressibleSolid< PressurePorosity, ParallelPlatesPermeability > FractureRock;
 
 REGISTER_CATALOG_ENTRY( ConstitutiveBase, CompressibleRockConstant, string const &, Group * const )
 REGISTER_CATALOG_ENTRY( ConstitutiveBase, CompressibleRockCK, string const &, Group * const )
+REGISTER_CATALOG_ENTRY( ConstitutiveBase, FractureRock, string const &, Group * const )
 
 
 }
