8662c183376ae79cdf0cbb9a58577376e8936fc0
Quan Bui <mquan.bui@gmail.com>
2021-09-06 12:51:10 -0700

    Use full matrix instead of SDC approximation for mechanics in hydrofrac solver. Fix bug in setting options for mechanics block solver.

--- src/coreComponents/linearAlgebra/interfaces/hypre/HyprePreconditioner.cpp
+++ src/coreComponents/linearAlgebra/interfaces/hypre/HyprePreconditioner.cpp
@@ -471,7 +471,7 @@ void HyprePreconditioner::setup( Matrix const & mat )
     // Perform setup of the MGR mechanics F-solver with SDC matrix, if used
     if( m_mgrData && m_mgrData->mechSolver.ptr )
     {
-      GEOSX_LAI_CHECK_ERROR( m_mgrData->mechSolver.setup( m_mgrData->mechSolver.ptr, m_precondMatrix.unwrapped(), nullptr, nullptr ) );
+      //GEOSX_LAI_CHECK_ERROR( m_mgrData->mechSolver.setup( m_mgrData->mechSolver.ptr, m_precondMatrix.unwrapped(), nullptr, nullptr ) );
     }
 
     // Perform setup of the main solver
