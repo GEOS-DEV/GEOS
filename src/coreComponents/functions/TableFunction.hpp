e86c933602c05ab97ac944969cae62d593c4e8d9
Hewei Tang <71804721+tang39@users.noreply.github.com>
2021-09-08 21:54:49 -0700

    Implemented time-dependent well rate and bhp tables (#1468)
    * add time-dependent totalrate, phaserate, and bhp tables
    
    * update .xml to test the well control tables
    
    * refactored well kernels
    
    * addressed Sergey's comments
    
    * update integratedTests submodule
    
    * added defaulted special member functions in BO and TableFunction classes
    
    * new attempt: added defaulted special member functions in MultiFluidBase
    
    Co-authored-by: Francois Hamon <fhamon@stanford.edu>

--- src/coreComponents/functions/TableFunction.hpp
+++ src/coreComponents/functions/TableFunction.hpp
@@ -60,11 +60,22 @@ public:
     /// @cond DO_NOT_DOCUMENT
     /// We need these SMFs to enable array1d< KernelWrapper > and avoid
     /// host-device errors with CUDA. Otherwise rule of 0 would be fine.
-    /// Note: move assignment suppressed but not deleted, on purpose!
+
     KernelWrapper() = default;
     KernelWrapper( KernelWrapper const & ) = default;
     KernelWrapper( KernelWrapper && ) = default;
     KernelWrapper & operator=( KernelWrapper const & ) = default;
+
+    /// Note: move assignment not deleted, not defaulted on purpose!
+    /// This is needed to avoid a compilation warning with CUDA
+    KernelWrapper & operator=( KernelWrapper && other )
+    {
+      m_coordinates = std::move( other.m_coordinates );
+      m_values = std::move( other.m_values );
+      m_interpolationMethod = other.m_interpolationMethod;
+      return *this;
+    }
+
     /// @endcond
 
     /**
