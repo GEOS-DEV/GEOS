f5154fc35bc7974a58d0624441f73fddbc58db6c
Herve Gross <40979822+herve-gross@users.noreply.github.com>
2021-09-09 19:46:29 -0700

    Change to TotalEnergies (#1562)

--- src/coreComponents/constitutive/solid/SolidModelDiscretizationOpsOrthotropic.hpp
+++ src/coreComponents/constitutive/solid/SolidModelDiscretizationOpsOrthotropic.hpp
@@ -4,7 +4,7 @@
  *
  * Copyright (c) 2018-2019 Lawrence Livermore National Security LLC
  * Copyright (c) 2018-2019 The Board of Trustees of the Leland Stanford Junior University
- * Copyright (c) 2018-2019 Total, S.A
+ * Copyright (c) 2018-2019 TotalEnergies
  * Copyright (c) 2019-     GEOSX Contributors
  * All right reserved
  *
