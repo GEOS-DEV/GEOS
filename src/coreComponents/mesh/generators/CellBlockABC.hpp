d13501569864152ea7a4e8c5bb3fd3694c20dfb3
Thomas Gazzola <thomas.gazzola@total.com>
2021-09-10 10:58:54 -0700

    Correct headers with TotalEnergies.

--- src/coreComponents/mesh/generators/CellBlockABC.hpp
+++ src/coreComponents/mesh/generators/CellBlockABC.hpp
@@ -4,9 +4,9 @@
  *
  * Copyright (c) 2018-2020 Lawrence Livermore National Security LLC
  * Copyright (c) 2018-2020 The Board of Trustees of the Leland Stanford Junior University
- * Copyright (c) 2018-2020 Total, S.A
+ * Copyright (c) 2018-2020 TotalEnergies
  * Copyright (c) 2020-     GEOSX Contributors
- * All right reserved
+ * All rights reserved
  *
  * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
  * ------------------------------------------------------------------------------------------------------------
