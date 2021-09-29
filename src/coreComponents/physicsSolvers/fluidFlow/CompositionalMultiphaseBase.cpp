c67781010fa8adc5681698ea446d005a747c6e5c
Francois Hamon <francois.hamon@totalenergies.com>
2021-09-29 12:31:55 -0700

    saved WIP

--- src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseBase.cpp
+++ src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseBase.cpp
@@ -1089,9 +1089,9 @@ void CompositionalMultiphaseBase::chopNegativeDensities( DomainPartition & domai
         for( localIndex ic = 0; ic < numComp; ++ic )
         {
           real64 const newDens = compDens[ei][ic] + dCompDens[ei][ic];
-          if( newDens < 0 )
+          if( newDens < 1e-12 )
           {
-            dCompDens[ei][ic] = -compDens[ei][ic];
+            dCompDens[ei][ic] = -compDens[ei][ic] + 1e-12;
           }
         }
       }
