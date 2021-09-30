371056fc6600050ee7cafdf23caf47d413e31a63
Christopher Sherman <sherman27@llnl.gov>
2021-09-30 11:25:35 -0700

    Adding help message, compiled name override to auto preprocessing script

--- scripts/automatic_xml_preprocess.sh
+++ scripts/automatic_xml_preprocess.sh
@@ -3,7 +3,8 @@
 # Parse and modify input arguments
 INPUT_ARGS=""
 INPUT_COUNTER=0
-OUTPUT_NAME=""
+COMPILED_XML_NAME=""
+COMPILED_XML_NAME_OVERRIDE=""
 PARAMETER_ARGS=""
 NEW_ARGS=""
 SCRIPT_DIR=$(dirname "$0")
@@ -15,7 +16,11 @@ do
         -i|--input)
         INPUT_ARGS="$INPUT_ARGS -i $2"
         INPUT_COUNTER=$(( INPUT_COUNTER + 1 ))
-        OUTPUT_NAME=$2.preprocessed
+        COMPILED_XML_NAME=$2.preprocessed
+        shift
+        ;;
+        -c|--compiled_name)
+        COMPILED_XML_NAME_OVERRIDE=$2
         shift
         ;;
         -p|--parameter)
@@ -23,6 +28,18 @@ do
         PARAMETER_ARGS="$PARAMETER_ARGS -p $2 $3"
         shift
         shift
+        ;;
+        -?|--help)
+        echo ""
+        echo "Preprocessor options:"
+        echo "-i/--input          Input xml file name"
+        echo "-p/--parameter      XML parameter overrides (name and value separated by a space)"
+        echo "-c/--compiled_name  Compiled xml filename (default=[input_name].preprocessed or composite.xml.preprocessed)"
+        echo "Note: multiple -i and -p arguments are allowed"
+        echo ""
+        echo "GEOSX options:"
+        $SCRIPT_DIR/geosx --help
+        exit
         ;;
         *)
         NEW_ARGS="$NEW_ARGS $key $2"
@@ -37,17 +54,22 @@ done
 if [ -f $SCRIPT_DIR/preprocess_xml ]; then
    if [ "$INPUT_COUNTER" -gt "1" ]
    then
-      OUTPUT_NAME="composite.xml.preprocessed"
+      COMPILED_XML_NAME="composite.xml.preprocessed"
+   fi
+
+   if [ ! -z "$COMPILED_XML_NAME_OVERRIDE" ]
+   then
+      COMPILED_XML_NAME=$COMPILED_XML_NAME_OVERRIDE
    fi
 
    # Preprocess the file
    echo "Preprocessing xml:"
-   $SCRIPT_DIR/preprocess_xml $INPUT_ARGS $PARAMETER_ARGS -o $OUTPUT_NAME -s $SCRIPT_DIR/../../src/coreComponents/schema/schema.xsd
+   $SCRIPT_DIR/preprocess_xml $INPUT_ARGS $PARAMETER_ARGS -o $COMPILED_XML_NAME -s $SCRIPT_DIR/../../src/coreComponents/schema/schema.xsd
 
    # Continue by running GEOSX
    echo "Running command:"
-   echo "$SCRIPT_DIR/geosx $NEW_ARGS -i $OUTPUT_NAME"
-   $SCRIPT_DIR/geosx $NEW_ARGS -i $OUTPUT_NAME
+   echo "$SCRIPT_DIR/geosx $NEW_ARGS -i $COMPILED_XML_NAME"
+   $SCRIPT_DIR/geosx $NEW_ARGS -i $COMPILED_XML_NAME
 else
    echo "Error: XML preprocessor not found"
    echo "To build it, run \"make geosx_xml_tools\""
