6b5c66662b16091cb21235747f9e2a9cd9bb1962
Nicola Castelletto <38361926+castelletto1@users.noreply.github.com>
2021-11-08 09:14:09 -0800

    Feature/castelletto1/lai unit testing (#1669)
    * Changing 2D Laplace operator construction
    
    * UMFPACK working again on Lassen (WIP)
    
    * fix LAI vector tests
    
    * fixed LAI matrix test
    
    * Unit test passing on Lassen except DirectParallel, testDofManager, testCompMultiphaseFlow
    
    * Integrated tests running on Lassen (fail check int32 vs int64)
    
    * Updating export methods for PETSc LAI
    
    * External solvers test
    
    * fixed testDofManager
    
    * Correction in HypreExport
    
    * fixed vem unit test on Lassen (#1670)
    
    * fix testCompMultiphaseFlow test
    
    * Working import method in HypreExport
    
    * Fix in HypreExport
    
    * update integrated tests baselines
    
    * Fix direct solvers interfacing hypre on GPU. Remove broken unit tests for deprecated vector functions. Cleanup.
    
    Co-authored-by: Randolph R. Settgast <settgast1@llnl.gov>
    Co-authored-by: Francois Hamon <francois.hamon@totalenergies.com>
    Co-authored-by: Sergey Klevtsov <klevtsov@stanford.edu>

--- src/coreComponents/linearAlgebra/interfaces/hypre/HypreExport.cpp
+++ src/coreComponents/linearAlgebra/interfaces/hypre/HypreExport.cpp
@@ -20,6 +20,7 @@
 
 #include "common/MpiWrapper.hpp"
 #include "linearAlgebra/interfaces/hypre/HypreMatrix.hpp"
+#include "linearAlgebra/interfaces/hypre/HypreUtils.hpp"
 #include "LvArray/src/sortedArrayManipulation.hpp"
 
 #include <_hypre_parcsr_mv.h>
@@ -31,7 +32,7 @@ namespace geosx
 HypreExport::HypreExport() = default;
 
 HypreExport::HypreExport( HypreMatrix const & mat,
-                          integer targetRank )
+                          integer const targetRank )
   : m_targetRank( targetRank )
 {
   // make a sub-communicator for scatter and ensure target rank is mapped to 0 in new comm
@@ -49,132 +50,197 @@ HypreExport::~HypreExport()
   }
 }
 
+namespace
+{
+
+template< typename T, typename U >
+std::enable_if_t< std::is_same< T, U >::value >
+copyOrTransform( T const * const first,
+                 T const * const last,
+                 U * const out )
+{
+  std::copy( first, last, out );
+}
+
+template< typename T, typename U >
+std::enable_if_t< !std::is_same< T, U >::value >
+copyOrTransform( T const * const first,
+                 T const * const last,
+                 U * const out )
+{
+  std::transform( first, last, out, []( T const v ) { return static_cast< U >( v ); } );
+}
+
+template< typename HYPRE_TYPE, typename GEOSX_TYPE >
+void exportArray( HYPRE_MemoryLocation const memorySpace,
+                  HYPRE_TYPE const * const hypreArray,
+                  arrayView1d< GEOSX_TYPE > const & geosxArray )
+{
+  if( memorySpace == HYPRE_MEMORY_HOST )
+  {
+    geosxArray.move( LvArray::MemorySpace::host, true );
+    copyOrTransform( hypreArray, hypreArray + geosxArray.size(), geosxArray.data() );
+  }
+  else // hypreArray is on device
+  {
+    forAll< hypre::execPolicy >( geosxArray.size(),
+                                 [geosxArray, hypreArray] GEOSX_HYPRE_HOST_DEVICE ( localIndex const i )
+    {
+      geosxArray[i] = static_cast< GEOSX_TYPE >( hypreArray[i] );
+    } );
+  }
+}
+
+template< typename HYPRE_TYPE, typename GEOSX_TYPE >
+void importArray( HYPRE_MemoryLocation const memorySpace,
+                  arrayView1d< GEOSX_TYPE const > const & geosxArray,
+                  HYPRE_TYPE * const hypreArray )
+{
+  if( memorySpace == HYPRE_MEMORY_HOST )
+  {
+    geosxArray.move( LvArray::MemorySpace::host, false );
+    copyOrTransform( geosxArray.data(), geosxArray.data() + geosxArray.size(), hypreArray );
+  }
+  else // hypreArray is on device
+  {
+    forAll< hypre::execPolicy >( geosxArray.size(),
+                                 [geosxArray, hypreArray] GEOSX_HYPRE_HOST_DEVICE ( localIndex const i )
+    {
+      hypreArray[i] = static_cast< HYPRE_TYPE >( geosxArray[i] );
+    } );
+  }
+}
+
+} // namespace
+
 template< typename OFFSET_TYPE, typename COLUMN_TYPE >
 void HypreExport::exportCRS( HypreMatrix const & mat,
-                             OFFSET_TYPE * const rowOffsets,
-                             COLUMN_TYPE * const colIndices,
-                             real64 * const values ) const
+                             arrayView1d< OFFSET_TYPE > const & rowOffsets,
+                             arrayView1d< COLUMN_TYPE > const & colIndices,
+                             arrayView1d< real64 > const & values ) const
 {
   int const rank = MpiWrapper::commRank( mat.getComm() );
 
-  // import on target rank if needed, or extract diag+offdiag part in each rank
-  hypre_CSRMatrix * localMatrix;
-  if( m_targetRank < 0 )
-  {
-    localMatrix = hypre_MergeDiagAndOffd( mat.unwrapped() );
-  }
-  else
-  {
-    GEOSX_ERROR_IF( rank == m_targetRank && !( rowOffsets && colIndices && values ),
-                    "HypreExport: must pass non-null pointers on the target rank" );
-    localMatrix = hypre_ParCSRMatrixToCSRMatrixAll( mat.unwrapped() );
-    GEOSX_ERROR_IF( rank == m_targetRank && !localMatrix, "HypreExport: matrix is empty on target rank" );
-  }
+  // import on target rank if needed, or extract diag+offd part in each rank
+  hypre_CSRMatrix * const localMatrix = m_targetRank < 0
+                                      ? hypre_MergeDiagAndOffd( mat.unwrapped() )
+                                      : hypre_ParCSRMatrixToCSRMatrixAll( mat.unwrapped() );
+  GEOSX_ERROR_IF( rank == m_targetRank && !localMatrix, "HypreExport: matrix is empty on target rank" );
 
-  // export the raw CRS data
   if( m_targetRank < 0 || m_targetRank == rank )
   {
-    HYPRE_Int const numRows = hypre_CSRMatrixNumRows( localMatrix );
-    HYPRE_Int const numNz = hypre_CSRMatrixNumNonzeros( localMatrix );
-    HYPRE_Int const * const ia = hypre_CSRMatrixI( localMatrix );
-    HYPRE_Real const * const va = hypre_CSRMatrixData( localMatrix );
+    HYPRE_Int const numRow = hypre_CSRMatrixNumRows( localMatrix );
+    HYPRE_Int const numNz  = hypre_CSRMatrixNumNonzeros( localMatrix );
 
-    std::transform( ia, ia + numRows + 1, rowOffsets, LvArray::integerConversion< OFFSET_TYPE, HYPRE_Int > );
-    std::copy( va, va + numNz, values );
+    GEOSX_LAI_ASSERT_EQ( rowOffsets.size(), numRow + 1 );
+    GEOSX_LAI_ASSERT_EQ( colIndices.size(), numNz );
+    GEOSX_LAI_ASSERT_EQ( values.size(), numNz );
+
+    HYPRE_MemoryLocation const memorySpace = hypre_CSRMatrixMemoryLocation( localMatrix );
+
+    exportArray( memorySpace, hypre_CSRMatrixI( localMatrix ), rowOffsets );
+    exportArray( memorySpace, hypre_CSRMatrixData( localMatrix ), values );
 
     // We have to handle two cases differently because hypre uses two different struct members
     // (j/big_j) to store the column indices depending on how we obtained the local matrix.
     if( m_targetRank < 0 )
     {
-      HYPRE_BigInt const * const ja = hypre_CSRMatrixBigJ( localMatrix );
-      std::transform( ja, ja + numNz, colIndices, LvArray::integerConversion< COLUMN_TYPE, HYPRE_BigInt > );
+      exportArray( memorySpace, hypre_CSRMatrixBigJ( localMatrix ), colIndices );
     }
     else
     {
-      HYPRE_Int const * const ja = hypre_CSRMatrixJ( localMatrix );
-      std::transform( ja, ja + numNz, colIndices, LvArray::integerConversion< COLUMN_TYPE, HYPRE_Int > );
+      exportArray( memorySpace, hypre_CSRMatrixJ( localMatrix ), colIndices );
     }
 
     // Sort the values by column index after copying (some solvers expect this)
-    for( localIndex i = 0; i < numRows; ++i )
+    forAll< hypre::execPolicy >( numRow, [rowOffsets, colIndices, values] GEOSX_HYPRE_HOST_DEVICE ( HYPRE_Int const i )
     {
       using LvArray::sortedArrayManipulation::dualSort;
-      dualSort( colIndices + rowOffsets[i], colIndices + rowOffsets[i + 1], values + rowOffsets[i] );
-    }
+      dualSort( colIndices.data() + rowOffsets[i],
+                colIndices.data() + rowOffsets[i + 1],
+                values.data() + rowOffsets[i] );
+    } );
   }
 
   GEOSX_LAI_CHECK_ERROR( hypre_CSRMatrixDestroy( localMatrix ) );
 }
 
 void HypreExport::exportVector( HypreVector const & vec,
-                                real64 * values ) const
+                                arrayView1d< real64 > const & values ) const
 {
+  int const rank = MpiWrapper::commRank( vec.getComm() );
+
+  // Gather vector on target rank, or just get the local part
+  hypre_Vector * const localVector = m_targetRank < 0
+                                   ? hypre_ParVectorLocalVector( vec.unwrapped() )
+                                   : hypre_ParVectorToVectorAll( vec.unwrapped() );
+  GEOSX_ERROR_IF( rank == m_targetRank && !localVector, "HypreExport: vector is empty on target rank" );
+
+  if( m_targetRank < 0 || m_targetRank == rank )
+  {
+    GEOSX_LAI_ASSERT_EQ( values.size(), hypre_VectorSize( localVector ) );
+    exportArray( hypre_VectorMemoryLocation( localVector ),
+                 hypre_VectorData( localVector ),
+                 values );
+  }
+
   if( m_targetRank >= 0 )
   {
-    int const rank = MpiWrapper::commRank( vec.getComm() );
-    GEOSX_ERROR_IF( rank == m_targetRank && !values, "HypreExport: must pass non-null pointers on the target rank" );
+    GEOSX_LAI_CHECK_ERROR( hypre_SeqVectorDestroy( localVector ) );
+  }
+}
 
-    hypre_Vector * const localVector = hypre_ParVectorToVectorAll( vec.unwrapped() );
-    if( rank == m_targetRank )
+void HypreExport::importVector( arrayView1d< real64 const > const & values,
+                                HypreVector & vec ) const
+{
+  if( m_subComm != MPI_COMM_NULL )
+  {
+    hypre_Vector * localVector{};
+    if( MpiWrapper::commRank( vec.getComm() ) == m_targetRank )
     {
-      HYPRE_Real const * const data = hypre_VectorData( localVector );
-      std::copy( data, data + vec.globalSize(), values );
+      GEOSX_LAI_ASSERT_EQ( values.size(), vec.globalSize() );
+      values.move( LvArray::MemorySpace::host, false );
+
+      // HACK: create a hypre vector that points to local data; we have to use const_cast,
+      //       but this is ok because we don't modify the values, only scatter the vector.
+      localVector = hypre_SeqVectorCreate( LvArray::integerConversion< HYPRE_Int >( values.size() ) );
+      hypre_VectorOwnsData( localVector ) = false;
+      hypre_VectorData( localVector ) = const_cast< real64 * >( values.data() );
+      hypre_SeqVectorInitialize_v2( localVector, HYPRE_MEMORY_HOST );
     }
+
+    // scatter the data
+    hypre_ParVector * const parVector = hypre_VectorToParVector( m_subComm,
+                                                                 localVector,
+                                                                 hypre_ParVectorPartitioning( vec.unwrapped() ) );
+    // copy local part of the data over to the output vector
+    HYPRE_Real const * const parVectorData = hypre_VectorData( hypre_ParVectorLocalVector( parVector ) );
+    if( hypre_ParVectorMemoryLocation( vec.unwrapped() ) == HYPRE_MEMORY_HOST )
+    {
+      std::copy( parVectorData, parVectorData + vec.localSize(), vec.extractLocalVector() );
+    }
+    else
+    {
+#ifdef GEOSX_USE_HYPRE_CUDA
+      cudaMemcpy( vec.extractLocalVector(),
+                  parVectorData,
+                  vec.localSize() * sizeof( HYPRE_Real ),
+                  cudaMemcpyHostToDevice );
+#else
+      GEOSX_ERROR( "HypreExport: invalid memory space of target vector" );
+#endif
+    }
+
+    GEOSX_LAI_CHECK_ERROR( hypre_ParVectorDestroy( parVector ) );
     GEOSX_LAI_CHECK_ERROR( hypre_SeqVectorDestroy( localVector ) );
   }
   else
   {
-    real64 const * const data = vec.extractLocalVector();
-    std::copy( data, data + vec.localSize(), values );
+    hypre_Vector * const localVector = hypre_ParVectorLocalVector( vec.unwrapped() );
+    importArray( hypre_VectorMemoryLocation( localVector ),
+                 values,
+                 hypre_VectorData( localVector ) );
   }
-}
-
-void HypreExport::importVector( real64 const * values,
-                                HypreVector & vec ) const
-{
-  if( m_targetRank >= 0 )
-  {
-    int const rank = MpiWrapper::commRank( vec.getComm() );
-    GEOSX_ERROR_IF( rank == m_targetRank && !values, "HypreExport: must pass non-null pointers on the target rank" );
-
-    if( m_subComm != MPI_COMM_NULL )
-    {
-      HYPRE_Vector localVector = nullptr;
-      if( rank == m_targetRank )
-      {
-        // HACK: create a hypre vector that points to local data; we have to use const_cast,
-        //       but this is ok because we don't modify the values, only scatter the vector.
-        localVector = HYPRE_VectorCreate( LvArray::integerConversion< HYPRE_Int >( vec.globalSize() ) );
-        hypre_VectorOwnsData( ( hypre_Vector * ) localVector ) = false;
-        hypre_VectorData( ( hypre_Vector * ) localVector ) = const_cast< real64 * >( values );
-        hypre_SeqVectorInitialize_v2( ( hypre_Vector * ) localVector, HYPRE_MEMORY_HOST );
-      }
-
-      // output vector partitioning array
-      HYPRE_BigInt * const partitioning = hypre_ParVectorPartitioning( vec.unwrapped() );
-
-      // scatter the data
-      HYPRE_ParVector parVector;
-      GEOSX_LAI_CHECK_ERROR( HYPRE_VectorToParVector( m_subComm,
-                                                      localVector,
-                                                      partitioning,
-                                                      &parVector ) );
-
-
-      // copy parVector local part over to the output vector
-      HYPRE_Real * const parVectorData = hypre_VectorData( hypre_ParVectorLocalVector( parVector ) );
-      std::copy( parVectorData, parVectorData + vec.localSize(), vec.extractLocalVector() );
-
-      GEOSX_LAI_CHECK_ERROR( HYPRE_ParVectorDestroy( parVector ) );
-      GEOSX_LAI_CHECK_ERROR( HYPRE_VectorDestroy( localVector ) );
-    }
-  }
-  else
-  {
-    real64 * const data = vec.extractLocalVector();
-    std::copy( values, values + vec.localSize(), data );
-  }
-
 }
 
 /**
@@ -186,9 +252,9 @@ void HypreExport::importVector( real64 const * values,
 #define INST_HYPRE_EXPORT_CRS( OFFSET_TYPE, COLUMN_TYPE ) \
   template void \
   HypreExport::exportCRS< OFFSET_TYPE, COLUMN_TYPE >( HypreMatrix const &, \
-                                                      OFFSET_TYPE * const, \
-                                                      COLUMN_TYPE * const, \
-                                                      real64 * const ) const
+                                                      arrayView1d< OFFSET_TYPE > const &, \
+                                                      arrayView1d< COLUMN_TYPE > const &, \
+                                                      arrayView1d< real64 > const & ) const
 
 // Add other instantiations as needed (only use built-in types)
 INST_HYPRE_EXPORT_CRS( int, int );
