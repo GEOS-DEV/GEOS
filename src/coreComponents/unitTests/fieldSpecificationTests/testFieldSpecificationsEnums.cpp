49369110c4735f96db970b37ba0a3edc44d9bae0
Thomas Gazzola <thomas.gazzola@total.com>
2022-02-23 11:15:41 -0800

    Other enums.

---
+++ src/coreComponents/unitTests/fieldSpecificationTests/testFieldSpecificationsEnums.cpp
@@ -0,0 +1,38 @@
+/*	
+ * ------------------------------------------------------------------------------------------------------------	
+ * SPDX-License-Identifier: LGPL-2.1-only	
+ *	
+ * Copyright (c) 2018-2020 Lawrence Livermore National Security LLC	
+ * Copyright (c) 2018-2020 The Board of Trustees of the Leland Stanford Junior University	
+ * Copyright (c) 2018-2020 Total, S.A	
+ * Copyright (c) 2020-     GEOSX Contributors	
+ * All right reserved	
+ *	
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.	
+ * ------------------------------------------------------------------------------------------------------------	
+ */
+
+#include "fieldSpecification/TractionBoundaryCondition.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace geosx;
+
+// The `ENUM_STRING` implementation relies on consistency between the order of the `enum`,
+// and the order of the `string` array provided. Since this consistency is not enforced, it can be corrupted anytime.
+// This unit test aims at preventing from this implicit relationship to bring a bug.
+
+TEST( TractionBoundaryConditionEnums, TractionType )
+{
+  using EnumType = TractionBoundaryCondition::TractionType;
+
+  ASSERT_EQ( "vector", toString( EnumType::vector ) );
+  ASSERT_EQ( "normal", toString( EnumType::normal ) );
+  ASSERT_EQ( "stress", toString( EnumType::stress ) );
+}
+
+
+int main( int argc, char * * argv )
+{
+  return RUN_ALL_TESTS();
+}
