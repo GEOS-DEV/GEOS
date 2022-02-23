b99a995888a43fbd329cb0fbee004dd066c1adc3
Thomas Gazzola <thomas.gazzola@total.com>
2022-02-22 17:46:42 -0800

    More enums.

--- src/coreComponents/linearAlgebra/unitTests/testLinearSolverParametersEnums.cpp
+++ src/coreComponents/linearAlgebra/unitTests/testLinearSolverParametersEnums.cpp
@@ -60,6 +60,94 @@ TEST( LinearSolverParametersEnums, PreconditionerType )
 }
 
 
+TEST( LinearSolverParametersEnums, DirectColPerm )
+{
+  using EnumType = LinearSolverParameters::Direct::ColPerm;
+
+  ASSERT_EQ( "none", toString( EnumType::none ) );
+  ASSERT_EQ( "MMD_AtplusA", toString( EnumType::MMD_AtplusA ) );
+  ASSERT_EQ( "MMD_AtA", toString( EnumType::MMD_AtA ) );
+  ASSERT_EQ( "colAMD", toString( EnumType::colAMD ) );
+  ASSERT_EQ( "metis", toString( EnumType::metis ) );
+  ASSERT_EQ( "parmetis", toString( EnumType::parmetis ) );
+}
+
+
+TEST( LinearSolverParametersEnums, DirectRowPerm )
+{
+  using EnumType = LinearSolverParameters::Direct::RowPerm;
+
+  ASSERT_EQ( "none", toString( EnumType::none ) );
+  ASSERT_EQ( "mc64", toString( EnumType::mc64 ) );
+}
+
+
+TEST( LinearSolverParametersEnums, MGRStrategyType )
+{
+  using EnumType = LinearSolverParameters::MGR::StrategyType;
+
+  ASSERT_EQ( "invalid", toString( EnumType::invalid ) );
+  ASSERT_EQ( "singlePhaseReservoirFVM", toString( EnumType::singlePhaseReservoirFVM ) );
+  ASSERT_EQ( "singlePhaseHybridFVM", toString( EnumType::singlePhaseHybridFVM ) );
+  ASSERT_EQ( "singlePhaseReservoirHybridFVM", toString( EnumType::singlePhaseReservoirHybridFVM ) );
+  ASSERT_EQ( "singlePhasePoromechanics", toString( EnumType::singlePhasePoromechanics ) );
+  ASSERT_EQ( "hybridSinglePhasePoromechanics", toString( EnumType::hybridSinglePhasePoromechanics ) );
+  ASSERT_EQ( "compositionalMultiphaseFVM", toString( EnumType::compositionalMultiphaseFVM ) );
+  ASSERT_EQ( "compositionalMultiphaseHybridFVM", toString( EnumType::compositionalMultiphaseHybridFVM ) );
+  ASSERT_EQ( "compositionalMultiphaseReservoirFVM", toString( EnumType::compositionalMultiphaseReservoirFVM ) );
+  ASSERT_EQ( "compositionalMultiphaseReservoirHybridFVM", toString( EnumType::compositionalMultiphaseReservoirHybridFVM ) );
+  ASSERT_EQ( "multiphasePoromechanics", toString( EnumType::multiphasePoromechanics ) );
+  ASSERT_EQ( "hydrofracture", toString( EnumType::hydrofracture ) );
+  ASSERT_EQ( "lagrangianContactMechanics", toString( EnumType::lagrangianContactMechanics ) );
+}
+
+
+TEST( LinearSolverParametersEnums, AMGCycleType )
+{
+  using EnumType = LinearSolverParameters::AMG::CycleType;
+
+  ASSERT_EQ( "V", toString( EnumType::V ) );
+  ASSERT_EQ( "W", toString( EnumType::W ) );
+}
+
+
+TEST( LinearSolverParametersEnums, AMGPreOrPost )
+{
+  using EnumType = LinearSolverParameters::AMG::PreOrPost;
+
+  ASSERT_EQ( "pre", toString( EnumType::pre ) );
+  ASSERT_EQ( "post", toString( EnumType::post ) );
+  ASSERT_EQ( "both", toString( EnumType::both ) );
+}
+
+
+TEST( LinearSolverParametersEnums, AMGSmootherType )
+{
+  using EnumType = LinearSolverParameters::AMG::SmootherType;
+
+  ASSERT_EQ( "default", toString( EnumType::default_ ) );
+  ASSERT_EQ( "jacobi", toString( EnumType::jacobi ) );
+  ASSERT_EQ( "l1jacobi", toString( EnumType::l1jacobi ) );
+  ASSERT_EQ( "fgs", toString( EnumType::fgs ) );
+  ASSERT_EQ( "bgs", toString( EnumType::bgs ) );
+  ASSERT_EQ( "sgs", toString( EnumType::sgs ) );
+  ASSERT_EQ( "l1sgs", toString( EnumType::l1sgs ) );
+  ASSERT_EQ( "chebyshev", toString( EnumType::chebyshev ) );
+  ASSERT_EQ( "ilu0", toString( EnumType::ilu0 ) );
+  ASSERT_EQ( "ilut", toString( EnumType::ilut ) );
+  ASSERT_EQ( "ic0", toString( EnumType::ic0 ) );
+  ASSERT_EQ( "ict", toString( EnumType::ict ) );
+}
+
+
+TEST( LinearSolverParametersEnums, AMGNullSpaceType )
+{
+  using EnumType = LinearSolverParameters::AMG::NullSpaceType;
+
+  ASSERT_EQ( "constantModes", toString( EnumType::constantModes ) );
+  ASSERT_EQ( "rigidBodyModes", toString( EnumType::rigidBodyModes ) );
+}
+
 int main( int argc, char * * argv )
 {
   geosx::testing::LinearAlgebraTestScope scope( argc, argv );
