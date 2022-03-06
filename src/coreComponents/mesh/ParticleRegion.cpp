1040d05160dcb3d37c69c3f1347c1f4a65ea807d
Stefan Povolny <povolny1@llnl.gov>
2022-03-06 15:17:27 -0800

    added setters for ParticleBlock to properly modify particle center, velocity and volume during read-in. VTK output now partially works for particles with the centers being successfully written

--- src/coreComponents/mesh/ParticleRegion.cpp
+++ src/coreComponents/mesh/ParticleRegion.cpp
@@ -46,6 +46,27 @@ void ParticleRegion::generateMesh( Group & particleBlocks )
   }
 }
 
+// TODO This isn't great because really this calculation should only happen once, classic speed/memory trade-off. It just seems silly having multiple versions of the particle coordinates owned by the manager, regions and subregions.
+array2d< real64 > ParticleRegion::getParticleCoordinates() const
+{
+  int size = this->size();
+  array2d< real64 > coords(size, 3);
+  int index = 0;
+  this->forParticleSubRegions( [&]( auto & subRegion )
+  {
+    arrayView2d< real64 const > particleCoords = subRegion.getParticleCenter(); // I don't understand why the various consts are needed here, but it works so... TODO
+    for(int i=0; i<subRegion.size(); i++)
+    {
+      for(int j=0; j<3; j++)
+      {
+        coords[index][j] = particleCoords[i][j];
+      }
+      index++;
+    }
+  } );
+  return coords;
+}
+
 REGISTER_CATALOG_ENTRY( ObjectManagerBase, ParticleRegion, string const &, Group * const )
 
 } /* namespace geosx */
