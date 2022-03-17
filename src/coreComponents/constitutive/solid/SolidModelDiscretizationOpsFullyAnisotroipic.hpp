9e189ef8e407c3085ab3fe9daed5143c8fb10b8d
Nicola Castelletto <38361926+castelletto1@users.noreply.github.com>
2022-03-17 09:51:15 -0700

    Poromechanics kernels refactoring (#1600)
    * Moving stuff from single phase poromech kernel to constitutive update
    * Using FiniteElementBase inner products in SinglePhasePoromechanics kernel
    * Adding full BTDB
    * Fixing porosity in mixture density computation
    * Starting adding bilinear forms utilities (naming convention TBD)
    * Improving bilinear form utilities design (WIP)
    * Adding linear form utilities
    * Including BTDB in bilinear form utilities
    * Working with cuda
    * Adding small strain update for multiphase poromechanics in PorousSolid
    * Only conserved quantities and their derivatives in kernels
    * Exact linearization, no lagged derivatives
    * Removing code duplication in PorousSolid.hpp
    * Addressing most review comments
    * First pass at documenting Bilinear|LinearFormUtilities
    * Merging develop

--- src/coreComponents/constitutive/solid/SolidModelDiscretizationOpsFullyAnisotroipic.hpp
+++ src/coreComponents/constitutive/solid/SolidModelDiscretizationOpsFullyAnisotroipic.hpp
@@ -33,6 +33,13 @@ struct SolidModelDiscretizationOpsFullyAnisotroipic : public SolidModelDiscretiz
   template< int NUM_SUPPORT_POINTS,
             typename BASIS_GRADIENT >
   GEOSX_HOST_DEVICE
+  void BTDB( BASIS_GRADIENT const & gradN,
+             real64 const & detJxW,
+             real64 ( &elementStiffness )[NUM_SUPPORT_POINTS*3][NUM_SUPPORT_POINTS*3] );
+
+  template< int NUM_SUPPORT_POINTS,
+            typename BASIS_GRADIENT >
+  GEOSX_HOST_DEVICE
   void upperBTDB( BASIS_GRADIENT const & gradN,
                   real64 const & detJxW,
                   real64 ( &elementStiffness )[NUM_SUPPORT_POINTS*3][NUM_SUPPORT_POINTS*3] );
@@ -61,6 +68,65 @@ struct SolidModelDiscretizationOpsFullyAnisotroipic : public SolidModelDiscretiz
 
   real64 m_c[6][6];
 };
+
+
+template< int NUM_SUPPORT_POINTS,
+          typename BASIS_GRADIENT >
+GEOSX_HOST_DEVICE
+GEOSX_FORCE_INLINE
+void SolidModelDiscretizationOpsFullyAnisotroipic::BTDB( BASIS_GRADIENT const & gradN,
+                                                         real64 const & detJxW,
+                                                         real64 (& elementStiffness)[NUM_SUPPORT_POINTS *3][NUM_SUPPORT_POINTS *3] )
+{
+  for( int a=0; a<NUM_SUPPORT_POINTS; ++a )
+  {
+    for( int b=0; b<NUM_SUPPORT_POINTS; ++b )
+    {
+      real64 const gradNa_gradNb[3][3] =
+      { { gradN[a][0] * gradN[b][0], gradN[a][0] * gradN[b][1], gradN[a][0] * gradN[b][2] },
+        { gradN[a][1] * gradN[b][0], gradN[a][1] * gradN[b][1], gradN[a][1] * gradN[b][2] },
+        { gradN[a][2] * gradN[b][0], gradN[a][2] * gradN[b][1], gradN[a][2] * gradN[b][2] } };
+
+
+      elementStiffness[a*3+0][b*3+0] = elementStiffness[a*3+0][b*3+0] +
+                                       ( m_c[0][0] * gradNa_gradNb[0][0] + m_c[0][5] * gradNa_gradNb[0][1] + m_c[0][4] * gradNa_gradNb[0][2] +
+                                         m_c[5][0] * gradNa_gradNb[1][0] + m_c[5][5] * gradNa_gradNb[1][1] + m_c[5][4] * gradNa_gradNb[1][2] +
+                                         m_c[4][0] * gradNa_gradNb[2][0] + m_c[4][5] * gradNa_gradNb[2][1] + m_c[4][4] * gradNa_gradNb[2][2] ) * detJxW;
+      elementStiffness[a*3+0][b*3+1] = elementStiffness[a*3+0][b*3+1] +
+                                       (m_c[0][5] * gradNa_gradNb[0][0] + m_c[0][1] * gradNa_gradNb[0][1] + m_c[0][3] * gradNa_gradNb[0][2] +
+                                        m_c[5][5] * gradNa_gradNb[1][0] + m_c[5][1] * gradNa_gradNb[1][1] + m_c[5][3] * gradNa_gradNb[1][2] +
+                                        m_c[4][5] * gradNa_gradNb[2][0] + m_c[4][1] * gradNa_gradNb[2][1] + m_c[4][3] * gradNa_gradNb[2][2] ) * detJxW;
+      elementStiffness[a*3+0][b*3+2] = elementStiffness[a*3+0][b*3+2] +
+                                       (m_c[0][4] * gradNa_gradNb[0][0] + m_c[0][3] * gradNa_gradNb[0][1] + m_c[0][2] * gradNa_gradNb[0][2] +
+                                        m_c[5][4] * gradNa_gradNb[1][0] + m_c[5][3] * gradNa_gradNb[1][1] + m_c[5][2] * gradNa_gradNb[1][2] +
+                                        m_c[4][4] * gradNa_gradNb[2][0] + m_c[4][3] * gradNa_gradNb[2][1] + m_c[4][2] * gradNa_gradNb[2][2] ) * detJxW;
+      elementStiffness[a*3+1][b*3+0] = elementStiffness[a*3+1][b*3+0] +
+                                       (m_c[5][0] * gradNa_gradNb[0][0] + m_c[5][5] * gradNa_gradNb[0][1] + m_c[5][4] * gradNa_gradNb[0][2] +
+                                        m_c[1][0] * gradNa_gradNb[1][0] + m_c[1][5] * gradNa_gradNb[1][1] + m_c[1][4] * gradNa_gradNb[1][2] +
+                                        m_c[3][0] * gradNa_gradNb[2][0] + m_c[3][5] * gradNa_gradNb[2][1] + m_c[3][4] * gradNa_gradNb[2][2] ) * detJxW;
+      elementStiffness[a*3+1][b*3+1] = elementStiffness[a*3+1][b*3+1] +
+                                       (m_c[5][5] * gradNa_gradNb[0][0] + m_c[5][1] * gradNa_gradNb[0][1] + m_c[5][3] * gradNa_gradNb[0][2] +
+                                        m_c[1][5] * gradNa_gradNb[1][0] + m_c[1][1] * gradNa_gradNb[1][1] + m_c[1][3] * gradNa_gradNb[1][2] +
+                                        m_c[3][5] * gradNa_gradNb[2][0] + m_c[3][1] * gradNa_gradNb[2][1] + m_c[3][3] * gradNa_gradNb[2][2] ) * detJxW;
+      elementStiffness[a*3+1][b*3+2] = elementStiffness[a*3+1][b*3+2] +
+                                       (m_c[5][4] * gradNa_gradNb[0][0] + m_c[5][3] * gradNa_gradNb[0][1] + m_c[5][2] * gradNa_gradNb[0][2] +
+                                        m_c[1][4] * gradNa_gradNb[1][0] + m_c[1][3] * gradNa_gradNb[1][1] + m_c[1][2] * gradNa_gradNb[1][2] +
+                                        m_c[3][4] * gradNa_gradNb[2][0] + m_c[3][3] * gradNa_gradNb[2][1] + m_c[3][2] * gradNa_gradNb[2][2] ) * detJxW;
+      elementStiffness[a*3+2][b*3+0] = elementStiffness[a*3+2][b*3+0] +
+                                       (m_c[4][0] * gradNa_gradNb[0][0] + m_c[4][5] * gradNa_gradNb[0][1] + m_c[4][4] * gradNa_gradNb[0][2] +
+                                        m_c[3][0] * gradNa_gradNb[1][0] + m_c[3][5] * gradNa_gradNb[1][1] + m_c[3][4] * gradNa_gradNb[1][2] +
+                                        m_c[2][0] * gradNa_gradNb[2][0] + m_c[2][5] * gradNa_gradNb[2][1] + m_c[2][4] * gradNa_gradNb[2][2] ) * detJxW;
+      elementStiffness[a*3+2][b*3+1] = elementStiffness[a*3+2][b*3+1] +
+                                       (m_c[4][5] * gradNa_gradNb[0][0] + m_c[4][1] * gradNa_gradNb[0][1] + m_c[4][3] * gradNa_gradNb[0][2] +
+                                        m_c[3][5] * gradNa_gradNb[1][0] + m_c[3][1] * gradNa_gradNb[1][1] + m_c[3][3] * gradNa_gradNb[1][2] +
+                                        m_c[2][5] * gradNa_gradNb[2][0] + m_c[2][1] * gradNa_gradNb[2][1] + m_c[2][3] * gradNa_gradNb[2][2] ) * detJxW;
+      elementStiffness[a*3+2][b*3+2] = elementStiffness[a*3+2][b*3+2] +
+                                       (m_c[4][4] * gradNa_gradNb[0][0] + m_c[4][3] * gradNa_gradNb[0][1] + m_c[4][2] * gradNa_gradNb[0][2] +
+                                        m_c[3][4] * gradNa_gradNb[1][0] + m_c[3][3] * gradNa_gradNb[1][1] + m_c[3][2] * gradNa_gradNb[1][2] +
+                                        m_c[2][4] * gradNa_gradNb[2][0] + m_c[2][3] * gradNa_gradNb[2][1] + m_c[2][2] * gradNa_gradNb[2][2] ) * detJxW;
+    }
+  }
+}
 
 
 template< int NUM_SUPPORT_POINTS,
---
+++ src/coreComponents/finiteElement/PDEUtilities.hpp
@@ -0,0 +1,63 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ * Copyright (c) 2018-2020 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2020 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2018-2020 TotalEnergies
+ * Copyright (c) 2019-     GEOSX Contributors
+ * All rights reserved
+ *
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
+/**
+ * @file PDEUtilities.hpp
+ */
+
+#ifndef GEOSX_FINITEELEMENT_PDEUTILITIES_HPP_
+#define GEOSX_FINITEELEMENT_PDEUTILITIES_HPP_
+
+#include "codingUtilities/EnumStrings.hpp"
+
+namespace geosx
+{
+
+namespace PDEUtilities
+{
+
+/**
+ * @brief Differential operator type.
+ */
+enum class DifferentialOperator : integer
+{
+  Divergence,
+  Gradient,
+  Identity,
+  SymmetricGradient
+};
+
+/**
+ * @brief Function space type.
+ */
+enum class FunctionSpace : integer
+{
+  invalid,
+  P0,
+  H1,
+  H1vector
+};
+
+/// Declare strings associated with enumeration values.
+ENUM_STRINGS( FunctionSpace,
+              "invalid",
+              "P0",
+              "H1",
+              "H1vector" );
+
+} // namespace PDEUtilities
+
+} // namespace geosx
+
+#endif //GEOSX_FINITEELEMENT_PDEUTILITIES_HPP_
