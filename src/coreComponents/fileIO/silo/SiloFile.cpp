901003157558fc74811701405562050388b0c849
Nicola Castelletto <castelletto1@llnl.gov>
2022-04-04 00:09:43 -0700

    Removing extra 0s in SiloFile.cpp

--- src/coreComponents/fileIO/silo/SiloFile.cpp
+++ src/coreComponents/fileIO/silo/SiloFile.cpp
@@ -1332,8 +1332,8 @@ static std::vector< int > getSiloNodeOrdering( ElementType const elementType )
     case ElementType::Quadrilateral: return { 0, 1, 2, 3 }; // TODO check
     case ElementType::Polygon:       return { 0, 1, 2, 3, 4, 5, 6, 7, 8 }; // TODO
     case ElementType::Tetrahedron:   return { 0, 1, 3, 2 };
-    case ElementType::Pyramid:       return { 0, 2, 3, 1, 4, 0, 0, 0 };
-    case ElementType::Prism:         return { 1, 0, 2, 3, 5, 4, 0, 0 };
+    case ElementType::Pyramid:       return { 0, 2, 3, 1, 4 };
+    case ElementType::Prism:         return { 1, 0, 2, 3, 5, 4 };
     case ElementType::Hexahedron:    return { 0, 1, 3, 2, 4, 5, 7, 6 };
     case ElementType::Polyhedron:    return { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }; // TODO
   }
