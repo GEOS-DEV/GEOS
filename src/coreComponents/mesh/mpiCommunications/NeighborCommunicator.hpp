616d6bca3100b21d97df19937df4ed144d7547ed
Stefan Povolny <povolny1@llnl.gov>
2022-04-06 14:02:09 -0700

    started writing functions for particle repartitioning

--- src/coreComponents/mesh/mpiCommunications/NeighborCommunicator.hpp
+++ src/coreComponents/mesh/mpiCommunications/NeighborCommunicator.hpp
@@ -21,6 +21,7 @@
 #include "common/GEOS_RAJA_Interface.hpp"
 #include "dataRepository/ReferenceWrapper.hpp"
 #include "LvArray/src/limits.hpp"
+#include "mesh/DomainPartition.hpp"
 
 namespace geosx
 {
