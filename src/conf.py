84bd4de13ba6750bba96f9e1de1c9be52bbc8c4e
Thomas Gazzola <thomas.gazzola@total.com>
2022-05-03 13:14:06 -0700

    escape the sandbox?

--- src/conf.py
+++ src/conf.py
@@ -104,8 +104,8 @@ extensions = [
 ]
 
 mermaid_output_format = "svg"
+mermaid_params = ['-p', '/tmp/puppeteer-config.json']
 
-plot_html_show_source_link = True
 plot_html_show_formats = False
 
 # Add any paths that contain templates here, relative to this directory.
