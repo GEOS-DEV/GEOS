f540122b91a145cab7db4e31812f3ed5c58cde6a
jacquesn7 <jacquesn7@gmail.com>
2022-05-04 22:05:14 -0700

    update postt for Class09_pb3

--- inputFiles/compositionalMultiphaseFlow/benchmarks/Class09Pb3/postt_class09_3.py
+++ inputFiles/compositionalMultiphaseFlow/benchmarks/Class09Pb3/postt_class09_3.py
@@ -4,61 +4,78 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 
-print('In file ' + os.getcwd() )
-pv_tbl = pd.read_csv('./postt/class09_pv.csv')
-pv = (pv_tbl['vol']*pv_tbl['rockPorosity_referencePorosity']).to_numpy()
+# print('In file ' + os.getcwd() )
 
-ecl = pd.read_csv('./post/ECLSLB_hyst.csv') 
+def form_block_value(suffix):
+	#loading pore vols
+	pv_tbl = pd.read_csv('./postt/class09_pv'+suffix+'.csv')
+	pv = (pv_tbl['vol']*pv_tbl['poro']).to_numpy()
+
+	#read hdf5 timeHistory
+	fr = h5py.File(os.getcwd() + '/rhoHistory'+suffix+'.hdf5','r');
+	fs = h5py.File(os.getcwd() + '/satHistory'+suffix+'.hdf5','r');
+	fx = h5py.File(os.getcwd() + '/xcompHistory'+suffix+'.hdf5','r');
+
+	#get keys
+	kr = list( fr.keys() )
+	ks = list( fs.keys() )
+	kx = list( fx.keys() )
+
+	#get the data
+	dens = fr.get('fluid_phaseDensity')
+	sat = fs.get('phaseVolumeFraction')
+	xcomp = fx.get('fluid_phaseCompFraction')
+
+	#get some constant
+	ntime, ncells, nphase = dens.shape
+	_, _, ncomp = xcomp.shape
+	times = np.arange(1,ntime,1)
+	nt = len(times)
+	ti = 0
+	mc = np.zeros((ncomp,nt,ncells))
+
+	for time in times:
+		for comp in range(ncomp):
+			mc[comp,ti,:] = pv*sat[time,:,int((comp-1)/2)]*dens[time,:,int((comp-1)/2)]*xcomp[time,:,comp]
+		ti = ti + 1
+
+	return mc
+
+ecl = pd.read_csv('./docs/ECLSLB_hyst.csv') 
 ref_co2g_ti = ecl['ECLSLB_co2g'].to_numpy()
 ref_co2g  = ecl['Unnamed: 1'].to_numpy()
 ref_co2l_ti = ecl['ECLSLB_co2l'].to_numpy()
 ref_co2l = ecl['Unnamed: 3'].to_numpy()
+##
 
- 
-fr = h5py.File(os.getcwd() + '/rhoHistory.hdf5','r');
-fs = h5py.File(os.getcwd() + '/satHistory.hdf5','r');
-fx = h5py.File(os.getcwd() + '/xcompHistory.hdf5','r');
+#get interior values
+mi = form_block_value('')
+#get interior values
+mc = form_block_value('_bc')
 
-kr = list( fr.keys() )
-ks = list( fs.keys() )
-kx = list( fx.keys() )
-
-dens = fr.get('fluid_phaseDensity')
-sat = fs.get('phaseVolumeFraction')
-xcomp = fx.get('fluid_phaseCompFraction')
-
-ntime, ncells, nphase = dens.shape
-_, _, ncomp = xcomp.shape
-
-times = np.arange(1,ntime,1)
-nt = len(times)
-ti = 0
-mc = np.zeros((ncomp,nt,ncells))
-
-for time in times: 
-	for comp in range(ncomp):
-		mc[comp,ti,:] = pv*sat[time,:,int((comp-1)/2)]*dens[time,:,int((comp-1)/2)]*xcomp[time,:,comp]
-	ti = ti + 1
-	print(ti)
-
-
+##
 id_co2_g = 0
 id_co2_l = 2 
 
+#resize as time
+nt = mi.shape[1]
 sco2_l = np.zeros(nt)
 sco2_g = np.zeros(nt)
+
 icount = 0
+#some conversion
 year_in_sec = 86400*365
 dt = 1.0e8/year_in_sec
 
 for ti in range(mc.shape[1]):
-	sco2_l[ti] = np.sum(mc[id_co2_l,ti,:])
-	sco2_g[ti] = np.sum(mc[id_co2_g,ti,:])
+	sco2_l[ti] = np.sum(mi[id_co2_l,ti,:])
+	sco2_l[ti] += np.sum(mc[id_co2_l,ti,:])
+	sco2_g[ti] = np.sum(mi[id_co2_g,ti,:])
+	sco2_g[ti] += np.sum(mc[id_co2_g,ti,:])
 
 plt.plot(np.arange(0,nt)*dt+dt, sco2_l)
 plt.plot(np.arange(0,nt)*dt+dt, sco2_g)
-#plt.plot(np.arange(0,nt)*dt, sco2_l + sco2_g)
-
+#
 plt.plot(ref_co2g_ti, ref_co2g*1e9,'k')
 plt.plot(ref_co2l_ti, ref_co2l*1e9,'--k')
 
