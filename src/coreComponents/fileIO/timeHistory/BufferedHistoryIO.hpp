b8ba54e8e1eb6c1034aa32a3c0e2d271ec9bce47
TotoGaz <49004943+TotoGaz@users.noreply.github.com>
2022-05-13 18:49:24 -0700

    Time history reorganization (#1873)
    - Created an abstract base class HistoryCollection with minimal interface
    - Created a HistoryCollectionBase to receive the implementations extracted from HistoryCollection.
      Let's see if we want to send it to unique implementation PackCollection.
    - Modified the buffer calls in HistoryCollection to get clearer responsibilities
    - Modified BufferedHistoryIO to create minimal interface
    - Refactor PackCollection::updateSetsIndices to limit side effects and improve readability (and correct a bug)
    - Change TimeHistoryOutput::m_recordCount to TimeHistoryOutput::m_hasRecordStarted which is treated as a boolean.

--- src/coreComponents/fileIO/timeHistory/BufferedHistoryIO.hpp
+++ src/coreComponents/fileIO/timeHistory/BufferedHistoryIO.hpp
@@ -12,18 +12,13 @@
  * ------------------------------------------------------------------------------------------------------------
  */
 
-/**
- * @file HistoryIO.hpp
- */
+#ifndef GEOSX_FILEIO_TIMEHISTORY_BUFFEREDHISTORYIO_HPP_
+#define GEOSX_FILEIO_TIMEHISTORY_BUFFEREDHISTORYIO_HPP_
 
-#ifndef GEOSX_FILEIO_TIMEHISTORY_HISTORYIO_HPP_
-#define GEOSX_FILEIO_TIMEHISTORY_HISTORYIO_HPP_
-
-#include "dataRepository/Group.hpp"
+#include "common/DataTypes.hpp"
 
 namespace geosx
 {
-class DataSpec;
 
 /**
  * @class BufferedHistoryIO
@@ -33,48 +28,32 @@ class BufferedHistoryIO
 {
 public:
 
-  /// Constructor
-  BufferedHistoryIO():
-    m_bufferedCount( 0 ),
-    m_bufferHead( nullptr ),
-    m_dataBuffer( 0 )
-  {}
-
   /// Destructor
   virtual ~BufferedHistoryIO() {}
 
   /**
    * @brief Get the head of the internal history storage buffer.
    * @return The head of the internal history storage buffer to be written to.
-   * @note Depends on the virtual function resizeBuffer() being implemented correctly in
-   *        an inheriting class.
    */
-  buffer_unit_type * getBufferHead( )
-  {
-    resizeBuffer( );
-    m_bufferedCount++;
-    buffer_unit_type * const currentBufferHead = m_bufferHead;
-    m_bufferHead += getRowBytes( );
-    return currentBufferHead;
-  }
+  virtual buffer_unit_type * getBufferHead() = 0;
 
   /**
    * @brief Perform and intialization needed for time-history output.
-   * @param existsOkay Whether it is acceptable for the intended output target to already exist ( false on start from scratch, true on
-   *                    restart ).
+   * @param[in] existsOkay Whether it is acceptable for the intended output target to already exist
+   *                       ( false on start from scratch, true on restart ).
    */
   virtual void init( bool existsOkay ) = 0;
 
   /**
    * @brief Write the buffered history data to the output target.
    */
-  virtual void write( ) = 0;
+  virtual void write() = 0;
 
   /**
    * @brief Ensure the repressentation of the data in the output target is dense and terse.
    * @note Typically the file will be oversized to receive data writes without constant resizing.
    */
-  virtual void compressInFile( ) = 0;
+  virtual void compressInFile() = 0;
 
   /**
    * @brief Update the number of items being stored for IO in this object.
@@ -83,38 +62,16 @@ public:
   virtual void updateCollectingCount( localIndex count ) = 0;
 
   /**
-   * @brief Get the size in bytes the buffer is currently set to hold per collection operation.
-   * @return The size in bytes.
-   */
-  virtual size_t getRowBytes( ) = 0;
-
-  /**
    * @brief Query the number of history states currently stored in the internal buffer.
    * @return The number of discrete time history records buffered to be written.
    * @note Since the size of each discrete time history can change, this should not be used
-   *        to calculate size, but is useful to check for consistency between collectors
-   *        that should be operating at the same cadence (ie the time collector and the data
-   *        collector).
+   *       to calculate size, but is useful to check for consistency between collectors
+   *       that should be operating at the same cadence (ie the time collector and the data collector).
+   * @deprecated Also, note that this member function is related to restarting HDF5 files.
+   *             Which means that getting this information popping through the abstract interface
+   *             is a shortcut that should eventually be fixed by keeping this information in the HDF5 buffer.
    */
-  localIndex getBufferedCount( ) { return m_bufferedCount; }
-
-protected:
-  /// @brief Resize the buffer to accomodate additional history collection.
-  virtual void resizeBuffer( ) = 0;
-
-  /// @brief Empty the history collection buffer
-  void emptyBuffer( )
-  {
-    m_bufferedCount = 0;
-    m_bufferHead = &m_dataBuffer[0];
-  }
-
-  /// The current number of records in the buffer
-  localIndex m_bufferedCount;
-  /// The write head of the buffer
-  buffer_unit_type * m_bufferHead;
-  /// The data buffer containing the history info
-  buffer_type m_dataBuffer;
+  virtual localIndex getBufferedCount() = 0;
 };
 
 }
