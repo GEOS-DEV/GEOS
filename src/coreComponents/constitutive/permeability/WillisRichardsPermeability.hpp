58302f6607fbcc4b75fdd5283aa69b3eafd6f38a
Jian HUANG <jian.huang@totalenergies.com>
2022-05-23 10:36:23 -0500

    add pressure to traction

--- src/coreComponents/constitutive/permeability/WillisRichardsPermeability.hpp
+++ src/coreComponents/constitutive/permeability/WillisRichardsPermeability.hpp
@@ -47,7 +47,8 @@ public:
   {}
 
   GEOSX_HOST_DEVICE
-  void compute( real64 const ( &dispJump )[3],
+  void compute( real64 const & pressure,
+                real64 const ( &dispJump )[3],
                 real64 const ( &traction )[3],
                 arraySlice1d< real64 > const & permeability,
                 arraySlice2d< real64 > const & dPerm_dDispJump,
@@ -62,9 +63,10 @@ public:
                                                        real64 const ( &dispJump )[3],
                                                        real64 const ( &traction )[3] ) const override final
   {
-    GEOSX_UNUSED_VAR( q, oldHydraulicAperture, newHydraulicAperture, pressure );
+    GEOSX_UNUSED_VAR( q, oldHydraulicAperture, newHydraulicAperture );
 
-    compute( dispJump,
+    compute( pressure,
+             dispJump,
              traction,
              m_permeability[k][0],
              m_dPerm_dDispJump[k][0],
@@ -154,7 +156,8 @@ private:
 
 GEOSX_HOST_DEVICE
 GEOSX_FORCE_INLINE
-void WillisRichardsPermeabilityUpdate::compute( real64 const ( &dispJump )[3],
+void WillisRichardsPermeabilityUpdate::compute( real64 const & pressure,
+                                                real64 const ( &dispJump )[3],
                                                 real64 const ( &traction )[3],
                                                 arraySlice1d< real64 > const & permeability,
                                                 arraySlice2d< real64 > const & dPerm_dDispJump,
@@ -162,7 +165,7 @@ void WillisRichardsPermeabilityUpdate::compute( real64 const ( &dispJump )[3],
 {
   real64 const shearMag = std::sqrt( dispJump[1]*dispJump[1] + dispJump[2]*dispJump[2] );
 
-  real64 const effNormalStress = std::abs( traction[0] );
+  real64 const effNormalStress = - (traction[0] - pressure);
 
   real64 const aperture = ( m_maxFracAperture + shearMag * m_dilationCoefficient ) / ( 1.0 + 9.0 * effNormalStress/m_refClosureStress );
 
