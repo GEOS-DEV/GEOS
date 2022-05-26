bf211f00edb1185ff65e2d3476f33ce7bf55844e
Christopher Sherman <sherman27@llnl.gov>
2022-05-26 15:10:52 -0700

    Adding initial framework for unit testing of string parsing

--- src/coreComponents/unitTests/xmlTests/testXML.cpp
+++ src/coreComponents/unitTests/xmlTests/testXML.cpp
@@ -21,6 +21,8 @@
 #include <gtest/gtest.h>
 #include <conduit.hpp>
 
+using namespace geosx;
+
 TEST( testXML, testXMLString )
 {
   char const *  xmlInput =
@@ -88,6 +90,51 @@ TEST( testXML, testXMLStringExpectedFail )
   EXPECT_THROW( geosx::getGlobalState().getProblemManager().parseInputString( xmlInput ), geosx::InputError );
 }
 
+
+template< typename T >
+void checkScalarParsing(string attributeString, T expectedValue)
+{
+  GEOSX_LOG_RANK_0( "Parsing string: " << attributeString );
+  T parsedValue;
+  xmlWrapper::stringToInputVariable< T >(parsedValue, attributeString);
+  ASSERT_NEAR( static_cast< real64 >(expectedValue), static_cast< real64 >(parsedValue), 1e-10 );
+}
+
+
+class real64AttributeTestFixture :public ::testing::TestWithParam<std::tuple<string, real64, bool>>
+{
+protected:
+    string attributeString;
+    real64 expectedValue;
+    bool failureFlag;
+};
+
+
+TEST_P(real64AttributeTestFixture, testParsing)
+{
+    auto testParams = GetParam();
+    attributeString = std::get<0>(testParams);
+    expectedValue = std::get<1>(testParams);
+    failureFlag = std::get<2>(testParams);
+
+    if (failureFlag)
+    {
+      EXPECT_THROW( checkScalarParsing< real64 >(attributeString, expectedValue), InputError );
+    }
+    else
+    {
+      checkScalarParsing< real64 >(attributeString, expectedValue);
+    }
+}
+
+INSTANTIATE_TEST_CASE_P(
+        real64AttributeTests,
+        real64AttributeTestFixture,
+        ::testing::Values(std::make_tuple("1", 1,  false),
+                          std::make_tuple("1.2", 1.2, false),
+                          std::make_tuple("1.234e5", 1.234e5, false),
+                          std::make_tuple("badnumber", 0, true),
+                          std::make_tuple("1badnumber234", 0, true)));
 
 int main( int argc, char * * argv )
 {
