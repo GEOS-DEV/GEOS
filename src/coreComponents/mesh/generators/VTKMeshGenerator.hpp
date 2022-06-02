5163882f92bd5ae96940d00d276a751b053f636e
Christopher Sherman <sherman27@llnl.gov>
2022-06-02 12:58:33 -0700

    Adding a prototype method to convert, import nodesets in vtk files

--- src/coreComponents/mesh/generators/VTKMeshGenerator.hpp
+++ src/coreComponents/mesh/generators/VTKMeshGenerator.hpp
@@ -116,6 +116,8 @@ private:
 
   real64 writeNodes( CellBlockManager & cellBlockManager ) const;
 
+  void importNodesets( vtkUnstructuredGrid & mesh, CellBlockManager & cellBlockManager ) const;
+
   void writeCells( CellBlockManager & cellBlockManager ) const;
 
   void writeSurfaces( CellBlockManager & cellBlockManager ) const;
@@ -132,6 +134,7 @@ private:
   struct viewKeyStruct
   {
     constexpr static char const * regionAttributeString() { return "regionAttribute"; }
+    constexpr static char const * nodesetNamesString() { return "nodesetNames"; }
     constexpr static char const * partitionRefinementString() { return "partitionRefinement"; }
   };
   /// @endcond
@@ -144,6 +147,9 @@ private:
 
   /// Name of VTK dataset attribute used to mark regions
   string m_attributeName;
+
+  /// Names of VTK nodesets to import
+  string_array m_nodesetNames;
 
   /// Number of graph partitioning refinement iterations
   integer m_partitionRefinement = 0;
