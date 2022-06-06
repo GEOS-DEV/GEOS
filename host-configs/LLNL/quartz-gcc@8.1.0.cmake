95a7866ad8ccb7181cf0274f8db90e3d57197309
Christopher Sherman <sherman27@llnl.gov>
2022-06-06 16:21:51 -0700

    Enabling yapf formatting of python files

--- host-configs/LLNL/quartz-gcc@8.1.0.cmake
+++ host-configs/LLNL/quartz-gcc@8.1.0.cmake
@@ -24,4 +24,7 @@ set(MKL_LIBRARIES ${MKL_ROOT}/lib/libmkl_intel_lp64.so
 set(ENABLE_PYGEOSX ON CACHE BOOL "")
 set(Python3_EXECUTABLE /usr/gapps/GEOSX/thirdPartyLibs/python/linux-rhel7-x86_64-gcc@8.1.0/python@3.8.5/bin/python3 CACHE PATH "")
 
+# Enable yapf formatting
+set(YAPF_EXECUTABLE /g/g17/sherman/Python/virtual/smart/bin/yapf CACHE PATH "" FORCE)
+
 include(${CMAKE_CURRENT_LIST_DIR}/quartz-base.cmake)
