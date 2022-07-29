08b6b9d4a16d9c048b3ebb97f370727eca4b2f81
Randolph R. Settgast <settgast1@llnl.gov>
2022-07-29 09:51:46 -0700

    add some compilation flags to CMAKE_CUDA_FLAGS_DEBUG in GeosxOptions.cmake

--- src/cmake/GeosxOptions.cmake
+++ src/cmake/GeosxOptions.cmake
@@ -122,6 +122,11 @@ blt_append_custom_compiler_flag( FLAGS_VAR GEOSX_NINJA_FLAGS
                                  CLANG   "-fcolor-diagnostics"
                                )
 
+blt_append_custom_compiler_flag( FLAGS_VAR CMAKE_CUDA_FLAGS_DEBUG
+                                 DEFAULT "-g -G -O0 -Xcompiler -O0"
+                               )
+
+
 if( ${CMAKE_MAKE_PROGRAM} STREQUAL "ninja" OR ${CMAKE_MAKE_PROGRAM} MATCHES ".*/ninja$" )
   set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${GEOSX_NINJA_FLAGS}" )
 endif()
