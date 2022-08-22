820a97622d256eba93a7c05c00f08fad79f7ddfc
Randolph Settgast <settgast1@llnl.gov>
2022-08-22 12:09:39 -0700

    Apply suggestions from code review

--- src/cmake/GeosxOptions.cmake
+++ src/cmake/GeosxOptions.cmake
@@ -75,7 +75,7 @@ endif()
 
 ### BUILD & BLT SETUP ###
 
-option( GEOSX_BUILD_OBJ_LIBS "Builds coreComponent modules as object libraries" ON )
+option( GEOSX_BUILD_OBJ_LIBS "Builds coreComponent modules as object libraries" OFF )
 
 option( GEOSX_BUILD_SHARED_LIBS "Builds geosx_core as a shared library " OFF )
 
