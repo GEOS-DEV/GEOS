3e047b114b3221155bd157df84024990ce182c39
Randolph Settgast <rrsettgast@gmail.com>
2022-09-04 11:34:47 -0700

    insert a preprocessor guard to avoid pre-calculation of shape function derivatives

--- src/coreComponents/mainInterface/ProblemManager.cpp
+++ src/coreComponents/mainInterface/ProblemManager.cpp
@@ -851,7 +851,13 @@ map< std::tuple< string, string, string, string >, localIndex > ProblemManager::
 
                   localIndex const numQuadraturePoints = FE_TYPE::numQuadraturePoints;
 
-                  feDiscretization->calculateShapeFunctionGradients< SUBREGION_TYPE, FE_TYPE >( X, &subRegion, meshData, finiteElement );
+#if !defined(GEOSX_USE_CUDA)
+                  feDiscretization->calculateShapeFunctionGradients< SUBREGION_TYPE, 
+                                                                     FE_TYPE >( X, 
+                                                                                &subRegion,
+                                                                                meshData,
+                                                                                finiteElement );
+#endif
 
                   localIndex & numQuadraturePointsInList = regionQuadrature[ std::make_tuple( meshBodyName,
                                                                                               meshLevel.getName(),
