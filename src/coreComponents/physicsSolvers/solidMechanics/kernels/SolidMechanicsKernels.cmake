7f808628ebf6bcdf17a2a64bddb4bfc5770a2164
Randolph R. Settgast <settgast1@llnl.gov>
2022-09-27 16:32:56 -0700

    change to more of a top down cmake  driven generation

---
+++ src/coreComponents/physicsSolvers/solidMechanics/kernels/SolidMechanicsKernels.cmake
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+set( ExplicitSmallStrainPolicy "geosx::parallelDevicePolicy<32>" )
+set( ExplicitFiniteStrainPolicy "geosx::parallelDevicePolicy<32>" )
+set( ImplicitSmallStrainQuasiStaticPolicy "geosx::parallelDevicePolicy<32>" )
+
+
+configure_file( ${CMAKE_SOURCE_DIR}/coreComponents/physicsSolvers/solidMechanics/kernels/policies.hpp.in
+                ${CMAKE_BINARY_DIR}/generatedSrc/coreComponents/physicsSolvers/solidMechanics/kernels/policies.hpp )
+
+
+set( kernelNames ExplicitSmallStrain ExplicitFiniteStrain ImplicitSmallStrainQuasiStatic )
+set( subregionList CellElementSubRegion )
+set( solidBaseDispatch "DamageSpectral<ElasticIsotropic>;DamageVolDev<ElasticIsotropic>;Damage<ElasticIsotropic>;DruckerPragerExtended;ModifiedCamClay;DelftEgg;DruckerPrager;ElasticIsotropic;ElasticTransverseIsotropic;ElasticIsotropicPressureDependent;ElasticOrthotropic")
+set( finiteElementDispatch H1_Hexahedron_Lagrange1_GaussLegendre2
+                           H1_Wedge_Lagrange1_Gauss6
+                           H1_Tetrahedron_Lagrange1_Gauss1
+                           H1_Pyramid_Lagrange1_Gauss5
+                           Q3_Hexahedron_Lagrange_GaussLobatto
+                           H1_Tetrahedron_VEM_Gauss1
+                           H1_Wedge_VEM_Gauss1
+                           H1_Hexahedron_VEM_Gauss1
+                           H1_Prism5_VEM_Gauss1
+                           H1_Prism6_VEM_Gauss1
+                           H1_Prism7_VEM_Gauss1
+                           H1_Prism8_VEM_Gauss1
+                           H1_Prism9_VEM_Gauss1
+                           H1_Prism10_VEM_Gauss1
+                           H1_Prism11_VEM_Gauss1 )
+
+  foreach( KERNELNAME ${kernelNames} )
+    foreach( SUBREGION_TYPE  ${subregionList} )
+      foreach( CONSTITUTIVE_TYPE ${solidBaseDispatch} )
+        foreach( FE_TYPE ${finiteElementDispatch} )
+
+        set( filename "${CMAKE_BINARY_DIR}/generatedSrc/coreComponents/physicsSolvers/solidMechanics/kernels/${KERNELNAME}_${SUBREGION_TYPE}_${CONSTITUTIVE_TYPE}_${FE_TYPE}.cpp" )
+        configure_file( ${CMAKE_SOURCE_DIR}/coreComponents/physicsSolvers/solidMechanics/kernels/SolidMechanicsKernels.cpp.template
+                        ${filename} )
+
+          list( APPEND physicsSolvers_sources ${filename} )
+        endforeach()
+      endforeach()
+    endforeach()
+  endforeach()
