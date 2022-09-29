a88c342baa962e85f8d076aa0a523301de7b02bf
Thomas Gazzola <thomas.gazzola@total.com>
2022-09-29 12:19:34 -0700

    documentation

--- scripts/meshDoctor/checks/collocated_nodes.py
+++ scripts/meshDoctor/checks/collocated_nodes.py
@@ -35,9 +35,14 @@ def __check(mesh, options: Options) -> Result:
     for i in range(points.GetNumberOfPoints()):
         is_inserted = locator.InsertUniquePoint(points.GetPoint(i), point_id)
         if not is_inserted:
+            # If it's not inserted, `point_id` contains the node that was already at that location.
+            # But in that case, `point_id` is the new numbering in the destination points array.
+            # It's more useful for the user to get the old index in the original mesh, so he can look for it in his data.
             logging.debug(f"Point {i} at {points.GetPoint(i)} has been rejected, point {filtered_to_original[point_id.get()]} is already inserted.")
             rejected_points[point_id.get()].append(i)
         else:
+            # If it's inserted, `point_id` contains the new index in the destination array.
+            # We store this information to be able to connect the source and destination arrays.
             # original_to_filtered[i] = point_id.get()
             filtered_to_original[point_id.get()] = i
 
