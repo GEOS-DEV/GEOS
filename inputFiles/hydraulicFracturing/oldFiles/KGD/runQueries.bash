748bfa29ce05c1336f9f8b0adc958ff1f99a586f
Matteo Cusini <49037133+CusiniM@users.noreply.github.com>
2022-09-30 12:18:23 -0700

    Reorganize fractures input files (#2094)
    * created dedicated fractures folders for fractures xmls.
    
    * remove old files.
    
    * renaming
    
    * update integratedTests

--- inputFiles/hydraulicFracturing/oldFiles/KGD/runQueries.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#visit -cli -nowin -l msub/srun -nn 1 -np 8 -p pbatch -b cbronze -t 10:00    -s kgdQueries.py $PWD/siloFiles "zeroToughness_* database" zeroToughness
-visit -cli -nowin -l msub/srun -nn 1 -np 16 -p pdebug                       -s kgdQueries.py $PWD/siloFiles "zeroToughness_* database" zeroToughness
-visit -cli -nowin -l msub/srun -nn 1 -np 16 -p pdebug                       -s kgdQueries.py $PWD/siloFiles "zeroViscosity_* database" zeroViscosity
