817d90d883e353de93b423a31825a709bc480eff
frankfeifan <frankfei0929@gmail.com>
2022-10-03 10:01:35 -0700

    modified Damage model for pressurized fracture

--- src/coreComponents/constitutive/solid/Damage.cpp
+++ src/coreComponents/constitutive/solid/Damage.cpp
@@ -18,7 +18,6 @@
  */
 
 #include "Damage.hpp"
-#include "ElasticIsotropic.hpp"
 
 namespace geosx
 {
@@ -30,21 +29,50 @@ namespace constitutive
 template< typename BASE >
 Damage< BASE >::Damage( string const & name, Group * const parent ):
   BASE( name, parent ),
-  m_damage(),
+  m_newDamage(),
+  m_oldDamage(),
+  m_damageGrad(),
   m_strainEnergyDensity(),
+  m_volStrain(),
+  m_extDrivingForce(),
   m_lengthScale(),
   m_criticalFractureEnergy(),
-  m_criticalStrainEnergy()
+  m_criticalStrainEnergy(),
+  m_extDrivingForceSwitch(),
+  m_tensileStrength(),
+  m_compressStrength(),
+  m_deltaCoefficient(),
+  m_biotCoefficient()
 {
-  this->registerWrapper( viewKeyStruct::damageString(), &m_damage ).
+  this->registerWrapper( viewKeyStruct::newDamageString(), &m_newDamage ).
     setApplyDefaultValue( 0.0 ).
     setPlotLevel( PlotLevel::LEVEL_0 ).
-    setDescription( "Material Damage Variable" );
+    setDescription( "Material New Damage Variable" );
+
+  this->registerWrapper( viewKeyStruct::oldDamageString(), &m_oldDamage ).
+    setApplyDefaultValue( 0.0 ).
+    setPlotLevel( PlotLevel::LEVEL_0 ).
+    setDescription( "Material Old Damage Variable" );
+
+  this->registerWrapper( viewKeyStruct::damageGradString(), &m_damageGrad ).
+    setApplyDefaultValue( 0.0 ).
+    setPlotLevel( PlotLevel::LEVEL_0 ).
+    setDescription( "Material Damage Gradient" );
 
   this->registerWrapper( viewKeyStruct::strainEnergyDensityString(), &m_strainEnergyDensity ).
     setApplyDefaultValue( 0.0 ).
     setPlotLevel( PlotLevel::LEVEL_0 ).
     setDescription( "Strain Energy Density" );
+
+  this->registerWrapper( viewKeyStruct::volumetricStrainString(), &m_volStrain ).
+    setApplyDefaultValue( 0.0 ).
+    setPlotLevel( PlotLevel::LEVEL_0 ).
+    setDescription( "Volumetric strain" );
+
+  this->registerWrapper( viewKeyStruct::extDrivingForceString(), &m_extDrivingForce ).
+    setApplyDefaultValue( 0.0 ).
+    setPlotLevel( PlotLevel::LEVEL_0 ).
+    setDescription( "External Driving Force" );
 
   this->registerWrapper( viewKeyStruct::lengthScaleString(), &m_lengthScale ).
     setInputFlag( InputFlags::REQUIRED ).
@@ -57,6 +85,30 @@ Damage< BASE >::Damage( string const & name, Group * const parent ):
   this->registerWrapper( viewKeyStruct::criticalStrainEnergyString(), &m_criticalStrainEnergy ).
     setInputFlag( InputFlags::REQUIRED ).
     setDescription( "Critical stress in a 1d tension test" );
+
+  this->registerWrapper( viewKeyStruct::extDrivingForceSwitchString(), &m_extDrivingForceSwitch ).
+    setInputFlag( InputFlags::REQUIRED ).
+    setDescription( "Whether to have external driving force. Can be True or False" );
+
+  this->registerWrapper( viewKeyStruct::tensileStrengthString(), &m_tensileStrength ).
+    setApplyDefaultValue( 0.0 ).
+    setInputFlag( InputFlags::OPTIONAL ).
+    setDescription( "Tensile strength from the uniaxial tension test" );
+
+  this->registerWrapper( viewKeyStruct::compressStrengthString(), &m_compressStrength ).
+    setApplyDefaultValue( 0.0 ).
+    setInputFlag( InputFlags::OPTIONAL ).
+    setDescription( "Compressive strength from the uniaxial compression test" );
+
+  this->registerWrapper( viewKeyStruct::deltaCoefficientString(), &m_deltaCoefficient ).
+    setApplyDefaultValue( 0.0 ).
+    setInputFlag( InputFlags::OPTIONAL ).
+    setDescription( "Coefficient in the calculation of the external driving force" );
+
+  this->registerWrapper( viewKeyStruct::biotCoefficientString(), &m_biotCoefficient ).
+    setApplyDefaultValue( 0.0 ).
+    setInputFlag( InputFlags::OPTIONAL ).
+    setDescription( "Biot coefficient" );
 }
 
 
@@ -64,15 +116,45 @@ template< typename BASE >
 void Damage< BASE >::postProcessInput()
 {
   BASE::postProcessInput();
+
+  if( m_extDrivingForceSwitch != "True" and m_extDrivingForceSwitch != "False" )
+  {
+    GEOSX_ERROR( "invalid external driving force option - must be True or False" );
+  }
 }
 
 template< typename BASE >
 void Damage< BASE >::allocateConstitutiveData( dataRepository::Group & parent,
                                                localIndex const numConstitutivePointsPerParentIndex )
 {
-  m_damage.resize( 0, numConstitutivePointsPerParentIndex );
+  m_newDamage.resize( 0, numConstitutivePointsPerParentIndex );
+  m_oldDamage.resize( 0, numConstitutivePointsPerParentIndex );
+  m_damageGrad.resize( 0, numConstitutivePointsPerParentIndex, 3 );
   m_strainEnergyDensity.resize( 0, numConstitutivePointsPerParentIndex );
+  m_volStrain.resize( 0, numConstitutivePointsPerParentIndex );
+  m_extDrivingForce.resize( 0, numConstitutivePointsPerParentIndex );
+  m_biotCoefficient.resize( parent.size() );
   BASE::allocateConstitutiveData( parent, numConstitutivePointsPerParentIndex );
+}
+
+template< typename BASE >
+void Damage< BASE >::saveConvergedState() const
+{
+  SolidBase::saveConvergedState(); // TODO: not ideal, as we have separate loops for base and derived data
+
+  localIndex const numE = SolidBase::numElem();
+  localIndex const numQ = SolidBase::numQuad();
+
+  arrayView2d< real64 const > newDamage = m_newDamage;
+  arrayView2d< real64 > oldDamage = m_oldDamage;
+
+  forAll< parallelDevicePolicy<> >( numE, [=] GEOSX_HOST_DEVICE ( localIndex const k )
+  {
+    for( localIndex q = 0; q < numQ; ++q )
+    {
+      oldDamage( k, q ) = newDamage( k, q );
+    }
+  } );
 }
 
 typedef Damage< ElasticIsotropic > DamageElasticIsotropic;
