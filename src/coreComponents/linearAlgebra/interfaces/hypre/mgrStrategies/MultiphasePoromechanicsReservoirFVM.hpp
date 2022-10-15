efb6aa43a606db18e573dfccd5400bc53eefabec
Francois Hamon <francois.hamon@totalenergies.com>
2022-10-14 23:33:16 -0700

    Added MGR recipes for poromechanics with wells + ILU global smoother (#2119)
    * added MGR recipes for poromechanics with wells
    
    * addressed Nicola's comments
    
    * final minor modification

--- src/coreComponents/linearAlgebra/interfaces/hypre/mgrStrategies/MultiphasePoromechanicsReservoirFVM.hpp
+++ src/coreComponents/linearAlgebra/interfaces/hypre/mgrStrategies/MultiphasePoromechanicsReservoirFVM.hpp
@@ -16,8 +16,8 @@
  * @file MultiphasePoromechanics.hpp
  */
 
-#ifndef GEOSX_LINEARALGEBRA_INTERFACES_HYPREMGRMULTIPHASEPOROMECHANICS_HPP_
-#define GEOSX_LINEARALGEBRA_INTERFACES_HYPREMGRMULTIPHASEPOROMECHANICS_HPP_
+#ifndef GEOSX_LINEARALGEBRA_INTERFACES_HYPREMGRMULTIPHASEPOROMECHANICSRESERVOIRFVM_HPP_
+#define GEOSX_LINEARALGEBRA_INTERFACES_HYPREMGRMULTIPHASEPOROMECHANICSRESERVOIRFVM_HPP_
 
 #include "linearAlgebra/interfaces/hypre/HypreMGR.hpp"
 
@@ -31,43 +31,54 @@ namespace mgr
 {
 
 /**
- * @brief MultiphasePoromechanics strategy.
+ * @brief MultiphasePoromechanicsReservoirFVM strategy.
  *
  * Labels description stored in point_marker_array
- *   - dofLabel: 0             = nodal displacement, x-component
- *   - dofLabel: 1             = nodal displacement, y-component
- *   - dofLabel: 2             = nodal displacement, z-component
- *   - dofLabel: 3             = pressure
- *   - dofLabel: 4             = density
- *             ...             = densities
- *   - dofLabel: numLabels - 1 = density
+ *   - dofLabel: 0                = nodal displacement, x-component
+ *   - dofLabel: 1                = nodal displacement, y-component
+ *   - dofLabel: 2                = nodal displacement, z-component
+ *   - dofLabel: 3                = pressure
+ *   - dofLabel: 4                = density
+ *             ...                = densities
+ *   - dofLabel: numResLabels + 2 = density
+ *   - dofLabel: numResLabels + 3 = well pressure
+ *   - dofLabel: numResLabels + 4 = well density
+ *             ...                = well densities
+ *   - dofLabel: numResLabels + numWellLabels + 1 = well density
+ *   - dofLabel: numResLabels + numWellLabels + 2 = well rate
  *
- * 3-level MGR reduction strategy based on CompositionalMultiphaseFVM
+ * 4-level MGR reduction strategy
  *   - 1st level: eliminate displacements (0,1,2)
- *   - 2nd level: eliminate the reservoir density associated with the volume constraint (numLabels - 1)
- *   - 3nd level: eliminate the other reservoir densities
+ *   - 2nd level: eliminate the well block
+ *   - 3nd level: eliminate the reservoir density associated with the volume constraint
+ *   - 4nd level: eliminate the other reservoir densities
  *   - The coarse grid is solved with BoomerAMG.
  *
  */
-class MultiphasePoromechanics : public MGRStrategyBase< 3 >
+class MultiphasePoromechanicsReservoirFVM : public MGRStrategyBase< 4 >
 {
 public:
   /**
    * @brief Constructor.
    * @param numComponentsPerField array with number of components for each field
    */
-  explicit MultiphasePoromechanics( arrayView1d< int const > const & numComponentsPerField )
-    : MGRStrategyBase( LvArray::integerConversion< HYPRE_Int >( numComponentsPerField[0] + numComponentsPerField[1] ) )
+  explicit MultiphasePoromechanicsReservoirFVM( arrayView1d< int const > const & numComponentsPerField )
+    : MGRStrategyBase( LvArray::integerConversion< HYPRE_Int >( numComponentsPerField[0] + numComponentsPerField[1] + numComponentsPerField[2] ) )
   {
     // Level 0: eliminate displacement degrees of freedom
     m_labels[0].resize( m_numBlocks - 3 );
     std::iota( m_labels[0].begin(), m_labels[0].end(), 3 );
-    // Level 1: eliminate last density which corresponds to the volume constraint equation
-    m_labels[1].resize( m_numBlocks - 4 );
+
+    HYPRE_Int const numResLabels = LvArray::integerConversion< HYPRE_Int >( numComponentsPerField[1] );
+
+    // Level 1: eliminate the well block
+    m_labels[1].resize( numResLabels );
     std::iota( m_labels[1].begin(), m_labels[1].end(), 3 );
-    // Level 2: eliminate the remaining reservoir densities
-    m_labels[2].resize( m_numBlocks - 5 );
+    // Level 2: eliminate reservoir last density which corresponds to the volume constraint equation
+    m_labels[2].resize( numResLabels - 1 );
     std::iota( m_labels[2].begin(), m_labels[2].end(), 3 );
+    // Level 3: eliminate the remaining reservoir densities
+    m_labels[3].push_back( 3 );
 
     setupLabels();
 
@@ -78,20 +89,27 @@ public:
     m_levelCoarseGridMethod[0] = MGRCoarseGridMethod::nonGalerkin;
 
     // Level 1
-    m_levelFRelaxMethod[1]     = MGRFRelaxationMethod::singleLevel; //default, i.e. Jacobi
-    m_levelInterpType[1]       = MGRInterpolationType::jacobi;
+    m_levelFRelaxMethod[1]     = MGRFRelaxationMethod::singleLevel;
+    m_levelInterpType[1]       = MGRInterpolationType::blockJacobi;
     m_levelRestrictType[1]     = MGRRestrictionType::injection;
     m_levelCoarseGridMethod[1] = MGRCoarseGridMethod::galerkin;
 
     // Level 2
     m_levelFRelaxMethod[2]     = MGRFRelaxationMethod::singleLevel; //default, i.e. Jacobi
-    m_levelInterpType[2]       = MGRInterpolationType::injection;
+    m_levelInterpType[2]       = MGRInterpolationType::jacobi;
     m_levelRestrictType[2]     = MGRRestrictionType::injection;
-    m_levelCoarseGridMethod[2] = MGRCoarseGridMethod::cprLikeBlockDiag;
+    m_levelCoarseGridMethod[2] = MGRCoarseGridMethod::galerkin;
+
+    // Level 3
+    m_levelFRelaxMethod[3]     = MGRFRelaxationMethod::singleLevel; //default, i.e. Jacobi
+    m_levelInterpType[3]       = MGRInterpolationType::injection;
+    m_levelRestrictType[3]     = MGRRestrictionType::injection;
+    m_levelCoarseGridMethod[3] = MGRCoarseGridMethod::cprLikeBlockDiag;
 
     // ILU smoothing for the system made of pressure and densities (except the last one)
-    m_levelSmoothType[2]  = 16;
-    m_levelSmoothIters[2] = 1;
+    m_levelSmoothType[3] = 16;
+    m_levelSmoothIters[3] = 1;
+
   }
 
   /**
@@ -136,4 +154,4 @@ public:
 
 } // namespace geosx
 
-#endif /*GEOSX_LINEARALGEBRA_INTERFACES_HYPREMGRMULTIPHASEPOROMECHANICS_HPP_*/
+#endif /*GEOSX_LINEARALGEBRA_INTERFACES_HYPREMGRMULTIPHASEPOROMECHANICSRESERVOIRFVM_HPP_*/
