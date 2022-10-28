b08b722c9a9ccf0173235cef54bc8d14071660b3
jafranc <jfranc@stanford.edu>
2022-10-28 22:36:48 +0000

    discard interpolation for direct reading

--- scripts/postProcessing/plotRelpermDriver.py
+++ scripts/postProcessing/plotRelpermDriver.py
@@ -1,14 +1,10 @@
-import os
 from matplotlib import pyplot as plt
 from scipy import interpolate
 import numpy as np
 import argparse
 
-
-
 def populate_input(fname):
     T = np.loadtxt(fname)
-
     return T
 
 def ternatry_figure(int):
@@ -41,42 +37,67 @@ def main(fname):
         raise NotImplemented()
 
     if(isThreePhase):
-        Kro = interpolate.interp2d( Sw, Sg, T[:,4], kind='linear' )
 
         #some preporcessing
-        X,Y = np.meshgrid(Sw,Sg)
-        X_ternary = ( (1-Y[:]) / np.cos(np.pi/6) ) - ( X[:] / np.tan(np.pi/3) )
-        X_ternary = np.reshape(X_ternary,(X.shape[0],X.shape[1]))
-        Y_ternary = X
+        # Kro = interpolate.interp2d( Sw, Sg, T[:,4], kind='linear' )
+        # X,Y = np.meshgrid(Sw,Sg)
+        # X_ternary = ( (1-Y[:]) / np.cos(np.pi/6) ) - ( X[:] / np.tan(np.pi/3) )
+        # X_ternary = np.reshape(X_ternary,(X.shape[0],X.shape[1]))
+        # Y_ternary = X
+
+    #try #2
+        npt = int(np.sqrt(T[:,4].shape[0]))
+        print(f'npt {npt}, shape{T[:,4].shape[0]}')
+        Kro_ = np.reshape(T[:,4],(npt,npt))
+        Sw = np.reshape(Sw,(npt,npt))
+        Sg = np.reshape(Sg,(npt,npt))
+        X_ternary = ( (1-Sg[:]) / np.cos(np.pi/6) ) - ( Sw[:] / np.tan(np.pi/3) )
+        X_ternary = np.reshape(X_ternary,(npt,npt))
+        Y_ternary = Sw
+
 
         h = ternatry_figure(1)
         kro_ = np.empty((X_ternary.shape[0],X_ternary.shape[1],))
         kro_[:] = np.nan
         for i in range(X_ternary.shape[0]):
             for j in range(X_ternary.shape[1]):
-                if(1-X[i,j]-Y[i,j]>=0):
-                    kro_[i,j] = Kro(X_ternary[i,j], Y_ternary[i,j])
+                if(1-Sw[i,j]-Sg[i,j]>=0):
+                    # kro_[i,j] = Kro(X_ternary[i,j], Y_ternary[i,j])
+                    kro_[i,j] = Kro_[i,j]
 
         plt.contourf(X_ternary,Y_ternary,kro_,levels=np.asarray([0,0.001,0.01,0.05,0.1,0.2,0.3,0.5,0.6,0.7,0.8,0.9,1.0]))
         # plt.show()
+        plt.title('kro')
+        plt.tight_layout()
         plt.savefig("relpermDriver_kro.pdf")
 
         plt.subplot(1,2,1)
-        plt.plot(T[:-2,3],T[:-2,6])
+        plt.plot(T[:-2,3],T[:-2,6],'or')
+        plt.legend(['krw'])
+        plt.xlabel('Sw')
+        plt.ylabel('krw')
         plt.subplot(1,2,2)
-        plt.plot(T[:-2,2],T[:-2,5])
+        plt.plot(T[:-2,2],T[:-2,5],'ok')
+        plt.legend(['krnw'])
+        plt.xlabel('Snw')
+        plt.ylabel('krnw')
         # plt.show()
+        plt.tight_layout()
         plt.savefig("relpermDriver_kwg.pdf")
     else:
         plt.subplot(1,2,1)
-        plt.plot(T[:-2,1],T[:-2,3])
+        plt.plot(T[:-2,1],T[:-2,3],'or')
+        plt.legend(['krw'])
+        plt.xlabel('Sw')
+        plt.ylabel('krw')
         plt.subplot(1,2,2)
-        plt.plot(T[:-2,2],T[:-2,4])
+        plt.plot(T[:-2,2],T[:-2,4],'ok')
+        plt.legend(['krnw'])
+        plt.xlabel('Snw')
+        plt.ylabel('krnw')
         # plt.show()
+        plt.tight_layout()
         plt.savefig("relpermDriver_kwg.pdf")
-
-
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
