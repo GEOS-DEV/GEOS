3af7cc2e8d123f1e9aaeca4a959f0fe36249124c
Yohann Dudouit <yohann.dudouit@gmail.com>
2022-10-31 16:49:49 -0700

    Add `Loop3D` functions.

--- src/coreComponents/finiteElement/TeamKernelInterface/common.hpp
+++ src/coreComponents/finiteElement/TeamKernelInterface/common.hpp
@@ -38,6 +38,105 @@ enum class ThreadingModel {
   Distributed3D
 };
 
+template < typename StackVariables,
+           typename Lambda3D,
+           std::enable_if_t< StackVariables::threading_model == ThreadingModel::Serial, bool > = true >
+GEOSX_HOST_DEVICE
+GEOSX_FORCE_INLINE
+void loop3D( StackVariables & stack,
+             localIndex loop_bound_1,
+             localIndex loop_bound_2,
+             localIndex loop_bound_3,
+             Lambda3D && lambda )
+{
+  for (localIndex ind_3 = 0; ind_3 < loop_bound_3; ind_3++)
+  {
+    for (localIndex ind_2 = 0; ind_2 < loop_bound_2; ind_2++)
+    {
+      for (localIndex ind_1 = 0; ind_1 < loop_bound_1; ind_1++)
+      {
+        lambda( ind_1, ind_2, ind_3 );
+      }
+    }
+  }
+}
+
+template < typename StackVariables,
+           typename Lambda3D,
+           std::enable_if_t< StackVariables::threading_model == ThreadingModel::Distributed1D, bool > = true >
+GEOSX_HOST_DEVICE
+GEOSX_FORCE_INLINE
+void loop3D( StackVariables & stack,
+             localIndex loop_bound_1,
+             localIndex loop_bound_2,
+             localIndex loop_bound_3,
+             Lambda3D && lambda )
+{
+  for (localIndex ind_3 = 0; ind_3 < loop_bound_3; ind_3++)
+  {
+    for (localIndex ind_2 = 0; ind_2 < loop_bound_2; ind_2++)
+    {
+      localIndex ind_1 = stack.tidx;
+      if ( ind_1 < loop_bound_1 )
+      {
+        lambda( ind_1, ind_2, ind_3 );
+      }
+    }
+  }
+}
+
+template < typename StackVariables,
+           typename Lambda3D,
+           std::enable_if_t< StackVariables::threading_model == ThreadingModel::Distributed2D, bool > = true >
+GEOSX_HOST_DEVICE
+GEOSX_FORCE_INLINE
+void loop3D( StackVariables & stack,
+             localIndex loop_bound_1,
+             localIndex loop_bound_2,
+             localIndex loop_bound_3,
+             Lambda3D && lambda )
+{
+  for (localIndex ind_3 = 0; ind_3 < loop_bound_3; ind_3++)
+  {
+    localIndex ind_2 = stack.tidy;
+    if ( ind_2 < loop_bound_2 )
+    {
+      localIndex ind_1 = stack.tidx;
+      if ( ind_1 < loop_bound_1 )
+      {
+        lambda( ind_1, ind_2, ind_3 );
+      }
+    }
+  }
+}
+
+template < typename StackVariables,
+           typename Lambda3D,
+           std::enable_if_t< StackVariables::threading_model == ThreadingModel::Distributed3D, bool > = true >
+GEOSX_HOST_DEVICE
+GEOSX_FORCE_INLINE
+void loop3D( StackVariables & stack,
+             localIndex loop_bound_1,
+             localIndex loop_bound_2,
+             localIndex loop_bound_3,
+             Lambda3D && lambda )
+{
+  // printf( "(e=%d,x=%d,y=%d,z=%d)\n", stack.element_index, stack.tidx, stack.tidy, stack.tidz );
+  localIndex ind_3 = stack.tidz;
+  if ( ind_3 < loop_bound_3 )
+  {
+    localIndex ind_2 = stack.tidy;
+    if ( ind_2 < loop_bound_2 )
+    {
+      localIndex ind_1 = stack.tidx;
+      if ( ind_1 < loop_bound_1 )
+      {
+        lambda( ind_1, ind_2, ind_3 );
+      }
+    }
+  }
+}
+
 } // namespace geosx
 
 #endif /* GEOSX_FINITEELEMENT_TEAMKERNELFUNCTION_COMMON_HPP_ */
