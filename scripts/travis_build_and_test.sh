1602d13b9ead166d5db95680c89f649a8d69f1ee
Thomas Gazzola <thomas.gazzola@total.com>
2022-11-08 11:55:22 -0800

    remove extra log

--- scripts/travis_build_and_test.sh
+++ scripts/travis_build_and_test.sh
@@ -75,9 +75,9 @@ fi
 # "Make" target check (builds geosx executable target only if true)
 # Use one process to prevent out-of-memory error
 if [[ "$*" == *--build-exe-only* ]]; then
-  or_die make -j $(nproc) geosx VERBOSE=1
+  or_die make -j $(nproc) geosx
 else
-  or_die make -j $(nproc) geosx VERBOSE=1
+  or_die make -j $(nproc) geosx
   make -j $(nproc)
   or_die make -j 2
 
@@ -85,7 +85,7 @@ else
   if [[ "$*" == *--reduce-install-logs* ]]; then
     or_die make install
   else
-    or_die make install VERBOSE=1
+    or_die make install
   fi
 fi
 
