8acc23c6296ff774e218646b3bc3b028b24c5d61
Aurelien <aurelien.citrain@inria.fr>
2022-11-16 16:07:50 +0100

    Add new header with wave utilities

---
+++ src/coreComponents/physicsSolvers/wavePropagation/WaveSolverUtils.hpp
@@ -0,0 +1,87 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ * Copyright (c) 2018-2020 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2020 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2018-2020 TotalEnergies
+ * Copyright (c) 2019-     GEOSX Contributors
+ * All rights reserved
+ *
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
+
+/**
+ * @file WaveSolverUtils.hpp
+ */
+
+#ifndef GEOSX_PHYSICSSOLVERS_WAVEPROPAGATION_WAVESOLVERUTILS_HPP_
+#define GEOSX_PHYSICSSOLVERS_WAVEPROPAGATION_WAVESOLVERUTILS_HPP_
+
+namespace geosx
+{
+
+struct WaveSolverUtils
+{
+
+  GEOSX_HOST_DEVICE
+  static real32 evaluateRicker( real64 const & time_n, real32 const & f0, localIndex order )
+  {
+    real32 const o_tpeak = 1.0/f0;
+    real32 pulse = 0.0;
+    if((time_n <= -0.9*o_tpeak) || (time_n >= 2.9*o_tpeak))
+    {
+      return pulse;
+    }
+  
+    constexpr real32 pi = M_PI;
+    real32 const lam = (f0*pi)*(f0*pi);
+  
+    switch( order )
+    {
+      case 2:
+      {
+        pulse = 2.0*lam*(2.0*lam*(time_n-o_tpeak)*(time_n-o_tpeak)-1.0)*exp( -lam*(time_n-o_tpeak)*(time_n-o_tpeak));
+      }
+      break;
+      case 1:
+      {
+        pulse = -2.0*lam*(time_n-o_tpeak)*exp( -lam*(time_n-o_tpeak)*(time_n-o_tpeak));
+      }
+      break;
+      case 0:
+      {
+        pulse = -(time_n-o_tpeak)*exp( -2*lam*(time_n-o_tpeak)*(time_n-o_tpeak) );
+      }
+      break;
+      default:
+        GEOSX_ERROR( "This option is not supported yet, rickerOrder must be 0, 1 or 2" );
+    }
+  
+    return pulse;
+  }
+
+   static void computeSeismoTrace( real64 const time_n,
+                                   real64 const dt,
+                                   real64 const timeSeismo,
+                                   localIndex iSeismo,
+                                   arrayView1d< real32 const > const var_np1,
+                                   arrayView1d< real32 const > const var_n,
+                                   arrayView2d< real32 > varAtReceivers ) {};
+
+  /**
+   * @brief Temporary debug function. Saves the sismo trace to a file.
+   * @param iSeismo index number of the seismo trace
+   * @param val value to be written in seismo
+   * @param filename name of the output file
+   */
+  static void saveSeismo( localIndex const iSeismo, real32 val, string const & filename ) {};
+
+
+};
+
+} /* namespace geosx */
+
+#endif /* GEOSX_PHYSICSSOLVERS_WAVEPROPAGATION_WAVESOLVERUTILS_HPP_ */
