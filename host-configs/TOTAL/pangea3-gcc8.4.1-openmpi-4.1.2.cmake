6a12e40f1eb10d480693df413a56b976a1cfec58
Xavier Lacoste <xl64100@gmail.com>
2022-11-18 05:29:41 +0100

    Enable hypre GPU on pangea3 (#2156)

--- host-configs/TOTAL/pangea3-gcc8.4.1-openmpi-4.1.2.cmake
+++ host-configs/TOTAL/pangea3-gcc8.4.1-openmpi-4.1.2.cmake
@@ -107,4 +107,7 @@ set(PETSC_OMP_DIR ${GEOSX_TPL_ROOT_DIR}/omp-links-for-petsc CACHE STRING "")
 # PETSc doesn't seem to work correctly with clang.
 set(ENABLE_PETSC OFF CACHE BOOL "")
 
+set(ENABLE_HYPRE ON CACHE BOOL "")
+set(ENABLE_HYPRE_CUDA ON CACHE BOOL "")
+
 include( ${CMAKE_CURRENT_LIST_DIR}/../tpls.cmake )
