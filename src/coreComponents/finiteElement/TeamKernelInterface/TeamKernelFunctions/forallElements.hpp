4a30c92e8b9f22633a4f46b411911d3480f85f36
Yohann Dudouit <yohann.dudouit@gmail.com>
2022-12-06 13:55:43 -0800

    Refactor `forallElements`.

--- src/coreComponents/finiteElement/TeamKernelInterface/TeamKernelFunctions/forallElements.hpp
+++ src/coreComponents/finiteElement/TeamKernelInterface/TeamKernelFunctions/forallElements.hpp
@@ -34,10 +34,51 @@ namespace geosx
 namespace finiteElement
 {
 
+// @brief Iterator on the elements of TeamKernelBase object. TODO descrine how to use this
+template < typename KernelConfig, typename KernelComponents, typename Lambda >
+void forallElements( localIndex const numElems, KernelComponents const & fields, Lambda && element_kernel )
+{
+  constexpr localIndex num_threads_x = KernelConfig::num_threads_x;
+  constexpr localIndex num_threads_y = KernelConfig::num_threads_y;
+  constexpr localIndex num_threads_z = KernelConfig::num_threads_z;
+  constexpr localIndex batch_size = KernelConfig::batch_size;
+  localIndex const num_batches = ( numElems + batch_size - 1 ) / batch_size;
+  // localIndex const num_SM = 80;
+  // localIndex const num_blocks = 64 * num_SM; //( numElems + batch_size - 1 ) / batch_size;
+  localIndex const num_blocks = num_batches;
+
+  launch< team_launch_policy >
+  ( GEOSX_RAJA_DEVICE, Grid( Teams( num_blocks ), Threads( num_threads_x, num_threads_y, num_threads_z ) ),
+  [=] GEOSX_HOST_DEVICE ( LaunchContext ctx )
+  {
+    using RAJA::RangeSegment;
+    typename KernelComponents::template StackVariables<KernelConfig> stack( ctx );
+
+    // Each block of threads treats "batch_size" elements.
+    loop<team_x>( ctx, RangeSegment( 0, num_batches ), [&]( localIndex const & block_index )
+    {
+      // We batch elements over the z-thread dimension
+      loop<thread_z>( ctx, RangeSegment( 0, batch_size ), [&]( localIndex const & batch_index )
+      {
+        localIndex const element_index = block_index * batch_size + batch_index;
+        if ( element_index < numElems )
+        {
+          stack.batch_index = batch_index;
+          stack.element_index = element_index;
+
+          element_kernel( stack );
+        }
+      } );
+    } );
+  } );
+  parallelDeviceSync();
+}
+
+// TODO document
 template < ThreadingModel threading_model, localIndex num_threads_1d, localIndex batch_size >
 struct KernelConfiguration;
 
-// Serial
+// @brief Serial kernel configuration for kernels using `forallElements`.
 template < localIndex num_threads_1d, localIndex batchSize >
 struct KernelConfiguration< ThreadingModel::Serial, num_threads_1d, batchSize >
 {
@@ -58,13 +99,7 @@ struct KernelConfiguration< ThreadingModel::Serial, num_threads_1d, batchSize >
   GEOSX_HOST_DEVICE
   KernelConfiguration( LaunchContext & ctx )
   : ctx( ctx )
-  {
-    // using RAJA::RangeSegment;
-    // loop<thread_z>( ctx, RangeSegment( 0, batch_size ), [&]( const int batch_ind )
-    // {
-    //   batch_index = batch_ind;
-    // } );
-  }
+  { }
 };
 
 // Distributed 1D
@@ -174,45 +209,6 @@ struct KernelConfiguration< ThreadingModel::Distributed3D, num_threads_1d, batch
     } );
   }
 };
-
-template < typename KernelConfig, typename KernelComponents, typename Lambda >
-void forallElements( localIndex const numElems, KernelComponents const & fields, Lambda && element_kernel )
-{
-  constexpr localIndex num_threads_x = KernelConfig::num_threads_x;
-  constexpr localIndex num_threads_y = KernelConfig::num_threads_y;
-  constexpr localIndex num_threads_z = KernelConfig::num_threads_z;
-  constexpr localIndex batch_size = KernelConfig::batch_size;
-  localIndex const num_batches = ( numElems + batch_size - 1 ) / batch_size;
-  // localIndex const num_SM = 80;
-  // localIndex const num_blocks = 64 * num_SM; //( numElems + batch_size - 1 ) / batch_size;
-  localIndex const num_blocks = num_batches;
-
-  launch< team_launch_policy >
-  ( GEOSX_RAJA_DEVICE, Grid( Teams( num_blocks ), Threads( num_threads_x, num_threads_y, num_threads_z ) ),
-  [=] GEOSX_HOST_DEVICE ( LaunchContext ctx )
-  {
-    using RAJA::RangeSegment;
-    typename KernelComponents::template StackVariables<KernelConfig> stack( ctx );
-
-    // Each block of threads treats "batch_size" elements.
-    loop<team_x>( ctx, RangeSegment( 0, num_batches ), [&]( localIndex const & block_index )
-    {
-      // We batch elements over the z-thread dimension
-      loop<thread_z>( ctx, RangeSegment( 0, batch_size ), [&]( localIndex const & batch_index )
-      {
-        localIndex const element_index = block_index * batch_size + batch_index;
-        if ( element_index < numElems )
-        {
-          stack.batch_index = batch_index;
-          stack.element_index = element_index;
-
-          element_kernel( stack );
-        }
-      } );
-    } );
-  } );
-  parallelDeviceSync();
-}
 
 } // namespace finiteElement
 
