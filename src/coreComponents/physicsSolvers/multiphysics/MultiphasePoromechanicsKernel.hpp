15676f6a9c157059c1e8a897eeb4c74f525bbd7f
Francois Hamon <francois.hamon@totalenergies.com>
2022-12-08 20:02:17 -0800

    Added a poromechanics option to solve only mechanics in the burdens (#2169)
    * implemented targetRegions in poromechanics solvers

--- src/coreComponents/physicsSolvers/multiphysics/MultiphasePoromechanicsKernel.hpp
+++ src/coreComponents/physicsSolvers/multiphysics/MultiphasePoromechanicsKernel.hpp
@@ -93,14 +93,14 @@ public:
               FE_TYPE const & finiteElementSpace,
               CONSTITUTIVE_TYPE & inputConstitutiveType,
               arrayView1d< globalIndex const > const inputDispDofNumber,
-              string const inputFlowDofKey,
               globalIndex const rankOffset,
+              CRSMatrixView< real64, globalIndex const > const inputMatrix,
+              arrayView1d< real64 > const inputRhs,
               real64 const (&inputGravityVector)[3],
+              string const inputFlowDofKey,
               localIndex const numComponents,
               localIndex const numPhases,
-              string const fluidModelKey,
-              CRSMatrixView< real64, globalIndex const > const inputMatrix,
-              arrayView1d< real64 > const inputRhs ):
+              string const fluidModelKey ):
     Base( nodeManager,
           edgeManager,
           faceManager,
@@ -656,14 +656,14 @@ protected:
 
 using MultiphaseKernelFactory = finiteElement::KernelFactory< Multiphase,
                                                               arrayView1d< globalIndex const > const,
-                                                              string const,
                                                               globalIndex const,
-                                                              real64 const (&)[3],
-                                                              localIndex const,
-                                                              localIndex const,
-                                                              string const,
                                                               CRSMatrixView< real64, globalIndex const > const,
-                                                              arrayView1d< real64 > const >;
+                                                              arrayView1d< real64 > const,
+                                                              real64 const (&)[3],
+                                                              string const,
+                                                              localIndex const,
+                                                              localIndex const,
+                                                              string const >;
 
 } // namespace poromechanicsKernels
 
