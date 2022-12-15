0cb2089c84a9a9a676e2e31e74a18b6564f3c866
Matteo Cusini <cusini1@llnl.gov>
2022-12-15 15:19:18 -0800

    verbose=1 and no objlibs

--- host-configs/darwin-clang.cmake
+++ host-configs/darwin-clang.cmake
@@ -20,7 +20,7 @@ set(ENABLE_OPENMP "OFF" CACHE PATH "" FORCE)
 
 set(ENABLE_CALIPER "OFF" CACHE PATH "" FORCE )
 
-set(GEOSX_BUILD_OBJ_LIBS ON CACHE BOOL "" FORCE)
+set(GEOSX_BUILD_OBJ_LIBS OFF CACHE BOOL "" FORCE)
 
 set( BLAS_LIBRARIES /usr/local/opt/openblas/lib/libblas.dylib CACHE PATH "" FORCE )
 set( LAPACK_LIBRARIES /usr/local/opt/openblas/lib/liblapack.dylib CACHE PATH "" FORCE )
