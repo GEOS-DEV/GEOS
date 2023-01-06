d7642ff7aa336945dcdf083319895e2f5d6f87d1
Xavier Lacoste <xavier.lacoste@totalenergies.com>
2023-01-06 10:14:56 +0100

    add GEOSX_UNUSED_PARAM when needed

--- src/coreComponents/mesh/generators/InternalWellGenerator.hpp
+++ src/coreComponents/mesh/generators/InternalWellGenerator.hpp
@@ -103,7 +103,7 @@ public:
    */
   virtual void generateMesh( DomainPartition & domain ) override;
 
-  virtual void importFieldsOnArray( string const cellBlockName, string const meshFieldName, dataRepository::WrapperBase & wrapper, bool importMaterial ) const override {}
+  virtual void importFieldsOnArray( string const GEOSX_UNUSED_PARAM( cellBlockName ), string const GEOSX_UNUSED_PARAM( meshFieldName ), dataRepository::WrapperBase & GEOSX_UNUSED_PARAM( wrapper ), bool GEOSX_UNUSED_PARAM( importMaterial ) ) const override {}
 
   ///@}
 
