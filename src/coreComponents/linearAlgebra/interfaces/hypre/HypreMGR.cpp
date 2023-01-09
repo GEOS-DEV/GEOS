f64f0b0c44d5202c3db824186ddd4caed1d99c2a
Victor A. P. Magri <paludettomag1@llnl.gov>
2023-01-09 12:49:21 -0800

    Change default smoother for MGR's coarsest level AMG to Chebyshev

--- src/coreComponents/linearAlgebra/interfaces/hypre/HypreMGR.cpp
+++ src/coreComponents/linearAlgebra/interfaces/hypre/HypreMGR.cpp
@@ -175,8 +175,8 @@ void hypre::mgr::createMGR( LinearSolverParameters const & params,
     GEOSX_LAI_CHECK_ERROR( HYPRE_BoomerAMGSetPrintLevel( mgrData.mechSolver.ptr, logLevel ) );
 #ifdef GEOSX_USE_HYPRE_CUDA
     GEOSX_LAI_CHECK_ERROR( HYPRE_BoomerAMGSetCoarsenType( mgrData.mechSolver.ptr, hypre::getAMGCoarseningType( "PMIS" ) ) );
-    GEOSX_LAI_CHECK_ERROR( HYPRE_BoomerAMGSetRelaxType( mgrData.mechSolver.ptr, hypre::getAMGRelaxationType( LinearSolverParameters::AMG::SmootherType::l1jacobi ) ) );
-    GEOSX_LAI_CHECK_ERROR( HYPRE_BoomerAMGSetNumSweeps( mgrData.mechSolver.ptr, 2 ) );
+    GEOSX_LAI_CHECK_ERROR( HYPRE_BoomerAMGSetRelaxType( mgrData.mechSolver.ptr, hypre::getAMGRelaxationType( LinearSolverParameters::AMG::SmootherType::chebyshev ) ) );
+    GEOSX_LAI_CHECK_ERROR( HYPRE_BoomerAMGSetNumSweeps( mgrData.mechSolver.ptr, 1 ) );
 #else
     // Note: commenting out the line below, and uncommenting what follows seems to consistently deteriorate
     // the behavior of the linear solver (number of iterations and wall-clock time). This requires further investigation.
