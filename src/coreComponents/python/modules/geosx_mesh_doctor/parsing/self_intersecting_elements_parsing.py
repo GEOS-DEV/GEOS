32f44dde8cf2b4b2134f6a67930cc6c7f7998f54
Thomas Gazzola <thomas.gazzola@total.com>
2023-01-23 18:24:02 -0800

    Changing the default threshold for self intersecting check. Adding warning and error messages.

--- src/coreComponents/python/modules/geosx_mesh_doctor/parsing/self_intersecting_elements_parsing.py
+++ src/coreComponents/python/modules/geosx_mesh_doctor/parsing/self_intersecting_elements_parsing.py
@@ -2,18 +2,20 @@ import logging
 import textwrap
 from typing import Dict
 
+import numpy
+
 from checks.self_intersecting_elements import Options, Result
 
 from . import cli_parsing, SELF_INTERSECTING_ELEMENTS
 
 __TOLERANCE = "min"
-__TOLERANCE_DEFAULT = 0.
+__TOLERANCE_DEFAULT = numpy.finfo(float).eps
 
 
 def get_help():
     msg = f"""\
     Checks if the faces of the elements are self intersecting.
-        {__TOLERANCE} [float]: The tolerance in the computation. Defaults to {__TOLERANCE_DEFAULT}.
+        {__TOLERANCE} [float]: The tolerance in the computation. Defaults to machine precision {__TOLERANCE_DEFAULT}.
     """
     return textwrap.dedent(msg)
 
@@ -26,10 +28,15 @@ def parse_cli_options(options_str: str) -> Options:
     """
     options: Dict[str, str] = cli_parsing.parse_cli_option(options_str)
     cli_parsing.validate_cli_options(SELF_INTERSECTING_ELEMENTS, {__TOLERANCE}, options)
-    return Options(float(options.get(__TOLERANCE, __TOLERANCE_DEFAULT)))
+    tolerance = float(options.get(__TOLERANCE, __TOLERANCE_DEFAULT))
+    if tolerance == 0:
+        logging.warning("Having tolerance set to 0 can induce lots of false positive results (adjacent faces may be considered intersecting).")
+    elif tolerance < 0:
+        raise ValueError(f"Negative tolerance ({tolerance}) in the {SELF_INTERSECTING_ELEMENTS} check is not allowed.")
+    return Options(tolerance=tolerance)
 
 
 def display_results(options: Options, result: Result):
     logging.error(f"You have {len(result.intersecting_faces_elements)} elements with self intersecting faces.")
     if result.intersecting_faces_elements:
-        logging.error("The elements indices are:\n" + "\n".join(map(str, result.intersecting_faces_elements)))
+        logging.error("The elements indices are:\n" + ", ".join(map(str, result.intersecting_faces_elements)))
