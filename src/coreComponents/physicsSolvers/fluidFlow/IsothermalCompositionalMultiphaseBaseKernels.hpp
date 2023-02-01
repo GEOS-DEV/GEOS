38f4bacac4819c6c5aae6427b58fbc606808f623
Francois Hamon <francois.hamon@totalenergies.com>
2023-01-31 17:25:34 -0800

    Added reporting of immobile phase mass for compositional simulations (#2259)
    * added reporting of immobile phase mass
    
    * updated integratedTests submodule

--- src/coreComponents/physicsSolvers/fluidFlow/IsothermalCompositionalMultiphaseBaseKernels.hpp
+++ src/coreComponents/physicsSolvers/fluidFlow/IsothermalCompositionalMultiphaseBaseKernels.hpp
@@ -1424,6 +1424,7 @@ struct StatisticsKernel
   launch( localIndex const size,
           integer const numComps,
           integer const numPhases,
+          real64 const relpermThreshold,
           arrayView1d< integer const > const & elemGhostRank,
           arrayView1d< real64 const > const & volume,
           arrayView1d< real64 const > const & pres,
@@ -1435,6 +1436,7 @@ struct StatisticsKernel
           arrayView4d< real64 const, multifluid::USD_PHASE_COMP > const & phaseCompFraction,
           arrayView2d< real64 const, compflow::USD_PHASE > const & phaseVolFrac,
           arrayView3d< real64 const, relperm::USD_RELPERM > const & phaseTrappedVolFrac,
+          arrayView3d< real64 const, relperm::USD_RELPERM > const & phaseRelperm,
           real64 & minPres,
           real64 & avgPresNumerator,
           real64 & maxPres,
@@ -1447,6 +1449,7 @@ struct StatisticsKernel
           arrayView1d< real64 > const & phaseDynamicPoreVol,
           arrayView1d< real64 > const & phaseMass,
           arrayView1d< real64 > const & trappedPhaseMass,
+          arrayView1d< real64 > const & immobilePhaseMass,
           arrayView2d< real64 > const & dissolvedComponentMass )
   {
     RAJA::ReduceMin< parallelDeviceReduce, real64 > subRegionMinPres( LvArray::NumericLimits< real64 >::max );
@@ -1465,6 +1468,7 @@ struct StatisticsKernel
 
     forAll< parallelDevicePolicy<> >( size, [numComps,
                                              numPhases,
+                                             relpermThreshold,
                                              elemGhostRank,
                                              volume,
                                              refPorosity,
@@ -1475,6 +1479,7 @@ struct StatisticsKernel
                                              phaseDensity,
                                              phaseVolFrac,
                                              phaseTrappedVolFrac,
+                                             phaseRelperm,
                                              phaseCompFraction,
                                              subRegionMinPres,
                                              subRegionAvgPresNumerator,
@@ -1488,6 +1493,7 @@ struct StatisticsKernel
                                              phaseDynamicPoreVol,
                                              phaseMass,
                                              trappedPhaseMass,
+                                             immobilePhaseMass,
                                              dissolvedComponentMass] GEOSX_HOST_DEVICE ( localIndex const ei )
     {
       if( elemGhostRank[ei] >= 0 )
@@ -1519,6 +1525,10 @@ struct StatisticsKernel
         RAJA::atomicAdd( parallelDeviceAtomic{}, &phaseDynamicPoreVol[ip], elemPhaseVolume );
         RAJA::atomicAdd( parallelDeviceAtomic{}, &phaseMass[ip], elemPhaseMass );
         RAJA::atomicAdd( parallelDeviceAtomic{}, &trappedPhaseMass[ip], elemTrappedPhaseMass );
+        if( phaseRelperm[ei][0][ip] < relpermThreshold )
+        {
+          RAJA::atomicAdd( parallelDeviceAtomic{}, &immobilePhaseMass[ip], elemPhaseMass );
+        }
         for( integer ic = 0; ic < numComps; ++ic )
         {
           // RAJA::atomicAdd used here because we do not use ReduceSum here (for the reason explained above)
@@ -1539,9 +1549,9 @@ struct StatisticsKernel
     totalUncompactedPoreVol = subRegionTotalUncompactedPoreVol.get();
 
     // dummy loop to bring data back to the CPU
-    forAll< serialPolicy >( 1, [phaseDynamicPoreVol, phaseMass, trappedPhaseMass, dissolvedComponentMass] ( localIndex const )
+    forAll< serialPolicy >( 1, [phaseDynamicPoreVol, phaseMass, trappedPhaseMass, immobilePhaseMass, dissolvedComponentMass] ( localIndex const )
     {
-      GEOSX_UNUSED_VAR( phaseDynamicPoreVol, phaseMass, trappedPhaseMass, dissolvedComponentMass );
+      GEOSX_UNUSED_VAR( phaseDynamicPoreVol, phaseMass, trappedPhaseMass, immobilePhaseMass, dissolvedComponentMass );
     } );
   }
 };
