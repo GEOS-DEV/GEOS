50101671ac512dd89bb417cc05ed72a35049d6a3
dkachuma <dickkachuma@googlemail.com>
2023-02-28 10:41:36 -0600

    Simplify centroid and normal computation

--- src/coreComponents/mesh/utilities/ComputationalGeometry.hpp
+++ src/coreComponents/mesh/utilities/ComputationalGeometry.hpp
@@ -206,47 +206,36 @@ real64 centroid_3DPolygon( arraySlice1d< localIndex const > const pointsIndices,
   LvArray::tensorOps::fill< 3 >( center, 0 );
   LvArray::tensorOps::fill< 3 >( normal, 0 );
 
-  GEOSX_ERROR_IF_LT( pointsIndices.size(), 2 );
-  real64 current[ 3 ], next[ 3 ];
-  for( localIndex a=0; a<(pointsIndices.size()-1); ++a )
+  localIndex const numberOfPoints = pointsIndices.size();
+
+  GEOSX_ERROR_IF_LT( numberOfPoints, 2 );
+
+  real64 current[ 3 ], next[ 3 ], crossProduct[ 3 ];
+
+  LvArray::tensorOps::copy< 3 >( next, points[ pointsIndices[ numberOfPoints - 1 ] ] );
+
+  for( localIndex a=0; a<numberOfPoints; ++a )
   {
-    LvArray::tensorOps::copy< 3 >( current, points[ pointsIndices[ a ] ] );
-    LvArray::tensorOps::copy< 3 >( next, points[ pointsIndices[ a + 1 ] ] );
+    LvArray::tensorOps::copy< 3 >( current, next );
+    LvArray::tensorOps::copy< 3 >( next, points[ pointsIndices[ a ] ] );
 
-    real64 vc[3];
-    vc[0] = (current[1] - next[1]) * (current[2] + next[2]);
-    vc[1] = (current[2] - next[2]) * (current[0] + next[0]);
-    vc[2] = (current[0] - next[0]) * (current[1] + next[1]);
+    LvArray::tensorOps::crossProduct( crossProduct, current, next );
 
-    LvArray::tensorOps::add< 3 >( normal, vc );
-
-    LvArray::tensorOps::add< 3 >( center, current );
+    LvArray::tensorOps::add< 3 >( normal, crossProduct );
+    LvArray::tensorOps::add< 3 >( center, next );
   }
 
-  // close the cycle
-  LvArray::tensorOps::copy< 3 >( current, next );
-  LvArray::tensorOps::copy< 3 >( next, points[ pointsIndices[ 0 ] ] );
-
-  real64 vc[3];
-  vc[0] = (current[1] - next[1]) * (current[2] + next[2]);
-  vc[1] = (current[2] - next[2]) * (current[0] + next[0]);
-  vc[2] = (current[0] - next[0]) * (current[1] + next[1]);
-
-  LvArray::tensorOps::add< 3 >( normal, vc );
-  LvArray::tensorOps::add< 3 >( center, current );
-
   area = LvArray::tensorOps::l2Norm< 3 >( normal );
-  LvArray::tensorOps::scale< 3 >( center, 1.0 / real64( pointsIndices.size()) );
+  LvArray::tensorOps::scale< 3 >( center, 1.0 / numberOfPoints );
 
   if( area > areaTolerance )
   {
-    LvArray::tensorOps::scale< 3 >( center, 1.0 / ( area * 3.0 ) );
     LvArray::tensorOps::normalize< 3 >( normal );
     area *= 0.5;
   }
   else if( area < -areaTolerance )
   {
-    for( localIndex a=0; a<pointsIndices.size(); ++a )
+    for( localIndex a=0; a<numberOfPoints; ++a )
     {
       GEOSX_LOG_RANK( "Points: " << points[ pointsIndices[ a ] ] << " " << pointsIndices[ a ] );
     }
