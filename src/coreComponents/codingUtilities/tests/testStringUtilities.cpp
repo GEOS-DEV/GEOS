0d6a372e9bdbc7eb809ee6ba9ac56e85c568fda4
Randolph Settgast <settgast1@llnl.gov>
2023-03-02 14:17:46 -0800

    add unit test for toMetricPrefixString

--- src/coreComponents/codingUtilities/tests/testStringUtilities.cpp
+++ src/coreComponents/codingUtilities/tests/testStringUtilities.cpp
@@ -19,6 +19,7 @@
 #include <gtest/gtest.h>
 
 using namespace geosx;
+using namespace stringutilities;
 
 TEST( testStringUtilities, tokenize )
 {
@@ -43,5 +44,85 @@ TEST( testStringUtilities, tokenize )
 
     EXPECT_TRUE( tokens0==values0 );
     EXPECT_TRUE( tokens1==values1 );
+  }
+}
+
+
+TEST( testStringUtilities, toMetricPrefixString )
+{
+  double const values[21] = { 1.2345678901234567890e0,
+                              1.2345678901234567890e1,
+                              1.2345678901234567890e2,
+                              1.2345678901234567890e3,
+                              1.2345678901234567890e4,
+                              1.2345678901234567890e5,
+                              1.2345678901234567890e6,
+                              1.2345678901234567890e7,
+                              1.2345678901234567890e8,
+                              1.2345678901234567890e9,
+                              1.2345678901234567890e10,
+                              1.2345678901234567890e11,
+                              1.2345678901234567890e12,
+                              1.2345678901234567890e13,
+                              1.2345678901234567890e14,
+                              1.2345678901234567890e15,
+                              1.2345678901234567890e16,
+                              1.2345678901234567890e17,
+                              1.2345678901234567890e18,
+                              1.2345678901234567890e19,
+                              1.2345678901234567890e20 };
+
+  string const answer[21] = { " 1.23  ",
+                              " 12.3  ",
+                              "  123  ",
+                              " 1.23 K",
+                              " 12.3 K",
+                              "  123 K",
+                              " 1.23 M",
+                              " 12.3 M",
+                              "  123 M",
+                              " 1.23 G",
+                              " 12.3 G",
+                              "  123 G",
+                              " 1.23 T",
+                              " 12.3 T",
+                              "  123 T",
+                              " 1.23 P",
+                              " 12.3 P",
+                              "  123 P",
+                              " 1.23 E",
+                              " 12.3 E",
+                              "  123 E" };
+
+  string const negAnswer[21] = { "-1.23  ",
+                                 "-12.3  ",
+                                 " -123  ",
+                                 "-1.23 K",
+                                 "-12.3 K",
+                                 " -123 K",
+                                 "-1.23 M",
+                                 "-12.3 M",
+                                 " -123 M",
+                                 "-1.23 G",
+                                 "-12.3 G",
+                                 " -123 G",
+                                 "-1.23 T",
+                                 "-12.3 T",
+                                 " -123 T",
+                                 "-1.23 P",
+                                 "-12.3 P",
+                                 " -123 P",
+                                 "-1.23 E",
+                                 "-12.3 E",
+                                 " -123 E" };
+
+
+  for( int a=0; a<21; ++a )
+  {
+    std::string const result = toMetricPrefixString( values[a] );
+    std::string const negResult = toMetricPrefixString( -values[a] );
+
+    EXPECT_STRCASEEQ( result.c_str(), answer[a].c_str() );
+    EXPECT_STRCASEEQ( negResult.c_str(), negAnswer[a].c_str() );
   }
 }
