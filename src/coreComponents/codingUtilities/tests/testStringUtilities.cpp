5891d8acfcffc6677f323a206d7f3cda320c9183
MelReyCG <122801580+MelReyCG@users.noreply.github.com>
2023-03-08 07:48:24 +0100

    Fix PVT file reading error (#2307)
    - Empty lines no longer result in a crash.
    - Whitespaces can no longer interfere with the table function parameters reading.

--- src/coreComponents/codingUtilities/tests/testStringUtilities.cpp
+++ src/coreComponents/codingUtilities/tests/testStringUtilities.cpp
@@ -20,28 +20,193 @@
 
 using namespace geosx;
 
+struct TokenizeTest
+{
+  string const strToTest;
+  string const delims;
+  bool const treatConsecutiveDelimAsOne;
+  bool const preTrimStr;
+  std::vector< string > const expected;
+
+  TokenizeTest( string const & strToTest_,
+                string const & delims_,
+                bool treatConsecutiveDelimAsOne_,
+                bool preTrimStr_,
+                std::vector< string > const & expected_ ):
+    strToTest( strToTest_ ),
+    delims( delims_ ),
+    treatConsecutiveDelimAsOne( treatConsecutiveDelimAsOne_ ),
+    preTrimStr( preTrimStr_ ),
+    expected( expected_ )
+  {}
+
+  friend std::ostream & operator<<( std::ostream & os, TokenizeTest const & test )
+  {
+    os << "TokenizeTests( \"" << test.strToTest
+       << "\", \"" << test.delims << '\"'
+       << ", delimsAsOne=" << test.treatConsecutiveDelimAsOne
+       << ", preTrim=" << test.preTrimStr << " )";
+    return os;
+  }
+};
+
+struct TokenizeBySpacesTest
+{
+  string const strToTest;
+  std::vector< string > const expected;
+
+  TokenizeBySpacesTest( string const & strToTest_,
+                        std::vector< string > const & expected_ ):
+    strToTest( strToTest_ ),
+    expected( expected_ )
+  {}
+
+  friend std::ostream & operator<<( std::ostream & os, TokenizeBySpacesTest const & test )
+  {
+    os << "TokenizeBySpacesTest( \"" << test.strToTest << "\" )";
+    return os;
+  }
+};
+
 TEST( testStringUtilities, tokenize )
 {
-  map< string, std::pair< std::vector< string >, std::vector< string > > >
-  entries =
+
+  // Path tokenizing test
   {
-    { "//entry0//entry1//entry2", { { "", "entry0", "entry1", "entry2" },
-        { "", "", "entry0", "", "entry1", "", "entry2" } } },
-    { "entry0//entry1/entry2", { { "entry0", "entry1", "entry2" },
-        { "entry0", "", "entry1", "entry2" } } }
-  };
+    map< string, std::pair< std::vector< string >, std::vector< string > > > const
+    entries =
+    {
+      { "//entry0//entry1//entry2", { { "", "entry0", "entry1", "entry2" },
+          { "", "", "entry0", "", "entry1", "", "entry2" } } },
+      { "entry0//entry1/entry2", { { "entry0", "entry1", "entry2" },
+          { "entry0", "", "entry1", "entry2" } } }
+    };
 
-  for( auto const & entry : entries )
-  {
-    string const & key = entry.first;
-    std::vector< string > const & values0 = entry.second.first;
-    std::vector< string > const & values1 = entry.second.second;
+    for( auto const & entry : entries )
+    {
+      string const & key = entry.first;
+      std::vector< string > const & values0 = entry.second.first;
+      std::vector< string > const & values1 = entry.second.second;
 
-    std::vector< string > tokens0 = stringutilities::tokenize< std::vector< string > >( key, "/", true );
-    std::vector< string > tokens1 = stringutilities::tokenize< std::vector< string > >( key, "/", false );
+      std::vector< string > tokens0 = stringutilities::tokenize( key, "/", true );
+      std::vector< string > tokens1 = stringutilities::tokenize( key, "/", false );
 
 
-    EXPECT_TRUE( tokens0==values0 );
-    EXPECT_TRUE( tokens1==values1 );
+      EXPECT_TRUE( tokens0==values0 );
+      EXPECT_TRUE( tokens1==values1 );
+    }
   }
+
+  // Various strings tokenizing test
+  {
+    std::vector< TokenizeTest > const tokenizeTests = {
+      TokenizeTest( string( "a|b" ), "|", false, false, { "a", "b" } ),
+      TokenizeTest( string( "a|b" ), "|", true, false, { "a", "b" } ),
+      TokenizeTest( string( "a|b" ), "|", false, true, { "a", "b" } ),
+      TokenizeTest( string( "a|b" ), "|", true, true, { "a", "b" } ),
+
+      TokenizeTest( string( "|a|b|" ), "|", false, false, { "", "a", "b", "" } ),
+      TokenizeTest( string( "|a|b|" ), "|", true, false, { "", "a", "b", "" } ),
+      TokenizeTest( string( "|a|b|" ), "|", false, true, { "a", "b" } ),
+      TokenizeTest( string( "|a|b|" ), "|", true, true, { "a", "b" } ),
+
+      TokenizeTest( string( "|a||b|" ), "|", false, false, { "", "a", "", "b", "" } ),
+      TokenizeTest( string( "|a||b|" ), "|", true, false, { "", "a", "b", "" } ),
+      TokenizeTest( string( "|a||b|" ), "|", false, true, { "a", "", "b" } ),
+      TokenizeTest( string( "|a||b|" ), "|", true, true, { "a", "b" } ),
+
+      TokenizeTest( string( "a|||b" ), "|", false, false, { "a", "", "", "b" } ),
+      TokenizeTest( string( "a|||b" ), "|", true, false, { "a", "b" } ),
+      TokenizeTest( string( "a|||b" ), "|", false, true, { "a", "", "", "b" } ),
+      TokenizeTest( string( "a|||b" ), "|", true, true, { "a", "b" } ),
+
+      TokenizeTest( string( "||a|||b||" ), "|", false, false, { "", "", "a", "", "", "b", "", "" } ),
+      TokenizeTest( string( "||a|||b||" ), "|", true, false, { "", "a", "b", "" } ),
+      TokenizeTest( string( "||a|||b||" ), "|", false, true, { "a", "", "", "b" } ),
+      TokenizeTest( string( "||a|||b||" ), "|", true, true, { "a", "b" } ),
+
+      TokenizeTest( string( "|" ), "|", false, false, { "", "" } ),
+      TokenizeTest( string( "|" ), "|", true, false, { "", "" } ),
+      TokenizeTest( string( "|" ), "|", false, true, { } ),
+      TokenizeTest( string( "|" ), "|", true, true, { } ),
+
+      TokenizeTest( string( "|||" ), "|", false, false, { "", "", "", "" } ),
+      TokenizeTest( string( "|||" ), "|", true, false, { "", "" } ),
+      TokenizeTest( string( "|||" ), "|", false, true, { } ),
+      TokenizeTest( string( "|||" ), "|", true, true, { } ),
+
+      TokenizeTest( string( "ab" ), "|", false, false, { "ab" } ),
+      TokenizeTest( string( "ab" ), "|", true, false, { "ab" } ),
+      TokenizeTest( string( "ab" ), "|", false, true, { "ab" } ),
+      TokenizeTest( string( "ab" ), "|", true, true, { "ab" } ),
+
+      TokenizeTest( string( "" ), "|", false, false, { } ),
+      TokenizeTest( string( "" ), "|", true, false, { } ),
+      TokenizeTest( string( "" ), "|", false, true, { } ),
+      TokenizeTest( string( "" ), "|", true, true, { } ),
+
+      TokenizeTest( string( "" ), "", false, false, { } ),
+      TokenizeTest( string( "" ), "", true, false, { } ),
+      TokenizeTest( string( "" ), "", false, true, { } ),
+      TokenizeTest( string( "" ), "", true, true, { } ),
+
+      TokenizeTest( string( "ab" ), "", false, false, { "ab" } ),
+      TokenizeTest( string( "ab" ), "", true, false, { "ab" } ),
+      TokenizeTest( string( "ab" ), "", false, true, { "ab" } ),
+      TokenizeTest( string( "ab" ), "", true, true, { "ab" } ),
+
+      TokenizeTest( string( "| a|  b |" ), "| ", false, false, { "", "", "a", "", "", "b", "", "" } ),
+      TokenizeTest( string( "| a|  b |" ), "| ", true, false, { "", "a", "b", "" } ),
+      TokenizeTest( string( "| a|  b |" ), "| ", false, true, { "a", "", "", "b" } ),
+      TokenizeTest( string( "| a|  b |" ), "| ", true, true, { "a", "b" } ),
+
+      TokenizeTest( string( " |a ||b| " ), "| ", false, false, { "", "", "a", "", "", "b", "", "" } ),
+      TokenizeTest( string( " |a ||b| " ), "| ", true, false, { "", "a", "b", "" } ),
+      TokenizeTest( string( " |a ||b| " ), "| ", false, true, { "a", "", "", "b" } ),
+      TokenizeTest( string( " |a ||b| " ), "| ", true, true, { "a", "b" } ),
+    };
+
+    for( TokenizeTest test : tokenizeTests )
+    {
+      std::vector< string > const r = stringutilities::tokenize( test.strToTest,
+                                                                 test.delims,
+                                                                 test.treatConsecutiveDelimAsOne,
+                                                                 test.preTrimStr );
+
+      if( r != test.expected )
+      {
+        string const result = "{ '" + stringutilities::join( r, "' , '" ) + "' }";
+        string const expected = "{ '" + stringutilities::join( test.expected, "' , '" ) + "' }";
+        FAIL() << test << " failed: " << result <<" results instead of " << expected << ".";
+      }
+    }
+
+  }
+
+  // Spaces tokenizing test
+  {
+    std::vector< TokenizeBySpacesTest > const tokenizeBSTests = {
+      TokenizeBySpacesTest( string( "a b" ), { "a", "b" } ),
+      TokenizeBySpacesTest( string( " a b " ), { "a", "b" } ),
+      TokenizeBySpacesTest( string( "  a  b  " ), { "a", "b" } ),
+      TokenizeBySpacesTest( string( "\ta \tb\n\r" ), { "a", "b" } ),
+      TokenizeBySpacesTest( string( " " ), { } ),
+      TokenizeBySpacesTest( string( "   " ), { } ),
+      TokenizeBySpacesTest( string( "ab" ), { "ab" } ),
+      TokenizeBySpacesTest( string( "\t ab \n\r" ), { "ab" } ),
+    };
+
+    for( TokenizeBySpacesTest test : tokenizeBSTests )
+    {
+      std::vector< string > const r = stringutilities::tokenizeBySpaces( test.strToTest );
+
+      if( r != test.expected )
+      {
+        string const result = "{ '" + stringutilities::concat( r, "' , '" ) + "' }";
+        string const expected = "{ '" + stringutilities::concat( test.expected, "' , '" ) + "' }";
+        FAIL() << test << " failed: " << result << " results instead of " << expected << ".";
+      }
+    }
+  }
+
 }
--- src/coreComponents/constitutive/fluid/ReactiveBrineFluid.cpp
+++ src/coreComponents/constitutive/fluid/ReactiveBrineFluid.cpp
@@ -140,32 +140,39 @@ void ReactiveBrineFluid< PHASE > ::createPVTModels()
     string str;
     while( std::getline( is, str ) )
     {
-      string_array const strs = stringutilities::tokenize( str, " " );
+      array1d< string > const strs = stringutilities::tokenizeBySpaces< array1d >( str );
 
-      if( strs[0] == "DensityFun" )
+      if( !strs.empty() )
       {
-        if( strs[1] == PHASE::Density::catalogName() )
+        GEOSX_THROW_IF( strs.size() < 2,
+                        GEOSX_FMT( "{}: missing PVT model in line '{}'", getFullName(), str ),
+                        InputError );
+
+        if( strs[0] == "DensityFun" )
         {
-          phase1InputParams[PHASE::InputParamOrder::DENSITY] = strs;
+          if( strs[1] == PHASE::Density::catalogName() )
+          {
+            phase1InputParams[PHASE::InputParamOrder::DENSITY] = strs;
+          }
         }
-      }
-      else if( strs[0] == "ViscosityFun" )
-      {
-        if( strs[1] == PHASE::Viscosity::catalogName() )
+        else if( strs[0] == "ViscosityFun" )
         {
-          phase1InputParams[PHASE::InputParamOrder::VISCOSITY] = strs;
+          if( strs[1] == PHASE::Viscosity::catalogName() )
+          {
+            phase1InputParams[PHASE::InputParamOrder::VISCOSITY] = strs;
+          }
         }
-      }
-      else if( strs[0] == "EnthalpyFun" )
-      {
-        if( strs[1] == PHASE::Enthalpy::catalogName() )
+        else if( strs[0] == "EnthalpyFun" )
         {
-          phase1InputParams[PHASE::InputParamOrder::ENTHALPY] = strs;
+          if( strs[1] == PHASE::Enthalpy::catalogName() )
+          {
+            phase1InputParams[PHASE::InputParamOrder::ENTHALPY] = strs;
+          }
         }
-      }
-      else
-      {
-        GEOSX_THROW( GEOSX_FMT( "{}: invalid PVT function type '{}'", getFullName(), strs[0] ), InputError );
+        else
+        {
+          GEOSX_THROW( GEOSX_FMT( "{}: invalid PVT function type '{}'", getFullName(), strs[0] ), InputError );
+        }
       }
     }
     is.close();
