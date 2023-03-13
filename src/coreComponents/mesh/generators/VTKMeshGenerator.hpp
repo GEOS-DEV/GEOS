424d937951c77c6527b46de163f099ddd9cb18ae
Thomas Gazzola <thomas.gazzola@total.com>
2023-03-13 16:59:32 -0700

    typo

--- src/coreComponents/mesh/generators/VTKMeshGenerator.hpp
+++ src/coreComponents/mesh/generators/VTKMeshGenerator.hpp
@@ -87,7 +87,7 @@ public:
    */
   virtual void generateMesh( DomainPartition & domain ) override;
 
-  void importFieldsOnArray( string const & cellBlockName, string const & meshFieldNam, bool isMaterialField, WrapperBase & wrapper ) const override;
+  void importFieldsOnArray( string const & cellBlockName, string const & meshFieldName, bool isMaterialField, WrapperBase & wrapper ) const override;
 
   virtual void freeResources() override;
 
