b5651c02f6cbb1aafb9988423e5032ff02812aac
Lionel Untereiner <lionel.untereiner@geosiris.com>
2023-03-15 18:14:19 +0100

    with good format

--- src/coreComponents/mesh/unitTests/testComputationalGeometry.cpp
+++ src/coreComponents/mesh/unitTests/testComputationalGeometry.cpp
@@ -24,21 +24,22 @@ namespace geosx
 
 TEST( testComputationalGeometry, checkCentroid3DPolygon )
 {
-  constexpr int n = 12;
+  constexpr localIndex n = 12;
   real64 t = 2.0*M_PI/n;
   real64 s = (1.0 + sin( t ))/cos( t ) - 1.0;
-  constexpr int nodeDim = 3;
+  constexpr localIndex nodeDim = 3;
+  constexpr localIndex numNodes = n*3;
 
   real64 e[3] = { -1.0e-6, 0.0, 1.0e-6 };
 
-  array2d< real64, nodes::REFERENCE_POSITION_PERM  > points;
+  array2d< real64, nodes::REFERENCE_POSITION_PERM > points;
 
-  points.resize( n*3, nodeDim );
+  points.resize( numNodes, nodeDim );
 
-  int idx = 0;
-  for( int z = 0; z < 3; ++z )
+  localIndex idx = 0;
+  for( localIndex z = 0; z < 3; ++z )
   {
-    for( int i = 0; i < n; ++i )
+    for( localIndex i = 0; i < n; ++i )
     {
       real64 r = 25.0*(1.0 + (i%2)*s + e[z]);
       real64 x = r*cos( i*t );
@@ -50,26 +51,20 @@ TEST( testComputationalGeometry, checkCentroid3DPolygon )
     }
   }
 
-  std::cout << idx << std::endl;
-
   constexpr real64 areaTolerance = 0.0001;
 
-  for( int ci = 0; ci < 3; ++ci )
+  for( localIndex ci = 0; ci < 3; ++ci )
   {
     array1d< localIndex > indices;
-    for( int i = 0; i < n; ++i )
+    for( localIndex i = 0; i < n; ++i )
     {
       indices.emplace_back( ci*n + i );
     }
 
-    GEOSX_LOG_VAR( indices );
-
     real64 faceCenter[ 3 ], faceNormal[ 3 ];
-    real64 const faceArea = computationalGeometry::centroid_3DPolygon( indices.toSliceConst(), points.toViewConst(), faceCenter, faceNormal, areaTolerance );
+    computationalGeometry::centroid_3DPolygon( indices.toSliceConst(), points.toViewConst(), faceCenter, faceNormal, areaTolerance );
 
     real64 norm = LvArray::math::square( faceNormal[0] ) + LvArray::math::square( faceNormal[1] ) + LvArray::math::square( faceNormal[2] );
-    GEOSX_LOG( GEOSX_FMT( "Cell: {} Normal: [{}, {}, {}], Area: {}", ci, faceNormal[0], faceNormal[1], faceNormal[2], faceArea ));
-    std::cout << norm << " " << abs( sqrt( norm ) - 1.0 ) << std::endl;
     EXPECT_EQ( abs( sqrt( norm ) - 1.0 ) < 0.0001, true );
   }
 }
