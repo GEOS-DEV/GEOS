03008e047e4ad12f3e876e6f327be13916aed6a7
Thomas Gazzola <thomas.gazzola@total.com>
2023-03-30 15:20:49 -0700

    Reorganization of the mesh data on the fracture.

--- src/coreComponents/python/modules/geosx_mesh_doctor/tests/test_generate_fractures.py
+++ src/coreComponents/python/modules/geosx_mesh_doctor/tests/test_generate_fractures.py
@@ -5,8 +5,8 @@ from vtk.util.numpy_support import (
     vtk_to_numpy,
 )
 
-from checks.generate_fractures import Options, __split_mesh_on_fracture, __find_involved_cells, __color_fracture_sides
-from .test_utils import (
+from checks.generate_fractures import Options, __split_mesh_on_fracture, find_involved_cells, __color_fracture_sides
+from test_utils import (
     build_rectilinear_blocks_mesh,
     XYZ,
 )
@@ -28,13 +28,15 @@ def test_generate_fracture():
                       field_values={0, 1, 2},
                       vtk_output=None,
                       split_on_domain_boundary=True)
-    cell_frac_info, node_frac_info = __find_involved_cells(mesh, options)
+    cell_frac_info, node_frac_info = find_involved_cells(mesh, options)
     assert set(cell_frac_info.cell_to_faces.keys()) == {0, 1, 2, 3, 4, 5, 7, 8}
     assert len(cell_frac_info.field_data) == 5
     assert numpy.array_equal(node_frac_info.is_internal_fracture_node,
                              numpy.array(([0, 1, 1, 0, 1, 1, 1, 0] + [0, 0, 1, 0] * 2) * 2, dtype=bool))
 
-    connected_cells = __color_fracture_sides(mesh, cell_frac_info, node_frac_info)
+    connected_cells = __color_fracture_sides(mesh,
+                                             cell_frac_info.cell_to_faces,
+                                             node_frac_info.is_boundary_fracture_node)
     cc = set(map(frozenset, connected_cells))
     assert cc == {frozenset({0}), frozenset({1, 3, 4, 7}), frozenset({2, 5, 8})}
 
