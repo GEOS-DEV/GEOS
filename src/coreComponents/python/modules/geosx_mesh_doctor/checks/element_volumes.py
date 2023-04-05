2060753797c15c282624340dd6c9c35e0c90c96d
Thomas Gazzola <thomas.gazzola@total.com>
2023-04-04 17:02:58 -0700

    Use mesh quality to compute volume for std FEM cells.

--- src/coreComponents/python/modules/geosx_mesh_doctor/checks/element_volumes.py
+++ src/coreComponents/python/modules/geosx_mesh_doctor/checks/element_volumes.py
@@ -2,8 +2,15 @@ from dataclasses import dataclass
 from typing import List, Tuple
 import uuid
 
+from vtkmodules.vtkCommonDataModel import (
+    VTK_HEXAHEDRON,
+    VTK_PYRAMID,
+    VTK_TETRA,
+    VTK_WEDGE,
+)
 from vtkmodules.vtkFiltersVerdict import (
-    vtkCellSizeFilter
+    vtkCellSizeFilter,
+    vtkMeshQuality,
 )
 from vtk.util.numpy_support import (
     vtk_to_numpy,
@@ -24,26 +31,42 @@ class Result:
 
 
 def __check(mesh, options: Options) -> Result:
-    f = vtkCellSizeFilter()
+    cs = vtkCellSizeFilter()
 
-    f.ComputeAreaOff()
-    f.ComputeLengthOff()
-    f.ComputeSumOff()
-    f.ComputeVertexCountOff()
-    f.ComputeVolumeOn()
+    cs.ComputeAreaOff()
+    cs.ComputeLengthOff()
+    cs.ComputeSumOff()
+    cs.ComputeVertexCountOff()
+    cs.ComputeVolumeOn()
     volume_array_name = "__MESH_DOCTOR_VOLUME-" + str(uuid.uuid4())  # Making the name unique
-    f.SetVolumeArrayName(volume_array_name)
+    cs.SetVolumeArrayName(volume_array_name)
 
-    f.SetInputData(mesh)
-    f.Update()
-    output = f.GetOutput()
-    volume = vtk_utils.get_cell_field_by_name(output, volume_array_name)
+    cs.SetInputData(mesh)
+    cs.Update()
+
+    mq = vtkMeshQuality()
+
+    mq.SetTetQualityMeasureToVolume()
+    mq.SetPyramidQualityMeasureToVolume()
+    mq.SetWedgeQualityMeasureToVolume()
+    mq.SetHexQualityMeasureToVolume()
+
+    mq.SetInputData(mesh)
+    mq.Update()
+
+    volume = cs.GetOutput().GetCellData().GetArray(volume_array_name)
+    quality = mq.GetOutput().GetCellData().GetArray("Quality")  # Name is imposed by vtk.
+
     assert volume is not None
+    assert quality is not None
     volume = vtk_to_numpy(volume)
+    quality = vtk_to_numpy(quality)
     small_volumes: List[Tuple[int, float]] = []
-    for i, v in enumerate(volume):
-        if v < options.min_volume:
-            small_volumes.append((i, v))
+    for i, pack in enumerate(zip(volume, quality)):
+        v, q = pack
+        vol = q if mesh.GetCellType(i) in (VTK_HEXAHEDRON, VTK_PYRAMID, VTK_TETRA, VTK_WEDGE) else v
+        if vol < options.min_volume:
+            small_volumes.append((i, vol))
     return Result(element_volumes=small_volumes)
 
 
