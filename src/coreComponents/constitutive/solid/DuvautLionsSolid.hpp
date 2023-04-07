592963b7dc4a32e3489ed758ad63e5d071e08f54
Jian HUANG <jian.huang@totalenergies.com>
2023-04-06 20:47:48 -0500

    remove debugging lines

--- src/coreComponents/constitutive/solid/DuvautLionsSolid.hpp
+++ src/coreComponents/constitutive/solid/DuvautLionsSolid.hpp
@@ -77,8 +77,7 @@ public:
     real64 trialStress[6];   // Trial stress (elastic predictor)
     real64 elasticStiffness[6][6];  //Elastic stiffness
     real64 timeRatio = 1.0 / (1.0 + timeIncrement / m_relaxationTime);
-    std::cout<<"time increment="<<timeIncrement<<" , time ratio = "<<timeRatio<<std::endl;
-
+    
     for( localIndex i=0; i<6; ++i )
     {
       trialStress[i] = stress[i];
