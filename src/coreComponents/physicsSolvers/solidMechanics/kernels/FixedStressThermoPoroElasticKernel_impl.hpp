b82ca38e69a25fa7250548a35f82f723182216a1
Jian HUANG <jian.huang@totalenergies.com>
2023-04-07 10:17:24 -0500

    initialize timeIncrement in FixedStressThermoPoroElasticKernel_impl.hpp

--- src/coreComponents/physicsSolvers/solidMechanics/kernels/FixedStressThermoPoroElasticKernel_impl.hpp
+++ src/coreComponents/physicsSolvers/solidMechanics/kernels/FixedStressThermoPoroElasticKernel_impl.hpp
@@ -123,7 +123,7 @@ quadraturePointKernel( localIndex const k,
 
   real64 strainInc[6] = {0};
   real64 totalStress[6] = {0};
-  real64 timeIncrement;
+  real64 timeIncrement = 0.0;
 
   typename CONSTITUTIVE_TYPE::KernelWrapper::DiscretizationOps stiffness;
 
