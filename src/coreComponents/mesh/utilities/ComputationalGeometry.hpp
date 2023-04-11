5c0795331eabc31269134257f0e41a142634fd7a
Lionel Untereiner <lionel.untereiner@geosiris.com>
2023-04-11 08:38:05 +0200

    Computed normal with Newell's method (#2280)
    * Computed normal with Newell's method
    
    * Updated integratedTests submodule

--- src/coreComponents/mesh/utilities/ComputationalGeometry.hpp
+++ src/coreComponents/mesh/utilities/ComputationalGeometry.hpp
@@ -206,39 +206,36 @@ real64 centroid_3DPolygon( arraySlice1d< localIndex const > const pointsIndices,
   LvArray::tensorOps::fill< 3 >( center, 0 );
   LvArray::tensorOps::fill< 3 >( normal, 0 );
 
-  GEOSX_ERROR_IF_LT( pointsIndices.size(), 2 );
-  for( localIndex a=0; a<(pointsIndices.size()-2); ++a )
+  localIndex const numberOfPoints = pointsIndices.size();
+
+  GEOSX_ERROR_IF_LT( numberOfPoints, 2 );
+
+  real64 current[ 3 ], next[ 3 ], crossProduct[ 3 ];
+
+  LvArray::tensorOps::copy< 3 >( next, points[ pointsIndices[ numberOfPoints - 1 ] ] );
+
+  for( localIndex a=0; a<numberOfPoints; ++a )
   {
-    real64 v1[ 3 ], v2[ 3 ], vc[ 3 ];
+    LvArray::tensorOps::copy< 3 >( current, next );
+    LvArray::tensorOps::copy< 3 >( next, points[ pointsIndices[ a ] ] );
 
-    LvArray::tensorOps::copy< 3 >( v1, points[ pointsIndices[ a + 1 ] ] );
-    LvArray::tensorOps::copy< 3 >( v2, points[ pointsIndices[ a + 2 ] ] );
+    LvArray::tensorOps::crossProduct( crossProduct, current, next );
 
-    LvArray::tensorOps::copy< 3 >( vc, points[ pointsIndices[ 0 ] ] );
-    LvArray::tensorOps::add< 3 >( vc, v1 );
-    LvArray::tensorOps::add< 3 >( vc, v2 );
-
-    LvArray::tensorOps::subtract< 3 >( v1, points[ pointsIndices[ 0 ] ] );
-    LvArray::tensorOps::subtract< 3 >( v2, points[ pointsIndices[ 0 ] ] );
-
-    real64 triangleNormal[ 3 ];
-    LvArray::tensorOps::crossProduct( triangleNormal, v1, v2 );
-    real64 const triangleArea = LvArray::tensorOps::l2Norm< 3 >( triangleNormal );
-
-    LvArray::tensorOps::add< 3 >( normal, triangleNormal );
-
-    area += triangleArea;
-    LvArray::tensorOps::scaledAdd< 3 >( center, vc, triangleArea );
+    LvArray::tensorOps::add< 3 >( normal, crossProduct );
+    LvArray::tensorOps::add< 3 >( center, next );
   }
+
+  area = LvArray::tensorOps::l2Norm< 3 >( normal );
+  LvArray::tensorOps::scale< 3 >( center, 1.0 / numberOfPoints );
+
   if( area > areaTolerance )
   {
-    LvArray::tensorOps::scale< 3 >( center, 1.0 / ( area * 3.0 ) );
     LvArray::tensorOps::normalize< 3 >( normal );
     area *= 0.5;
   }
   else if( area < -areaTolerance )
   {
-    for( localIndex a=0; a<pointsIndices.size(); ++a )
+    for( localIndex a=0; a<numberOfPoints; ++a )
     {
       GEOSX_LOG_RANK( "Points: " << points[ pointsIndices[ a ] ] << " " << pointsIndices[ a ] );
     }
