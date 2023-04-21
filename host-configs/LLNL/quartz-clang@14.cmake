8f78855f3c83ad3df23d153cc5b55ae03f5121dc
Matteo Cusini <matteo.cusini@gmail.com>
2023-04-21 12:16:05 -0700

    renamed files + paths to gfortran and mpi.

--- host-configs/LLNL/quartz-clang@14.cmake
+++ host-configs/LLNL/quartz-clang@14.cmake
@@ -1,15 +1,15 @@
-include(${CMAKE_CURRENT_LIST_DIR}/../../src/coreComponents/LvArray/host-configs/LLNL/quartz-clang@14.0.6.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../../src/coreComponents/LvArray/host-configs/LLNL/quartz-clang@14.cmake)
 
 # Fortran
-set(CMAKE_Fortran_COMPILER /usr/tce/packages/gcc/gcc-4.9.3/bin/gfortran CACHE PATH "")
+set(CMAKE_Fortran_COMPILER /usr/tce/packages/gcc/gcc-12.1.1/bin/gfortran CACHE PATH "")
 set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -DNDEBUG -march=native -mtune=native" CACHE STRING "")
 
 # MPI
-set(MPI_HOME /usr/tce/packages/mvapich2/mvapich2-2.3-clang-10.0.0 CACHE PATH "")
+set(MPI_HOME /usr/tce/packages/mvapich2/mvapich2-2.3-clang-14.0.6 CACHE PATH "")
 
 # MKL
 set(ENABLE_MKL ON CACHE BOOL "")
-set(MKL_ROOT /usr/tce/packages/mkl/mkl-2019.0)
+set(MKL_ROOT /usr/tce/packages/mkl/mkl-2022.1.0-gcc-12.1.1/)
 set(MKL_INCLUDE_DIRS ${MKL_ROOT}/include CACHE STRING "")
 set(MKL_LIBRARIES ${MKL_ROOT}/lib/intel64/libmkl_intel_lp64.so
                   ${MKL_ROOT}/lib/intel64/libmkl_gnu_thread.so
