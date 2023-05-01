67266c9c4540fc3a2c46768bfdb77f742524b56f
Randolph R. Settgast <settgast1@llnl.gov>
2023-05-01 15:59:35 -0700

    update LC compilers and TPL paths

--- host-configs/LLNL/quartz-gcc@12.cmake
+++ host-configs/LLNL/quartz-gcc@12.cmake
@@ -5,11 +5,11 @@ include(${CMAKE_CURRENT_LIST_DIR}/../../src/coreComponents/LvArray/host-configs/
 set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG" CACHE STRING "" FORCE)
 
 # Fortran
-set(CMAKE_Fortran_COMPILER /usr/tce/packages/gcc/gcc-12.1.1/bin/gfortran CACHE PATH "")
+set(CMAKE_Fortran_COMPILER /usr/tce/packages/gcc/gcc-12.1.1-magic/bin/gfortran CACHE PATH "")
 set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -DNDEBUG -march=native -mtune=native" CACHE STRING "")
 
 # MPI
-set(MPI_HOME /usr/tce/packages/mvapich2/mvapich2-2.3.6-gcc-12.1.1 CACHE PATH "")
+set(MPI_HOME /usr/tce/packages/mvapich2/mvapich2-2.3.6-gcc-12.1.1-magic CACHE PATH "")
 
 set(ENABLE_TRILINOS OFF CACHE BOOL "" FORCE)
 
