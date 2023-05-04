79419c0db41962e30108ffd4c723cfce482aeb26
Stefan Povolny <povolny1@llnl.gov>
2023-05-04 13:09:53 -0700

    refactored mpm-specific functions out of particle region source files and into mpm solver files; switched all loops to serialPolicy to facilitate debugging non-determinacy; mapping arrays are now populated and stored instead of being computed on-the-fly

--- src/coreComponents/mesh/ParticleRegion.cpp
+++ src/coreComponents/mesh/ParticleRegion.cpp
@@ -43,59 +43,66 @@ void ParticleRegion::generateMesh( Group & particleBlocks )
   {
     ParticleSubRegion & subRegion = particleSubRegions.registerGroup< ParticleSubRegion >( particleBlockName );
     ParticleBlockABC & source = particleBlocks.getGroup< ParticleBlockABC >( subRegion.getName() );
+
+    // Copy data from particle blocks into subregions. This also sets particle type.
     subRegion.copyFromParticleBlock( source );
+
+    // Set the rank of particles on each subregion
     int const mpiRank = MpiWrapper::commRank( MPI_COMM_GEOSX );
-    subRegion.setParticleRank( mpiRank, subRegion.size() );
+    subRegion.setParticleRank( mpiRank );
+
+    // Set the number of vertices of the particles on each subregion
+    subRegion.setNumberOfVerticesPerParticle( subRegion.getParticleType() );
   }
 }
 
 // TODO This should be changed to call a ParticleSubRegion::getParticleCoordinates (and/or getParticleCorners) on each subregion such that we can access those functions directly if needed
 array2d< real64 > ParticleRegion::getParticleCorners() const
 {
-  int size = 8 * ( this->size() ); // number of particle corners in this region
+  int const size = 8 * ( this->size() ); // number of particle corners in this region
   array2d< real64 > coords(size, 3);
   int index = 0;
-  int signs[8][3] = { { 1,  1,  1}, // These have to be in the order VTK wants
-                      { 1,  1, -1},
-                      { 1, -1,  1},
-                      { 1, -1, -1},
-                      {-1,  1,  1},
-                      {-1,  1, -1},
-                      {-1, -1,  1},
-                      {-1, -1, -1} };
+  int const signs[8][3] = { { 1,  1,  1}, // These have to be in the order VTK wants
+                            { 1,  1, -1},
+                            { 1, -1,  1},
+                            { 1, -1, -1},
+                            {-1,  1,  1},
+                            {-1,  1, -1},
+                            {-1, -1,  1},
+                            {-1, -1, -1} };
   this->forParticleSubRegions( [&]( auto & subRegion )
   {
-    arrayView2d< real64 const > particleCenter = subRegion.getParticleCenter();
+    arrayView2d< real64 const > const particleCenter = subRegion.getParticleCenter();
     if( !subRegion.hasRVectors() ) // if no r-vectors, return coordinates of a cube centered at the particle with side length = volume^(1/3)
     {
-      arrayView1d< real64 const > particleVolume = subRegion.getParticleVolume();
-      for( int p=0; p<subRegion.size(); p++ )
+      arrayView1d< real64 const > const particleVolume = subRegion.getParticleVolume();
+      forAll< serialPolicy >( subRegion.size(), [=, &coords, &index] GEOSX_HOST ( localIndex const p )
       {
-        real64 a = 0.5 * std::pow( particleVolume[p], 1.0/3.0 ); // cube half-side-length
+        real64 a = 0.5 * pow( particleVolume[p], 1.0/3.0 ); // cube half-side-length
         for( int corner=0; corner<8; corner++ )
         {
           for( int i=0; i<3; i++ )
           {
-            coords[index][i] = particleCenter[p][i] + signs[corner][i]*a;
+            coords[index][i] = particleCenter[p][i] + signs[corner][i] * a;
           }
           index++;
         }
-      }
+      } );
     }
     else
     {
-      arrayView3d< real64 const > particleRVectors = subRegion.getParticleRVectors(); 
-      for( int p=0; p<subRegion.size(); p++ )
+      arrayView3d< real64 const > const particleRVectors = subRegion.getParticleRVectors(); 
+      forAll< serialPolicy >( subRegion.size(), [=, &coords, &index] GEOSX_HOST ( localIndex const p )
       {
         for( int corner=0; corner<8; corner++ )
         {
           for( int i=0; i<3; i++ )
           {
-            coords[index][i] = particleCenter[p][i] + signs[corner][0]*particleRVectors[p][0][i] + signs[corner][1]*particleRVectors[p][1][i] + signs[corner][2]*particleRVectors[p][2][i];
+            coords[index][i] = particleCenter[p][i] + signs[corner][0] * particleRVectors[p][0][i] + signs[corner][1] * particleRVectors[p][1][i] + signs[corner][2] * particleRVectors[p][2][i];
           }
           index++;
         }
-      }
+      } );
     }
   } );
   return coords;
