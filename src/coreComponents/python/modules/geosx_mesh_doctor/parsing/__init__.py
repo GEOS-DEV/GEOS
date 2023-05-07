31ba5dad6a181e80d575e8f2c405fc6a13adb10a
TotoGaz <49004943+TotoGaz@users.noreply.github.com>
2023-05-06 21:07:54 -0700

    Improvements in `mesh_doctor` (#2274)
    - Check and fixes for wrong support nodes ordering.
    - Checks and fixes polyhedron with normals not directed inward. (Not exposed through the CLI though).
    - Checks if the mesh contains "non-conformal" elements (elements are one in front of the other with non matching nodes).
    - Checks that all elements are supported by geos, including that polyhedra can be converted into supported elements.
    - Generates a cube in vtk.
    - The registration of checks has also been improved. And many other code improvements.

--- src/coreComponents/python/modules/geosx_mesh_doctor/parsing/__init__.py
+++ src/coreComponents/python/modules/geosx_mesh_doctor/parsing/__init__.py
@@ -1,16 +1,21 @@
+import argparse
 from dataclasses import dataclass
 from typing import Callable, Any
 
 
 COLLOCATES_NODES = "collocated_nodes"
 ELEMENT_VOLUMES = "element_volumes"
+FIX_ELEMENTS_ORDERINGS = "fix_elements_orderings"
+GENERATE_CUBE = "generate_cube"
 GENERATE_FRACTURES = "generate_fractures"
 GENERATE_GLOBAL_IDS = "generate_global_ids"
+NON_CONFORMAL = "non_conformal"
 SELF_INTERSECTING_ELEMENTS = "self_intersecting_elements"
+SUPPORTED_ELEMENTS = "supported_elements"
 
 
 @dataclass(frozen=True)
 class CheckHelper:
-    parse_cli_options: Callable[[str], Any]
+    fill_subparser: Callable[[Any], argparse.ArgumentParser]
+    convert: Callable[[Any], Any]
     display_results: Callable[[Any, Any], None]
-    get_help: Callable[[None], str]
--- src/coreComponents/python/modules/geosx_mesh_doctor/parsing/generate_global_ids_parsing.py
+++ src/coreComponents/python/modules/geosx_mesh_doctor/parsing/generate_global_ids_parsing.py
@@ -1,34 +1,56 @@
+from dataclasses import dataclass
 import logging
-import textwrap
 
 from checks.generate_global_ids import Options, Result
 
-from . import cli_parsing, GENERATE_GLOBAL_IDS
-
-__OUTPUT_FILE = "output"
-
-__ALL_KEYWORDS = {__OUTPUT_FILE}
+from . import vtk_output_parsing, GENERATE_GLOBAL_IDS
 
 
-def get_help():
-    msg = f"""\
-    Adds globals ids for points and cells.
-    
-    {__OUTPUT_FILE} [string]: The vtk output destination.
-    """
-    return textwrap.dedent(msg)
+__CELLS, __POINTS = "cells", "points"
 
 
-def parse_cli_options(options_str: str) -> Options:
-    """
-    From the parsed cli options, return the converted options for collocated nodes check.
-    :param options_str: Parsed cli options.
-    :return: Options instance.
-    """
-    options = cli_parsing.parse_cli_option(options_str)
-    cli_parsing.validate_cli_options(GENERATE_GLOBAL_IDS, __ALL_KEYWORDS, options)
-    output = options.get(__OUTPUT_FILE, "")
-    return Options(output=output)
+@dataclass(frozen=True)
+class GlobalIdsInfo:
+    cells: bool
+    points: bool
+
+
+def convert_global_ids(parsed_options) -> GlobalIdsInfo:
+    return GlobalIdsInfo(cells=parsed_options[__CELLS],
+                         points=parsed_options[__POINTS])
+
+
+def convert(parsed_options) -> Options:
+    gids: GlobalIdsInfo = convert_global_ids(parsed_options)
+    return Options(vtk_output=vtk_output_parsing.convert(parsed_options),
+                   generate_cells_global_ids=gids.cells,
+                   generate_points_global_ids=gids.points)
+
+
+def fill_generate_global_ids_subparser(p):
+    p.add_argument('--' + __CELLS,
+                   action="store_true",
+                   help=f"[bool]: Generate global ids for cells. Defaults to true.")
+    p.add_argument('--no-' + __CELLS,
+                   action="store_false",
+                   dest=__CELLS,
+                   help=f"[bool]: Don't generate global ids for cells.")
+    p.set_defaults(**{__CELLS: True})
+    p.add_argument('--' + __POINTS,
+                   action="store_true",
+                   help=f"[bool]: Generate global ids for points. Defaults to true.")
+    p.add_argument('--no-' + __POINTS,
+                   action="store_false",
+                   dest=__POINTS,
+                   help=f"[bool]: Don't generate global ids for points.")
+    p.set_defaults(**{__POINTS: True})
+
+
+def fill_subparser(subparsers) -> None:
+    p = subparsers.add_parser(GENERATE_GLOBAL_IDS,
+                              help="Adds globals ids for points and cells.")
+    fill_generate_global_ids_subparser(p)
+    vtk_output_parsing.fill_vtk_output_subparser(p)
 
 
 def display_results(options: Options, result: Result):
