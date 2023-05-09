113c391db545f19573455e30b2f65e3fe853aeee
Stefano Frambati <stefano.frambati@totalenergies.com>
2023-05-09 16:14:36 +0200

    uncrustify

--- src/coreComponents/mesh/DomainPartition.cpp
+++ src/coreComponents/mesh/DomainPartition.cpp
@@ -216,7 +216,7 @@ void DomainPartition::setupCommunications( bool use_nonblocking )
       }
       else
       {
-        GEOS_LOG_LEVEL_RANK_0( 3, "No communication setup is needed since it is a shallow copy of the base discretization.");
+        GEOS_LOG_LEVEL_RANK_0( 3, "No communication setup is needed since it is a shallow copy of the base discretization." );
       }
     } );
   } );
