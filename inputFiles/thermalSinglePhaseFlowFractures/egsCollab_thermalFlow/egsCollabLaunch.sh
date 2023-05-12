2e604db14f3f6598a4a394b80485aa186de33035
Matteo Cusini <matteo.cusini@gmail.com>
2023-05-12 14:02:17 -0700

    fixes to egs collab models

--- inputFiles/thermalSinglePhaseFlowFractures/egsCollab_thermalFlow/egsCollabLaunch.sh
+++ inputFiles/thermalSinglePhaseFlowFractures/egsCollab_thermalFlow/egsCollabLaunch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/tcsh
 ##### These lines are for Slurm
 #SBATCH -N 1
 #SBATCH -J egsCollab
@@ -6,11 +6,12 @@
 #SBATCH -p pbatch
 #SBATCH --mail-type=ALL
 #SBATCH -A vortex
-#SBATCH -o /usr/workspace/cusini1/geosx/geosx_dev/GEOSX/egsCollab.out
+#SBATCH -o /usr/workspace/cusini1/geosx/geosx_integratedTests/GEOSX/egsCollab.out
 
 ##### These are shell commands
 date
-cd /usr/workspace/cusini1/geosx/geosx_dev/GEOSX/build-quartz-gcc@12-release
+cd /usr/workspace/cusini1/geosx/geosx_integratedTests/GEOSX/build-quartz-gcc@12-release
 ##### Launch parallel job using srun
-OMP_NUM_THREADS=1 srun -n8 bin/geosx -i ../inputFiles/thermalSinglePhaseFlowFractures/egsCollab_thermalFlow/egsCollab_thermalFlow_initialCond_fine.xml -x 2 -y 2 -z 2
+setenv OMP_NUM_THREADS 1
+srun -n8 /bin/geosx -i ../inputFiles/thermalSinglePhaseFlowFractures/egsCollab_thermalFlow/egsCollab_thermalFlow_initialCond_fine.xml -x 2 -y 2 -z 2 -o Output_collab
 echo 'Done'
