b260d413fdbb64bddf447660768674ba20680e51
Xavier Lacoste <xl64100@gmail.com>
2023-05-22 13:52:15 +0200

    Update src/coreComponents/mesh/generators/MeshGeneratorBase.hpp
    Co-authored-by: TotoGaz <49004943+TotoGaz@users.noreply.github.com>

--- src/coreComponents/mesh/generators/MeshGeneratorBase.hpp
+++ src/coreComponents/mesh/generators/MeshGeneratorBase.hpp
@@ -137,7 +137,7 @@ public:
 private:
   /**
    * @brief Fill the cellBlockManager object .
-   * @param[in] cellBlockManager to fill with the mesh informations
+   * @param[inout] cellBlockManager the CellBlockManager that will receive the meshing information
    * @param[in] partition The number of domains in each dimesion (X,Y,Z)
    */
   virtual void fillCellBlockManager( CellBlockManager & cellBlockManager, array1d< int > const & partition ) = 0;
