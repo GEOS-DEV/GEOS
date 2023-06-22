3dd5635b7e9e03f39cb1c2a6fb10666c2454bf55
Randolph R. Settgast <rrsettgast@gmail.com>
2023-06-22 15:24:29 -0700

    forgot to add a new file in last commit

---
+++ src/coreComponents/physicsSolvers/solidMechanics/kernels/SolidMechanicsSmallStrainResidualKernels.cpp
@@ -0,0 +1,25 @@
+
+
+#include "physicsSolvers/solidMechanics/kernels/SmallStrainResidual_impl.hpp"
+#include "policies.hpp"
+
+
+#define INSTANTIATION( NAME )\
+template class NAME < CellElementSubRegion, ElasticIsotropic, H1_Hexahedron_Lagrange1_GaussLegendre2 >; \
+template real64 NAME < CellElementSubRegion, ElasticIsotropic, H1_Hexahedron_Lagrange1_GaussLegendre2 >::kernelLaunch< NAME##Policy, \
+                                                                                        NAME < CellElementSubRegion, ElasticIsotropic, H1_Hexahedron_Lagrange1_GaussLegendre2 > > \
+                                                                                      ( localIndex const, \
+                                                                                        NAME < CellElementSubRegion, ElasticIsotropic, H1_Hexahedron_Lagrange1_GaussLegendre2 > const & ); \
+
+
+namespace geos
+{
+using namespace constitutive;
+using namespace finiteElement;
+namespace solidMechanicsLagrangianFEMKernels
+{
+  INSTANTIATION( SmallStrainResidual )
+}
+}
+
+#undef INSTANTIATION
