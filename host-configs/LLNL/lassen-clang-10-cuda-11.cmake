76ced54c7e9152366f989db3f7391d8af7c2ec2b
Matteo Cusini <matteo.cusini@gmail.com>
2023-06-23 08:46:12 -0700

    cuda-11.8.0

--- host-configs/LLNL/lassen-clang-10-cuda-11.cmake
+++ host-configs/LLNL/lassen-clang-10-cuda-11.cmake
@@ -9,7 +9,7 @@ set(OpenMP_Fortran_LIB_NAMES "" CACHE STRING "")
 
 # MPI
 set(MPI_HOME /usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-clang-10.0.1-gcc-8.3.1 CACHE PATH "")
-set(MPI_Fortran_COMPILER /usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-xl-2022.08.19-cuda-11.2.0/bin/mpifort CACHE PATH "")
+set(MPI_Fortran_COMPILER /usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-xl-2022.08.19-cuda-11.8.0/bin/mpifort CACHE PATH "")
 
 include(${CMAKE_CURRENT_LIST_DIR}/lassen-base.cmake)
 
