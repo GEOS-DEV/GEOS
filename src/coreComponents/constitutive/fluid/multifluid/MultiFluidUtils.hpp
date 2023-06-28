d195026d6703a9da1b2b02dc40e0f7569080550f
wrtobin <tobin6@llnl.gov>
2023-06-28 10:33:49 -0700

    suppressing multifluid warnings with cuda active

--- src/coreComponents/constitutive/fluid/multifluid/MultiFluidUtils.hpp
+++ src/coreComponents/constitutive/fluid/multifluid/MultiFluidUtils.hpp
@@ -47,6 +47,7 @@ using ArraySliceOrRef = typename ArraySliceOrRefHelper< T, DIM, USD >::type;
 
 } // namespace internal
 
+
 /**
  * @brief Helper struct used to represent a variable and its compositional derivatives
  * @tparam DIM number of dimensions
@@ -61,9 +62,14 @@ struct MultiFluidVarSlice
     derivs( inputDerivs )
   {}
 
+  GEOS_HOST_DEVICE MultiFluidVarSlice( const MultiFluidVarSlice & );
+
   internal::ArraySliceOrRef< T, DIM, USD > value;        /// variable value
   internal::ArraySliceOrRef< T, DIM + 1, USD_DC > derivs; /// derivative w.r.t. pressure, temperature, compositions
 };
+
+template< typename T, int DIM, int USD, int USD_DC >
+GEOS_HOST_DEVICE MultiFluidVarSlice<T,DIM,USD,USD_DC>::MultiFluidVarSlice( const MultiFluidVarSlice & ) = default;
 
 /**
  * @brief Struct holding views into fluid data, used to simplify parameter passing in kernel wrapper constructors.
@@ -74,12 +80,18 @@ struct MultiFluidVarSlice
 template< typename T, int NDIM, int USD, int USD_DC >
 struct MultiFluidVarView
 {
-  #if defined( GEOS_USE_HIP )
-    MultiFluidVarView() = default;
-  #endif
+  GEOS_HOST_DEVICE MultiFluidVarView();
+  GEOS_HOST_DEVICE MultiFluidVarView( const MultiFluidVarView & );
 
   ArrayView< T, NDIM, USD > value;        ///< View into property values
   ArrayView< T, NDIM + 1, USD_DC > derivs; ///< View into property derivatives w.r.t. pressure, temperature, compositions
+
+  GEOS_HOST_DEVICE
+  MultiFluidVarView( ArrayView< T, NDIM, USD > inputValue,
+                     ArrayView< T, NDIM + 1, USD_DC > inputDerivs ):
+    value( inputValue ),
+    derivs( inputDerivs )
+  {}
 
   using SliceType = MultiFluidVarSlice< T, NDIM - 2, USD - 2, USD_DC - 2 >;
 
@@ -89,6 +101,12 @@ struct MultiFluidVarView
     return { value[k][q], derivs[k][q] };
   }
 };
+
+template< typename T, int NDIM, int USD, int USD_DC >
+GEOS_HOST_DEVICE MultiFluidVarView<T,NDIM,USD,USD_DC>::MultiFluidVarView() = default;
+
+template< typename T, int NDIM, int USD, int USD_DC >
+GEOS_HOST_DEVICE MultiFluidVarView<T,NDIM,USD,USD_DC>::MultiFluidVarView( const MultiFluidVarView & ) = default;
 
 /**
  * @brief Struct holding views into fluid data, used to simplify parameter passing in kernel wrapper constructors.
