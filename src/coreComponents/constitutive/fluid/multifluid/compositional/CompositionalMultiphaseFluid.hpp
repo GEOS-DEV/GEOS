a156957fdf37936c70b9d776f7b0a01e0208bd5e
wrtobin <tobin6@llnl.gov>
2023-07-20 08:29:20 -0700

    remove maybe_unused for m_fluid

--- src/coreComponents/constitutive/fluid/multifluid/compositional/CompositionalMultiphaseFluid.hpp
+++ src/coreComponents/constitutive/fluid/multifluid/compositional/CompositionalMultiphaseFluid.hpp
@@ -114,7 +114,7 @@ private:
                    PhaseComp::ViewType phaseCompFraction,
                    FluidProp::ViewType totalDensity );
 
-    GEOS_MAYBE_UNUSED pvt::MultiphaseSystem & m_fluid;
+    pvt::MultiphaseSystem & m_fluid;
 
     arrayView1d< pvt::PHASE_TYPE > m_phaseTypes;
   };
