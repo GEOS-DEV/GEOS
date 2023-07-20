e9bcd67173e4de55a08ea4d61903c2d2057a1ad4
Julien Fausty <julien.fausty@kitware.com>
2023-07-20 18:03:54 +0200

    GCC13: fixes for warnings and errors

--- src/coreComponents/mesh/generators/VTKFaceBlockUtilities.cpp
+++ src/coreComponents/mesh/generators/VTKFaceBlockUtilities.cpp
@@ -583,7 +583,7 @@ ArrayOfArrays< localIndex > computeElem2dToNodes( vtkIdType num2dElements,
       std::set< localIndex > tmp;
       for( auto j = 0; j < faceToNodes[faceIndex].size(); ++j )
       {
-        localIndex const & nodeIndex = faceToNodes[faceIndex][j];
+        localIndex const nodeIndex = faceToNodes[faceIndex][j];
         tmp.insert( nodeIndex );
       }
       for( localIndex const & nodeIndex: tmp )
