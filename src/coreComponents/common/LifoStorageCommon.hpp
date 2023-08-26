befc14ffd21bf0e7fcd85a0619f5f091bee36b47
Randolph R. Settgast <rrsettgast@gmail.com>
2023-08-25 17:20:38 -0700

    remove whitespace

--- src/coreComponents/common/LifoStorageCommon.hpp
+++ src/coreComponents/common/LifoStorageCommon.hpp
@@ -160,7 +160,6 @@ public:
 #else
     size_t const free = 0;
     GEOS_ERROR( "To use LifoStorage, both _SC_AVPHYS_PAGES and _SC_PAGESIZE must be defined.");
-    
 #endif
     int numberOfBuffersToStoreOnHost = std::max( 1, std::min( ( int )( 0.01 * percent * free / bufferSize ), maxNumberOfBuffers - numberOfBuffersToStoreOnDevice ) );
     double freeGB = ( ( double ) free ) / ( 1024.0 * 1024.0 * 1024.0 ) / MpiWrapper::nodeCommSize();
