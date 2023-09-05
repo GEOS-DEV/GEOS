9a8e41fe4d3452ebcd997895b6ba11062e3a3eb4
MelReyCG <122801580+MelReyCG@users.noreply.github.com>
2023-09-05 19:31:01 +0200

    Add Group & Wrapper source XML line in errors (#2404)
    * Adding context to errors when reading wrapper from xml
    
    * added XML hierarchy to the wrapper parsing error
    
    * Modified testXmlWrapper so it expect exceptions
    
    * Changed the regexs so the schema detects more errors
    
    * Adding documentation
    
    * added C++17 string_view comments
    
    * - Added the ability to know the line of xml nodes & attributes
    - Added SourceContext to know where Groups/Wrappers has been declared
    - Added a test for the node & attribute line infos
    
    * merge branch "feature/rey/error-xml-line-2" with PR branch "bugfix/rey/2320-xml-errors-feedback"
    - using getSourceContext() instead of getPath()
    
    * Renaming SourceContext to DataContext
    - also renaming FileContext to DataFileContext
    
    * - Added xml line to messages from PR 2341 & 2357
    - code refactoring + doc edit
    
    * Added getWrapperDataContext()
    
    * uncrustify + documentation corrections
    
    * added compatibility with GEOS_FMT
    + virtual destructors
    
    * Adapted getGroupByPath test with new error message
    
    * error message improvements + bugfixes + removed unused header
    
    * Group::printDataHierarchy : wrapper type fix + layout
    
    * improved messages with group/wrapper path
    
    * removing circular dependency:
    - moved GroupContext & WrapperContext in a dedicated file
    - removed dependency of Group&Wrapper in DataFileContext
    - reorganised inclusions
    
    * Refactorization: removing "isDataFileContext()"
    (work in progress)
    
    * memleak fix
    
    * Refactorization: removed getWrapperSeparator()
    
    * getTargetNameInPath() docs
    
    * removed virtual destructors
    
    * Refactorings
    
    * Added a test for DataContext string formatting
    
    * improved getAttributeLine() robustness
    
    * Refactoring: hide xml_document implementation
    
    * xmlDocument refactorings
    
    * WIP: more wrapping (xmlNode+xmlAttribute)
    
    * Revert "WIP: more wrapping (xmlNode+xmlAttribute)"
    this work will be integrated in another PR
    
    This reverts commit d5ab61ab7ce285dece8af858f5c0de5e59d7b464.
    
    * Merge commit '535b1fb81e9ec8b56d070a313caef1442dd77062' into feature/rey/error-xml-line-2
    
    * DataContext architecture refactorization
    
    * GroupContext / WrapperContext fixes
    
    * Extended test for WrapperContext::toString()
    
    * improving testGroupPath with more implicit groups & wrapper
    + got back to '/' char for WrapperContext
    
    * GroupContext::toString() rewrite
    
    * removed loadBuffer in favor of loadString
    (used string_view to support raw string loading)
    
    * missing a pugi::node_declaration wrapping
    
    * Added a dedicated WrapperContext cpp & hpp file
    
    * DataContext toString bugfix when no DataFileContext exist
    
    * Added docs for xmlWrapper attribute line search
    
    
    
    ---------
    
    Co-authored-by: Jeanne Pellerin <48675268+jeannepellerin@users.noreply.github.com>
    Co-authored-by: TotoGaz <49004943+TotoGaz@users.noreply.github.com>

--- src/coreComponents/constitutive/unitTests/testModifiedCamClay.cpp
+++ src/coreComponents/constitutive/unitTests/testModifiedCamClay.cpp
@@ -86,8 +86,7 @@ void testModifiedCamClayDriver()
     "</Constitutive>";
 
   xmlWrapper::xmlDocument xmlDocument;
-  xmlWrapper::xmlResult xmlResult = xmlDocument.load_buffer( inputStream.c_str(),
-                                                             inputStream.size() );
+  xmlWrapper::xmlResult xmlResult = xmlDocument.loadString( inputStream );
   if( !xmlResult )
   {
     GEOS_LOG_RANK_0( "XML parsed with errors!" );
@@ -95,8 +94,8 @@ void testModifiedCamClayDriver()
     GEOS_LOG_RANK_0( "Error offset: " << xmlResult.offset );
   }
 
-  xmlWrapper::xmlNode xmlConstitutiveNode = xmlDocument.child( "Constitutive" );
-  constitutiveManager.processInputFileRecursive( xmlConstitutiveNode );
+  xmlWrapper::xmlNode xmlConstitutiveNode = xmlDocument.getChild( "Constitutive" );
+  constitutiveManager.processInputFileRecursive( xmlDocument, xmlConstitutiveNode );
   constitutiveManager.postProcessInputRecursive();
 
   localIndex constexpr numElem = 2;
