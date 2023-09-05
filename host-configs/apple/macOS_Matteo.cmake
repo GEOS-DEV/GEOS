3e86d7b3cfa77f95645045d662442c3de0575f05
Randolph Settgast <settgast1@llnl.gov>
2023-09-05 07:28:28 -1000

    Build/modify apple builds (#2635)
    * organize macOS builds

---
+++ host-configs/apple/macOS_Matteo.cmake
@@ -0,0 +1,4 @@
+set( HOMEBREW_DIR "/Users/cusini1/local" )
+set( CONFIG_NAME "macOS_Matteo" ) 
+
+include(${CMAKE_CURRENT_LIST_DIR}/macOS_base.cmake)
---
+++ host-configs/apple/macOS_arm.cmake
@@ -0,0 +1,4 @@
+set( HOMEBREW_DIR "/opt/homebrew" )
+set( CONFIG_NAME "macOS_arm" ) 
+
+include(${CMAKE_CURRENT_LIST_DIR}/macOS_base.cmake)
