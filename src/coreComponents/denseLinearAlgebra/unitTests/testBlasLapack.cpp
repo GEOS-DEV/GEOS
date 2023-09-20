1c615fd20562e5335bc4603019aa3f4880eef0c1
frankfeifan <frankfei0929@gmail.com>
2023-09-20 09:43:25 -0700

    Update src/coreComponents/denseLinearAlgebra/unitTests/testBlasLapack.cpp
    Co-authored-by: Matteo Cusini <49037133+CusiniM@users.noreply.github.com>

--- src/coreComponents/denseLinearAlgebra/unitTests/testBlasLapack.cpp
+++ src/coreComponents/denseLinearAlgebra/unitTests/testBlasLapack.cpp
@@ -1154,7 +1154,6 @@ void matrix_linear_system_least_square_solve_test()
   // Assign component values to A
   A( 0, 0 ) = 0.0;
   A( 0, 1 ) = 1.0;
-  // A( 0, 2 ) = 1.0;
   A( 1, 0 ) = 1.0;
   A( 1, 1 ) = 1.0;
   // A( 1, 2 ) = 1.0;
