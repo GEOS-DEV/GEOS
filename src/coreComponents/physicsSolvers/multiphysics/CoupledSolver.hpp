443653521efbe0d0fe27a89578118004ab6fe533
sohailwaziri <90070246+sohailwaziri@users.noreply.github.com>
2023-09-21 13:46:59 -0500

    Update CoupledSolver.hpp to log statistics for subsolvers (#2680)
    Statistics for nonlinear subsolvers in sequential scheme are not reported in the current version of develop branch. Relevant code is added so that subsolvers' statistics are recorded and reported.

--- src/coreComponents/physicsSolvers/multiphysics/CoupledSolver.hpp
+++ src/coreComponents/physicsSolvers/multiphysics/CoupledSolver.hpp
@@ -314,6 +314,18 @@ public:
     return nextDt;
   }
 
+  virtual void cleanup( real64 const time_n,
+                        integer const cycleNumber,
+                        integer const eventCounter,
+                        real64 const eventProgress,
+                        DomainPartition & domain ) override
+  {
+    forEachArgInTuple( m_solvers, [&]( auto & solver, auto )
+    {
+      solver->cleanup( time_n, cycleNumber, eventCounter, eventProgress, domain );
+    } );
+    SolverBase::cleanup( time_n, cycleNumber, eventCounter, eventProgress, domain );
+  }
 
   /**@}*/
 
@@ -393,6 +405,7 @@ protected:
         forEachArgInTuple( m_solvers, [&]( auto & solver, auto )
         {
           solver->resetStateToBeginningOfStep( domain );
+          solver->getSolverStatistics().initializeTimeStepStatistics(); // initialize counters for subsolvers
         } );
         resetStateToBeginningOfStep( domain );
       }
@@ -427,6 +440,11 @@ protected:
 
       if( isConverged )
       {
+        // Save Time step statistics for the subsolvers
+        forEachArgInTuple( m_solvers, [&]( auto & solver, auto )
+        {
+          solver->getSolverStatistics().saveTimeStepStatistics();
+        } );
         break;
       }
       // Add convergence check:
