f398320c374064d482a1825b24f5c2e56bdd2540
tbeltzun <129868353+tbeltzun@users.noreply.github.com>
2023-09-26 14:34:25 +0200

    reduce procs to avoid build oom in `Debug`

--- scripts/ci_build_and_test_in_container.sh
+++ scripts/ci_build_and_test_in_container.sh
@@ -80,11 +80,16 @@ if [[ "$*" == *--test-documentation* ]]; then
 fi
 
 # "Make" target check (builds geosx executable target only if true)
-# Use one process to prevent out-of-memory error
-if [[ "$*" == *--build-exe-only* ]]; then
-  or_die ninja -j $(nproc) geosx
+# Reduce n° of processes to prevent out-of-memory error
+if [ ${CMAKE_BUILD_TYPE} == 'Debug' ]; then
+  np=2
 else
-  or_die ninja -j $(nproc)
+  np=$(nproc)
+fi
+if [[ "$*" == *--build-exe-only* ]]; then
+  or_die ninja -j $np geosx
+else
+  or_die ninja -j $np
   or_die ninja install
 fi
 
