a311fd398f8ac3d7e186fcb09ae3fe978591f8ab
Christopher Sherman <sherman27@llnl.gov>
2023-10-06 16:48:27 -0700

    Testing pydata theme for rtd builds

--- src/conf.py
+++ src/conf.py
@@ -150,30 +150,9 @@ pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
+html_theme = "pydata_sphinx_theme"
+html_theme_options = {'navigation_depth': -1, 'collapse_navigation': False}
 
-try:
-    if read_the_docs_build:
-        import sphinx_rtd_theme
-    else:
-        import pydata_sphinx_theme
-except:
-    html_theme = 'classic'
-    html_theme_options = {
-        'codebgcolor': 'lightgrey',
-        'stickysidebar': 'true'
-    }
-    html_theme_path = []
-else:
-    html_theme = "pydata_sphinx_theme"
-    html_theme_options = {'navigation_depth': -1, 'collapse_navigation': False}
-    html_theme_path = [
-        "_themes",
-    ]
-
-    if read_the_docs_build:
-        html_theme = 'sphinx_rtd_theme'
-        html_theme_options = {}
-        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -194,54 +173,14 @@ else:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
 html_static_path = ['./docs/sphinx/_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-     }
+html_css_files = [
+    'theme_overrides.css',
+]
 
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
 
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-#html_use_smartypants = True
-
-# Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-#html_additional_pages = {}
-
-# If false, no module index is generated.
-#html_domain_indices = True
-
-# If false, no index is generated.
-#html_use_index = True
-
-# If true, the index is split into individual pages for each letter.
-#html_split_index = False
-
-# If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
-
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
-
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
-
-# If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-#html_use_opensearch = ''
-
-# This is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = None
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
@@ -308,22 +247,19 @@ numfig = True
 
 # Additional stuff for the LaTeX preamble.
 latex_elements['preamble'] = '\\usepackage{amsmath}\n\\usepackage{amssymb}\n\\usepackage[retainorgcmds]{IEEEtrantools}\n'
-
-imgmath_image_format='svg'
+imgmath_image_format='png'
 imgmath_font_size=14
+
 #####################################################
 # add LaTeX macros
 
 f = open('docs/sphinx/latex_macros.sty')
-
-try:
-    imgmath_latex_preamble  # check whether this is already defined
-except NameError:
-    imgmath_latex_preamble = ""
+imgmath_latex_preamble = ""
 
 for macro in f:
     # used when building latex and pdf versions
     latex_elements['preamble'] += macro + '\n'
     # used when building html version
     imgmath_latex_preamble += macro + '\n'
+
 #####################################################
