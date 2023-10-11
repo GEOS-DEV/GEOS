497fe3eef89f52e3ff20a84209580e331cf82971
Pavel Tomin <paveltomin@users.noreply.github.com>
2023-10-11 15:16:10 -0500

    Update HydrofractureSolverKernels.hpp

--- src/coreComponents/physicsSolvers/multiphysics/HydrofractureSolverKernels.hpp
+++ src/coreComponents/physicsSolvers/multiphysics/HydrofractureSolverKernels.hpp
@@ -90,8 +90,7 @@ struct DeformationUpdateKernel
         }
       }
 #endif
-//      deltaVolume[kfe] = hydraulicAperture[kfe] * area[kfe] - volume[kfe];
-      deltaVolume[kfe] = aperture[kfe] * area[kfe] - volume[kfe];
+      deltaVolume[kfe] = hydraulicAperture[kfe] * area[kfe] - volume[kfe];
     } );
   }
 };
@@ -125,13 +124,12 @@ struct FluidMassResidualDerivativeAssemblyKernel
 
           real64 const dGap_dU = kfSign[kf] * Nbar[i] / numNodesPerFace;
 
-//          real64 dHydraulicAperture_dAperture = 0;
-//          real64 const hydraulicAperture = contactWrapper.computeHydraulicAperture( aperture, dHydraulicAperture_dAperture );
-//          GEOS_UNUSED_VAR( hydraulicAperture );
-//          real64 const dAper_dU = dHydraulicAperture_dAperture * dGap_dU;
-//
-//          dRdU( kf * 3 * numNodesPerFace + 3 * a + i ) = dens * area * dAper_dU;
-          dRdU( kf * 3 * numNodesPerFace + 3 * a + i ) = dens * area * dGap_dU;
+          real64 dHydraulicAperture_dAperture = 0;
+          real64 const hydraulicAperture = contactWrapper.computeHydraulicAperture( aperture, dHydraulicAperture_dAperture );
+          GEOS_UNUSED_VAR( hydraulicAperture );
+          real64 const dAper_dU = dHydraulicAperture_dAperture * dGap_dU;
+
+          dRdU( kf * 3 * numNodesPerFace + 3 * a + i ) = dens * area * dAper_dU;
         }
       }
     }
