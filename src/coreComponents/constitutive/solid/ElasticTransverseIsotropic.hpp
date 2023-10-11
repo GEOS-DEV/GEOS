e01c64d7122129e4e14f703816e5a8a81bf121f0
sytuannguyen <stuan.nguyen@gmail.com>
2023-10-11 11:26:26 +0200

    add get apparent shear modulus for computing the stabilization scalling

--- src/coreComponents/constitutive/solid/ElasticTransverseIsotropic.hpp
+++ src/coreComponents/constitutive/solid/ElasticTransverseIsotropic.hpp
@@ -143,6 +143,17 @@ public:
   GEOS_HOST_DEVICE
   virtual void getElasticStiffness( localIndex const k, localIndex const q, real64 ( &stiffness )[6][6] ) const override final;
 
+  /**
+   * @brief Getter for apparent shear modulus.
+   * @return reference to shear modulus that will be used for computing stabilization scalling parameter.
+   */
+  GEOS_HOST_DEVICE
+  virtual real64 getShearModulus( localIndex const k ) const override final
+  {
+    return (m_c44[k] + 2*m_c66[k]) / 3.0;
+  }
+
+
 private:
 
   /// A reference to the ArrayView holding c11 for each element.
