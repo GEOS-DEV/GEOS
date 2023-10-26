02d3a8f368c2f84b6a3b19b6dc9932c730e08325
MelReyCG <122801580+MelReyCG@users.noreply.github.com>
2023-10-26 03:08:22 +0200

    Adding an error when input temperature / pressure is out of PVT tables range (#2552)
    * crash removed when throwing messages without "Rank X:"
    + slight refactoring
    
    * added "checkPVTTablesRanges" to be able to disable the error
    
    * Added docs + inverted K & C conversion
    
    * Compliations fixes + changed MathAndPhysConstant.hpp to UOM.hpp
    
    * BlackOilFluid & DeadOilFluid support
    
    * moved the tables params so it is no longer a virtual call
    
    * BlackOilFlui & DeadOilFluid compilation fixes
    
    * Initialized tables at initializePostSubGroups()
    
    * Added units metadata to TableFunctions
    ---------

--- src/coreComponents/constitutive/fluid/multifluid/compositional/CompositionalMultiphaseFluid.hpp
+++ src/coreComponents/constitutive/fluid/multifluid/compositional/CompositionalMultiphaseFluid.hpp
@@ -45,6 +45,15 @@ public:
 
   virtual string getCatalogName() const override { return catalogName(); }
 
+  // TODO: This method should be implemented if an incorrect extrapolation of the pressure and temperature is encountered in the kernel
+  /**
+   * @copydoc MultiFluidBase::checkTablesParameters( real64 pressure, real64 temperature )
+   */
+  virtual void checkTablesParameters( real64 pressure, real64 temperature ) const override final
+  {
+    GEOS_UNUSED_VAR( pressure, temperature );
+  }
+
   virtual integer getWaterPhaseIndex() const override final;
 
   struct viewKeyStruct : MultiFluidBase::viewKeyStruct
