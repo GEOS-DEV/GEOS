e7d76e08cee92d482337ac680aa6693bd09bd96b
Cameron Mikel Crook <crook5@quartz1154.llnl.gov>
2023-10-27 16:58:56 -0700

    Added untracked files for mpmEvents and constitutive models

---
+++ src/coreComponents/constitutive/solid/VonMisesJ.cpp
@@ -0,0 +1,72 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ * Copyright (c) 2018-2020 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2020 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2018-2020 TotalEnergies
+ * Copyright (c) 2019-     GEOSX Contributors
+ * All rights reserved
+ *
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
+/**
+ *  @file VonMisesJ.cpp
+ */
+
+#include "VonMisesJ.hpp"
+
+namespace geos
+{
+using namespace dataRepository;
+namespace constitutive
+{
+
+VonMisesJ::VonMisesJ( string const & name, Group * const parent ):
+  ElasticIsotropic( name, parent ),
+  m_defaultYieldStrength(),
+  m_yieldStrength(),
+  m_deformationGradient(),
+  m_velocityGradient(),
+  m_plasticStrain()
+{
+  registerWrapper( viewKeyStruct::defaultYieldStrengthString(), &m_defaultYieldStrength ).
+    setInputFlag( InputFlags::REQUIRED ).
+    setDescription( "Default yield strength parameter" );
+
+  registerWrapper( viewKeyStruct::yieldStrengthString(), &m_yieldStrength ).
+    setApplyDefaultValue( -1 ).
+    setDescription( "Yield strength field" );
+
+  registerWrapper( viewKeyStruct::deformationGradientString(), &m_deformationGradient ).
+    setApplyDefaultValue( 1.0 ).
+    setPlotLevel( PlotLevel::NOPLOT ).
+    setDescription( "Deformation gradient field" );
+
+  registerWrapper( viewKeyStruct::velocityGradientString(), &m_velocityGradient ).
+    setApplyDefaultValue( 0.0 ).
+    setPlotLevel( PlotLevel::NOPLOT ).  
+    setDescription( "Velocity gradient field" );
+
+registerWrapper( viewKeyStruct::plasticStrainString(), &m_plasticStrain ).
+    setApplyDefaultValue( 0.0 ).
+    setPlotLevel( PlotLevel::NOPLOT ).
+    setDescription( "Plastic strain field" );
+}
+
+VonMisesJ::~VonMisesJ()
+{}
+
+void VonMisesJ::postProcessInput()
+{
+  ElasticIsotropic::postProcessInput();
+
+  this->getWrapper< array1d< real64 > >( viewKeyStruct::yieldStrengthString() ).
+    setApplyDefaultValue( m_defaultYieldStrength );
+}
+
+REGISTER_CATALOG_ENTRY( ConstitutiveBase, VonMisesJ, string const &, Group * const )
+}
+} /* namespace geos */
---
+++ src/coreComponents/events/mpmEvents/TemperatureRampMPMEvent.hpp
@@ -0,0 +1,73 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ * Copyright (c) 2018-2020 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2020 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2018-2020 TotalEnergies
+ * Copyright (c) 2019-     GEOSX Contributors
+ * All rights reserved
+ *
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
+/**
+ * @file TemperatureRampMPMEvent.hpp
+ */
+
+#ifndef GEOSX_TEMPERATURERAMP_MPMEVENT_HPP_
+#define GEOSX_TEMPERATURERAMP_MPMEVENT_HPP_
+
+#include "MPMEventBase.hpp"
+
+namespace geos
+{
+
+/**
+ * @class TemperatureRampMPMEvent
+ *
+ * This class implements the material swap mpm event for the solid mechanics material point method solver
+ */
+class TemperatureRampMPMEvent : public MPMEventBase
+{
+public:
+  /// @copydoc geos::dataRepository::Group::Group( string const & name, Group * const parent )
+  TemperatureRampMPMEvent( const string & name,
+                  Group * const parent );
+
+  /// Destructor
+  virtual ~TemperatureRampMPMEvent() override;
+
+  /**
+   * @brief Catalog name interface.
+   * @return This type's catalog name.
+   **/
+  static string catalogName() { return "TemperatureRamp"; }
+
+ /// @cond DO_NOT_DOCUMENT
+  struct viewKeyStruct
+  {
+    static constexpr char const * startTemperatureString() { return "startTemperature"; }
+    static constexpr char const * endTemperatureString() { return "endTemperature"; }
+    static constexpr char const * interpTypeString() { return "interpType"; }
+  } TemperatureRampMPMEventViewKeys;
+  /// @endcond
+
+  real64 getStartTemperature() const { return m_startTemperature; } 
+  real64 getEndTemperature() const { return m_endTemperature; } 
+  int getInterpType() const { return m_interpType; } 
+
+protected:
+  virtual void postProcessInput() override final;
+
+  // Event variables
+  real64 m_startTemperature;
+  real64 m_endTemperature;
+  int m_interpType;
+
+};
+
+} /* namespace geos */
+
+#endif /* GEOSX_TEMPERATURERAMP_MPMEVENT_HPP_ */
