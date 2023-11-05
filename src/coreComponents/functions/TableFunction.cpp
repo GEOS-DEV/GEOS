a1cc2c4551806567aefed6a95d3e25d2ceb02af1
Pavel Tomin <paveltomin@users.noreply.github.com>
2023-11-05 11:17:04 -0600

    general table output

--- src/coreComponents/functions/TableFunction.cpp
+++ src/coreComponents/functions/TableFunction.cpp
@@ -182,39 +182,67 @@ void TableFunction::checkCoord( real64 const coord, localIndex const dim ) const
 
 void TableFunction::print( std::string const & filename ) const
 {
-  integer const numDimensions = LvArray::integerConversion< integer >( m_coordinates.size() );
-  GEOS_ERROR_IF( numDimensions > 2, "Printing tables with more than 2 dimensions is not supported" );
-
   std::ofstream os( filename + ".csv" );
 
-  if( numDimensions == 1 )
+  integer const numDimensions = LvArray::integerConversion< integer >( m_coordinates.size() );
+
+  if( numDimensions != 2 )
   {
-    arraySlice1d< real64 const > const coords = m_coordinates[0];
-    integer const N = coords.size();
-    os << units::getDescription( getDimUnit( 0 )) << "," << units::getDescription( m_valueUnit ) << "\n";
-    for( integer i = 0; i < N; i++ )
+    // print header
+
+    for( integer d = 0; d < numDimensions; d++ )
     {
-      os << coords[i] << "," << m_values[i] << "\n";
+      os << units::getDescription( getDimUnit( d )) << ",";
+    }
+    os << units::getDescription( m_valueUnit ) << "\n";
+
+    // print values
+
+    // prepare dividers
+    std::vector< integer > div( numDimensions );
+    div[0] = 1;
+    for( integer d = 1; d < numDimensions; d++ )
+    {
+      div[d] = div[d-1] * m_coordinates[d-1].size();
+    }
+    // loop through all the values
+    for( integer v = 0; v < m_values.size(); v++ )
+    {
+      // find coords indices
+      std::vector< integer > idx( numDimensions );
+      integer r = v;
+      for( integer d = numDimensions-1; d >= 0; d-- )
+      {
+        idx[d] = r / div[d];
+        r = r % div[d];
+      }
+      // finally print out in right order
+      for( integer d = 0; d < numDimensions; d++ )
+      {
+        arraySlice1d< real64 const > const coords = m_coordinates[d];
+        os << coords[idx[d]] << ",";
+      }
+      os << m_values[v] << "\n";
     }
   }
-  else if( numDimensions == 2 )
+  else // numDimensions == 2
   {
-    arraySlice1d< real64 const > const coords0 = m_coordinates[0];
-    arraySlice1d< real64 const > const coords1 = m_coordinates[1];
-    integer const N0 = coords0.size();
-    integer const N1 = coords1.size();
+    arraySlice1d< real64 const > const coordsX = m_coordinates[0];
+    arraySlice1d< real64 const > const coordsY = m_coordinates[1];
+    integer const nX = coordsX.size();
+    integer const nY = coordsY.size();
     os<<units::getDescription( getDimUnit( 0 ));
-    for( integer j = 0; j < N1; j++ )
+    for( integer j = 0; j < nY; j++ )
     {
-      os << "," << units::getDescription( getDimUnit( 1 )) << "=" << coords1[j];
+      os << "," << units::getDescription( getDimUnit( 1 )) << "=" << coordsY[j];
     }
     os << "\n";
-    for( integer i = 0; i < N0; i++ )
+    for( integer i = 0; i < nX; i++ )
     {
-      os << coords0[i];
-      for( integer j = 0; j < N1; j++ )
+      os << coordsX[i];
+      for( integer j = 0; j < nY; j++ )
       {
-        os << "," << m_values[ j*N0 + i ];
+        os << "," << m_values[ j*nX + i ];
       }
       os << "\n";
     }
