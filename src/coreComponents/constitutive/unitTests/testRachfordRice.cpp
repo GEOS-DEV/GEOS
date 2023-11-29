179e17ba0fc97c4816f7717afb1495054a73d2f5
dkachuma <dickkachuma@googlemail.com>
2023-11-29 14:33:24 -0600

    Add liquid only conditions

--- src/coreComponents/constitutive/unitTests/testRachfordRice.cpp
+++ src/coreComponents/constitutive/unitTests/testRachfordRice.cpp
@@ -129,6 +129,25 @@ TEST( RachfordRiceTest, testRachfordRiceTwoComponents )
                          presentComponentIds.toSliceConst() );
 
   checkRelativeError( vaporFraction5, expectedVaporFraction5, relTol );
+
+  ////////////////////////////////////////
+
+  kValues[0] = 1.09733;
+  kValues[1] = 0.9;
+
+  feed[0] = 1.0e-10;
+  feed[1] = 1.0 - feed[0];
+
+  presentComponentIds[0] = 0;
+  presentComponentIds[1] = 1;
+
+  real64 const expectedVaporFraction6 = 0.0;
+  real64 const vaporFraction6 =
+    RachfordRice::solve( kValues.toSliceConst(),
+                         feed.toSliceConst(),
+                         presentComponentIds.toSliceConst() );
+
+  checkRelativeError( vaporFraction6, expectedVaporFraction6, relTol );  
 }
 
 TEST( RachfordRiceTest, testRachfordRiceFourComponents )
