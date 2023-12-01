a64a05e0c503280162b30b45d18c644bd30145f9
Mazuyer Antoine <115551354+AJ0435640@users.noreply.github.com>
2023-12-01 11:56:54 +0100

    Add the ability to use external mesh with nternal surfaces already defined

--- src/coreComponents/python/modules/geosx_mesh_doctor/parsing/generate_fractures_parsing.py
+++ src/coreComponents/python/modules/geosx_mesh_doctor/parsing/generate_fractures_parsing.py
@@ -6,7 +6,8 @@ from . import vtk_output_parsing, GENERATE_FRACTURES
 
 __POLICY = "policy"
 __FIELD_POLICY = "field"
-__POLICIES = (__FIELD_POLICY, )
+__INTERNAL_SURFACES_POLICY = "internal_surfaces"
+__POLICIES = (__FIELD_POLICY, __INTERNAL_SURFACES_POLICY )
 
 __FIELD_NAME = "name"
 __FIELD_TYPE = "type"
@@ -38,14 +39,14 @@ def fill_subparser(subparsers) -> None:
                    help=f"[bool]: Split policy if the fracture touches the boundary of the mesh. Defaults to true.")
     p.add_argument('--' + __FIELD_NAME,
                    type=str,
-                   help=f"[string]: If the \"{__FIELD_POLICY}\" {__POLICY} is selected, defines which field will be considered to define the fractures.")
+                   help=f"[string]: If the \"{__FIELD_POLICY}\" {__POLICY} is selected, defines which field will be considered to define the fractures. If the \"{__INTERNAL_SURFACES_POLICY}\" {__POLICY}  is selected, defines the name of the attribute will be considered to identify the fractures. ")
     p.add_argument('--' + __FIELD_TYPE,
                    type=str,
                    default=__FIELD_DEFAULT_TYPE,
                    help=f"[string]: The type of field we're going to split on. Options are \"{', '.join(__FIELD_TYPES)}\". Defaults to \"{__FIELD_DEFAULT_TYPE}\".")
     p.add_argument('--' + __FIELD_VALUES,
                    type=str,
-                   help=f"[list of comma separated integers]: If the \"{__FIELD_POLICY}\" {__POLICY} is selected, which changes of the field will be considered as a fracture.")
+                   help=f"[list of comma separated integers]: If the \"{__FIELD_POLICY}\" {__POLICY} is selected, which changes of the field will be considered as a fracture. If the \"{__INTERNAL_SURFACES_POLICY}\" {__POLICY} is selected, list of the fracture attributes.")
     vtk_output_parsing.fill_vtk_output_subparser(p)
     vtk_output_parsing.fill_vtk_output_subparser(p, prefix=__FRACTURE_PREFIX)
 
@@ -54,10 +55,12 @@ def convert(parsed_options) -> Options:
     policy = parsed_options[__POLICY]
     split_on_domain_boundary = parsed_options[__SPLIT_ON_DOMAIN_BOUNDARY]
     assert policy in __POLICIES
+    field = parsed_options[__FIELD_NAME]
+    field_values = tuple(map(int, parsed_options[__FIELD_VALUES].split(",")))
     if policy == __FIELD_POLICY:
-        field = parsed_options[__FIELD_NAME]
         field_type = parsed_options[__FIELD_TYPE]
-        field_values = tuple(map(int, parsed_options[__FIELD_VALUES].split(",")))
+    elif policy == __INTERNAL_SURFACES_POLICY:
+        field_type = ""
     vtk_output = vtk_output_parsing.convert(parsed_options)
     vtk_fracture_output = vtk_output_parsing.convert(parsed_options, prefix=__FRACTURE_PREFIX)
     return Options(policy=policy,
