36b07a5842f2449e3b1240e077dafbe3ca2f3f5d
jfranc <jfranc@stanford.edu>
2023-12-14 18:38:40 +0100

    wip unit test

--- src/coreComponents/unitTests/fluidFlowTests/testVelocityReconstruction.cpp
+++ src/coreComponents/unitTests/fluidFlowTests/testVelocityReconstruction.cpp
@@ -13,105 +13,105 @@
  */
 
 // Source includes
-#include "codingUtilities/UnitTestUtilities.hpp"
 #include "common/Logger.hpp"
 #include "mainInterface/initialization.hpp"
 #include "finiteVolume/CellElementStencilTPFA.hpp"
 #include "testFlowKernelHelpers.hpp"
-
 
 // TPL includes
 #include <gtest/gtest.h>
 
 using namespace geos;
 
-enum class CASE {
-    ALIGNED,
-    TILTED
-};
+void setReferences(const real64 flux, const real64 dx, const real64 dy, real64 (&expectedVel)[3]) {
 
-void compareVelocities(real64 const (&computedVel)[3],
-                       real64 const (&expectedVel)[3]) {
-    for (int dir = 0; dir < 3; ++dir) {
-        geos::testing::checkRelativeError(computedVel[dir], expectedVel[dir], 1e-15);
-    }
+        expectedVel[0] = flux/dx;
+        expectedVel[1] = flux/dy;
+        expectedVel[2] = 0.0;
+
 }
 
-
-void setReferences(CASE const &tested, real64 (&expectedVel)[3]) {
-    if (tested == CASE::ALIGNED)//faces are aligned with x-y-z
-    {
-        expectedVel[0] = 1.0;
-        expectedVel[1] = 1.0;
-        expectedVel[2] = 1.0;
-    }
-//    else if(tested == CASE::TILTED)//case is tilted through normals
-//    {
-//        expectedVel[0] = xxx;
-//        expectedVel[1] = xxx;
-//        expectedVel[2] = xxx;
-//    }
-}
-
-
-TEST(testAligned, Velocity_aligned) {
-    constexpr int nfaces = 1;  //2-hexa problem
+TEST( testAligned2D, Velocity_aligned2D )
+{
+    constexpr int nfaces = 4;  //2-hexa problem
     constexpr int numElemtInStencil = 2;
 
-    real64 const faceNormal[nfaces][3] = {{1., 0., 0.}};
-    real64 const cellToFaceVec[nfaces][numElemtInStencil][3] = {{{-1., 0., 0.}, {1., 0., 0}}};
-    real64 const transMult[nfaces] = {1.};
+    real64 const faceNormal[nfaces][3] = {{1., 0., 0.},
+                                          {1., 0., 0.},
+                                          {0., 1., 0.},
+                                          {0., 1., 0.}};
+    real64 const cellToFaceVec[nfaces][numElemtInStencil][3] = { {{1., 0., 0.}, {-1., 0., 0}},
+                                                                 {{1., 0., 0.}, {-1., 0., 0}},
+                                                                 {{0., 1., 0.}, {0., -1., 0}},
+                                                                 {{0., 1., 0.}, {0., -1., 0}}
+                                                                 };
+    real64 const transMult[nfaces] = {1.,1.,1.,1.};
+    real64 weight[] = {1, -1};
     real64 const geomStabSum = 1.;  //irrelevant for now
 
     localIndex elementRegionIndices[] = {0, 0};
     localIndex elementSubRegionIndices[] = {0, 0};
-    localIndex elementIndices[] = {0, 1};
-    real64 weight[] = {1, -1};
-
-    localIndex kf = 0;   //aka face index
-    CellElementStencilTPFA tpfa;
-    tpfa.add(2, elementRegionIndices, elementSubRegionIndices, elementIndices, weight, kf);
-    tpfa.addVectors(transMult[kf], geomStabSum, faceNormal[kf], cellToFaceVec[kf]);
-
-    CellElementStencilTPFA::KernelWrapper wrapper = tpfa.createKernelWrapper();
+    localIndex ei_[4][numElemtInStencil] = {
+            {0,1},//face #1
+            {2,3},//face #2
+            {0,2},//face #3
+            {1,3}//face #4
+    };
 
     const real64 flux = 0.001;
+    const int numElem = 4;
     constexpr localIndex nPhases = 2;
     constexpr localIndex nDirs = 3;
-    const real64 phaseVelocity[numElemtInStencil][nPhases][nDirs] = { {{0.,0.,0.}, {0.,0.,0.}}, {{0.,0.,0.}, {0.,0.,0.}}, };
+    const real64 phaseVelocity[numElem][nPhases][nDirs] = {
+            {{0.,0.,0.}, {0.,0.,0.}},
+            {{0.,0.,0.}, {0.,0.,0.}},
+            {{0.,0.,0.}, {0.,0.,0.}},
+            {{0.,0.,0.}, {0.,0.,0.}}
+    };
 
-    array2d<real64> arr(2,3);
-//    std::ptrdiff_t const sizes[2] = {1, 3};
-//    arr.resize(2, sizes);
-    arr[0][0] = 2.;
-    arr[1][0] = -1.;
-    arr[0][1] = 0.;
-    arr[1][1] = 0.;
-    arr[0][2] = 0.;
-    arr[1][2] = 0.;
-    arrayView2d<const real64> const view = arr.toViewConst();
+    CellElementStencilTPFA tpfa;
+    for (int kf = 0; kf < nfaces; ++kf) {
+
+        localIndex elementIndices[] = {ei_[kf][0], ei_[kf][1]};
+        tpfa.add(2, elementRegionIndices, elementSubRegionIndices, elementIndices, weight, kf);
+        tpfa.addVectors(transMult[kf], geomStabSum, faceNormal[kf], cellToFaceVec[kf]);
+    }
+
+    CellElementStencilTPFA::KernelWrapper wrapper = tpfa.createKernelWrapper();
+    array2d<real64> globalCellDim(2, 3);
+    globalCellDim[0][0] = 2.; globalCellDim[1][0] = 2.;
+    globalCellDim[0][1] = 2.; globalCellDim[1][1] = 2.;
+    globalCellDim[0][2] = 0.; globalCellDim[1][2] = 0.;
+    arrayView2d<const real64> const globalCellDimView = globalCellDim.toViewConst();
 
     CellElementStencilTPFA::IndexContainerViewConstType const &seri = tpfa.getElementRegionIndices();
     CellElementStencilTPFA::IndexContainerViewConstType const &sesri = tpfa.getElementSubRegionIndices();
     CellElementStencilTPFA::IndexContainerViewConstType const &sei = tpfa.getElementIndices();
 
-    auto const phaseVelocityView = AccessorHelper<true>::makeElementAccessor<3>( phaseVelocity[0][0],
-                                                                                 tpfa.stencilSize(0),
-                                                                                 seri[0],
-                                                                                 sesri[0],
-                                                                                 sei[0],
-                                                                                 nPhases,
-                                                                                 nDirs);
+    ElementRegionManager::ElementViewAccessor< array3d<real64>> phaseVelocityView = AccessorHelper<true>::makeElementAccessor<3>( phaseVelocity[0][0],
+                                                                                                                                  tpfa.stencilSize(0),
+                                                                                                                                  seri[nfaces-1],
+                                                                                                                                  sesri[nfaces-1],
+                                                                                                                                  sei[nfaces-1],
+                                                                                                                                  nPhases,
+                                                                                                                                  nDirs);
+    for (int iconn = 0; iconn < nfaces; ++iconn) {
 
-    wrapper.computeVelocity(0/*iconn*/, 0/*ip*/, flux, {view[0], view[1]}, phaseVelocityView.toNestedView());
-    wrapper.computeVelocity(0/*iconn*/, 1/*ip*/, 100*flux, {view[0], view[1]}, phaseVelocityView.toNestedView());
+        wrapper.computeVelocity(iconn/*iconn*/, 0/*ip*/, flux, {globalCellDimView[0], globalCellDimView[1]}, phaseVelocityView.toNestedView());
+        wrapper.computeVelocity(iconn/*iconn*/, 1/*ip*/, 100*flux, {globalCellDimView[0], globalCellDimView[1]}, phaseVelocityView.toNestedView());
+    }
 
-    GEOS_LOG_RANK(GEOS_FMT("phase velocity : {}",phaseVelocityView));
-
+    for (int ip = 0; ip < 2; ++ip) {
+        auto fip = (99*ip+1)*flux;//shorthand to get the 1:100 ratio between phases as imposed above
+        for (int ib = 0; ib < numElem; ++ib) {
+            real64 expectedVel[3];
+            setReferences(fip, globalCellDim[0][0], globalCellDim[0][1], expectedVel);
+            EXPECT_EQ(phaseVelocityView[0][0][ib][ip][0], expectedVel[0]);
+            EXPECT_EQ(phaseVelocityView[0][0][ib][ip][1], expectedVel[1]);
+            EXPECT_EQ(phaseVelocityView[0][0][ib][ip][2], expectedVel[2]);
+        }
+    }
 }
-//TEST( testTilter, Velocity_tilted )
-//{
-//}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
