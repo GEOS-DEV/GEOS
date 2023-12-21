210aa03c842871fd24ddbcbc413ee7683b97082e
wrtobin <tobin6@llnl.gov>
2023-12-21 15:27:30 -0500

    lvarray + maybe_unused

--- src/coreComponents/common/TypeDispatch.hpp
+++ src/coreComponents/common/TypeDispatch.hpp
@@ -368,7 +368,7 @@ bool dispatch( LIST const combinations,
   if( !success )
   {
     auto typePrinter = []( auto t ){ return LvArray::system::demangle( typeid( typename decltype(t)::type ).name() ); };
-    auto typeListPrinter = [typePrinter]( auto tlist ){ return internal::listToString( typename decltype( tlist )::type{}, "\n  ", "", typePrinter ); };
+    GEOS_MAYBE_UNUSED auto typeListPrinter = [typePrinter]( auto tlist ){ return internal::listToString( typename decltype( tlist )::type{}, "\n  ", "", typePrinter ); };
 
     GEOS_ERROR( "Types were not dispatched. The types of the input objects are:\n" <<
                 "( "<<(  ( "\n  " + LvArray::system::demangle( internal::typeIdWrapper( objects ).name() ) ) + ... )<<" \n)\n"<<
