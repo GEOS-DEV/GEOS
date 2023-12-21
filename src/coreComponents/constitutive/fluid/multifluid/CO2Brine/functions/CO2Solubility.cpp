2e41e753779306a5584b8b0440e518391f2a893d
Dickson Kachuma <81433670+dkachuma@users.noreply.github.com>
2023-12-20 22:44:33 -0600

    Include water vapourisation in CO2/Brine flash (#2811)

--- src/coreComponents/constitutive/fluid/multifluid/CO2Brine/functions/CO2Solubility.cpp
+++ src/coreComponents/constitutive/fluid/multifluid/CO2Brine/functions/CO2Solubility.cpp
@@ -245,7 +245,7 @@ TableFunction const * makeSolubilityTable( string_array const & inputParams,
   array1d< real64 > values( tableCoords.nPressures() * tableCoords.nTemperatures() );
   calculateCO2Solubility( functionName, tolerance, tableCoords, salinity, values );
 
-  string const tableName = functionName + "_table";
+  string const tableName = functionName + "_co2Dissolution_table";
   if( functionManager.hasGroup< TableFunction >( tableName ) )
   {
     return functionManager.getGroupPointer< TableFunction >( tableName );
@@ -258,6 +258,35 @@ TableFunction const * makeSolubilityTable( string_array const & inputParams,
     solubilityTable->setTableValues( values, units::Solubility );
     solubilityTable->setInterpolationMethod( TableFunction::InterpolationType::Linear );
     return solubilityTable;
+  }
+}
+
+TableFunction const * makeVapourisationTable( string_array const & inputParams,
+                                              string const & functionName,
+                                              FunctionManager & functionManager )
+{
+  // initialize the (p,T) coordinates
+  PTTableCoordinates tableCoords;
+  PVTFunctionHelpers::initializePropertyTable( inputParams, tableCoords );
+
+  // Currently initialise to all zeros
+
+  array1d< real64 > values( tableCoords.nPressures() * tableCoords.nTemperatures() );
+  values.zero();
+
+  string const tableName = functionName + "_waterVaporization_table";
+  if( functionManager.hasGroup< TableFunction >( tableName ) )
+  {
+    return functionManager.getGroupPointer< TableFunction >( tableName );
+  }
+  else
+  {
+    TableFunction * const vapourisationTable = dynamicCast< TableFunction * >( functionManager.createChild( "TableFunction", tableName ) );
+    vapourisationTable->setTableCoordinates( tableCoords.getCoords(),
+                                             { units::Pressure, units::TemperatureInC } );
+    vapourisationTable->setTableValues( values, units::Solubility );
+    vapourisationTable->setInterpolationMethod( TableFunction::InterpolationType::Linear );
+    return vapourisationTable;
   }
 }
 
@@ -293,8 +322,12 @@ CO2Solubility::CO2Solubility( string const & name,
   m_phaseLiquidIndex = PVTFunctionHelpers::findName( phaseNames, expectedWaterPhaseNames, "phaseNames" );
 
   m_CO2SolubilityTable = makeSolubilityTable( inputParams, m_modelName, FunctionManager::getInstance() );
+  m_WaterVapourisationTable = makeVapourisationTable( inputParams, m_modelName, FunctionManager::getInstance() );
   if( printTable )
+  {
     m_CO2SolubilityTable->print( m_CO2SolubilityTable->getName() );
+    m_WaterVapourisationTable->print( m_WaterVapourisationTable->getName() );
+  }
 }
 
 void CO2Solubility::checkTablesParameters( real64 const pressure,
@@ -302,12 +335,15 @@ void CO2Solubility::checkTablesParameters( real64 const pressure,
 {
   m_CO2SolubilityTable->checkCoord( pressure, 0 );
   m_CO2SolubilityTable->checkCoord( temperature, 1 );
+  m_WaterVapourisationTable->checkCoord( pressure, 0 );
+  m_WaterVapourisationTable->checkCoord( temperature, 1 );
 }
 
 CO2Solubility::KernelWrapper CO2Solubility::createKernelWrapper() const
 {
   return KernelWrapper( m_componentMolarWeight,
                         *m_CO2SolubilityTable,
+                        *m_WaterVapourisationTable,
                         m_CO2Index,
                         m_waterIndex,
                         m_phaseGasIndex,
--- src/coreComponents/unitTests/constitutiveTests/testCO2BrinePVTModels.cpp
+++ src/coreComponents/unitTests/constitutiveTests/testCO2BrinePVTModels.cpp
@@ -216,7 +216,8 @@ void testNumericalDerivatives( FLASH_WRAPPER const & flashModelWrapper,
                                real64 const temperature,
                                arraySlice1d< real64 const > const & compFraction,
                                real64 const perturbParameter,
-                               real64 const relTol )
+                               real64 const relTol,
+                               real64 const absTol = std::numeric_limits< real64 >::epsilon() )
 {
   using namespace multifluid;
   using Deriv = multifluid::DerivativeOffset;
@@ -312,12 +313,12 @@ void testNumericalDerivatives( FLASH_WRAPPER const & flashModelWrapper,
     {
       checkRelativeError( (perturbedPhaseFracAndDeriv.value[j]-phaseFracAndDeriv.value[j])/dC,
                           phaseFracAndDeriv.derivs[j][Deriv::dC+i],
-                          relTol );
+                          relTol, absTol );
       for( integer k = 0; k < numComp; ++k )
       {
         checkRelativeError( (perturbedPhaseCompFracAndDeriv.value[j][k]-phaseCompFracAndDeriv.value[j][k])/dC,
                             phaseCompFracAndDeriv.derivs[j][k][Deriv::dC+i],
-                            relTol );
+                            relTol, absTol );
       }
     }
   }
@@ -876,8 +877,9 @@ TEST_F( CO2SolubilityTest, co2SolubilityValuesAndDeriv )
   real64 const deltaComp = 0.2;
 
   real64 const eps = sqrt( std::numeric_limits< real64 >::epsilon());
-  real64 const relTolPrevImpl = 5e-4;
-  real64 const relTolDeriv = 5e-5;
+  real64 constexpr relTolPrevImpl = 5e-4;
+  real64 constexpr relTolDeriv = 5e-5;
+  real64 constexpr absTolDeriv = 1.0e-7;
 
   real64 const savedGasPhaseFrac[] = { 0.298158785, 0.298183347, 0.2982033821, 0.295950309, 0.2959791448, 0.2960026365, 0.2926988393,
                                        0.292724834, 0.2927459702, 0.499837295, 0.499854799, 0.4998690769, 0.4982634386, 0.4982839883,
@@ -901,7 +903,7 @@ TEST_F( CO2SolubilityTest, co2SolubilityValuesAndDeriv )
         testValuesAgainstPreviousImplementation( flashModelWrapper,
                                                  P[iPres], TC[iTemp], comp,
                                                  savedGasPhaseFrac[counter], savedWaterPhaseGasComp[counter], relTolPrevImpl );
-        testNumericalDerivatives( flashModelWrapper, P[iPres], TC[iTemp], comp, eps, relTolDeriv );
+        testNumericalDerivatives( flashModelWrapper, P[iPres], TC[iTemp], comp, eps, relTolDeriv, absTolDeriv );
         counter++;
       }
     }
