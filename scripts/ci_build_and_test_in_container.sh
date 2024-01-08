fd9457fd7661c037ab5df1244b65f51be99ca070
Randolph Settgast <settgast1@llnl.gov>
2024-01-08 15:00:33 -0800

    use ninja instead

--- scripts/ci_build_and_test_in_container.sh
+++ scripts/ci_build_and_test_in_container.sh
@@ -214,8 +214,8 @@ else
 fi
 
 if [[ "$*" == *--code-coverage* ]]; then
-  or_die make -j $(nproc) VERBOSE=1
-  or_die make coreComponents_coverage
+  or_die ninja -j $(nproc) VERBOSE=1
+  or_die ninja coreComponents_coverage
   cp -r ${GEOSX_BUILD_DIR}/coreComponents_coverage.info.cleaned /tmp/Shiva/geos_coverage.info.cleaned
 fi
 
