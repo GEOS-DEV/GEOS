919436c0ce04bf748e5f6906d0a905edfc957d70
Victor A. P. Magri <50467563+victorapm@users.noreply.github.com>
2024-01-28 12:41:30 -0500

    Improve `SinglePhasePoromechanicsEmbeddedFractures` and introduce `SinglePhasePoromechanicsConformingFractures` MGR strategy (#2747)

--- src/coreComponents/linearAlgebra/interfaces/hypre/mgrStrategies/SinglePhasePoromechanicsConformingFractures.hpp
+++ src/coreComponents/linearAlgebra/interfaces/hypre/mgrStrategies/SinglePhasePoromechanicsConformingFractures.hpp
@@ -13,13 +13,15 @@
  */
 
 /**
- * @file SinglePhasePoromechanicsEmbeddedFractures.hpp
+ * @file SinglePhasePoromechanicsConformingFractures.hpp
  */
 
-#ifndef GEOS_LINEARALGEBRA_INTERFACES_HYPREMGRSINGLEPHASEPOROMECHANICSEMBEDDEDFRACTURES_HPP_
-#define GEOS_LINEARALGEBRA_INTERFACES_HYPREMGRSINGLEPHASEPOROMECHANICSEMBEDDEDFRACTURES_HPP_
+#ifndef GEOS_LINEARALGEBRA_INTERFACES_HYPREMGRSINGLEPHASEPOROMECHANICSCONFORMINGFRACTURES_HPP_
+#define GEOS_LINEARALGEBRA_INTERFACES_HYPREMGRSINGLEPHASEPOROMECHANICSCONFORMINGFRACTURES_HPP_
 
 #include "linearAlgebra/interfaces/hypre/HypreMGR.hpp"
+
+#define BRANCH_MGR_FSOLVER
 
 namespace geos
 {
@@ -31,53 +33,59 @@ namespace mgr
 {
 
 /**
- * @brief SinglePhasePoromechanicsEmbeddedFractures strategy.
+ * @brief SinglePhasePoromechanicsConformingFractures strategy.
  *
  * dofLabel: 0 = displacement, x-component
  * dofLabel: 1 = displacement, y-component
  * dofLabel: 2 = displacement, z-component
- * dofLabel: 3 = wn
- * dofLabel: 4 = wt1
- * dofLabel: 5 = wt2
- * dofLabel: 6 = pressure (cell elem + fracture elems)
+ * dofLabel: 3 = pressure (cell elem + fracture elems)
+ * dofLabel: 4 = face-centered lagrange multiplier (tn)
+ * dofLabel: 5 = face-centered lagrange multiplier (tt1)
+ * dofLabel: 6 = face-centered lagrange multiplier (tt2)
+
  *
  * Ingredients:
- * 1. Level 1: F-points w (3,4,5), C-points pressure (0,1,2,6)
- * 2. Level 2: F-points displacement (0,1,2), C-points pressure (6)
- * 3. F-points smoother: BoomerAMG, single V-cycle
- * 4. C-points coarse-grid/Schur complement solver: BoomerAMG
- * 5. Global smoother: none
+ * 1. Level 1: F-points displacement (4,5,6), C-points pressure (0,1,2,3)
+ * 2. Level 2: F-points displacement (0,1,2), C-points pressure (3)
+ * 2. F-points smoother: BoomerAMG, single V-cycle
+ * 3. C-points coarse-grid/Schur complement solver: BoomerAMG
+ * 4. Global smoother: none
  */
-class SinglePhasePoromechanicsEmbeddedFractures : public MGRStrategyBase< 2 >
+class SinglePhasePoromechanicsConformingFractures : public MGRStrategyBase< 2 >
 {
 public:
 
   /**
    * @brief Constructor.
    */
-  explicit SinglePhasePoromechanicsEmbeddedFractures( arrayView1d< int const > const & )
+  explicit SinglePhasePoromechanicsConformingFractures( arrayView1d< int const > const & )
     : MGRStrategyBase( 7 )
   {
+
     // we keep u and p
     m_labels[0].push_back( 0 );
     m_labels[0].push_back( 1 );
     m_labels[0].push_back( 2 );
-    m_labels[0].push_back( 6 );
+    m_labels[0].push_back( 3 );
     // we keep p
-    m_labels[1].push_back( 6 );
+    m_labels[1].push_back( 3 );
 
     setupLabels();
 
     // Level 0
-    m_levelFRelaxType[0]         = MGRFRelaxationType::gsElimWPivoting; // gaussian elimination for the dispJump block
-    m_levelFRelaxIters[0]        = 1;
-    m_levelGlobalSmootherType[0] = MGRGlobalSmootherType::none;
-    m_levelInterpType[0]         = MGRInterpolationType::blockJacobi;
-    m_levelRestrictType[0]       = MGRRestrictionType::injection;
-    m_levelCoarseGridMethod[0]   = MGRCoarseGridMethod::galerkin;
+    m_levelFRelaxType[0]          = MGRFRelaxationType::none;
+    m_levelFRelaxIters[0]         = 0;
+
+    m_levelGlobalSmootherType[0]  = MGRGlobalSmootherType::ilu0;
+    m_levelGlobalSmootherIters[0] = 1;
+
+    m_levelInterpType[0]          = MGRInterpolationType::blockJacobi;
+    m_levelRestrictType[0]        = MGRRestrictionType::injection;
+    m_levelCoarseGridMethod[0]    = MGRCoarseGridMethod::galerkin;
 
     // Level 1
     m_levelFRelaxType[1]         = MGRFRelaxationType::amgVCycle;
+    m_levelFRelaxIters[1]        = 1;
     m_levelGlobalSmootherType[1] = MGRGlobalSmootherType::none;
     m_levelInterpType[1]         = MGRInterpolationType::jacobi;
     m_levelRestrictType[1]       = MGRRestrictionType::injection;
@@ -95,7 +103,6 @@ public:
               HypreMGRData & mgrData )
   {
     setReduction( precond, mgrData );
-
     GEOS_LAI_CHECK_ERROR( HYPRE_MGRSetPMaxElmts( precond.ptr, 0 ));
 
     // Configure the BoomerAMG solver used as F-relaxation for the second level
@@ -127,4 +134,4 @@ public:
 
 } // namespace geos
 
-#endif /*GEOS_LINEARALGEBRA_INTERFACES_HYPREMGRSINGLEPHASEPOROMECHANICSEMBEDDEDFRACTURES_HPP_*/
+#endif /*GEOS_LINEARALGEBRA_INTERFACES_HYPREMGRSINGLEPHASEPOROMECHANICSCONFORMINGFRACTURES_HPP_*/
