09855a32447fb3f8231d15d104cf4806ba36b565
jay-a <jay.appleton@colorado.edu>
2024-01-29 19:23:51 -0800

    runs basic uniaxial compression on a cylinder

--- host-configs/LLNL/quartz-clang@14.cmake
+++ host-configs/LLNL/quartz-clang@14.cmake
@@ -1,5 +1,9 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../../src/coreComponents/LvArray/host-configs/LLNL/quartz-clang@14.cmake)
 
+# C++
+# The "-march=native -mtune=native" which LvArray adds breaks the PVT package.
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG" CACHE STRING "" FORCE)
+
 # Fortran
 set(CMAKE_Fortran_COMPILER /usr/tce/packages/gcc/gcc-12.1.1-magic/bin/gfortran CACHE PATH "")
 set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -DNDEBUG -march=native -mtune=native" CACHE STRING "")
