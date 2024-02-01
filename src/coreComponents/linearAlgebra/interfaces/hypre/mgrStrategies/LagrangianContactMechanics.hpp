9c7311776d09e1e8bbae2404f6bd39c848a90fc3
Nicola Castelletto <castelletto1@llnl.gov>
2024-02-01 11:50:59 -0800

    Updating MGR strategy

--- src/coreComponents/linearAlgebra/interfaces/hypre/mgrStrategies/LagrangianContactMechanics.hpp
+++ src/coreComponents/linearAlgebra/interfaces/hypre/mgrStrategies/LagrangianContactMechanics.hpp
@@ -65,15 +65,12 @@ public:
     setupLabels();
 
     // Level 0
-    m_levelFRelaxType[0]          = MGRFRelaxationType::none;
-    m_levelFRelaxIters[0]         = 0;
-
-    m_levelGlobalSmootherType[0]  = MGRGlobalSmootherType::blockJacobi;
-    m_levelGlobalSmootherIters[0] = 1;
-
+    m_levelFRelaxType[0]          = MGRFRelaxationType::l1jacobi;
+    m_levelFRelaxIters[0]         = 1;
     m_levelInterpType[0]          = MGRInterpolationType::blockJacobi;
     m_levelRestrictType[0]        = MGRRestrictionType::injection;
     m_levelCoarseGridMethod[0]    = MGRCoarseGridMethod::galerkin;
+    m_levelGlobalSmootherType[0]  = MGRGlobalSmootherType::none;
   }
 
   /**
