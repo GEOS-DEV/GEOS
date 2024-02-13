0f0f8d16e692475a8a3cb7a3589fd82db185f2b1
Pavel Tomin <paveltomin@users.noreply.github.com>
2024-02-12 18:45:36 -0600

    Make possible to provide input PVT tables instead of internally computed (#2919)

--- src/coreComponents/constitutive/fluid/multifluid/CO2Brine/functions/CO2Solubility.hpp
+++ src/coreComponents/constitutive/fluid/multifluid/CO2Brine/functions/CO2Solubility.hpp
@@ -58,9 +58,7 @@ public:
     m_waterIndex( waterIndex ),
     m_phaseGasIndex( phaseGasIndex ),
     m_phaseLiquidIndex( phaseLiquidIndex )
-  {
-    GEOS_UNUSED_VAR( waterVapourisationTable );
-  }
+  {}
 
   template< int USD1 >
   GEOS_HOST_DEVICE
