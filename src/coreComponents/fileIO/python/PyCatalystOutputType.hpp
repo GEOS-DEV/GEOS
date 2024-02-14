3482d3643b2ebc99b16ed19c46f5197835a28110
Charles Gueunet <charles.gueunet@gmail.com>
2024-02-14 17:10:02 +0100

    Add python wrapping for Catalyst (attempt)

---
+++ src/coreComponents/fileIO/python/PyCatalystOutputType.hpp
@@ -0,0 +1,17 @@
+#ifndef GEOS_PYTHON_PYCATALYSTOUTPUTTYPE_HPP_
+#define GEOS_PYTHON_PYCATALYSTOUTPUTTYPE_HPP_
+
+#include "LvArray/src/python/pythonForwardDeclarations.hpp"
+#include "mesh/DomainPartition.hpp"
+
+namespace geos
+{
+namespace python
+{
+
+PyTypeObject * getPyCatalystOutputType();
+
+} // namespace python
+} // namespace geos
+
+#endif
