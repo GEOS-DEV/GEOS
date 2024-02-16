ee038bf68a5aba87efdbf4536f4121cd35fb1f9a
acitrain <60715545+acitrain@users.noreply.github.com>
2024-02-16 03:05:55 +0100

    Refactor wave solver files: Split field namespace for each physics (#2947)
    * Move fields for second solver into WaveSolverBaseFields
    * Split huge namespace inside WaveSolverBaseFields into several files with one namespace for each physics
    * Modify CMakeList
    * Move acousto-elastic coupling arrays into one specific file + do some cleanup
    * Remove unused include inside unit test
    * Remove default value (will be done inside last refactor PR)
    * Fix little bug when taking wrappers (wrong name)
    * Add unit test for first order elastic wave solver
    * uncrustify
    * Remove commented code
    * Fix CMakeLists
    * Fix CMakeList
    * Updating the integratedTests hash
    
    ---------
    
    Co-authored-by: Nicola Castelletto <castelletto1@llnl.gov>

--- src/coreComponents/physicsSolvers/wavePropagation/AcousticVTIWaveEquationSEMKernel.hpp
+++ src/coreComponents/physicsSolvers/wavePropagation/AcousticVTIWaveEquationSEMKernel.hpp
@@ -21,12 +21,12 @@
 
 #include "finiteElement/elementFormulations/Qk_Hexahedron_Lagrange_GaussLobatto.hpp"
 #include "finiteElement/kernelInterface/KernelBase.hpp"
-#include "WaveSolverBaseFields.hpp"
 #include "WaveSolverUtils.hpp"
-
+#include "AcousticVTIFields.hpp"
 
 namespace geos
 {
+using namespace fields;
 
 /// Namespace to contain the acoustic wave kernels.
 namespace acousticVTIWaveEquationSEMKernels
@@ -430,13 +430,13 @@ public:
           finiteElementSpace,
           inputConstitutiveType ),
     m_nodeCoords( nodeManager.getField< fields::referencePosition32 >() ),
-    m_p_n( nodeManager.getField< fields::wavesolverfields::Pressure_p_n >() ),
-    m_q_n( nodeManager.getField< fields::wavesolverfields::Pressure_q_n >() ),
-    m_stiffnessVector_p( nodeManager.getField< fields::wavesolverfields::StiffnessVector_p >() ),
-    m_stiffnessVector_q( nodeManager.getField< fields::wavesolverfields::StiffnessVector_q >() ),
-    m_epsilon( elementSubRegion.template getField< fields::wavesolverfields::Epsilon >() ),
-    m_delta( elementSubRegion.template getField< fields::wavesolverfields::Delta >() ),
-    m_vti_f( elementSubRegion.template getField< fields::wavesolverfields::F >() ),
+    m_p_n( nodeManager.getField< acousticvtifields::Pressure_p_n >() ),
+    m_q_n( nodeManager.getField< acousticvtifields::Pressure_q_n >() ),
+    m_stiffnessVector_p( nodeManager.getField< acousticvtifields::StiffnessVector_p >() ),
+    m_stiffnessVector_q( nodeManager.getField< acousticvtifields::StiffnessVector_q >() ),
+    m_epsilon( elementSubRegion.template getField< acousticvtifields::Epsilon >() ),
+    m_delta( elementSubRegion.template getField< acousticvtifields::Delta >() ),
+    m_vti_f( elementSubRegion.template getField< acousticvtifields::F >() ),
     m_dt( dt )
   {
     GEOS_UNUSED_VAR( edgeManager );
