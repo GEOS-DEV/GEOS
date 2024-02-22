fa3a9e3e6e9864592cb47e7d9645cf516c753c0e
matteofrigo5 <43966286+matteofrigo5@users.noreply.github.com>
2024-02-22 15:41:49 +0100

    Adding Sherlock GPU to CI tests (#2977)
    * Adding Sherlock GPU in CI tests
    * Update TPL build
    * TPL build - bug DATA_BASENAME_WE fixed - sherlock-modules updated
    * GCP_BUCKET updated
    * BUILD_AND_TEST_CLI_ARGS updated
    * Cuda Capability modified
    
    ---------
    
    Co-authored-by: TotoGaz <49004943+TotoGaz@users.noreply.github.com>
    Co-authored-by: Nicola Castelletto <castelletto1@llnl.gov>

--- host-configs/Stanford/sherlock-modules.sh
+++ host-configs/Stanford/sherlock-modules.sh
@@ -1,11 +1,12 @@
 module load system
-module load git/2.12.2
-module load git-lfs/2.4.0
-module load cmake/3.20.3
-module load ninja/1.9.0
-module load py-mpi4py/3.0.3_py36
-module load py-h5py/2.10.0_py36
+module load git
+module load git-lfs
+module load cmake
 module load gcc/10.1.0
-module load openmpi/4.1.0
-module load cuda/11.2.0
+module load openmpi/4.1.2
+module load openblas/0.3.10
 module load libevent/2.1.12 # needed for silo
+module load py-mpi4py/3.1.3_py39
+module load py-h5py/3.7.0_py39
+module unload cuda
+module load cuda/11.7.1
