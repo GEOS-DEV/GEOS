da69f82e7dda159fe634f6e97383876e7f399862
wrtobin <tobin6@llnl.gov>
2024-03-06 10:45:49 -0800

    fix restart flag value

--- src/coreComponents/fileIO/Outputs/TimeHistoryOutput.cpp
+++ src/coreComponents/fileIO/Outputs/TimeHistoryOutput.cpp
@@ -55,10 +55,9 @@ TimeHistoryOutput::TimeHistoryOutput( string const & name,
   registerWrapper( viewKeys::timeHistoryMPIOString(), &m_useMPIO ).
     setApplyDefaultValue( 1 ).
     setInputFlag( InputFlags::OPTIONAL ).
-    setRestartFlags( RestartFlags::WRITE_AND_READ ).
     setDescription( "Whether to use MPIO to write a single file or a separate file for each rank (only applicable to HDF5 format)." );
 
-}
+}`
 
 void TimeHistoryOutput::initCollectorParallel( DomainPartition const & domain, HistoryCollection & collector )
 {
