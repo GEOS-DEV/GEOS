6096feaaee5644c28fcb3a3d799491637c6cd6d5
Matteo Cusini <matteo.cusini@gmail.com>
2024-04-22 14:49:51 -0700

    force ordering of faces.

--- src/coreComponents/mainInterface/ProblemManager.cpp
+++ src/coreComponents/mainInterface/ProblemManager.cpp
@@ -690,7 +690,7 @@ void ProblemManager::generateMesh()
       // because the ghosting ensures that the neighbor cells of the fracture elements are available.
       // These neighbor cells are providing the node information to the fracture elements.
 
-      // 2. We flip the face normals of faces adiajent to the faceElements if they are not pointing in the
+      // 2. We flip the face normals of faces adjacent to the faceElements if they are not pointing in the
       // direction of the fracture.
       auto elemCenter = meshLevel.getElemManager().constructArrayViewAccessor< real64, 2 >( CellElementSubRegion::viewKeyStruct::elementCenterString() );
 
@@ -700,16 +700,28 @@ void ProblemManager::generateMesh()
         subRegion.calculateElementGeometricQuantities( nodeManager, faceManager );
 
         // 2.
-        ArrayOfArraysView< localIndex const > const & elemsToFaces = subRegion.faceList().toViewConst();
+        ArrayOfArraysView< localIndex > const & elemsToFaces = subRegion.faceList().toView();
         arrayView2d< localIndex const > const & faceToElementRegionIndex = faceManager.elementRegionList();
         arrayView2d< localIndex const > const & faceToElementSubRegionIndex = faceManager.elementSubRegionList();
         arrayView2d< localIndex const > const & faceToElementIndex = faceManager.elementList();
 
         arrayView2d< real64 const > const faceCenter = faceManager.faceCenter();
         FaceManager::NodeMapType & faceToNodes = faceManager.nodeList();
+        auto faceLocalToGlobal = faceManager.localToGlobalMap();
+        auto fracElemLocalToGlobal = subRegion.localToGlobalMap();
+        int const thisRank = MpiWrapper::commRank();
+        auto ghostRank = subRegion.ghostRank();
 
-        // We nee to modify the normals and the nodes ordering to be consistent.
+        // We need to modify the normals and the nodes ordering to be consistent.
         arrayView2d< real64 > const faceNormal = faceManager.faceNormal();
+        int count = 0;
+        for( int i = 0; i < subRegion.size(); i++)
+        {
+          if ( ghostRank[i] < 0 )
+          {
+            count++;
+          }
+        }
         forAll< parallelHostPolicy >( subRegion.size(), [=, &faceToNodes]( localIndex const kfe )
         {
           if( elemsToFaces.sizeOfArray( kfe ) != 2 )
@@ -717,9 +729,22 @@ void ProblemManager::generateMesh()
             return;
           }
 
+          
+
+          if ( faceLocalToGlobal[ elemsToFaces[kfe][0] ] > faceLocalToGlobal [ elemsToFaces[kfe][1] ] )
+          {
+            localIndex const temp = elemsToFaces[kfe][0];
+            elemsToFaces[kfe][0]  =  elemsToFaces[kfe][1];
+            elemsToFaces[kfe][1]  = temp;
+          }
+
           localIndex const f0 = elemsToFaces[kfe][0];
           localIndex const f1 = elemsToFaces[kfe][1];
-
+          
+          if (faceLocalToGlobal[ f0 ] > faceLocalToGlobal [ f1 ])
+          {
+            std::cout << "rank: " << thisRank << " - fracElem: " << fracElemLocalToGlobal[kfe] << " f0: " << faceLocalToGlobal[f0] << " f1: " << faceLocalToGlobal[f1] << std::endl;   
+          }
           /// Note: I am assuming that the 0 element is the elementSubregion one for faces
           /// touching both a 3D and a 2D cell.
           localIndex const er0  = faceToElementRegionIndex[f0][0];
@@ -752,7 +777,7 @@ void ProblemManager::generateMesh()
             std::reverse( faceToNodes[f1].begin(), faceToNodes[f1].end() );
           }
         } );
-
+        std::cout << "rank" << thisRank  << "n surfaceElements:" << count << std::endl;
       } );
 
       faceManager.setIsExternal();
