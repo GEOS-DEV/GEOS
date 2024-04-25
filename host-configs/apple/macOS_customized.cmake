41ef548f108844331572e6e63202968bc3f6f755
Matteo Cusini <matteo.cusini@gmail.com>
2024-04-25 16:54:40 -0700

    cmake file.

--- host-configs/apple/macOS_customized.cmake
+++ host-configs/apple/macOS_customized.cmake
@@ -1,7 +1,7 @@
-set( HOMEBREW_DIR "/Users/cusini1/local" )
-set( CONFIG_NAME "macOS_Matteo" ) 
+set( HOMEBREW_DIR $ENV{HOMEBREW_DIR} )
+set( CONFIG_NAME $ENV{GEOS_CONFIG_NAME} ) 
 
-set(Python3_ROOT_DIR /Users/cusini1/pythonVenv/geosxEnv CACHE PATH "")
+set(Python3_ROOT_DIR $ENV{GEOS_PYTHON_DIR} CACHE PATH "")
 set(Python3_EXECUTABLE ${Python3_ROOT_DIR}/bin/python3 CACHE PATH "")
 
 include(${CMAKE_CURRENT_LIST_DIR}/macOS_base.cmake)
