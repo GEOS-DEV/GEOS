55810e56a7d4f1b69e2dcf0cd174a81fa6c27405
dkachuma <dickkachuma@googlemail.com>
2024-05-13 15:00:20 -0500

    Move thermal kernel

--- src/coreComponents/physicsSolvers/fluidFlow/DirichletFaceBasedAssemblyKernelFVMImpl.hpp
+++ src/coreComponents/physicsSolvers/fluidFlow/DirichletFaceBasedAssemblyKernelFVMImpl.hpp
@@ -64,6 +64,50 @@ DirichletFaceBasedAssemblyKernel( integer const numPhases,
 
 } // namespace isothermalCompositionalMultiphaseFVMKernels
 
+namespace thermalCompositionalMultiphaseFVMKernels
+{
+
+template< integer NUM_COMP, integer NUM_DOF, typename FLUIDWRAPPER >
+DirichletFaceBasedAssemblyKernel< NUM_COMP, NUM_DOF, FLUIDWRAPPER >::
+DirichletFaceBasedAssemblyKernel( integer const numPhases,
+                                  globalIndex const rankOffset,
+                                  FaceManager const & faceManager,
+                                  BoundaryStencilWrapper const & stencilWrapper,
+                                  FLUIDWRAPPER const & fluidWrapper,
+                                  DofNumberAccessor const & dofNumberAccessor,
+                                  CompFlowAccessors const & compFlowAccessors,
+                                  ThermalCompFlowAccessors const & thermalCompFlowAccessors,
+                                  MultiFluidAccessors const & multiFluidAccessors,
+                                  ThermalMultiFluidAccessors const & thermalMultiFluidAccessors,
+                                  CapPressureAccessors const & capPressureAccessors,
+                                  PermeabilityAccessors const & permeabilityAccessors,
+                                  ThermalConductivityAccessors const & thermalConductivityAccessors,
+                                  real64 const dt,
+                                  CRSMatrixView< real64, globalIndex const > const & localMatrix,
+                                  arrayView1d< real64 > const & localRhs,
+                                  BitFlags< isothermalCompositionalMultiphaseFVMKernels::FaceBasedAssemblyKernelFlags > kernelFlags )
+  : Base( numPhases,
+          rankOffset,
+          faceManager,
+          stencilWrapper,
+          fluidWrapper,
+          dofNumberAccessor,
+          compFlowAccessors,
+          multiFluidAccessors,
+          capPressureAccessors,
+          permeabilityAccessors,
+          dt,
+          localMatrix,
+          localRhs,
+          kernelFlags ),
+  m_temp( thermalCompFlowAccessors.get( fields::flow::temperature {} ) ),
+  m_phaseEnthalpy( thermalMultiFluidAccessors.get( fields::multifluid::phaseEnthalpy {} ) ),
+  m_dPhaseEnthalpy( thermalMultiFluidAccessors.get( fields::multifluid::dPhaseEnthalpy {} ) ),
+  m_thermalConductivity( thermalConductivityAccessors.get( fields::thermalconductivity::effectiveConductivity {} ) )
+{}
+
+} //namespace thermalCompositionalMultiphaseFVMKernels
+
 } // namespace geos
 
 #endif //GEOS_PHYSICSSOLVERS_FLUIDFLOW_DIRICHLETFACEBASEDASSEMBLYKERNELFVMIMPL_HPP
