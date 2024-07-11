364fe6815df8ba777c0a4c8c27008458c2ae623f
Matteo Cusini <matteo.cusini@gmail.com>
2024-07-10 21:34:43 -0700

    revert raja update related changes

--- src/coreComponents/common/unitTests/testLifoStorage.cpp
+++ src/coreComponents/common/unitTests/testLifoStorage.cpp
@@ -58,8 +58,8 @@ struct RAJAHelper< parallelHostPolicy >
 template< unsigned long THREADS_PER_BLOCK >
 using devicePolicy = RAJA::cuda_exec< THREADS_PER_BLOCK >;
 
-template< typename X, typename Y, typename C, size_t BLOCK_SIZE, bool ASYNC >
-struct RAJAHelper< RAJA::policy::cuda::cuda_exec_explicit< X, Y, C, BLOCK_SIZE, ASYNC > >
+template< typename X, typename Y, size_t BLOCK_SIZE, bool ASYNC >
+struct RAJAHelper< RAJA::policy::cuda::cuda_exec_explicit< X, Y, BLOCK_SIZE, ASYNC > >
 {
   using ReducePolicy = RAJA::cuda_reduce;
   using AtomicPolicy = RAJA::cuda_atomic;
