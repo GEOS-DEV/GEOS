5aa4985b926053b0e51778100f0e3ca8d9fcca80
MelReyCG <melvin.rey@capgemini.com>
2024-08-22 15:54:23 +0200

    Precising a bit signatures, extending the docs

--- src/coreComponents/common/logger/LoggingObject.hpp
+++ src/coreComponents/common/logger/LoggingObject.hpp
@@ -29,7 +29,8 @@ namespace logging
 {
 
 /**
- * @brief Interface (implementation contract) of any class that can send log messages (GeneralLogger, Group)
+ * @brief Interface (implementation contract) of any class that can send log messages (GeneralLogger, Group).
+ * @tparam CONTEXT_T Data structure of the context of this class (LogMsgGeneralContext, LogMsgTargetContext).
  * @todo adapt the new call convention everywhere
  * @todo Group class should implement it to log contextualized messages (implement logMsg() as private)
  * @todo check / add documentation
@@ -37,32 +38,46 @@ namespace logging
 template< typename CONTEXT_T >
 class LoggingObject
 {
+protected:
+
+  /**
+   * @brief setting / logging methods. Meant to be public in inheriting classes
+   */
+  ///@{
 
   void setLogLevel( LogLevel value );
 
+  virtual void logRank0( LogMsg::CallParams callParams, INPUTS ... msgs ) const = 0;
+  virtual void logRank0( LogMsg::CallParams callParams, LogRouter router, INPUTS ... msg ) const = 0;
+  virtual void logRank0Fmt( LogMsg::CallParams callParams, string_view msg, INPUTS ... fmtParams ) const = 0;
+  virtual void logRank0Fmt( LogMsg::CallParams callParams, LogRouter router, string_view msg, INPUTS ... fmtParams ) const = 0;
 
-  virtual void logRank0( LogMsg::CallParams loc, INPUTS ... msgs ) const = 0;
-  virtual void logRank0( LogMsg::CallParams loc, LogRouter router, INPUTS ... msg ) const = 0;
-  virtual void logRank0Fmt( LogMsg::CallParams loc, LogRouter router, string_view msg, INPUTS ... parameters ) const = 0;
-
-  virtual void log( LogMsg::CallParams loc, INPUTS ... msgs ) const = 0;
-  virtual void log( LogMsg::CallParams loc, LogRouter router, INPUTS ... msg ) const = 0;
-  virtual void logFmt( LogMsg::CallParams loc, LogRouter router, string_view msg, INPUTS ... parameters ) const = 0;
-
-  // TODO not exactly sure how to implement that (avoid formating if cond is false, keeping condition as metadata). Should we drop this in favor of classic compact if() expressions?
-  // virtual void logRank0If( bool cond, LogMsg::CallParams loc, string_view msg, LogRouter router = LogRouter::m_main ) const = 0;
+  virtual void log( LogMsg::CallParams callParams, INPUTS ... msgs ) const = 0;
+  virtual void log( LogMsg::CallParams callParams, LogRouter router, INPUTS ... msg ) const = 0;
+  virtual void logFmt( LogMsg::CallParams callParams, string_view msg, INPUTS ... fmtParams ) const = 0;
+  virtual void logFmt( LogMsg::CallParams callParams, LogRouter router, string_view msg, INPUTS ... fmtParams ) const = 0;
 
   // TODO not exactly sure how to implement that (avoid formating if cond is false, keeping condition as metadata). Should we drop this in favor of classic compact if() expressions?
-  // virtual void logIf( bool cond, LogMsg::CallParams loc, string_view msg, LogRouter router = LogRouter::m_main ) const = 0;
+  // virtual void logRank0If( bool cond, LogMsg::CallParams callParams, string_view msg, LogRouter router = LogRouter::m_main ) const = 0;
+
+  // TODO not exactly sure how to implement that (avoid formating if cond is false, keeping condition as metadata). Should we drop this in favor of classic compact if() expressions?
+  // virtual void logIf( bool cond, LogMsg::CallParams callParams, string_view msg, LogRouter router = LogRouter::m_main ) const = 0;
 
   //TODO : analyze how we could support this in an non-intrusive way (about performances / CUDA cache usage) or as a developer limited feature
-  // logDevice( SrcCodeLoc loc, INPUTS ... inputs )
+  // logDevice( SrcCodeLoc callParams, INPUTS ... inputs )
 
-protected:
+  ///@}
 
-  LogMsg::Context createMessageContext( LogMsg::CallParams callParams ) const =0;
+  /**
+   * @brief Internal functionnal methods.
+   */
+  ///@{
 
-  CONTEXT_T getLoggingContext() const =0;
+  LogMsg::Context createMessageContext( LogMsg::CallParams callParams ) const = 0;
+
+  CONTEXT_T getLoggingContext() const = 0;
+
+  ///@}
 
 };
 
