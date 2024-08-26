a44164a78b8c2cac5e0265d272df40b46a1b4a96
Randolph R. Settgast <settgast1@llnl.gov>
2024-08-25 23:26:01 -0700

    some modifications for fpe on testFlowStatistics

--- host-configs/LLNL/ruby-clang-14.cmake
+++ host-configs/LLNL/ruby-clang-14.cmake
@@ -2,5 +2,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/../../src/coreComponents/LvArray/host-configs/
 
 # MPI
 set(MPI_HOME /usr/tce/packages/mvapich2/mvapich2-2.3.7-clang-14.0.6-magic CACHE PATH "")
+#set( CMAKE_CXX_FLAGS "-march=x86-64-v2 -mno-avx512f" CACHE STRING "" FORCE)
+#set( CMAKE_CXX_FLAGS "-g -O2 -fno-omit-frame-pointer -fsanitize=address" CACHE STRING "" FORCE)
 
 include(${CMAKE_CURRENT_LIST_DIR}/llnl-cpu-base.cmake)
