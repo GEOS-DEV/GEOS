2fb8cd9193394651f6209060b6fa5e167666d6d0
Randolph Settgast <settgast1@llnl.gov>
2024-09-06 00:23:32 -0700

    add path argument to mandelFigure.py

--- src/docs/sphinx/advancedExamples/validationStudies/poromechanics/mandel/mandelFigure.py
+++ src/docs/sphinx/advancedExamples/validationStudies/poromechanics/mandel/mandelFigure.py
@@ -7,7 +7,8 @@ from mpmath import *
 import math
 from math import sin, cos, tan, exp, atan, asin
 from scipy.optimize import newton
-
+import os
+import argparse
 
 class Mandel:
 
@@ -140,11 +141,24 @@ def getGeometryFromXML(xmlFilePath):
 
 
 def main():
+   # Initialize the argument parser
+    parser = argparse.ArgumentParser(description="generate plots for Mandel example problem.")
+
+    # Add arguments to accept individual file paths
+    parser.add_argument('--geosDir', help='Path to the GEOS repository ', required=True)
+    parser.add_argument('--outputDir', help='Path to output directory', required=True)
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+
     # File path
-    hdf5File1Path = "pressure_history.hdf5"
-    hdf5File2Path = "displacement_history.hdf5"
-    xmlFile1Path = "../../../../../../../inputFiles/poromechanics/PoroElastic_Mandel_base.xml"
-    xmlFile2Path = "../../../../../../../inputFiles/poromechanics/PoroElastic_Mandel_benchmark_fim.xml"
+    outputDir = args.outputDir
+    geosDir = args.geosDir
+    hdf5File1Path = outputDir + "/pressure_history.hdf5"
+    hdf5File2Path = outputDir + "/displacement_history.hdf5"
+    xmlFile1Path = geosDir + "/inputFiles/poromechanics/PoroElastic_Mandel_base.xml"
+    xmlFile2Path = geosDir + "/inputFiles/poromechanics/PoroElastic_Mandel_benchmark_fim.xml"
 
     # Read HDF5
     # Global Coordinate of Element Center
