2f6d238d7bdfa4bc5f602b1008dd17e2bbf86eb4
j0405284 <stefano.frambati@total.com>
2024-09-20 17:51:58 +0200

    do not install

--- scripts/ci_build_and_test_in_container.sh
+++ scripts/ci_build_and_test_in_container.sh
@@ -257,7 +257,7 @@ if [[ "${BUILD_EXE_ONLY}" = true ]]; then
   or_die cmake --build . -j $NPROC --target geosx
 else
   or_die cmake --build . -j $NPROC --target testLifoStorage
-  or_die cmake --install .
+  #or_die cmake --install .
   or_die ./tests/testLifoStorage
 
   if [[ ! -z "${DATA_BASENAME_WE}" ]]; then
