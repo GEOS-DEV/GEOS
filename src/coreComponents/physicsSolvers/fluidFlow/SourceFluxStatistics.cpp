7cd2ddb4d2d14c7c936814513744c8bca842173d
Randolph R. Settgast <settgast1@llnl.gov>
2024-09-22 20:20:37 -0700

    remove some debugging output

--- src/coreComponents/physicsSolvers/fluidFlow/SourceFluxStatistics.cpp
+++ src/coreComponents/physicsSolvers/fluidFlow/SourceFluxStatistics.cpp
@@ -31,14 +31,12 @@ SourceFluxStatsAggregator::SourceFluxStatsAggregator( const string & name,
                                                       Group * const parent ):
   Base( name, parent )
 {
-  std::cout<<"breakpoint 1"<<std::endl;
   getWrapperBase( Group::viewKeyStruct::logLevelString() ).
     appendDescription( GEOS_FMT( "\n- Log Level 1 outputs the sum of all {0}(s) produced rate & mass,\n"
                                  "- Log Level 2 details values for each {0},\n"
                                  "- Log Level 3 details values for each region.",
                                  SourceFluxBoundaryCondition::catalogName() ) );
 
-  std::cout<<"breakpoint 2"<<std::endl;
   registerWrapper( viewKeyStruct::fluxNamesString().data(), &m_fluxNames ).
     setRTTypeName( rtTypes::CustomTypes::groupNameRefArray ).
     setInputFlag( InputFlags::OPTIONAL ).
@@ -47,8 +45,6 @@ SourceFluxStatsAggregator::SourceFluxStatsAggregator( const string & name,
     setDescription( GEOS_FMT( "Name(s) array of the {0}(s) for which we want the statistics. "
                               "Use \"*\" to target all {0}.",
                               SourceFluxBoundaryCondition::catalogName() ) );
-  std::cout<<"breakpoint 3"<<std::endl;
-
 }
 
 void SourceFluxStatsAggregator::postInputInitialization()
