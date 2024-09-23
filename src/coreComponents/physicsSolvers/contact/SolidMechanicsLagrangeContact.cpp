766728cf6d7f51be85358405ce4775e3613cde1c
Pavel Tomin <paveltomin@users.noreply.github.com>
2024-09-23 17:20:45 -0500

    Update SolidMechanicsLagrangeContact.cpp

--- src/coreComponents/physicsSolvers/contact/SolidMechanicsLagrangeContact.cpp
+++ src/coreComponents/physicsSolvers/contact/SolidMechanicsLagrangeContact.cpp
@@ -2293,7 +2293,6 @@ bool SolidMechanicsLagrangeContact::updateConfiguration( DomainPartition & domai
               {
                 currentTau *= (1.0 + m_slidingCheckTolerance);
               }
-
               if( currentTau > limitTau )
               {
                 if( originalFractureState == FractureState::Stick )
