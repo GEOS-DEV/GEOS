36046933030639b4bc8457e010aa04dea9029fa7
Pavel Tomin <ptls@PW0BMYGN>
2024-09-24 08:58:14 -0500

    this should do

--- src/coreComponents/physicsSolvers/fluidFlow/SinglePhaseBase.cpp
+++ src/coreComponents/physicsSolvers/fluidFlow/SinglePhaseBase.cpp
@@ -700,8 +700,8 @@ void SinglePhaseBase::implicitStepSetup( real64 const & GEOS_UNUSED_PARAM( time_
 
     } );
 
-    mesh.getElemManager().forElementSubRegions< FaceElementSubRegion, SurfaceElementSubRegion >( regionNames, [&]( localIndex const,
-                                                                                                                   auto & subRegion )
+    mesh.getElemManager().forElementSubRegions< SurfaceElementSubRegion >( regionNames, [&]( localIndex const,
+                                                                                             SurfaceElementSubRegion & subRegion )
     {
       arrayView1d< real64 const > const aper = subRegion.getField< fields::flow::hydraulicAperture >();
       arrayView1d< real64 > const aper0 = subRegion.getField< fields::flow::aperture0 >();
