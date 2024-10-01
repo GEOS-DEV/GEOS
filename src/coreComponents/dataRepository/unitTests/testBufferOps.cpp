33af708a5651fad0abe97e66243e9d10a1274db6
wrtobin <tobin6@llnl.gov>
2024-10-01 11:51:36 -0700

    what if we just turn those tests off on gcc-9.4.0 due to the double-fun compiler bugs

--- src/coreComponents/dataRepository/unitTests/testBufferOps.cpp
+++ src/coreComponents/dataRepository/unitTests/testBufferOps.cpp
@@ -91,51 +91,45 @@ TEST( testBufferOps, test_pack_unpack_data_array )
 
 }
 
+#if !defined(__GNUC__) || (__GNUC__ != 9 || __GNUC_MINOR__ != 4 || __GNUC_PATCHLEVEL__ != 0)
 TEST( testBufferOps, test_pack_unpack_data_arrayofarrays )
 {
-  // ArrayOfArrays< int > val1( 2, 1 );
-  // val1.emplaceBack( 0, 8675309 );
-  // val1.emplaceBack( 1, 9035768 );
-  // ASSERT_EQ( val1.size(), 2 );
-  // ASSERT_EQ( val1.sizeOfArray( 0 ), 1 );
-  // ASSERT_EQ( val1.sizeOfArray( 1 ), 1 );
-  // buffer_unit_type * buff_head = nullptr;
-  // localIndex sz = PackData< false >( buff_head, val1 );
-  // ASSERT_EQ( sz, 8 );
-  // buffer_type buff( sz );
-  // buff_head = &buff[0];
-  // localIndex sz1 = PackData< true >( buff_head, val1 );
-  // // There is no UnpackData as we only use the Pack*Data functions for history output
-  // // Packing raw data should result in the buffer containing only the integers themselves
-  // ASSERT_EQ( sz1, sz );
-  // ASSERT_EQ( *reinterpret_cast< int * >( &buff[0] ), 8675309 );
-  // ASSERT_EQ( *reinterpret_cast< int * >( &buff[4] ), 9035768 );
+  ArrayOfArrays< int > val1( 2, 1 );
+  val1.emplaceBack( 0, 8675309 );
+  val1.emplaceBack( 1, 9035768 );
+  ASSERT_EQ( val1.size(), 2 );
+  ASSERT_EQ( val1.sizeOfArray( 0 ), 1 );
+  ASSERT_EQ( val1.sizeOfArray( 1 ), 1 );
+  buffer_unit_type * buff_head = nullptr;
+  localIndex sz = PackData< false >( buff_head, val1 );
+  ASSERT_EQ( sz, 8 );
+  buffer_type buff( sz );
+  buff_head = &buff[0];
+  localIndex sz1 = PackData< true >( buff_head, val1 );
+  // There is no UnpackData as we only use the Pack*Data functions for history output
+  // Packing raw data should result in the buffer containing only the integers themselves
+  ASSERT_EQ( sz1, sz );
+  ASSERT_EQ( *reinterpret_cast< int * >( &buff[0] ), 8675309 );
+  ASSERT_EQ( *reinterpret_cast< int * >( &buff[4] ), 9035768 );
 }
 
 TEST( testBufferOps, test_pack_unpack_data_arrayofsets )
 {
-#if !defined(__clang__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Warray-bounds"
-#pragma GCC diagnostic ignored "-Wstringop-overflow="
-#endif
   ArrayOfSets< int > val1( 2, 1 );
-  // val1.insertIntoSet( 0, 8675309 );
-  // val1.insertIntoSet( 1, 9035768 );
-  // buffer_unit_type * buff_head = nullptr;
-  // localIndex sz = PackData< false >( buff_head, val1 );
-  // buffer_type buff( sz );
-  // buff_head = &buff[0];
-  // localIndex sz1 = PackData< true >( buff_head, val1 );
-  // // There is no UnpackData as we only use the Pack*Data functions for history output
-  // // Packing raw data should result in the buffer containing only the integers themselves
-  // ASSERT_EQ( sz1, sz );
-  // ASSERT_EQ( *reinterpret_cast< int * >( &buff[0] ), 8675309 );
-  // ASSERT_EQ( *reinterpret_cast< int * >( &buff[4] ), 9035768 );
-#if !defined(__clang__)
-#pragma GCC diagnostic pop
-#endif
+  val1.insertIntoSet( 0, 8675309 );
+  val1.insertIntoSet( 1, 9035768 );
+  buffer_unit_type * buff_head = nullptr;
+  localIndex sz = PackData< false >( buff_head, val1 );
+  buffer_type buff( sz );
+  buff_head = &buff[0];
+  localIndex sz1 = PackData< true >( buff_head, val1 );
+  // There is no UnpackData as we only use the Pack*Data functions for history output
+  // Packing raw data should result in the buffer containing only the integers themselves
+  ASSERT_EQ( sz1, sz );
+  ASSERT_EQ( *reinterpret_cast< int * >( &buff[0] ), 8675309 );
+  ASSERT_EQ( *reinterpret_cast< int * >( &buff[4] ), 9035768 );
 }
+#endif
 
 TEST( testBufferOps, test_pack_unpack_data_maps )
 {
@@ -204,58 +198,60 @@ TEST( testBufferOps, test_pack_unpack_data_by_index_array )
   ASSERT_EQ( *reinterpret_cast< int * >( &buff[0] ), 8675309 );
 }
 
+#if !defined(__GNUC__) || (__GNUC__ != 9 || __GNUC_MINOR__ != 4 || __GNUC_PATCHLEVEL__ != 0)
 TEST( testBufferOps, test_pack_unpack_data_by_index_arrayofarrays )
 {
-  // ArrayOfArrays< int > val1( 2, 1 );
-  // val1.emplaceBack( 0, 8675309 );
-  // val1.emplaceBack( 1, 9035768 );
-  // ASSERT_EQ( val1.size(), 2 );
-  // ASSERT_EQ( val1.sizeOfArray( 0 ), 1 );
-  // ASSERT_EQ( val1.sizeOfArray( 1 ), 1 );
-  // array1d< localIndex > idx( 1 );
-  // idx[0] = 1;
-  // ASSERT_EQ( idx.size(), 1 );
-  // ASSERT_EQ( idx[0], 1 );
-  // buffer_unit_type * buff_head = nullptr;
-  // localIndex sz = PackDataByIndex< false >( buff_head, val1, idx );
-  // buffer_type buff( sz );
-  // buff_head = &buff[0];
-  // localIndex sz1 = PackDataByIndex< true >( buff_head, val1, idx );
-  // // There is no UnpackDataByIndex as we only use the Pack*Data functions for history output
-  // // Packing raw data should result in the buffer containing only the integer itself
-  // ASSERT_EQ( sz1, sz );
-  // ASSERT_EQ( *reinterpret_cast< int * >( &buff[0] ), 9035768 );
+  ArrayOfArrays< int > val1( 2, 1 );
+  val1.emplaceBack( 0, 8675309 );
+  val1.emplaceBack( 1, 9035768 );
+  ASSERT_EQ( val1.size(), 2 );
+  ASSERT_EQ( val1.sizeOfArray( 0 ), 1 );
+  ASSERT_EQ( val1.sizeOfArray( 1 ), 1 );
+  array1d< localIndex > idx( 1 );
+  idx[0] = 1;
+  ASSERT_EQ( idx.size(), 1 );
+  ASSERT_EQ( idx[0], 1 );
+  buffer_unit_type * buff_head = nullptr;
+  localIndex sz = PackDataByIndex< false >( buff_head, val1, idx );
+  buffer_type buff( sz );
+  buff_head = &buff[0];
+  localIndex sz1 = PackDataByIndex< true >( buff_head, val1, idx );
+  // There is no UnpackDataByIndex as we only use the Pack*Data functions for history output
+  // Packing raw data should result in the buffer containing only the integer itself
+  ASSERT_EQ( sz1, sz );
+  ASSERT_EQ( *reinterpret_cast< int * >( &buff[0] ), 9035768 );
 }
 
 TEST( testBufferOps, test_pack_unpack_by_index_arrayofarrays )
 {
-  // ArrayOfArrays< int > val1( 2, 1 );
-  // val1.emplaceBack( 0, 8675309 );
-  // val1.emplaceBack( 1, 9035768 );
-  // ASSERT_EQ( val1.size(), 2 );
-  // ASSERT_EQ( val1.sizeOfArray( 0 ), 1 );
-  // ASSERT_EQ( val1.sizeOfArray( 1 ), 1 );
-  // array1d< localIndex > idx( 1 );
-  // idx[0] = 1;
-  // buffer_unit_type * buff_head = nullptr;
-  // localIndex sz = PackByIndex< false >( buff_head, val1, idx );
-  // buffer_type buff( sz );
-  // buff_head = &buff[0];
-  // localIndex sz1 = PackByIndex< true >( buff_head, val1, idx );
-  // ArrayOfArrays< int > val2( 2, 1 );
-  // const buffer_unit_type * c_buff_head = &buff[0];
-  // localIndex sz2 = UnpackByIndex( c_buff_head, val2, idx, MPI_REPLACE );
-  // ASSERT_EQ( sz1, sz );
-  // ASSERT_EQ( sz2, sz );
-  // ASSERT_EQ( val2[1][0], 9035768 );
-  // c_buff_head = &buff[0];
-  // UnpackByIndex( c_buff_head, val2, idx, MPI_SUM );
-  // ASSERT_EQ( val2[1][0], 9035768*2 );
-  // val2[1][0] = 9035768 - 1;
-  // c_buff_head = &buff[0];
-  // UnpackByIndex( c_buff_head, val2, idx, MPI_MAX );
-  // ASSERT_EQ( val2[1][0], 9035768 );
+  ArrayOfArrays< int > val1( 2, 1 );
+  val1.emplaceBack( 0, 8675309 );
+  val1.emplaceBack( 1, 9035768 );
+  ASSERT_EQ( val1.size(), 2 );
+  ASSERT_EQ( val1.sizeOfArray( 0 ), 1 );
+  ASSERT_EQ( val1.sizeOfArray( 1 ), 1 );
+  array1d< localIndex > idx( 1 );
+  idx[0] = 1;
+  buffer_unit_type * buff_head = nullptr;
+  localIndex sz = PackByIndex< false >( buff_head, val1, idx );
+  buffer_type buff( sz );
+  buff_head = &buff[0];
+  localIndex sz1 = PackByIndex< true >( buff_head, val1, idx );
+  ArrayOfArrays< int > val2( 2, 1 );
+  const buffer_unit_type * c_buff_head = &buff[0];
+  localIndex sz2 = UnpackByIndex( c_buff_head, val2, idx, MPI_REPLACE );
+  ASSERT_EQ( sz1, sz );
+  ASSERT_EQ( sz2, sz );
+  ASSERT_EQ( val2[1][0], 9035768 );
+  c_buff_head = &buff[0];
+  UnpackByIndex( c_buff_head, val2, idx, MPI_SUM );
+  ASSERT_EQ( val2[1][0], 9035768*2 );
+  val2[1][0] = 9035768 - 1;
+  c_buff_head = &buff[0];
+  UnpackByIndex( c_buff_head, val2, idx, MPI_MAX );
+  ASSERT_EQ( val2[1][0], 9035768 );
 }
+#endif
 
 TEST( testBufferOps, test_pack_unpack_arrayslice )
 {
