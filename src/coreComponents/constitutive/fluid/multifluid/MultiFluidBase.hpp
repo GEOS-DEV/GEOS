6114c72e4f2622e845c216d82722054f580aef89
Pavel Tomin <ptls@PW0BMYGN>
2024-10-02 15:34:04 -0500

    two hard crash fixes

--- src/coreComponents/constitutive/fluid/multifluid/MultiFluidBase.hpp
+++ src/coreComponents/constitutive/fluid/multifluid/MultiFluidBase.hpp
@@ -783,12 +783,14 @@ MultiFluidBase::KernelWrapper::
   real64 totalMolality = 0.0;
   for( integer ic = 0; ic < numComps; ++ic )
   {
+    GEOS_ERROR_IF( m_componentMolarWeight[ic] < LvArray::NumericLimits< real64 >::epsilon, "Zero component molecular weight" );
     real64 const mwInv = 1.0 / m_componentMolarWeight[ic];
     compMoleFrac[ic] = composition[ic] * mwInv; // this is molality (units of mole/mass)
     dCompMoleFrac_dCompMassFrac[ic][ic] = mwInv;
     totalMolality += compMoleFrac[ic];
   }
 
+  GEOS_ERROR_IF( totalMolality < LvArray::NumericLimits< real64 >::epsilon, "Zero total molality, concentrations are zeros for all components" );
   real64 const totalMolalityInv = 1.0 / totalMolality;
   for( integer ic = 0; ic < numComps; ++ic )
   {
