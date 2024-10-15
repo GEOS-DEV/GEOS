GEOS-DEV/GEOS

commit 5c8917ec11b28581b32c8f569dd6070cd2f34905

feat: Separate mass and volume residuals for output in compositional flow solver (#3364)

src/coreComponents/physicsSolvers/fluidFlow/ThermalCompositionalMultiphaseBaseKernels.hpp
@@ -836,11 +836,11 @@ public:
 /**
  * @class ResidualNormKernel
  */
-class ResidualNormKernel : public solverBaseKernels::ResidualNormKernelBase< 2 >
+class ResidualNormKernel : public solverBaseKernels::ResidualNormKernelBase< 3 >
 {
 public:
 
-  using Base = ResidualNormKernelBase< 2 >;
+  using Base = ResidualNormKernelBase< 3 >;
   using Base::m_minNormalizer;
   using Base::m_rankOffset;
   using Base::m_localResidual;
@@ -913,7 +913,7 @@ public:
     real64 const valVol = LvArray::math::abs( m_localResidual[stack.localRow + m_numComponents] ) / volumeNormalizer;
     if( valVol > stack.localValue[0] )
     {
-      stack.localValue[0] = valVol;
+      stack.localValue[1] = valVol;
     }
 
     // step 3: energy residual
@@ -921,7 +921,7 @@ public:
     real64 const valEnergy = LvArray::math::abs( m_localResidual[stack.localRow + m_numComponents + 1] ) / energyNormalizer;
     if( valEnergy > stack.localValue[1] )
     {
-      stack.localValue[1] = valEnergy;
+      stack.localValue[2] = valEnergy;
     }
   }
 
@@ -945,13 +945,13 @@ public:
 
     real64 const valVol = m_localResidual[stack.localRow + m_numComponents] * m_totalDens_n[ei][0]; // we need a mass here, hence the
                                                                                                     // multiplication
-    stack.localValue[0] += valVol * valVol;
-    stack.localNormalizer[0] += massNormalizer;
+    stack.localValue[1] += valVol * valVol;
+    stack.localNormalizer[1] += massNormalizer;
 
     // step 3: energy residual
 
-    stack.localValue[1] += m_localResidual[stack.localRow + m_numComponents + 1] * m_localResidual[stack.localRow + m_numComponents + 1];
-    stack.localNormalizer[1] += energyNormalizer;
+    stack.localValue[2] += m_localResidual[stack.localRow + m_numComponents + 1] * m_localResidual[stack.localRow + m_numComponents + 1];
+    stack.localNormalizer[2] += energyNormalizer;
   }
 
 protected:
@@ -1015,8 +1015,8 @@ public:
                    constitutive::CoupledSolidBase const & solid,
                    constitutive::SolidInternalEnergy const & solidInternalEnergy,
                    real64 const minNormalizer,
-                   real64 (& residualNorm)[2],
-                   real64 (& residualNormalizer)[2] )
+                   real64 (& residualNorm)[3],
+                   real64 (& residualNormalizer)[3] )
   {
     arrayView1d< globalIndex const > const dofNumber = subRegion.getReference< array1d< globalIndex > >( dofKey );
     arrayView1d< integer const > const ghostRank = subRegion.ghostRank();

.integrated_tests.yaml
@@ -1,6 +1,6 @@
 baselines:
   bucket: geosx
-  baseline: integratedTests/baseline_integratedTests-pr3149-7884-f97a068
+  baseline: integratedTests/baseline_integratedTests-pr3364-7912-d967424
 
 allow_fail:
   all: ''

src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseFVM.cpp
@@ -328,7 +328,7 @@ real64 CompositionalMultiphaseFVM::calculateResidualNorm( real64 const & GEOS_UN
 {
   GEOS_MARK_FUNCTION;
 
-  integer constexpr numNorm = 2; // mass balance and energy balance
+  integer constexpr numNorm = 3; // mass/volume balance and energy balance
   array1d< real64 > localResidualNorm;
   array1d< real64 > localResidualNormalizer;
   localResidualNorm.resize( numNorm );
@@ -381,8 +381,8 @@ real64 CompositionalMultiphaseFVM::calculateResidualNorm( real64 const & GEOS_UN
       }
       else
       {
-        real64 subRegionFlowResidualNorm[1]{};
-        real64 subRegionFlowResidualNormalizer[1]{};
+        real64 subRegionFlowResidualNorm[2]{};
+        real64 subRegionFlowResidualNormalizer[2]{};
         isothermalCompositionalMultiphaseBaseKernels::
           ResidualNormKernelFactory::
           createAndLaunch< parallelDevicePolicy<> >( normType,
@@ -396,8 +396,12 @@ real64 CompositionalMultiphaseFVM::calculateResidualNorm( real64 const & GEOS_UN
                                                      m_nonlinearSolverParameters.m_minNormalizer,
                                                      subRegionFlowResidualNorm,
                                                      subRegionFlowResidualNormalizer );
+        // mass
         subRegionResidualNorm[0] = subRegionFlowResidualNorm[0];
         subRegionResidualNormalizer[0] = subRegionFlowResidualNormalizer[0];
+        // volume
+        subRegionResidualNorm[1] = subRegionFlowResidualNorm[1];
+        subRegionResidualNormalizer[1] = subRegionFlowResidualNormalizer[1];
       }
 
       // step 2: first reduction across meshBodies/regions/subRegions
@@ -420,9 +424,30 @@ real64 CompositionalMultiphaseFVM::calculateResidualNorm( real64 const & GEOS_UN
   real64 residualNorm = 0.0;
   if( m_isThermal )
   {
-
     array1d< real64 > globalResidualNorm;
     globalResidualNorm.resize( numNorm );
+    if( normType == solverBaseKernels::NormType::Linf )
+    {
+      solverBaseKernels::LinfResidualNormHelper::
+        computeGlobalNorm( localResidualNorm, globalResidualNorm );
+    }
+    else
+    {
+      solverBaseKernels::L2ResidualNormHelper::
+        computeGlobalNorm( localResidualNorm, localResidualNormalizer, globalResidualNorm );
+    }
+    residualNorm = sqrt( globalResidualNorm[0] * globalResidualNorm[0] + globalResidualNorm[1] * globalResidualNorm[1]  + globalResidualNorm[2] * globalResidualNorm[2] );
+
+    if( getLogLevel() >= 1 && logger::internal::rank == 0 )
+    {
+      std::cout << GEOS_FMT( "        ( Rmass Rvol ) = ( {:4.2e} {:4.2e} )        ( Renergy ) = ( {:4.2e} )",
+                             globalResidualNorm[0], globalResidualNorm[1], globalResidualNorm[2] );
+    }
+  }
+  else
+  {
+    array1d< real64 > globalResidualNorm;
+    globalResidualNorm.resize( numNorm - 1 );
     if( normType == solverBaseKernels::NormType::Linf )
     {
       solverBaseKernels::LinfResidualNormHelper::
@@ -437,27 +462,11 @@ real64 CompositionalMultiphaseFVM::calculateResidualNorm( real64 const & GEOS_UN
 
     if( getLogLevel() >= 1 && logger::internal::rank == 0 )
     {
-      std::cout << GEOS_FMT( "        ( R{} ) = ( {:4.2e} )        ( Renergy ) = ( {:4.2e} )",
-                             coupledSolverAttributePrefix(), globalResidualNorm[0], globalResidualNorm[1] );
+      std::cout << GEOS_FMT( "        ( Rmass Rvol ) = ( {:4.2e} {:4.2e} )",
+                             globalResidualNorm[0], globalResidualNorm[1] );
     }
   }
-  else
-  {
 
-    if( normType == solverBaseKernels::NormType::Linf )
-    {
-      solverBaseKernels::LinfResidualNormHelper::computeGlobalNorm( localResidualNorm[0], residualNorm );
-    }
-    else
-    {
-      solverBaseKernels::L2ResidualNormHelper::computeGlobalNorm( localResidualNorm[0], localResidualNormalizer[0], residualNorm );
-    }
-
-    if( getLogLevel() >= 1 && logger::internal::rank == 0 )
-    {
-      std::cout << GEOS_FMT( "        ( R{} ) = ( {:4.2e} )", coupledSolverAttributePrefix(), residualNorm );
-    }
-  }
   return residualNorm;
 }
 

src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseHybridFVM.cpp
@@ -611,8 +611,8 @@ real64 CompositionalMultiphaseHybridFVM::calculateResidualNorm( real64 const & G
                                                               [&]( localIndex const,
                                                                    ElementSubRegionBase const & subRegion )
     {
-      real64 subRegionResidualNorm[1]{};
-      real64 subRegionResidualNormalizer[1]{};
+      real64 subRegionResidualNorm[2]{};
+      real64 subRegionResidualNormalizer[2]{};
 
       string const & fluidName = subRegion.getReference< string >( viewKeyStruct::fluidNamesString() );
       MultiFluidBase const & fluid = getConstitutiveModel< MultiFluidBase >( subRegion, fluidName );
@@ -640,6 +640,8 @@ real64 CompositionalMultiphaseHybridFVM::calculateResidualNorm( real64 const & G
 
       if( normType == solverBaseKernels::NormType::Linf )
       {
+        // take max between mass and volume residual
+        subRegionResidualNorm[0] = LvArray::math::max( subRegionResidualNorm[0], subRegionResidualNorm[1] );
         if( subRegionResidualNorm[0] > localResidualNorm )
         {
           localResidualNorm = subRegionResidualNorm[0];
@@ -647,6 +649,8 @@ real64 CompositionalMultiphaseHybridFVM::calculateResidualNorm( real64 const & G
       }
       else
       {
+        // sum up mass and volume residual
+        subRegionResidualNorm[0] = subRegionResidualNorm[0] + subRegionResidualNorm[1];
         localResidualNorm += subRegionResidualNorm[0];
         localResidualNormalizer += subRegionResidualNormalizer[0];
       }

src/coreComponents/physicsSolvers/fluidFlow/IsothermalCompositionalMultiphaseBaseKernels.hpp
@@ -1787,11 +1787,11 @@ public:
 /**
  * @class ResidualNormKernel
  */
-class ResidualNormKernel : public solverBaseKernels::ResidualNormKernelBase< 1 >
+class ResidualNormKernel : public solverBaseKernels::ResidualNormKernelBase< 2 >
 {
 public:
 
-  using Base = solverBaseKernels::ResidualNormKernelBase< 1 >;
+  using Base = solverBaseKernels::ResidualNormKernelBase< 2 >;
   using Base::m_minNormalizer;
   using Base::m_rankOffset;
   using Base::m_localResidual;
@@ -1839,9 +1839,9 @@ public:
     // step 2: volume residual
 
     real64 const valVol = LvArray::math::abs( m_localResidual[stack.localRow + m_numComponents] ) / volumeNormalizer;
-    if( valVol > stack.localValue[0] )
+    if( valVol > stack.localValue[1] )
     {
-      stack.localValue[0] = valVol;
+      stack.localValue[1] = valVol;
     }
   }
 
@@ -1865,8 +1865,8 @@ public:
 
     real64 const val = m_localResidual[stack.localRow + m_numComponents] * m_totalDens_n[ei][0]; // we need a mass here, hence the
                                                                                                  // multiplication
-    stack.localValue[0] += val * val;
-    stack.localNormalizer[0] += massNormalizer;
+    stack.localValue[1] += val * val;
+    stack.localNormalizer[1] += massNormalizer;
   }
 
 
@@ -1918,8 +1918,8 @@ public:
                    constitutive::MultiFluidBase const & fluid,
                    constitutive::CoupledSolidBase const & solid,
                    real64 const minNormalizer,
-                   real64 (& residualNorm)[1],
-                   real64 (& residualNormalizer)[1] )
+                   real64 (& residualNorm)[2],
+                   real64 (& residualNormalizer)[2] )
   {
     arrayView1d< globalIndex const > const dofNumber = subRegion.getReference< array1d< globalIndex > >( dofKey );
     arrayView1d< integer const > const ghostRank = subRegion.ghostRank();

commit d89cbfdda6ddb48b054dc3a7120d17936eee5865

fix: Bug fix for residual computation
introduced in https://github.com/GEOS-DEV/GEOS/pull/3364

src/coreComponents/physicsSolvers/fluidFlow/ThermalCompositionalMultiphaseBaseKernels.hpp
@@ -911,7 +911,7 @@ public:
     // step 2: volume residual
 
     real64 const valVol = LvArray::math::abs( m_localResidual[stack.localRow + m_numComponents] ) / volumeNormalizer;
-    if( valVol > stack.localValue[0] )
+    if( valVol > stack.localValue[1] )
     {
       stack.localValue[1] = valVol;
     }
@@ -919,7 +919,7 @@ public:
     // step 3: energy residual
 
     real64 const valEnergy = LvArray::math::abs( m_localResidual[stack.localRow + m_numComponents + 1] ) / energyNormalizer;
-    if( valEnergy > stack.localValue[1] )
+    if( valEnergy > stack.localValue[2] )
     {
       stack.localValue[2] = valEnergy;
     }