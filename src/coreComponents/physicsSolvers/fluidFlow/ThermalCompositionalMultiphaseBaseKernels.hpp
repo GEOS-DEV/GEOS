7309a218587093d1b3915e581c9c43c5b9458df6
Pavel Tomin <paveltomin@users.noreply.github.com>
2024-10-26 10:15:18 -0500

    fix: Bug fix for residual computation (#3399)

--- src/coreComponents/physicsSolvers/fluidFlow/ThermalCompositionalMultiphaseBaseKernels.hpp
+++ src/coreComponents/physicsSolvers/fluidFlow/ThermalCompositionalMultiphaseBaseKernels.hpp
@@ -911,7 +911,7 @@ public:
     // step 2: volume residual
 
     real64 const valVol = LvArray::math::abs( m_localResidual[stack.localRow + m_numComponents] ) / volumeNormalizer;
-    if( valVol > stack.localValue[0] )
+    if( valVol > stack.localValue[1] )
     {
       stack.localValue[1] = valVol;
     }
@@ -919,7 +919,7 @@ public:
     // step 3: energy residual
 
     real64 const valEnergy = LvArray::math::abs( m_localResidual[stack.localRow + m_numComponents + 1] ) / energyNormalizer;
-    if( valEnergy > stack.localValue[1] )
+    if( valEnergy > stack.localValue[2] )
     {
       stack.localValue[2] = valEnergy;
     }
