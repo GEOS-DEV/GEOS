4f382df0e21f66d3a7b51519d6353f70b6cdf786
Matteo Cusini <49037133+CusiniM@users.noreply.github.com>
2025-03-11 20:29:20 -0700

    feat:  add aging law. (#3555)
    * feat: add aging law.
    
    * use friction dispatch
    
    * Move definition to hpp.
    
    * Fix aging law to use nondimensional form.
    
    * create separate files and dedicated launches for RG kernels.
    
    * add aging law integratedTests.
    
    * rebaseline.

--- src/coreComponents/constitutive/ConstitutivePassThru.hpp
+++ src/coreComponents/constitutive/ConstitutivePassThru.hpp
@@ -96,8 +96,9 @@ struct ConstitutivePassThru< FrictionBase >
   void execute( ConstitutiveBase & constitutiveRelation, LAMBDA && lambda )
   {
     ConstitutivePassThruHandler< CoulombFriction,
-                                 RateAndStateFriction >::execute( constitutiveRelation,
-                                                                  std::forward< LAMBDA >( lambda ) );
+                                 RateAndStateFriction< std::integral_constant< bool, true > >,
+                                 RateAndStateFriction< std::integral_constant< bool, false > > >::execute( constitutiveRelation,
+                                                                                                           std::forward< LAMBDA >( lambda ) );
   }
 };
 
@@ -113,6 +114,22 @@ struct ConstitutivePassThru< CoulombFriction >
   {
     ConstitutivePassThruHandler< CoulombFriction >::execute( constitutiveRelation,
                                                              std::forward< LAMBDA >( lambda ) );
+  }
+};
+
+/**
+ * Specialization for models that derive from CoulombFriction.
+ */
+template<>
+struct ConstitutivePassThru< RateAndStateFrictionBase >
+{
+  template< typename LAMBDA >
+  static
+  void execute( ConstitutiveBase & constitutiveRelation, LAMBDA && lambda )
+  {
+    ConstitutivePassThruHandler< RateAndStateFriction< std::integral_constant< bool, true > >,
+                                 RateAndStateFriction< std::integral_constant< bool, false > > >::execute( constitutiveRelation,
+                                                                                                           std::forward< LAMBDA >( lambda ) );
   }
 };
 
