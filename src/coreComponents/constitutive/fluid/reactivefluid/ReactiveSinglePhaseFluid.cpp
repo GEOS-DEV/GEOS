2b25ae8bb5c08f581f51651ec178bf96862839f1
Matteo Cusini <matteo.cusini@gmail.com>
2025-04-02 14:49:05 -0700

    feat: add HPCReact as a submodule

---
+++ src/coreComponents/constitutive/fluid/reactivefluid/ReactiveSinglePhaseFluid.cpp
@@ -0,0 +1,91 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ * Copyright (c) 2016-2024 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2024 TotalEnergies
+ * Copyright (c) 2018-2024 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2023-2024 Chevron
+ * Copyright (c) 2019-     GEOS/GEOSX Contributors
+ * All rights reserved
+ *
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
+/**
+ * @file ReactiveSinglePhaseFluid.cpp
+ */
+#include "ReactiveSinglePhaseFluid.hpp"
+
+namespace geos
+{
+
+using namespace dataRepository;
+
+namespace constitutive
+{
+
+template< typename BASE >
+ReactiveSinglePhaseFluid< BASE >::
+  ReactiveSinglePhaseFluid( string const & name, Group * const parent ):
+  BASE( name, parent )
+{
+
+  registerWrapper( viewKeyStruct::chemicalSystemNameString(), &m_chemicalSystemName ).
+  setInputFlag( InputFlags::REQUIRED ).
+  setDescription( "AMG smoother type. Available options are: "
+                  "``" + EnumStrings< LinearSolverParameters::AMG::SmootherType >::concat( "|" ) + "``" );
+  // For now this is being hardcoded. We will see where this should come from.
+  m_numPrimarySpecies = 7;
+  m_numSecondarySpecies = 11;
+
+  registerField( fields::reactivefluid::primarySpeciesConcentration{}, &m_primarySpeciesConcentration );
+  registerField( fields::reactivefluid::secondarySpeciesConcentration{}, &m_secondarySpeciesConcentration );
+  registerField( fields::reactivefluid::primarySpeciesTotalConcentration{}, &m_primarySpeciesTotalConcentration );
+}
+
+template< typename BASE >
+std::unique_ptr< ConstitutiveBase > ReactiveSinglePhaseFluid< BASE >::
+  deliverClone( string const & name, Group * const parent ) const
+{
+  std::unique_ptr< ConstitutiveBase > clone = MultiFluidBase::deliverClone( name, parent );
+
+  ReactiveSinglePhaseFluid & newConstitutiveRelation = dynamicCast< ReactiveSinglePhaseFluid & >( *clone );
+
+  newConstitutiveRelation.createChemicalReactions();
+
+  return clone;
+}
+
+template< typename BASE >
+void ReactiveSinglePhaseFluid< BASE >::postInputInitialization()
+{
+  BASE::postInputInitialization();
+  GEOS_THROW_IF_NE_MSG( numFluidPhases(), 1,
+                        GEOS_FMT( "{}: invalid number of phases", getFullName() ),
+                        InputError );
+
+  createChemicalReactions();
+}
+
+template< typename BASE >
+void ReactiveSinglePhaseFluid< BASE >::resizeFields( localIndex const size, localIndex const numPts )
+{
+  BASE::resizeFields( size, numPts );
+
+  integer const numPrimarySpecies = this->numPrimarySpecies();
+  integer const numSecondarySpecies = this->numSecondarySpecies();
+  
+  m_primarySpeciesConcentration.resize( size, numPrimarySpecies );
+  m_secondarySpeciesConcentration.resize( size, numSecondarySpecies );
+  m_primarySpeciesTotalConcentration.resize( size, numPrimarySpecies );
+}
+
+template< typename BASE >
+void ReactiveSinglePhaseFluid< BASE >::createChemicalReactions()
+{}
+
+} //namespace constitutive
+
+} //namespace geos
---
+++ src/coreComponents/constitutive/fluid/reactivefluid/ReactiveSinglePhaseFluid.hpp
@@ -0,0 +1,142 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ * Copyright (c) 2016-2024 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2024 TotalEnergies
+ * Copyright (c) 2018-2024 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2023-2024 Chevron
+ * Copyright (c) 2019-     GEOS/GEOSX Contributors
+ * All rights reserved
+ *
+ * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
+/**
+ * @file ReactiveSinglePhaseFluid.hpp
+ */
+
+#ifndef GEOS_CONSTITUTIVE_FLUID_REACTIVEFLUID_REACTIVESINGLEPHASEFLUID_HPP_
+#define GEOS_CONSTITUTIVE_FLUID_REACTIVEFLUID_REACTIVESINGLEPHASEFLUID_HPP_
+
+#include "common/format/EnumStrings.hpp"
+
+#include "constitutive/ConstitutiveBase.hpp"
+#include "constitutive/fluid/reactivefluid/ReactiveFluidLayouts.hpp"
+
+#include "constitutive/HPCReact/src/reactions/bulkGeneric/Parameters.hpp"
+#include "constitutive/HPCReact/src/reactions/bulkGeneric/EquilibriumReactions.hpp"
+#include <memory>
+
+namespace geos
+{
+
+namespace constitutive
+{
+  
+namespace reactivefluid
+{
+
+template< typename BASE >
+class ReactiveSinglePhaseFluid : public BASE
+{
+public:
+
+  ReactiveSinglePhaseFluid( string const & name,
+                            dataRepository::Group * const parent );
+
+  virtual std::unique_ptr< ConstitutiveBase >
+  deliverClone( string const & name,
+                dataRepository::Group * const parent ) const override;
+
+  virtual bool isThermal() const override;
+
+  arrayView2d< real64 const, compflow::USD_COMP > primarySpeciesConcentration() const
+  { return m_primarySpeciesConcentration; }
+
+  arrayView2d< real64 const, compflow::USD_COMP > secondarySpeciesConcentration() const
+  { return m_secondarySpeciesConcentration; }
+
+  arrayView2d< real64 const, compflow::USD_COMP > kineticReactionRates() const
+  { return m_kineticReactionRates; }
+
+  integer numPrimarySpecies() const { return m_numPrimarySpecies; }
+
+  integer numSecondarySpecies() const { return m_numSecondarySpecies; }
+
+  integer numKineticReactions() const { return m_numKineticReactions; }
+
+  enum class ChemicalSystemType : integer
+  {
+    carbonate,
+    ultramafic
+  };
+
+  /**
+   * @brief Kernel wrapper class for ReactiveSinglePhaseFluid.
+   */
+  class KernelWrapper : public BASE::KernelWrapper
+  {
+
+public:
+
+    using EquilibriumReactionsType = hpcReact::bulkGeneric::EquilibriumReactions< real64, integer, localIndex >;
+
+protected:
+
+    arrayView2d< real64, reactiveFluid::USD_COMP >  m_primarySpeciesConcentration;
+
+    arrayView2d< real64, reactiveFluid::USD_COMP >  m_secondarySpeciesConcentration;
+
+    arrayView2d< real64, reactiveFluid::USD_COMP >  m_primarySpeciesTotalConcentration;
+
+    arrayView2d< real64, reactiveFluid::USD_COMP >  m_kineticReactionRates;
+
+
+  };
+
+  struct viewKeyStruct : ConstitutiveBase::viewKeyStruct
+  {};
+
+protected:
+
+  virtual void postInputInitialization() override;
+
+  virtual void resizeFields( localIndex const size, localIndex const numPts ) override;
+
+  array2d< real64, constitutive::reactivefluid::LAYOUT_FLUID >  m_primarySpeciesConcentration;
+
+  array2d< real64, constitutive::reactivefluid::LAYOUT_FLUID >  m_secondarySpeciesConcentration;
+
+  array2d< real64, constitutive::reactivefluid::LAYOUT_FLUID >  m_primarySpeciesTotalConcentration;
+
+  array2d< real64, constitutive::reactivefluid::LAYOUT_FLUID >  m_kineticReactionRates;
+
+  ChemicalSystemType m_chemicalSystemType;
+};
+
+inline void
+ReactiveSinglePhaseFluid::KernelWrapper::
+  computeChemistry( real64 const pressure,
+                    real64 const temperature,
+                    arraySlice1d< real64 const, compflow::USD_COMP - 1 > const & primarySpeciesTotalConcentration,
+                    arraySlice1d< real64, compflow::USD_COMP - 1 > const & primarySpeciesConcentration,
+                    arraySlice1d< real64, compflow::USD_COMP - 1 > const & secondarySpeciesConcentration,
+                    arraySlice1d< real64, compflow::USD_COMP - 1 > const & kineticReactionRates ) const
+{
+  auto params = hpcReact::bulkGeneric::carbonateSystem;
+  EquilibriumReactionsType::enforceEquilibrium_Extents( temperature, params, speciesConcentration0, speciesConcentration );
+}
+
+ENUM_STRINGS( ChemicalSystemType,
+              "carbonate",
+              "ultramafic" );
+
+} // namespace reactivefluid
+
+} // namespace constitutive
+
+} // namespace geos
+
+#endif //GEOS_CONSTITUTIVE_FLUID_ReactiveSinglePhaseFluid_HPP
