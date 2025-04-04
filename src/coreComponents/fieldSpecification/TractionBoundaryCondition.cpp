65ba6e6ec90285771020d0852795fa7953ff064e
Matteo Cusini <49037133+CusiniM@users.noreply.github.com>
2025-04-04 15:16:00 -0700

    Update src/coreComponents/fieldSpecification/TractionBoundaryCondition.cpp

--- src/coreComponents/fieldSpecification/TractionBoundaryCondition.cpp
+++ src/coreComponents/fieldSpecification/TractionBoundaryCondition.cpp
@@ -247,7 +247,7 @@ void TractionBoundaryCondition::reinitScaleSet( FaceManager const & faceManager,
 
   m_scaleSet.resize( faceSize );
 
-  auto scaleSet = m_scaleSet.toView();
+  auto const scaleSet = m_scaleSet.toView();
 
   // Loop over targetSet to assign damage values to m_scaleSet
   forAll< parallelDevicePolicy<> >( targetSet.size(), [=] GEOS_HOST_DEVICE ( localIndex const i )
