acea035f3b64ab800c5adc0b5bfd3d43b2a2f09c
MelReyCG <melvin.rey@capgemini.com>
2025-04-09 17:29:19 +0200

    🐛 please don't look

--- src/coreComponents/common/initializeEnvironment.cpp
+++ src/coreComponents/common/initializeEnvironment.cpp
@@ -263,7 +263,7 @@ void cleanupEnvironment()
 {
   LvArray::system::resetSignalHandling();
   finalizeLogger();
-  MemoryLogging::getInstance()::umpireStatsReport();
+  MemoryLogging::getInstance().umpireStatsReport();
   finalizeCaliper();
   finalizeMPI();
 }
