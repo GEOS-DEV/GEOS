c9fda2f2f56a766184ed315566d3163994fac8c0
Randolph Settgast <rrsettgast@gmail.com>
2025-04-23 10:02:56 -0700

    change NodeManager::setGeometricalRelations calls to assimilate s.t. they assume unsorted with duplicates

--- src/coreComponents/dataRepository/wrapperHelpers.hpp
+++ src/coreComponents/dataRepository/wrapperHelpers.hpp
@@ -206,21 +206,21 @@ resizeDefault( T & value,
                DefaultValue< T > const & GEOS_UNUSED_PARAM( defaultValue ),
                string const & name )
 {
-#if !defined(NDEBUG)
-  GEOS_LOG_RANK_0( GEOS_FMT( "Warning: For Wrapper<{}>::name() = {}:\n"
-                             "  wrapperHelpers::resizeDefault<{}>() called, but the SFINAE filter failed:\n"
-                             "    traits::HasMemberFunction_resizeDefault< {} > = {}\n "
-                             "    DefaultValue< {} >::has_default_value = {}",
-                             LvArray::system::demangleType< T >(),
-                             name,
-                             LvArray::system::demangleType< T >(),
-                             LvArray::system::demangleType< T >(),
-                             traits::HasMemberFunction_resizeDefault< T >,
-                             LvArray::system::demangleType< T >(),
-                             DefaultValue< T >::has_default_value ) );
-#else
+// #if !defined(NDEBUG)
+//   GEOS_LOG_RANK_0( GEOS_FMT( "Warning: For Wrapper<{}>::name() = {}:\n"
+//                              "  wrapperHelpers::resizeDefault<{}>() called, but the SFINAE filter failed:\n"
+//                              "    traits::HasMemberFunction_resizeDefault< {} > = {}\n "
+//                              "    DefaultValue< {} >::has_default_value = {}",
+//                              LvArray::system::demangleType< T >(),
+//                              name,
+//                              LvArray::system::demangleType< T >(),
+//                              LvArray::system::demangleType< T >(),
+//                              traits::HasMemberFunction_resizeDefault< T >,
+//                              LvArray::system::demangleType< T >(),
+//                              DefaultValue< T >::has_default_value ) );
+// #else
   GEOS_UNUSED_VAR( name );
-#endif
+//#endif
   resize( value, newSize );
 }
 
