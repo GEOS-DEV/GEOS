46b23e3ca6036df6169cd1085f0e61524a21e35c
Pavel Tomin <ptls@PW0BMYGN>
2025-04-24 15:49:24 -0500

    fix: error message for vtk import to avoid hard crash

--- src/coreComponents/mesh/generators/VTKFaceBlockUtilities.cpp
+++ src/coreComponents/mesh/generators/VTKFaceBlockUtilities.cpp
@@ -647,6 +647,11 @@ void importFractureNetwork( string const & faceBlockName,
   faceBlock.set2dElemToFaces( std::move( elem2dTo3d.elem2dToFaces ) );
   faceBlock.set2dElemToElems( std::move( elem2dTo3d.elem2dToElem3d ) );
 
+  GEOS_ERROR_IF( !vtkIdTypeArray::FastDownCast( faceMesh->GetCellData()->GetGlobalIds() ),
+                 "Global IDs are required for not found for fracture mesh" );
+  GEOS_ERROR_IF( !vtkIdTypeArray::FastDownCast( mesh->GetCellData()->GetGlobalIds() ),
+                 "Global IDs are required for not found for main mesh" );
+
   faceBlock.setLocalToGlobalMap(
     buildLocalToGlobal( vtkIdTypeArray::FastDownCast( faceMesh->GetCellData()->GetGlobalIds() ),
                         vtkIdTypeArray::FastDownCast( mesh->GetCellData()->GetGlobalIds() ) )
