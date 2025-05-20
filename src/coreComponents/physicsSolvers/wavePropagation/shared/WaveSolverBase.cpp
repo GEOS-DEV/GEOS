ad0dc0372d54c8c5ab227506c0fcca893873f50b
aurelien <aurelien.citrain@inria.fr>
2025-05-20 17:07:08 +0200

    print

--- src/coreComponents/physicsSolvers/wavePropagation/shared/WaveSolverBase.cpp
+++ src/coreComponents/physicsSolvers/wavePropagation/shared/WaveSolverBase.cpp
@@ -294,6 +294,7 @@ void WaveSolverBase::registerDataOnMesh( Group & meshBodies )
 
     printf("avantregister\n");
     nodeManager.registerField< fields::taperCoeff >( this->getName());
+    printf("apresregister\n");
 
   } );
 }
