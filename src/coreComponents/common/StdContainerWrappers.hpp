ac6ee657ad2c4dc6beee7f1569a0e8bb7c5738b0
arng40 <arnaud.dudes@capgemini.com>
2025-05-20 11:24:46 +0200

    attempt #3

--- src/coreComponents/common/StdContainerWrappers.hpp
+++ src/coreComponents/common/StdContainerWrappers.hpp
@@ -199,13 +199,13 @@ template< typename TKEY, typename TVAL >
 class mapBase< TKEY, TVAL, std::integral_constant< bool, true > > : public stdMap< TKEY, TVAL >
 {
 public:
-  using stdMap = typename stdMap< TKEY, TVAL >::StdMapWrapper; 
+  using stdMap< TKEY, TVAL >::StdMapWrapper; 
 };
 
 template< typename TKEY, typename TVAL >
 class mapBase< TKEY, TVAL, std::integral_constant< bool, false > > : public stdUnorderedMap< TKEY, TVAL >
 {
-  using stdUnorderedMap = typename stdUnorderedMap< TKEY, TVAL >::StdMapWrapper; 
+  using stdUnorderedMap< TKEY, TVAL >::StdMapWrapper; 
 };
 /// @endcond
 
