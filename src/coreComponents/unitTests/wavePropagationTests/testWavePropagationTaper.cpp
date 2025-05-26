533c3375a1925d0f7dccf7124ebb1785aa0038e9
aurelien <aurelien.citrain@inria.fr>
2025-05-26 16:11:58 +0200

    Restore old version of test

--- src/coreComponents/unitTests/wavePropagationTests/testWavePropagationTaper.cpp
+++ src/coreComponents/unitTests/wavePropagationTests/testWavePropagationTaper.cpp
@@ -2,11 +2,12 @@
  * ------------------------------------------------------------------------------------------------------------
  * SPDX-License-Identifier: LGPL-2.1-only
  *
- * Copyright (c) 2018-2020 Lawrence Livermore National Security LLC
- * Copyright (c) 2018-2020 The Board of Trustees of the Leland Stanford Junior University
- * Copyright (c) 2018-2020 Total, S.A
- * Copyright (c) 2020-     GEOSX Contributors
- * All right reserved
+ * Copyright (c) 2016-2024 Lawrence Livermore National Security LLC
+ * Copyright (c) 2018-2024 TotalEnergies
+ * Copyright (c) 2018-2024 The Board of Trustees of the Leland Stanford Junior University
+ * Copyright (c) 2023-2024 Chevron
+ * Copyright (c) 2019-     GEOS/GEOSX Contributors
+ * All rights reserved
  *
  * See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
  * ------------------------------------------------------------------------------------------------------------
@@ -15,6 +16,8 @@
 // using some utility classes from the following unit test
 #include "unitTests/fluidFlowTests/testCompFlowUtils.hpp"
 
+#include <iostream>
+#include <cstdio>
 #include "common/DataTypes.hpp"
 #include "mainInterface/initialization.hpp"
 #include "mainInterface/ProblemManager.hpp"
@@ -40,7 +43,6 @@ char const * xmlInput =
     <Solvers>
       <AcousticSEM
         name="acousticSolver"
-        cflFactor="0.25"
         discretization="FE1"
         targetRegions="{ Region }"
         sourceCoordinates="{ { 50, 50, 50 } }"
@@ -49,11 +51,12 @@ char const * xmlInput =
                                 { 99.9, 0.1, 0.1 }, { 99.9, 0.1, 99.9 }, { 99.9, 99.9, 0.1 }, { 99.9, 99.9, 99.9 },
                                 { 50, 50, 50 } }"
         outputSeismoTrace="0"
-        dtSeismoTrace="0.1"
+        timestepStabilityLimit="1"
+        cflFactor="0.95"
         useTaper="1"
         reflectivityCoeff="0.000001"
-        thicknessTaper="50.0"
-        />
+        thicknessTaper="50"
+        dtSeismoTrace="0.1"/>
     </Solvers>
     <Mesh>
       <InternalMesh
@@ -102,7 +105,7 @@ char const * xmlInput =
     <ElementRegions>
       <CellElementRegion
         name="Region"
-        cellBlocks="{ cb }"
+        cellBlocks="{ * }"
         materialList="{ nullModel }"/>
     </ElementRegions>
     <Constitutive>
@@ -111,19 +114,39 @@ char const * xmlInput =
     </Constitutive>
     <FieldSpecifications>
       <FieldSpecification
+        name="initialPressureN"
+        initialCondition="1"
+        setNames="{ all }"
+        objectPath="nodeManager"
+        fieldName="pressure_n"
+        scale="0.0"/>
+      <FieldSpecification
+        name="initialPressureNm1"
+        initialCondition="1"
+        setNames="{ all }"
+        objectPath="nodeManager"
+        fieldName="pressure_nm1"
+        scale="0.0"/>
+      <FieldSpecification
         name="cellVelocity"
         initialCondition="1"
-        objectPath="mesh/Level0/ElementRegions/Region/cb"
+        objectPath="ElementRegions/Region/cb"
         fieldName="acousticVelocity"
         scale="1500"
         setNames="{ all }"/>
       <FieldSpecification
         name="cellDensity"
         initialCondition="1"
-        objectPath="mesh/Level0/ElementRegions/Region/cb"
+        objectPath="ElementRegions/Region/cb"
         fieldName="acousticDensity"
         scale="1"
         setNames="{ all }"/>
+      <FieldSpecification
+        name="zposFreeSurface"
+        objectPath="faceManager"
+        fieldName="FreeSurface"
+        scale="0.0"
+        setNames="{ zpos }"/>
     </FieldSpecifications>
     <Tasks>
       <PackCollection
@@ -154,9 +177,7 @@ protected:
 
   void SetUp() override
   {
-    printf("setup1\n");
     setupProblemFromXML( state.getProblemManager(), xmlInput );
-    printf("setup2\n");
   }
 
   static real64 constexpr time = 0.0;
@@ -176,11 +197,21 @@ TEST_F( AcousticWaveEquationSEMTest, SeismoTrace )
 
   DomainPartition & domain = state.getProblemManager().getDomainPartition();
   propagator = &state.getProblemManager().getPhysicsSolverManager().getGroup< AcousticWaveEquationSEM >( "acousticSolver" );
+
+
+  //Assert on time-step computed with the automatci time-step routine
+  real64 const dtOut = propagator->getReference< real64 >( AcousticWaveEquationSEM::viewKeyStruct::timeStepString() );
+  real64 const Vp = 1500.0;
+  real64 const h = 100.0;
+  real64 const cflConstant = 1/sqrt( 3 );
+  real64 const dtTheo = (cflConstant*h)/Vp;
+  ASSERT_TRUE( dtOut < dtTheo );
+
   real64 time_n = time;
   // run for 1s (10 steps)
   for( int i=0; i<10; i++ )
   {
-    propagator->explicitStepForward( time_n, dt, i, domain, false );
+    propagator->explicitStepForward( time_n, dt, i, domain, 0 );
     time_n += dt;
   }
   // cleanup (triggers calculation of the remaining seismograms data points)
@@ -203,16 +234,36 @@ TEST_F( AcousticWaveEquationSEMTest, SeismoTrace )
   {
     if( i > 0 )
     {
-      ASSERT_TRUE( std::abs( pReceivers[i][8] ) <= 0.0000001 );
+      ASSERT_TRUE( std::abs( pReceivers[i][8] ) > 0 );
     }
+    double avg = 0;
+    for( int r=0; r<8; r++ )
+    {
+      avg += pReceivers[i][r];
+    }
+    avg /= 8.0;
+    ASSERT_TRUE( std::abs( pReceivers[i][8] - avg ) < 0.00001 );
+  }
+  // run adjoint solver
+  for( int i = 0; i < 10; i++ )
+  {
+    propagator->explicitStepBackward( time_n, dt, i, domain, 0 );
+    time_n += dt;
   }
   // check again the seismo content.
   for( int i = 0; i < 11; i++ )
   {
     if( i > 0 )
     {
-      ASSERT_TRUE( std::abs( pReceivers[i][8] ) <= 0.0000001 );
+      ASSERT_TRUE( std::abs( pReceivers[i][8] ) > 0 );
     }
+    double avg = 0;
+    for( int r=0; r<8; r++ )
+    {
+      avg += pReceivers[i][r];
+    }
+    avg /= 8.0;
+    ASSERT_TRUE( std::abs( pReceivers[i][8] - avg ) < 0.00001 );
   }
 }
 
