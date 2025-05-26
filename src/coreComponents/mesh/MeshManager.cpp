c8457ae92508fc7216b4eb77a956119a5519fa7d
aurelien <aurelien.citrain@inria.fr>
2025-05-26 17:13:36 +0200

    ci debug

--- src/coreComponents/mesh/MeshManager.cpp
+++ src/coreComponents/mesh/MeshManager.cpp
@@ -140,6 +140,7 @@ void MeshManager::importFields( DomainPartition & domain )
       {
         GEOS_LOG_RANK_0( GEOS_FMT( "  volumic fields on {}/{}", region.getName(), subRegion.getName() ) );
         importFields( generator, region.getName(), subRegion, MeshGeneratorBase::Block::VOLUMIC, generator.getVolumicFieldsMapping(), fieldsToBeSync );
+        printf("afterimportfield\n");
       } );
       meshLevel.getElemManager().forElementSubRegionsComplete< FaceElementSubRegion >(
         [&]( localIndex,
