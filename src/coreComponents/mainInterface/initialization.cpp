2c0f5aafcfd3b40adf011fdd0b100680852fcd8a
amandinehry <amandine.henry@capgemini.com>
2025-05-27 14:20:51 +0200

    errors cli modification: add --errors-output

--- src/coreComponents/mainInterface/initialization.cpp
+++ src/coreComponents/mainInterface/initialization.cpp
@@ -124,7 +124,7 @@ std::unique_ptr< CommandLineOptions > parseCommandLineOptions( int argc, char * 
     { TRACE_DATA_MIGRATION, 0, "", "trace-data-migration", Arg::None, "\t--trace-data-migration, \t Trace host-device data migration" },
     { MEMORY_USAGE, 0, "m", "memory-usage", Arg::nonEmpty, "\t-m, --memory-usage, \t Minimum threshold for printing out memory allocations in a member of the data repository." },
     { PAUSE_FOR, 0, "", "pause-for", Arg::numeric, "\t--pause-for, \t Pause geosx for a given number of seconds before starting execution" },
-    { ERRORS, 0, "e", "errorsOutput", Arg::nonEmpty, "\t-e, \t Output path for the errors file" },
+    { ERRORS, 0, "e", "errorsOutput", Arg::nonEmpty, "\t-e, --errors-output, \t Output path for the errors file" },
     { 0, 0, nullptr, nullptr, nullptr, nullptr }
   };
 
