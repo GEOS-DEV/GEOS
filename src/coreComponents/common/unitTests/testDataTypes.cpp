698db4c4e8a3414b8298aed6d1e1ef1f18ecb85b
arng40 <arnaud.dudes@capgemini.com>
2025-06-13 11:19:45 +0200

    add boundscCheckinng test

--- src/coreComponents/common/unitTests/testDataTypes.cpp
+++ src/coreComponents/common/unitTests/testDataTypes.cpp
@@ -13,10 +13,89 @@
  * ------------------------------------------------------------------------------------------------------------
  */
 
+// Source includes
+#include "common/format/table/TableData.hpp"
+#include "common/format/table/TableFormatter.hpp"
+#include "common/format/table/TableLayout.hpp"
+#include "dataRepository/Group.hpp"
+// TPL includes
 #include <gtest/gtest.h>
-
-#include "common/DataTypes.hpp"
-
-#include <typeindex>
+#include <gtest/gtest-spi.h>
 
 using namespace geos;
+
+TEST( testDataTypes, testBoundChecking )
+{
+  internal::StdVectorWrapper< std::string,
+                              std::allocator< std::string >,
+                              true > vectorBoundsChecking = {"test"};
+  EXPECT_THROW( {
+    try
+    {
+      std::string crash = vectorBoundsChecking[1];
+    }
+    catch( const std::out_of_range & e )
+    {
+      throw;
+    }
+  }, std::out_of_range );
+
+  internal::StdMapWrapper< std::map< integer, integer >,
+                           true > mapBoundsChecking{{0, 1}};
+  EXPECT_THROW( {
+    try
+    {
+      integer crash = mapBoundsChecking[1];
+    }
+    catch( const std::out_of_range & e )
+    {
+      throw;
+    }
+  }, std::out_of_range );
+
+  internal::StdMapWrapper< std::unordered_map< integer, integer >,
+                           true > unorderedMapBoundsChecking{{0, 1}};
+  EXPECT_THROW( {
+    try
+    {
+      integer crash = unorderedMapBoundsChecking[1];
+    }
+    catch( const std::out_of_range & e )
+    {
+      throw;
+    }
+  }, std::out_of_range );
+
+}
+
+TEST( testDataTypes, testNoBoundChecking )
+{
+  internal::StdVectorWrapper< std::string,
+                              std::allocator< std::string >,
+                              false > boundChecking = {"test"};
+
+  EXPECT_NO_THROW( {
+    std::string crash = boundChecking[1];
+  } );
+
+  internal::StdMapWrapper< std::map< integer, integer >,
+                           false > mapBoundsChecking{{0, 1}};
+
+  EXPECT_NO_THROW( {
+    integer crash = mapBoundsChecking[1];
+  } );
+
+  internal::StdMapWrapper< std::unordered_map< integer, integer >,
+                           false > unorderedMapBoundsChecking{{0, 1}};
+
+  EXPECT_NO_THROW( {
+    integer crash = unorderedMapBoundsChecking[1];
+  } );
+
+}
+
+int main( int argc, char * * argv )
+{
+  testing::InitGoogleTest( &argc, argv );
+  return RUN_ALL_TESTS();
+}
