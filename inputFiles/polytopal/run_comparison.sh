30d04b8b500f0e5c23bff8b64282f4d64167574b
Omar Duran <omaryesiduran@gmail.com>
2025-06-23 20:03:25 -0700

    wip: solutions differ

--- inputFiles/polytopal/run_comparison.sh
+++ inputFiles/polytopal/run_comparison.sh
@@ -9,7 +9,7 @@ echo "Starting GEOSX batch simulations..."
 # --- First Simulation ---
 echo "Running simulation with tpfa.xml input..."
 # ../../build-macOS_arm-release/bin/Debug/geosx -i buoyancy_column_co2_3d_tpfa.xml -o ../../../geos_output
-../../build-macOS_arm-release/bin/Release/geosx -i buoyancy_staircase_co2_3d_tpfa.xml -o ../../../geos_output
+#../../build-macOS_arm-release/bin/Release/geosx -i buoyancy_staircase_co2_3d_tpfa.xml -o ../../../geos_output
 echo "First simulation finished."
 
 # --- Second Simulation ---
