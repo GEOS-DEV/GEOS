cf8d459d96066624df4b1b7133b8331ca853bcdf
Thomas James Byer <byer3@llnl.gov>
2025-06-26 13:41:07 -0700

    fix rel mode errors

--- src/coreComponents/unitTests/wellsTests/testReservoirThermalSinglePhaseMSWells.cpp
+++ src/coreComponents/unitTests/wellsTests/testReservoirThermalSinglePhaseMSWells.cpp
@@ -221,6 +221,7 @@ void testNumericalJacobian( SinglePhaseReservoirAndWells<> & solver,
                             real64 const relTol, bool diag_check, std::string const & testName,
                             LAMBDA && assembleFunction )
 {
+  GEOS_UNUSED_VAR( testName );
   SinglePhaseWell & wellSolver = *solver.wellSolver();
   SinglePhaseFVM< SinglePhaseBase > & flowSolver = dynamicCast< SinglePhaseFVM< SinglePhaseBase > & >( *solver.reservoirSolver() );
 
