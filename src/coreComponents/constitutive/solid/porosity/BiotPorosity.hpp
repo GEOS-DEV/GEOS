af52dd23ae11188a81968116ce93225cde035356
Jian HUANG <jian.huang@totalenergies.com>
2025-06-27 15:26:59 -0500

    add checks for mechanical properties

--- src/coreComponents/constitutive/solid/porosity/BiotPorosity.hpp
+++ src/coreComponents/constitutive/solid/porosity/BiotPorosity.hpp
@@ -190,6 +190,14 @@ public:
     m_bulkModulus[k] = bulkModulus;
     m_shearModulus[k] = shearModulus;
 
+    GEOS_THROW_IF( m_bulkModulus[k] <= 0,
+                   GEOS_FMT( "{}: the rock bulk modulus {} must be strictly positive", getFullName(), k ),
+                   InputError );
+
+    GEOS_THROW_IF_GT_MSG( m_bulkModulus[k], m_grainBulkModulus[k],
+                          GEOS_FMT( "{}: the rock bulk modulus {} cannot be greater than the grain bulk modulus {}", getFullName(), k ),
+                          InputError );
+
     m_biotCoefficient[k] =  1.0 - bulkModulus / m_grainBulkModulus[k];
   }
 
