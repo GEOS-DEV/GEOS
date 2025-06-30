ca7c57fc882c7d0b604c762e56457fe801876ccf
Randolph R. Settgast <settgast1@llnl.gov>
2025-06-29 19:52:25 -0700

    hack fix for another bug in TimeHistory

--- src/coreComponents/fileIO/timeHistory/HistoryCollectionBase.cpp
+++ src/coreComponents/fileIO/timeHistory/HistoryCollectionBase.cpp
@@ -68,7 +68,10 @@ bool HistoryCollectionBase::execute( real64 const time_n,
     this->updateSetsIndices( domain );
     HistoryMetadata hmd = this->getMetaData( domain, collectionIdx );
     buffer_unit_type * buffer = m_bufferProviders[collectionIdx]( hmd.size( 0 ) );
-    collect( domain, collectionIdx, buffer );
+    if( buffer != nullptr )
+    {
+      collect( domain, collectionIdx, buffer );
+    }
   }
   for( auto & metaCollector: m_metaDataCollectors )
   {
