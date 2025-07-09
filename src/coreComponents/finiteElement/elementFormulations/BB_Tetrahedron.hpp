ab0d441852e25f568130bbb8b387b640e584f48c
aurelien <aurelien.citrain@inria.fr>
2025-07-09 10:29:08 +0200

    Some fix for CI

--- src/coreComponents/finiteElement/elementFormulations/BB_Tetrahedron.hpp
+++ src/coreComponents/finiteElement/elementFormulations/BB_Tetrahedron.hpp
@@ -722,15 +722,15 @@ public:
   GEOS_FORCE_INLINE
   static constexpr void conditionalBasisLoop( FUNC && func )
   {
-    loop< ORDER + 1 >( [&] ( auto const i )
+    loop< ORDER + 1 >( [&func] ( auto const i )
     {
       std::integral_constant< int, ORDER -  decltype(i)::value > i1;
-      loop< ORDER + 1 >( [&] ( auto const j )
+      loop< ORDER + 1 >( [&func,i1,i] ( auto const j )
       {
         std::integral_constant< int, ORDER - decltype(j)::value > j1;
         if constexpr ( j1 <= decltype(i)::value )
         {
-          loop< ORDER + 1 >( [&] ( auto const k )
+          loop< ORDER + 1 >( [&func,i1,j1] ( auto const k )
           {
             std::integral_constant< int, ORDER - k > k1;
             std::integral_constant< int, ORDER - i1 - j1 > ji1;
