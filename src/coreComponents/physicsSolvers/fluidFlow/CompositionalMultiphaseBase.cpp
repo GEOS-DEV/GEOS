fe29f6ca6ac1697e19b7731d81c5387e1da6b423
Dickson Kachuma <81433670+dkachuma@users.noreply.github.com>
2025-07-15 18:20:37 -0500

    fix: Fix validation of depth tables (#3726)

--- src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseBase.cpp
+++ src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseBase.cpp
@@ -1044,6 +1044,7 @@ void CompositionalMultiphaseBase::initializeThermalState( MeshLevel & mesh, stri
 void CompositionalMultiphaseBase::computeHydrostaticEquilibrium( DomainPartition & domain )
 {
   FieldSpecificationManager & fsManager = FieldSpecificationManager::getInstance();
+  FunctionManager & functionManager = FunctionManager::getInstance();
 
   integer const numComps = m_numComponents;
   integer const numPhases = m_numPhases;
@@ -1073,6 +1074,17 @@ void CompositionalMultiphaseBase::computeHydrostaticEquilibrium( DomainPartition
                    "   - Remove the hydrostatic equilibrium initial condition from the XML file",
                    InputError );
 
+    // ensure that the temperature and composition tables are defined
+    GEOS_THROW_IF( bc.getTemperatureVsElevationTableName().empty(),
+                   getCatalogName() << " " << bc.getDataContext()
+                                    << ": " << EquilibriumInitialCondition::viewKeyStruct::temperatureVsElevationTableNameString()
+                                    << " must be provided for a multiphase simulation",
+                   InputError );
+    GEOS_THROW_IF( bc.getComponentFractionVsElevationTableNames().empty(),
+                   getCatalogName() << " " << bc.getDataContext()
+                                    << ": " << EquilibriumInitialCondition::viewKeyStruct::componentFractionVsElevationTableNamesString()
+                                    << " must be provided for a multiphase simulation",
+                   InputError );
   } );
 
   if( equilCounter == 0 )
@@ -1143,8 +1155,6 @@ void CompositionalMultiphaseBase::computeHydrostaticEquilibrium( DomainPartition
       pressureValues.resize( numPointsInTable );
 
       // Step 3.2: retrieve the user-defined tables (temperature and comp fraction)
-
-      FunctionManager & functionManager = FunctionManager::getInstance();
 
       array1d< TableFunction::KernelWrapper > compFracTableWrappers;
       string_array const & compFracTableNames = fs.getComponentFractionVsElevationTableNames();
