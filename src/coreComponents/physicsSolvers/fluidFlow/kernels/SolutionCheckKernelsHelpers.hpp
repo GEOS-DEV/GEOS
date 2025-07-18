7a839613533aaa907f0d2354350a792761aae8f7
MelReyCG <melvin.rey@capgemini.com>
2025-07-18 15:42:12 +0200

    📝 added an idea

--- src/coreComponents/physicsSolvers/fluidFlow/kernels/SolutionCheckKernelsHelpers.hpp
+++ src/coreComponents/physicsSolvers/fluidFlow/kernels/SolutionCheckKernelsHelpers.hpp
@@ -31,7 +31,7 @@ struct ElementReport
 {
   /// the global id of the reported element
   globalIndex m_id;
-  /// a single value to report for the given element (i.e. a negative pressure, a density...)
+  /// a value to report for the given element (i.e. a negative pressure, a density... Or if needed could be of a templated type for composite values)
   real64 m_value;
 };
 
