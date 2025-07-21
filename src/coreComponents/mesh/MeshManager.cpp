8a24a01cc19985a6a739d0d42fd93a8e89e50b8f
Stefano Frambati <stefano.frambati@totalenergies.com>
2025-07-21 17:30:41 +0200

    more debug msgs

--- src/coreComponents/mesh/MeshManager.cpp
+++ src/coreComponents/mesh/MeshManager.cpp
@@ -147,7 +147,9 @@ void MeshManager::importFields( DomainPartition & domain )
              CellElementSubRegion & subRegion )
       {
     printf("here8\n");
-    printf("region.getName()=%f\n");
+    printf("region.getName()=%f\n", region.getName());
+    printf("subRegion.getName()=%f\n",subRegion.getName());
+    printf("still here8\n");
         //GEOS_LOG_RANK_0( GEOS_FMT( "  volumic fields on {}/{}", region.getName(), subRegion.getName() ) );
     printf("here9\n");
         importFields( generator, region.getName(), subRegion, MeshGeneratorBase::Block::VOLUMIC, generator.getVolumicFieldsMapping(), fieldsToBeSync );
