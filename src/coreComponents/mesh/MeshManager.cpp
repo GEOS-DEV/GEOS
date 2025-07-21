19b507eaa58be60e943e67dbc28250a6d84d8dbd
aurelien <aurelien.citrain@inria.fr>
2025-07-21 17:28:37 +0200

    A try

--- src/coreComponents/mesh/MeshManager.cpp
+++ src/coreComponents/mesh/MeshManager.cpp
@@ -147,7 +147,8 @@ void MeshManager::importFields( DomainPartition & domain )
              CellElementSubRegion & subRegion )
       {
     printf("here8\n");
-        GEOS_LOG_RANK_0( GEOS_FMT( "  volumic fields on {}/{}", region.getName(), subRegion.getName() ) );
+    printf("region.getName()=%f\n");
+        //GEOS_LOG_RANK_0( GEOS_FMT( "  volumic fields on {}/{}", region.getName(), subRegion.getName() ) );
     printf("here9\n");
         importFields( generator, region.getName(), subRegion, MeshGeneratorBase::Block::VOLUMIC, generator.getVolumicFieldsMapping(), fieldsToBeSync );
         printf("afterimportfield\n");
