c8cc625e67e57aee3cad51c69d852e42cf66c3a6
aurelien <aurelien.citrain@inria.fr>
2025-07-23 12:01:34 +0200

    Try without shallow mesh level

--- src/coreComponents/mainInterface/ProblemManager.cpp
+++ src/coreComponents/mainInterface/ProblemManager.cpp
@@ -180,7 +180,7 @@ void ProblemManager::problemSetup()
   meshGenerationLog.begin();
 
   printf("start generateMesh\n");
-// generateMesh();
+ generateMesh();
 
   printf("end generateMesh\n");
   meshGenerationLog.end();
@@ -689,8 +689,8 @@ void ProblemManager::generateMesh()
         else if( order==1 )
         {
       printf("inshallowcopy\n");
-          meshBody.createShallowMeshLevel( MeshBody::groupStructKeys::baseDiscretizationString(),
-                                           discretizationName );
+          //meshBody.createShallowMeshLevel( MeshBody::groupStructKeys::baseDiscretizationString(),
+          //                                 discretizationName );
         }
       }
       else // this is a finite volume discretization...i hope
