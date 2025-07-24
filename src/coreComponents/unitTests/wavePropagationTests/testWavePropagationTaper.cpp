74ec214f962f1f6c5f2d64f1b8d039fb3c866585
aurelien <aurelien.citrain@inria.fr>
2025-07-24 10:46:46 +0200

    Test

--- src/coreComponents/unitTests/wavePropagationTests/testWavePropagationTaper.cpp
+++ src/coreComponents/unitTests/wavePropagationTests/testWavePropagationTaper.cpp
@@ -238,12 +238,6 @@ TEST_F( AcousticWaveEquationSEMTest, SeismoTrace )
     avg /= 8.0;
     ASSERT_TRUE( std::abs( pReceivers[i][8] - avg ) < 0.00001 );
   }
-  // run adjoint solver
-  for( int i = 0; i < 10; i++ )
-  {
-    propagator->explicitStepBackward( time_n, dt, i, domain, 0 );
-    time_n += dt;
-  }
   // check again the seismo content.
   for( int i = 0; i < 11; i++ )
   {
