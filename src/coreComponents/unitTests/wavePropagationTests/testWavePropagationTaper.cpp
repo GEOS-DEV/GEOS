4a8731fa222c0abc1b8459650fab3b3d3b18539d
aurelien <aurelien.citrain@inria.fr>
2025-07-24 15:27:01 +0200

    Test

--- src/coreComponents/unitTests/wavePropagationTests/testWavePropagationTaper.cpp
+++ src/coreComponents/unitTests/wavePropagationTests/testWavePropagationTaper.cpp
@@ -173,7 +173,7 @@ protected:
   {
     setupProblemFromXML( state.getProblemManager(), xmlInput );
 
-    propagator = &state.getProblemManager().getPhysicsSolverManager().getGroup< AcousticWaveEquationSEM >( "acousticSolverTaper" );
+   // propagator = &state.getProblemManager().getPhysicsSolverManager().getGroup< AcousticWaveEquationSEM >( "acousticSolverTaper" );
   }
 
   static real64 constexpr time = 0.0;
