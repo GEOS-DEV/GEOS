155074ac08b55fe6f13bce700a68c0770e2aba81
aurelien <aurelien.citrain@inria.fr>
2025-07-24 16:57:10 +0200

    Test

--- src/coreComponents/unitTests/wavePropagationTests/testWavePropagationTaper.cpp
+++ src/coreComponents/unitTests/wavePropagationTests/testWavePropagationTaper.cpp
@@ -26,6 +26,7 @@
 #include "physicsSolvers/PhysicsSolverManager.hpp"
 #include "physicsSolvers/wavePropagation/shared/WaveSolverBase.hpp"
 #include "physicsSolvers/wavePropagation/sem/acoustic/secondOrderEqn/isotropic/AcousticWaveEquationSEM.hpp"
+#include "physicsSolvers/wavePropagation/shared/TaperKernel.hpp"
 
 #include <gtest/gtest.h>
 
