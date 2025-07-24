fa1bbc7a446225d7b5bb3a28308e2fc92a3c5f02
aurelien <aurelien.citrain@inria.fr>
2025-07-24 17:59:11 +0200

    Bugfix

--- src/coreComponents/physicsSolvers/wavePropagation/sem/acoustic/secondOrderEqn/isotropic/AcousticWaveEquationSEM.cpp
+++ src/coreComponents/physicsSolvers/wavePropagation/sem/acoustic/secondOrderEqn/isotropic/AcousticWaveEquationSEM.cpp
@@ -114,7 +114,7 @@ void AcousticWaveEquationSEM::registerDataOnMesh( Group & meshBodies )
 
     if(m_useTaper)
     {
-      nodeManager.registerField<fields::TaperCoeff> (getName() );
+      nodeManager.registerField<fields::taperCoeff> (getName() );
     }
 
     FaceManager & faceManager = mesh.getFaceManager();
