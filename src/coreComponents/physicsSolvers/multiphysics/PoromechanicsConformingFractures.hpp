87eb904182ffa1f5bb465ff55c40043f1f596642
Jian HUANG <jian.huang@totalenergies.com>
2025-07-25 14:45:38 -0500

    fix compilation error

--- src/coreComponents/physicsSolvers/multiphysics/PoromechanicsConformingFractures.hpp
+++ src/coreComponents/physicsSolvers/multiphysics/PoromechanicsConformingFractures.hpp
@@ -606,7 +606,7 @@ protected:
         arrayView1d< real64 > const aperture                 = subRegion.getElementAperture();
         arrayView1d< real64 > const hydraulicAperture        = subRegion.getField< fields::flow::hydraulicAperture >();
         arrayView1d< real64 > const deltaVolume              = subRegion.getField< fields::flow::deltaVolume >();
-        arrayView1d< integer > const & fractureState   = subRegion.getField< contact::fractureState >();
+        arrayView1d< integer > const & fractureState   = subRegion.getField< fields::contact::fractureState >();
 
         string const porousSolidName = subRegion.getReference< string >( FlowSolverBase::viewKeyStruct::solidNamesString() );
         CoupledSolidBase & porousSolid = subRegion.getConstitutiveModel< CoupledSolidBase >( porousSolidName );
