d4c465b99070e2d80a305f7cc0ce0961b0bb65df
MelReyCG <melvin.rey@capgemini.com>
2025-08-01 12:23:29 +0200

    🎨 format

--- src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseFVM.cpp
+++ src/coreComponents/physicsSolvers/fluidFlow/CompositionalMultiphaseFVM.cpp
@@ -210,7 +210,8 @@ void CompositionalMultiphaseFVM::initializePreSubGroups()
   FiniteVolumeManager const & fvManager = numericalMethodManager.getFiniteVolumeManager();
   FluxApproximationBase const & fluxApprox = fvManager.getFluxApproximation( m_discretizationName );
   GEOS_ERROR_CTX_IF( fluxApprox.upwindingParams().upwindingScheme == UpwindingScheme::HU2PH && m_numPhases != 2,
-                     GEOS_FMT( "{}: upwinding scheme {} only supports 2-phase flow", getDataContext(),
+                     GEOS_FMT( "{}: upwinding scheme {} only supports 2-phase flow",
+                               getDataContext(),
                                EnumStrings< UpwindingScheme >::toString( UpwindingScheme::HU2PH )),
                      getDataContext() );
 }
