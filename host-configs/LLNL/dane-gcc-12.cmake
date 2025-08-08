1e627df4b4d1cfde1abae635e62749354fa14128
jay-a <jay.appleton@colorado.edu>
2025-08-08 14:16:39 -0700

    force overwritten tpl path from LvArray host-configs in root host-configs to avoid unnecessary modifications to the LvArray submodule

--- host-configs/LLNL/dane-gcc-12.cmake
+++ host-configs/LLNL/dane-gcc-12.cmake
@@ -1,4 +1,5 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../../src/coreComponents/LvArray/host-configs/LLNL/dane-gcc-12.cmake)
+set(GEOS_TPL_DIR "${GEOS_TPL_ROOT_DIR}/2024-12-06/install-${CONFIG_NAME}-release" CACHE PATH "Override path" FORCE)
 
 # MPI
 set(MPI_HOME /usr/tce/packages/mvapich2/mvapich2-2.3.7-gcc-12.1.1-magic CACHE PATH "")
