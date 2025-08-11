64cf52ee0a242fcbc16d78cb64333115f67b745d
Pavel Tomin <paveltomin@users.noreply.github.com>
2025-08-11 16:44:38 -0500

    Update PoromechanicsSolver.hpp

--- src/coreComponents/physicsSolvers/multiphysics/PoromechanicsSolver.hpp
+++ src/coreComponents/physicsSolvers/multiphysics/PoromechanicsSolver.hpp
@@ -105,13 +105,21 @@ public:
       setDescription( "Constant multiplier of stabilization strength" );
   }
 
-  virtual void initializePostInitialConditionsPreSubGroups() override
+  virtual void postInputInitialization() override
   {
-    Base::initializePostInitialConditionsPreSubGroups();
+    Base::postInputInitialization();
 
     GEOS_THROW_IF( this->m_isThermal && !this->flowSolver()->isThermal(),
                    GEOS_FMT( "{} {}: The attribute `{}` of the flow solver must be thermal since the poromechanics solver is thermal",
                              this->getCatalogName(), this->getName(), this->flowSolver()->getName() ),
+                   InputError );
+
+    GEOS_THROW_IF( this->solidMechanicsSolver()->timeIntegrationOption() != SolidMechanicsLagrangianFEM::TimeIntegrationOption::QuasiStatic,
+                   GEOS_FMT( "{} {}: The attribute `{}` of solid mechanics solver `{}` must be `{}`",
+                             this->getCatalogName(), this->getName(),
+                             SolidMechanicsLagrangianFEM::viewKeyStruct::timeIntegrationOptionString(),
+                             this->solidMechanicsSolver()->getName(),
+                             EnumStrings< SolidMechanicsLagrangianFEM::TimeIntegrationOption >::toString( SolidMechanicsLagrangianFEM::TimeIntegrationOption::QuasiStatic ) ),
                    InputError );
   }
 
