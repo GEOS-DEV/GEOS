6c561afb7ba90ed01fb0cd60b4a61b6fe45ce59b
Omar Duran <omaryesiduran@gmail.com>
2025-08-17 22:53:52 -0700

    Update testPolyhedralDiscretizations.cpp

--- src/coreComponents/integrationTest/singlePhaseFlow/polyhedralDiscretizations/testPolyhedralDiscretizations.cpp
+++ src/coreComponents/integrationTest/singlePhaseFlow/polyhedralDiscretizations/testPolyhedralDiscretizations.cpp
@@ -1,6 +1,7 @@
 /*
  * ------------------------------------------------------------------------------------------------------------
  * SPDX-License-Identifier: LGPL-2.1-only
+ *
  * Copyright (c) 2016-2024 Lawrence Livermore National Security LLC
  * Copyright (c) 2018-2024 TotalEnergies
  * Copyright (c) 2018-2024 The Board of Trustees of the Leland Stanford Junior University
@@ -11,116 +12,174 @@
  */
 
 #include <gtest/gtest.h>
+#include "unitTests/fluidFlowTests/testCompFlowUtils.hpp"
 #include "mainInterface/initialization.hpp"
 #include "mainInterface/ProblemManager.hpp"
 #include "mainInterface/GeosxState.hpp"
 #include "mesh/DomainPartition.hpp"
-#include "mesh/MeshManager.hpp"
-#include "mesh/NodeManager.hpp"
-#include "mesh/FaceManager.hpp"
-#include "mesh/CellElementSubRegion.hpp"
 #include "physicsSolvers/fluidFlow/SinglePhaseFVM.hpp"
-#include "finiteVolume/TwoPointFluxApproximation.hpp"
-#include "constitutive/fluid/singlefluid/CompressibleSinglePhaseFluid.hpp"
-//#include "constitutive/CompressibleSolidConstantPermeability.hpp"
-#include "constitutive/NullModel.hpp"
-#include "constitutive/solid/porosity/PressurePorosity.hpp"
-#include "constitutive/permeability/ConstantPermeability.hpp"
 
 using namespace geos;
+using namespace geos::dataRepository;
+using namespace geos::testing;
 
-constexpr double maxCoordInX = 1.0;
-constexpr double maxCoordInY = 1.0;
-constexpr double maxCoordInZ = 1.0;
-constexpr localIndex numElemsInX = 10;
-constexpr localIndex numElemsInY = 10;
-constexpr localIndex numElemsInZ = 10;
+CommandLineOptions g_commandLineOptions;
+
+// Define the XML input for the test
+char const * xmlInput =
+  R"xml(
+  <Problem>
+
+  <Mesh>
+      <VTKMesh
+        name="mesh"
+        logLevel="5"  
+        partitionRefinement="0"
+        useGlobalIds="0"
+        file="polyhedral_extruded.vtk"/>
+    </Mesh>
+
+    <Geometry>
+        <Box
+        name="westBC"
+        xMin="{ -0.001, 0.0, 0.0}"
+        xMax="{ +0.001, 1.0, 1.0}"/>
+        <Box
+        name="eastBC"
+        xMin="{ +0.999, 0.0, 0.0}"
+        xMax="{ +1.001, 1.0, 1.0}"/>
+    </Geometry>
+
+    <ElementRegions>
+      <CellElementRegion
+        name="Domain"
+        cellBlocks="{ * }"
+        materialList="{rock, fluid }"/>
+    </ElementRegions>
+
+    <Solvers gravityVector="{ 0.0, 0.0, 0.0}"> </Solvers>
+
+    <Constitutive>
+
+      <CompressibleSinglePhaseFluid
+        name="fluid"
+        defaultDensity="1000"
+        defaultViscosity="0.001"
+        referencePressure="0.0"
+        compressibility="0.0"
+        viscosibility="0.0"/>
+
+      <CompressibleSolidConstantPermeability
+        name="rock"
+        solidModelName="nullSolid"
+        porosityModelName="rockPorosity"
+        permeabilityModelName="rockPerm"/>
+
+      <NullModel
+        name="nullSolid"/>
+
+      <PressurePorosity
+        name="rockPorosity"
+        defaultReferencePorosity="0.1"
+        referencePressure="0.0"
+        compressibility="0.0"/>
+
+      <ConstantPermeability
+        name="rockPerm"
+        permeabilityComponents="{ 1.0e-13, 1.0e-13, 1.0e-13 }"/>
+
+    </Constitutive>
+
+    <FieldSpecifications>
+
+      <FieldSpecification
+        name="initialPressure"
+        initialCondition="1"
+        setNames="{ all }"
+        objectPath="ElementRegions/Domain"
+        fieldName="pressure"
+        scale="1.0e7"/>    
+      <FieldSpecification
+        name="west_pressure"
+        setNames="{ westBC }"
+        objectPath="faceManager"
+        fieldName="pressure"
+        scale="2.0e7" />
+      <FieldSpecification
+        name="east_pressure"
+        setNames="{ eastBC }"
+        objectPath="faceManager"
+        fieldName="pressure"
+        scale="1.0e7" />      
+
+    </FieldSpecifications>
+
+    <NumericalMethods>
+      <FiniteVolume>
+        <TwoPointFluxApproximation
+          name="singlePhaseTPFA"/>
+      </FiniteVolume>
+    </NumericalMethods>
+
+    <Solvers>
+      <SinglePhaseFVM
+        name="SinglePhaseFlow"
+        logLevel="1"
+        discretization="singlePhaseTPFA"
+        targetRegions="{ Domain }">
+        <NonlinearSolverParameters
+          newtonTol="1.0e-5"
+          newtonMaxIter="2"/>
+        <LinearSolverParameters
+          directParallel="0"/>
+      </SinglePhaseFVM>
+    </Solvers>
+
+    <Events
+      minTime="0.0"
+      maxTime="86400">
+      <PeriodicEvent
+        name="outputs"
+        timeFrequency="86400"
+        target="/Outputs/vtkConsistencyTPFA"/>
+      <PeriodicEvent
+        name="solverApplications"
+        endTime="86400"
+        maxEventDt="86400"
+        target="/Solvers/SinglePhaseFlow"/>
+    </Events>
+
+    <Outputs>
+      <VTK
+        name="vtkConsistencyTPFA"/>
+    </Outputs>
+
+  </Problem>
+  )xml";
 
 class TPFAIntegrationTest : public ::testing::Test {
+
+public:
+  TPFAIntegrationTest() : state( std::make_unique< CommandLineOptions >( g_commandLineOptions ) ) {}
+
 protected:
   void SetUp() override {
-    // Setup mesh and problem from XML string (unit cube, TPFA, single phase)
-    string const inputStream = GEOS_FMT(
-      "<Problem>"
-      "  <Mesh>"
-      "    <InternalMesh name=\"mesh1\" elementTypes=\"{{C3D8}}\" xCoords=\"{{0,1}}\" yCoords=\"{{0,1}}\" zCoords=\"{{0,1}}\" nx=\"{{10}}\" ny=\"{{10}}\" nz=\"{{10}}\" cellBlockNames=\"{{cb1}}\"/>"
-      "  </Mesh>"
-      "  <ElementRegions>"
-      "    <CellElementRegion name=\"Domain\" cellBlocks=\"{{cb1}}\" materialList=\"{{rock,fluid}}\"/>"
-      "  </ElementRegions>"
-      "  <NumericalMethods>"
-      "    <FiniteVolume>"
-      "      <TwoPointFluxApproximation name=\"singlePhaseTPFA\"/>"
-      "    </FiniteVolume>"
-      "  </NumericalMethods>"
-      "  <Solvers>"
-      "    <SinglePhaseFVM name=\"SinglePhaseFlow\" logLevel=\"1\" discretization=\"singlePhaseTPFA\" targetRegions=\"{Domain}\">"
-      "      <NonlinearSolverParameters newtonTol=\"1.0e-5\" newtonMaxIter=\"2\"/>"
-      "      <LinearSolverParameters directParallel=\"0\"/>"
-      "    </SinglePhaseFVM>"
-      "  </Solvers>"
-      "  <Constitutive>"
-      "    <CompressibleSinglePhaseFluid name=\"fluid\" defaultDensity=\"1000\" defaultViscosity=\"0.001\" referencePressure=\"0.0\" compressibility=\"0.0\" viscosibility=\"0.0\"/>"
-      "    <CompressibleSolidConstantPermeability name=\"rock\" solidModelName=\"nullSolid\" porosityModelName=\"rockPorosity\" permeabilityModelName=\"rockPerm\"/>"
-      "    <NullModel name=\"nullSolid\"/>"
-      "    <PressurePorosity name=\"rockPorosity\" defaultReferencePorosity=\"0.1\" referencePressure=\"0.0\" compressibility=\"0.0\"/>"
-      "    <ConstantPermeability name=\"rockPerm\" permeabilityComponents=\"{1.0e-13,1.0e-13,1.0e-13}\"/>"
-      "  </Constitutive>"
-      "  <FieldSpecifications>"
-      "    <FieldSpecification name=\"initialPressure\" initialCondition=\"1\" setNames=\"{all}\" objectPath=\"ElementRegions/Domain\" fieldName=\"pressure\" scale=\"1.0e7\"/>"
-      "    <FieldSpecification name=\"west_pressure\" setNames=\"{westBC}\" objectPath=\"faceManager\" fieldName=\"pressure\" scale=\"2.0e7\"/>"
-      "    <FieldSpecification name=\"east_pressure\" setNames=\"{eastBC}\" objectPath=\"faceManager\" fieldName=\"pressure\" scale=\"1.0e7\"/>"
-      "  </FieldSpecifications>"
-      "  <Geometry>"
-      "    <Box name=\"westBC\" xMin=\"{-0.001,0.0,0.0}\" xMax=\"{+0.001,1.0,1.0}\"/>"
-      "    <Box name=\"eastBC\" xMin=\"{+0.999,0.0,0.0}\" xMax=\"{+1.001,1.0,1.0}\"/>"
-      "  </Geometry>"
-      "</Problem>");
-
-    xmlWrapper::xmlDocument xmlDocument;
-    xmlWrapper::xmlResult xmlResult = xmlDocument.loadString(inputStream);
-    ASSERT_TRUE(xmlResult);
-    xmlWrapper::xmlNode xmlProblemNode = xmlDocument.getChild(dataRepository::keys::ProblemManager);
-    ProblemManager & problemManager = getGlobalState().getProblemManager();
-    problemManager.processInputFileRecursive(xmlDocument, xmlProblemNode);
-    DomainPartition & domain = problemManager.getDomainPartition();
-    MeshManager & meshManager = problemManager.getGroup<MeshManager>(problemManager.groupKeys.meshManager);
-    meshManager.generateMeshLevels(domain);
-    ElementRegionManager & elementManager = domain.getMeshBody(0).getBaseDiscretization().getElemManager();
-    xmlWrapper::xmlNode topLevelNode = xmlProblemNode.child(elementManager.getName().c_str());
-    elementManager.processInputFileRecursive(xmlDocument, topLevelNode);
-    elementManager.postInputInitializationRecursive();
-    problemManager.problemSetup();
-    problemManager.applyInitialConditions();
+    // Setup problem from XML input
+    setupProblemFromXML( state.getProblemManager(), xmlInput );
   }
+
+  GeosxState state;
 };
 
 TEST_F(TPFAIntegrationTest, PressureFieldL2Error) {
-  ProblemManager & problemManager = getGlobalState().getProblemManager();
+  ProblemManager & problemManager = state.getProblemManager();
   DomainPartition & domain = problemManager.getDomainPartition();
-  MeshLevel & mesh = domain.getMeshBody(0).getBaseDiscretization();
-  CellElementSubRegion & subRegion = mesh.getElemManager().getRegion(0).getSubRegion<CellElementSubRegion>(0);
-  localIndex numElems = subRegion.size();
-  arrayView1d<real64 const> pressure = subRegion.getField<real64>("pressure");
-  arrayView2d<real64 const> centers = subRegion.getElementCenter();
-  // Reference solution: linear from 2e7 to 1e7 in x
-  std::vector<real64> reference(numElems);
-  for(localIndex elemID = 0; elemID < numElems; ++elemID) {
-    reference[elemID] = 2.0e7 - centers[elemID][0] * 1.0e7;
-  }
-  // Compute L2 error
-  double l2err = 0.0;
-  for(localIndex elemID = 0; elemID < numElems; ++elemID) {
-    l2err += std::pow(pressure[elemID] - reference[elemID], 2);
-  }
-  l2err = std::sqrt(l2err / numElems);
-  std::cout << "TPFA L2 error: " << l2err << std::endl;
-  EXPECT_GT(l2err, 1e-6) << "TPFA solution should be inexact";
+  // Add test logic here to validate the pressure field or other properties
 }
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  GeosxState state(geos::basicSetup(argc, argv));
+  g_commandLineOptions = *geos::basicSetup(argc, argv);
   int result = RUN_ALL_TESTS();
   geos::basicCleanup();
   return result;
