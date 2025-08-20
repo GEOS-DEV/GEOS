b1dbc6a1e4226eecdcd7247110ff94f618e40b77
aurelien <aurelien.citrain@inria.fr>
2025-08-20 11:45:46 +0200

    Test with LvArray log

--- src/coreComponents/physicsSolvers/wavePropagation/shared/TaperKernel.hpp
+++ src/coreComponents/physicsSolvers/wavePropagation/shared/TaperKernel.hpp
@@ -119,7 +119,7 @@ struct TaperKernel
 
       if( dist<sizeT )
       {
-        taperCoeff[a] = LvArray::math::exp((((3*vMin)/(2*sizeT))*logR*pow((sizeT-dist)/sizeT, 2 ))*dt );
+        taperCoeff[a] = LvArray::math::exp((((3*vMin)/(2*sizeT))*LvArray::math::log(r)*pow((sizeT-dist)/sizeT, 2 ))*dt );
       }
       else
       {
