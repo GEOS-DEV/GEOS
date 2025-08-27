7eaa72912803a5f483316016d891f0614c7993a9
Pavel Tomin <ptls@PW0BMYGN.localdomain>
2025-08-27 12:10:10 -0500

    code style

--- src/coreComponents/constitutive/solid/SolidBase.hpp
+++ src/coreComponents/constitutive/solid/SolidBase.hpp
@@ -575,7 +575,7 @@ public:
     static constexpr char const * densityString() { return "density"; }                ///< Density key
     static constexpr char const * defaultDensityString() { return "defaultDensity"; }  ///< Default density key
     static constexpr char const * thermalExpansionCoefficientString() { return "solidThermalExpansionCoefficient"; } // Thermal expansion
-                                                                                                                // coefficient key
+    // coefficient key
     static constexpr char const * defaultThermalExpansionCoefficientString() { return "defaultDrainedLinearTEC"; } // Default
                                                                                                                    // drained
                                                                                                                    // linear
