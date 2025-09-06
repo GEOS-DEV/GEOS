80e58d1deb8a3b3ff3a98e927e7c9df2feaf781e
Omar Duran <omaryesiduran@gmail.com>
2025-09-06 15:55:06 -0700

    Update ImmiscibleMultiphaseFlowMFD.cpp

--- src/coreComponents/physicsSolvers/fluidFlow/ImmiscibleMultiphaseFlowMFD.cpp
+++ src/coreComponents/physicsSolvers/fluidFlow/ImmiscibleMultiphaseFlowMFD.cpp
@@ -277,7 +277,7 @@ void ImmiscibleMultiphaseFlowMFD::setupDofs( DomainPartition const & domain, Dof
   // face pressure
   dofManager.addField( flow::facePressure::key(), FieldLocation::Face, 1, getMeshTargets() );
   // ensure symmetric sparsity between element and face blocks
-  dofManager.addCoupling( viewKeyStruct::elemDofFieldString(), flow::facePressure::key(), DofManager::Connector::Elem );
+  dofManager.addCoupling( viewKeyStruct::elemDofFieldString(), flow::facePressure::key(), DofManager::Connector::Face );
   dofManager.addCoupling( flow::facePressure::key(), flow::facePressure::key(), DofManager::Connector::Elem );
   dofManager.addCoupling( flow::facePressure::key(), viewKeyStruct::elemDofFieldString(), DofManager::Connector::Elem );
 }
