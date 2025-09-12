dcf43e11edb9c9823174d0802d8a8cb1cdb3c8f9
Pavel Tomin <paveltomin@users.noreply.github.com>
2025-09-12 18:49:49 -0500

    fix: stats output logic (#3815)

--- src/coreComponents/physicsSolvers/PhysicsSolverBase.cpp
+++ src/coreComponents/physicsSolvers/PhysicsSolverBase.cpp
@@ -106,7 +106,7 @@ PhysicsSolverBase::PhysicsSolverBase( string const & name,
     setApplyDefaultValue( 0 ).
     setInputFlag( InputFlags::OPTIONAL ).
     setRestartFlags( RestartFlags::NO_WRITE ).
-    setDescription( "When set to 1, output iterations information to a csv\nWhen set to 2 output convergence information to a csv" );
+    setDescription( "When set to 1, output iterations information to a csv\nWhen set to 2 also output convergence information to a csv" );
 
   addLogLevel< logInfo::Convergence >();
   addLogLevel< logInfo::Fields >();
@@ -127,11 +127,11 @@ PhysicsSolverBase::PhysicsSolverBase( string const & name,
 void PhysicsSolverBase::postInputInitialization()
 {
   m_solverStatistics.setOutputFilesName( getName() );
-  m_solverStatistics.makeDir( m_writeStatisticsCSV >= 2 );
+  m_solverStatistics.makeDir( m_writeStatisticsCSV >= 1 );
 
   getIterationStats().setTableName( getName() );
-  getIterationStats().setLogOutputState( m_writeStatisticsCSV >= 1 );
-  getIterationStats().setCSVOutputState( m_writeStatisticsCSV >= 2 );
+  getIterationStats().setLogOutputState( true );
+  getIterationStats().setCSVOutputState( m_writeStatisticsCSV >= 1 );
   getConvergenceStats().setCSVOutputState( m_writeStatisticsCSV >= 2 );
 }
 
--- src/coreComponents/physicsSolvers/SolverStatistics.cpp
+++ src/coreComponents/physicsSolvers/SolverStatistics.cpp
@@ -184,15 +184,18 @@ void IterationsStatistics::outputStatistics() const
   TableData iterationDataLog;
   iterationDataLog.addRow( "Time steps", m_numTimeSteps );
   iterationDataLog.addRow( "Time step cuts", m_numTimeStepCuts );
-  iterationDataLog.addRow( "Successful configuration iterations", m_numSuccessfulConfigIterations );
+  if( m_numSuccessfulConfigIterations > 0 ) // only print when solver did any configuration iterations
+    iterationDataLog.addRow( "Successful configuration iterations", m_numSuccessfulConfigIterations );
   iterationDataLog.addRow( "Successful nonlinear iterations", m_numSuccessfulNonlinearIterations );
-  iterationDataLog.addRow( "Successful linear iterations", m_numSuccessfulLinearIterations );
-  iterationDataLog.addRow( "Discarded configuration iterations", m_numDiscardedConfigIterations );
+  if( m_numSuccessfulLinearIterations > 0 ) // only print when solver did any linear iterations
+    iterationDataLog.addRow( "Successful linear iterations", m_numSuccessfulLinearIterations );
+  if( m_numSuccessfulConfigIterations > 0 ) // only print when solver did any configuration iterations
+    iterationDataLog.addRow( "Discarded configuration iterations", m_numDiscardedConfigIterations );
   iterationDataLog.addRow( "Discarded nonlinear iterations", m_numDiscardedNonlinearIterations );
-  iterationDataLog.addRow( "Discarded linear iterations", m_numDiscardedLinearIterations );
+  if( m_numSuccessfulLinearIterations > 0 ) // only print when solver did any linear iterations
+    iterationDataLog.addRow( "Discarded linear iterations", m_numDiscardedLinearIterations );
 
   GEOS_LOG_RANK_0( statsFormatter.toString( iterationDataLog ));
-  GEOS_LOG_RANK_0( "" ); // blank line for readability
 }
 
 ConvergenceStatistics::ConvergenceStatistics():
