0cbeeab4b4d5f0b693faee0b66898cc47815f83c
arng40 <arnaud.dudes@capgemini.com>
2025-09-15 15:01:32 +0200

    1st iteration description wrapper of fieldSpecificationBase

--- src/coreComponents/fieldSpecification/FieldSpecificationBase.cpp
+++ src/coreComponents/fieldSpecification/FieldSpecificationBase.cpp
@@ -30,7 +30,9 @@ FieldSpecificationBase::FieldSpecificationBase( string const & name, Group * par
     setRTTypeName( rtTypes::CustomTypes::groupNameRefArray ).
     setInputFlag( InputFlags::REQUIRED ).
     setSizedFromParent( 0 ).
-    setDescription( "Name of sets that boundary condition is applied to." );
+    setDescription( "Names of sets that boundary condition is applied to."
+                    "A set can contains heterogeneous elements in the mesh (volumnes, nodes, faces, edges)."
+                    "A set can be defined by a `Geometry` component, or can be targeted with externalSet in cases of external mesh" );
 
   registerWrapper( viewKeyStruct::objectPathString(), &m_objectPath ).
     setRTTypeName( rtTypes::CustomTypes::groupNameRef ).
@@ -40,12 +42,14 @@ FieldSpecificationBase::FieldSpecificationBase( string const & name, Group * par
   registerWrapper( viewKeyStruct::fieldNameString(), &m_fieldName ).
     setRTTypeName( rtTypes::CustomTypes::groupNameRef ).
     setInputFlag( InputFlags::OPTIONAL ).
-    setDescription( "Name of field that boundary condition is applied to." );
+    setDescription( "Name of field that boundary condition is applied to."
+                    "A field can represent a physical variable. (pressure, temperature, global composition fraction of the fluid, ...)" );
 
   registerWrapper( viewKeyStruct::componentString(), &m_component ).
     setApplyDefaultValue( -1 ).
     setInputFlag( InputFlags::OPTIONAL ).
-    setDescription( "Component of field (if tensor) to apply boundary condition to." );
+    setDescription( "Component of field (if tensor) to apply boundary condition to."
+                    "The component must use the order in which the phaseNames have been defined in the Constitutive Element." );
 
   registerWrapper( viewKeyStruct::directionString(), &m_direction ).
     setApplyDefaultValue( {0, 0, 0} ).
@@ -65,7 +69,7 @@ FieldSpecificationBase::FieldSpecificationBase( string const & name, Group * par
   registerWrapper( viewKeyStruct::scaleString(), &m_scale ).
     setApplyDefaultValue( 0.0 ).
     setInputFlag( InputFlags::OPTIONAL ).
-    setDescription( "Scale factor for value of the boundary condition." );
+    setDescription( "Apply a scaling factor for the value of the boundary condition." );
 
   registerWrapper( viewKeyStruct::initialConditionString(), &m_initialCondition ).
     setApplyDefaultValue( 0 ).
--- src/coreComponents/functions/TableFunction.cpp
+++ src/coreComponents/functions/TableFunction.cpp
@@ -69,7 +69,8 @@ TableFunction::TableFunction( const string & name,
     setInputFlag( InputFlags::OPTIONAL ).
     setRestartFlags( RestartFlags::NO_WRITE ).
     setDescription( "When set to 1, write PVT tables into a CSV file.\n "
-                    "if the table is requested to be output in the log, and it is too large, a CSV file will be generated even if `writeCSV` is set to 0." );
+                    "if the table is requested to be output in the log, and it is too large, a CSV file will be generated even if `writeCSV` is set to 0."
+                    "Set to 0 by default" );
 
   addLogLevel< logInfo::TableLogOutput >();
 }
