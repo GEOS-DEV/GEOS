c2aeb606076644c891996f903391ca5ff877f6d1
Pavel Tomin <ptls@PW0BMYGN.localdomain>
2025-09-15 21:11:58 -0500

    rel perm tests fix

--- src/coreComponents/integrationTests/constitutiveTests/testRelPermHysteresis.cpp
+++ src/coreComponents/integrationTests/constitutiveTests/testRelPermHysteresis.cpp
@@ -121,7 +121,6 @@ TableRelativePermeabilityHysteresis & makeTableRelPermHysteresisTwoPhase( string
   auto & imbibitionGasTableName = relPerm.getReference< string >( keys::imbibitionNonWettingRelPermTableNameString() );
   imbibitionGasTableName = "imbibitionGas_swg";
 
-  relPerm.allocateConstitutiveData( parent, 1 );
   relPerm.postInputInitializationRecursive();
   relPerm.initialize(); // to test all the checks
   return relPerm;
