56b13e6415cae171770bcc7cfd884792375805dd
MelReyCG <melvin.rey@capgemini.com>
2025-10-03 10:45:59 +0200

    🐛 compil bugfix attempt for debug target clang10, cuda11.8.89

--- src/coreComponents/linearAlgebra/multiscale/msrsb/MsrsbLevelBuilderCoupled.cpp
+++ src/coreComponents/linearAlgebra/multiscale/msrsb/MsrsbLevelBuilderCoupled.cpp
@@ -263,7 +263,9 @@ bool MsrsbLevelBuilderCoupled< LAI >::updateProlongation( Matrix const & fineMat
       {
         auto const blockVals = block.getEntries( localRow );
         auto const coupledVals = prolongation.getEntries( rowOffset + localRow );
-        GEOS_ASSERT_EQ( blockVals.size(), coupledVals.size() );
+        localIndex const blockSize = blockVals.size();
+        localIndex const coupledSize = coupledVals.size();
+        GEOS_ASSERT_EQ( blockSize, coupledSize );
         std::copy( blockVals.dataIfContiguous(),
                    blockVals.dataIfContiguous() + blockVals.size(),
                    coupledVals.dataIfContiguous() );
