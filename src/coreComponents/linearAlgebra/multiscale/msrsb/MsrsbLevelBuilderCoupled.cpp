53b37f123312674f20a5594547d2cb1fe2708129
MelReyCG <melvin.rey@capgemini.com>
2025-10-07 11:16:01 +0200

    🐛 silly test 2

--- src/coreComponents/linearAlgebra/multiscale/msrsb/MsrsbLevelBuilderCoupled.cpp
+++ src/coreComponents/linearAlgebra/multiscale/msrsb/MsrsbLevelBuilderCoupled.cpp
@@ -263,8 +263,8 @@ bool MsrsbLevelBuilderCoupled< LAI >::updateProlongation( Matrix const & fineMat
       {
         auto const blockVals = block.getEntries( localRow );
         auto const coupledVals = prolongation.getEntries( rowOffset + localRow );
-        auto const blocksCount = blockVals.size();
-        auto const coupledCount = coupledVals.size();
+        GEOS_MAYBE_UNUSED auto const blocksCount = blockVals.size();
+        GEOS_MAYBE_UNUSED auto const coupledCount = coupledVals.size();
         GEOS_ASSERT_EQ( blocksCount, coupledCount );
         std::copy( blockVals.dataIfContiguous(),
                    blockVals.dataIfContiguous() + blockVals.size(),
