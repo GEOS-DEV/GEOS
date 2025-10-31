2c68dbba894cfdb5acd055f5767ccbb062181513
Pavel Tomin <ptls@PW0BMYGN.localdomain>
2025-10-31 14:23:59 -0500

    root

--- src/coreComponents/physicsSolvers/PhysicsSolverBase.cpp
+++ src/coreComponents/physicsSolvers/PhysicsSolverBase.cpp
@@ -1390,7 +1390,7 @@ void PhysicsSolverBase::solveLinearSystem( DofManager const & dofManager,
 
   matrix.setDofManager( &dofManager );
 
-  GEOS_WARNING_IF( isDirectSolver && dofManager.numGlobalDofs() > 100000,
+  GEOS_WARNING_IF( logger::internal::rank == 0 && isDirectSolver && dofManager.numGlobalDofs() > 100000,
                    "Direct solver used for large system ( > 100,000 DOFs ). "
                    "This may lead to high memory consumption and long computation times. "
                    "Consider using an iterative solver for better performance." );
