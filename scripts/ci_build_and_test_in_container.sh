183262c9de3ed8542bc1841b8cac8170a7588fda
arng40 <arnaud.dudes@capgemini.com>
2025-11-18 17:21:16 +0100

    test code for detecting std::map

--- scripts/ci_build_and_test_in_container.sh
+++ scripts/ci_build_and_test_in_container.sh
@@ -284,7 +284,60 @@ fi
 
 # Documentation check
 if [[ "${TEST_CODE_RULES}" = true ]]; then
-  echo "Test if the build workds"
+  FILE_PATTERNS=(
+            "src/coreComponents/codingUtilities/*"
+            "src/coreComponents/dataRepository/*"
+            "src/coreComponents/denseLinearAlgebra/*"
+            "src/coreComponents/discretizationMethods/*"
+            "src/coreComponents/events/*"
+            "src/coreComponents/fieldSpecification/*"
+            "src/coreComponents/fileIO/*"
+            "src/coreComponents/finiteElement/*"
+            "src/coreComponents/finiteVolume/*"
+            "src/coreComponents/functions/*"
+            "src/coreComponents/linearAlgebra/*"
+            "src/coreComponents/mainInterface/*"
+            "src/coreComponents/mesh/*"
+            "src/coreComponents/physicsSolvers/*"
+          )
+
+    EXCLUDE_PATTERNS=(
+          "src/coreComponents/common/Datatype.hpp"
+          "src/coreComponents/common/StdContaienrWrappers.hpp"
+        )
+
+  echo "Test if the build works"
+    FIND_CMD="find"
+    for pattern in "${FILE_PATTERNS[@]}"; do
+      FIND_CMD="$FIND_CMD -path '$pattern' -o"
+    done
+    FIND_CMD="${FIND_CMD% -o}"
+
+    VIOLATIONS_FOUND=0
+
+    FILES=$(eval "$FIND_CMD" 2>/dev/null || echo "");
+        
+    for file in $FILES; do
+      SKIP=0
+      for exclude in "${EXCLUDE_PATTERNS[@]}"; do
+        if [[ "$file" == *"$exclude"* ]]; then
+          SKIP=1
+          break
+        fi
+      done
+      
+      if [ $SKIP -eq 1 ]; then
+        continue
+      fi
+      
+      if grep -n "std::map\s*<" "$file" ; then
+        echo "Found forbidden std::map usage in: $file"
+        grep -n "std::map\s*<" "$file" | while read line; do
+          echo "   Line: $line"
+        done
+        VIOLATIONS_FOUND=1
+      fi
+    done
   exit 0
 fi
 
