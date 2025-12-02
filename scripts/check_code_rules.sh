a1b5b2fbd386dc945e781e22012f0bba28125f11
arng40 <arnaud.dudes@capgemini.com>
2025-12-02 10:06:37 +0100

    echo file

--- scripts/check_code_rules.sh
+++ scripts/check_code_rules.sh
@@ -79,12 +79,14 @@ for pattern in "${FILE_PATTERNS[@]}"; do
     fi
 done
 
+ARRAY_FILES=()
 # -print0 : ask find to separate file paths by (\0)
 # mapfile used for reading input line sinto an array; -d $'\0': Specifies that the delimiter is (\0).
 mapfile -d $'\0' ARRAY_FILES < <(find "${FILE_PATH_ARGS[@]}" -type f \( -name "*.hpp" -o -name "*.cpp" \) -print0 2>/dev/null)
 
 # Main loop
 for file in ${ARRAY_FILES}; do
+  echo $(file)
   SKIP=0
   for exclude in "${EXCLUDE_PATTERNS[@]}"; do
     if [[ "$file" == *"$exclude"* ]]; then
