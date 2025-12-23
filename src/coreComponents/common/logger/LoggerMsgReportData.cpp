ce6a7dd80a7ed42df262542f270b6deac87f7346
arng40 <arnaud.dudes@capgemini.com>
2025-12-23 17:22:18 +0100

    simplify toString

--- src/coreComponents/common/logger/LoggerMsgReportData.cpp
+++ src/coreComponents/common/logger/LoggerMsgReportData.cpp
@@ -12,7 +12,6 @@ namespace geos
 
 void LoggerMsgReportData::increment( string const & logPartName, MsgType msgType )
 {
-  std::cout << "DEBUG-- "<< logPartName<< " " << EnumStringsCore< MsgType >::toRawString( msgType ) << std::endl;
   if( numMsgByPart.count( logPartName ) ==0 )
   {
     NumMsg numMsg{ { {msgType, 1}}, {{msgType, 1} }};
@@ -20,8 +19,7 @@ void LoggerMsgReportData::increment( string const & logPartName, MsgType msgType
   }
   else
   {
-    std::cout << "check "<< std::endl;
-    NumMsg & numMsg = numMsgByPart.at(logPartName);
+    NumMsg & numMsg = numMsgByPart.at( logPartName );
     numMsg.numMsg[msgType]++;
     numMsg.numMsgLoc[msgType]++;
   }
@@ -32,8 +30,6 @@ string TableTextFormatter::toString< LoggerMsgReportData >( LoggerMsgReportData 
 {
   TableLayout tableLayoutPerSection;
   TableData dataPerSection;
-  /// {Numerical Methods : { Warning : 4 }, { Exception : 2 } }
-  stdMap< string, stdMap< string, int > > cells;
 
   std::ostringstream oss;
   tableLayoutPerSection.addColumn( " Types " );
@@ -41,37 +37,23 @@ string TableTextFormatter::toString< LoggerMsgReportData >( LoggerMsgReportData 
   for( auto const & [ logPartName, numMsg ] : report.numMsgByPart )
   {
     tableLayoutPerSection.addColumn( logPartName );
-    for( auto const & [ msgType, count ] : numMsg.numMsg )
-    {
-      string const msgTypeStr = EnumStringsCore< MsgType >::toRawString( msgType );
-      cells.get_inserted( logPartName ).get_inserted( msgTypeStr ) = count;
-    }
-
-    for( size_t fooInt = (size_t) MsgType::Error; fooInt != (size_t)MsgType::Undefined; fooInt++ )
-    {
-      string type =  EnumStringsCore< MsgType >::toRawString( (MsgType) fooInt );
-      if( cells.get_inserted( logPartName ).count( type ) == 0 )
-        cells.get_inserted( logPartName ).get_inserted( type ) = 0;
-    }
   }
 
-  for( size_t fooInt = (size_t) MsgType::Error; fooInt != (size_t)MsgType::Undefined; fooInt++ )
+
+  for( size_t i  = (size_t) MsgType::Error; i  != (size_t)MsgType::Undefined; i++ )
   {
-    stdVector< TableData::CellData > countPerLogPart;
-    string const type=  EnumStringsCore< MsgType >::toRawString( (MsgType) fooInt );
-    countPerLogPart.push_back( {CellType::Value, type } );
-    for( auto const & [ logPartName, msgTypes ] : cells )
+    MsgType const currentType = (MsgType) i;
+    stdVector< TableData::CellData > row;
+    
+    row.push_back( {CellType::Value,  EnumStringsCore< MsgType >::toRawString( (MsgType) i ) } );
+    for( auto const & [ _, msgTypes ] : report.numMsgByPart )
     {
-      if( msgTypes.count( type ) != 0 )
-      {
-        countPerLogPart.push_back( {CellType::Value, std::to_string( msgTypes.at( type ) )} );
-      }
-      else
-      {
-        countPerLogPart.push_back( {CellType::Value, 0} );
-      }
+      auto it =  msgTypes.numMsg.find( currentType );
+      int const count = ( it != msgTypes.numMsg.end() ) ? it->second : 0;
+      row.push_back( {CellType::Value, std::to_string( count )} );
+
     }
-    dataPerSection.addRow( countPerLogPart );
+    dataPerSection.addRow( row );
   }
 
   TableTextFormatter textFormatter( tableLayoutPerSection );
