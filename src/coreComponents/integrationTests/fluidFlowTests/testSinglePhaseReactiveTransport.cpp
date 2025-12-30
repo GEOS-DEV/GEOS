60dc3160a6d5487e1a4c40a68e769346df3ca72a
frankfeifan <fei2@llnl.gov>
2025-12-30 12:18:02 -0800

    fix some code coverage complaints

--- src/coreComponents/integrationTests/fluidFlowTests/testSinglePhaseReactiveTransport.cpp
+++ src/coreComponents/integrationTests/fluidFlowTests/testSinglePhaseReactiveTransport.cpp
@@ -14,11 +14,11 @@
  */
 
 #include "constitutive/fluid/singlefluid/SingleFluidBase.hpp"
-#include "constitutive/fluid/singlefluid/SingleFluidBase.hpp"
 #include "finiteVolume/FiniteVolumeManager.hpp"
 #include "finiteVolume/FluxApproximationBase.hpp"
 #include "mainInterface/initialization.hpp"
 #include "mainInterface/GeosxState.hpp"
+#include "physicsSolvers/PhysicsSolverBase.hpp"
 #include "physicsSolvers/PhysicsSolverManager.hpp"
 #include "physicsSolvers/fluidFlow/SinglePhaseBaseFields.hpp"
 #include "physicsSolvers/fluidFlow/SinglePhaseReactiveTransportFields.hpp"
@@ -42,10 +42,12 @@ char const * xmlInputCarbonate =
     <Solvers>
       <SinglePhaseReactiveTransport name="SinglePhaseReactiveFlow"
                                     logLevel="1"
+                                    isUpdateReactivePorosity="1"
                                     discretization="fluidTPFA"
                                     targetRegions="{ region }">
-        <NonlinearSolverParameters newtonTol="1.0e-6"
-                                   newtonMaxIter="100" />
+        <NonlinearSolverParameters newtonTol="1.0e-4"
+                                   newtonMaxIter="100"
+                                   maxAllowedResidualNorm="1e100" />
         <LinearSolverParameters directParallel="0"/>
       </SinglePhaseReactiveTransport>
     </Solvers>
@@ -64,9 +66,6 @@ char const * xmlInputCarbonate =
       <Box name="source"
            xMin="{ -0.01, -0.01, -0.01 }"
            xMax="{ 1.01, 1.01, 1.01 }"/>
-      <Box name="sink"
-           xMin="{ 1.99, -0.01, -0.01 }"
-           xMax="{ 3.01, 1.01, 1.01 }"/>
     </Geometry>
     <NumericalMethods>
       <FiniteVolume>
@@ -76,20 +75,21 @@ char const * xmlInputCarbonate =
     <ElementRegions>
       <CellElementRegion name="region"
                          cellBlocks="{ * }"
-                         materialList="{ water, rock }" />
+                         materialList="{ water, rock, diffusion }" />
     </ElementRegions>
     <Constitutive>
-      <CompressibleSolidConstantPermeability name="rock"
-                                             solidModelName="nullSolid"
-                                             porosityModelName="rockPorosity"
-                                             permeabilityModelName="rockPerm" />
+      <ReactiveSolidConstantPermeability name="rock"
+                                         solidModelName="nullSolid"
+                                         porosityModelName="rockPorosity"
+                                         permeabilityModelName="rockPerm" />
       <NullModel name="nullSolid" />
-      <PressurePorosity name="rockPorosity"
+      <ReactivePorosity name="rockPorosity"
                         defaultReferencePorosity="0.1"
-                        referencePressure="0.0"
-                        compressibility="0.0" />
+                        defaultInitialVolumeFractions="{ 0.1 }"
+                        molarWeights="{ 0.100087 }"
+                        mineralDensities="{ 2710 }"/>
       <ConstantPermeability name="rockPerm"
-                            permeabilityComponents="{ 2.0e-13, 2.0e-13, 2.0e-13 }" />
+                            permeabilityComponents="{ 3.0e-16, 3.0e-16, 3.0e-16 }" />
       <ReactiveCompressibleSinglePhaseFluid name="water"
                                             defaultDensity="1000"
                                             defaultViscosity="0.001"
@@ -97,6 +97,10 @@ char const * xmlInputCarbonate =
                                             compressibility="5e-10"
                                             viscosibility="0.0"
                                             chemicalSystemType="carbonate"/>
+      <ConstantDiffusion name="diffusion"
+                         phaseNames="{ water }"
+                         defaultPhaseDiffusivityMultipliers="{ 1 }"
+                         diffusivityComponents="{ 1e-2, 1e-2, 1e-2 }"/>
     </Constitutive>
     <FieldSpecifications>
       <FieldSpecification name="Porosity"
@@ -112,15 +116,15 @@ char const * xmlInputCarbonate =
                           fieldName="pressure"
                           scale="1e6" />
       <FieldSpecification name="sinkPressure"
-                          setNames="{ sink }"
-                          objectPath="ElementRegions/region/cb"
+                          setNames="{ xpos }"
+                          objectPath="faceManager"
                           fieldName="pressure"
-                          scale="1e6" />
+                          scale="1.0e6"/>
       <FieldSpecification name="sourcePressure"
                           setNames="{ source }"
                           objectPath="ElementRegions/region/cb"
                           fieldName="pressure"
-                          scale="1.2e6" />
+                          scale="2.0e6" />
 
       <FieldSpecification name="surfaceArea"
                           initialCondition="1"
@@ -128,7 +132,15 @@ char const * xmlInputCarbonate =
                           objectPath="ElementRegions/region/cb"
                           fieldName="initialSurfaceArea"
                           component="0"
-                          scale="1.0"/>
+                          scale="2.4573e+06"/>
+
+      <FieldSpecification name="surfaceAreaSource"
+                          initialCondition="1"
+                          setNames="{ source }"
+                          objectPath="ElementRegions/region/cb"
+                          fieldName="initialSurfaceArea"
+                          component="0"
+                          scale="0.0"/>
 
       <FieldSpecification
         name="initialAggregate_H_all"
@@ -518,7 +530,7 @@ protected:
   }
 
   static real64 constexpr time = 0.0;
-  static real64 constexpr dt = 1000;
+  static real64 constexpr dt = 1.0;
   static real64 constexpr eps = std::numeric_limits< real64 >::epsilon();
 
   GeosxState state;
@@ -558,6 +570,83 @@ TEST_F( SinglePhaseReactiveTransportTest, jacobianNumericalCheck_accumulationBal
   {
     solver->assembleAccumulationTermsInMassBalanceAndSpeciesAmountEqs( dt, domain, solver->getDofManager(), localMatrix, localRhs );
   } );
+}
+
+class ReactivePorosityUpdateTest : public ::testing::Test
+{
+protected:
+  ReactivePorosityUpdateTest() = default;
+};
+
+// Helper function to copy arrayView2d<real64 const> to std::vector<real64>
+static inline std::vector< real64 > arrayView2dToVector( arrayView2d< real64 const > & arr, localIndex n_k, localIndex n_q )
+{
+  arr.move( hostMemorySpace, false );
+
+  std::vector< real64 > outputVector;
+  outputVector.resize( static_cast<localIndex>( n_k ) * static_cast<localIndex>( n_q ) );
+
+  localIndex k = 0;
+  for( localIndex i = 0; i < n_k; ++i )
+  {
+    for( localIndex j = 0; j < n_q; ++j )
+    {
+      outputVector[k++] = arr( i, j );
+    }
+  }
+  return outputVector;
+}
+
+TEST_F( ReactivePorosityUpdateTest, ReactivePorosityUpdate )
+{
+  GeosxState state( std::make_unique< CommandLineOptions >( g_commandLineOptions ));
+
+  setupProblemFromXML( state.getProblemManager(), xmlInputCarbonate );
+
+  ProblemManager & pm = state.getProblemManager();
+  DomainPartition & domain = pm.getDomainPartition();
+
+  auto & solver =
+      dynamic_cast< SinglePhaseReactiveTransport & >(
+        pm.getPhysicsSolverManager().getGroup< SinglePhaseReactiveTransport >( "SinglePhaseReactiveFlow" ));
+  
+  static real64 constexpr dt = 10;
+
+  solver.setupSystem( domain, solver.getDofManager(),
+                      solver.getLocalMatrix(), solver.getSystemRhs(),
+                      solver.getSystemSolution());
+  solver.implicitStepSetup( 0.0, dt, domain );
+  solver.solverStep( 0.0, dt, 0, domain );
+  solver.implicitStepComplete( 0.0, dt, domain );
+  
+  MeshLevel & mesh = domain.getMeshBody( 0 ).getBaseDiscretization();
+  CellElementSubRegion & subRegion =
+      mesh.getElemManager().getRegion( 0 ).getSubRegion< CellElementSubRegion >( 0 );
+  
+  solver.updatePorosityAndPermeability( subRegion );
+  solver.updateSurfaceArea( subRegion );
+  
+  CoupledSolidBase const & solid =
+        subRegion.getConstitutiveModel< CoupledSolidBase >( subRegion.getReference< string >( FlowSolverBase::viewKeyStruct::solidNamesString() ) );
+
+  arrayView2d< real64 const > phi_h = solid.getPorosity();
+  localIndex const n_k = phi_h.size( 0 );
+  localIndex const n_q = phi_h.size( 1 );
+
+  std::vector< real64 > const phi = arrayView2dToVector( phi_h, n_k, n_q );
+
+  real64 const expectedPorosity[3] = { 0.1, 0.099999989106167683, 0.099999995467245847 };
+
+  // --- Compare cellwise ---
+  ASSERT_EQ( n_k*n_q, 3 );
+
+  for( localIndex i = 0; i < n_k*n_q; ++i )
+  {
+    real64 phi_num = phi[i];
+    real64 phi_diff = std::abs( phi_num - expectedPorosity[i] );
+    // Assert that the phi_diff is within machine precision
+    EXPECT_NEAR( phi_diff, 0.0, 1.0e-10 );
+  }
 }
 
 int main( int argc, char * * argv )
