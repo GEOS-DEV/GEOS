35077dd29faccc933e8e7b90a4d552f946d8be33
li97 <li97@llnl.gov>
2026-01-07 12:25:42 -0800

    finite strain kernel compiled
    (cherry picked from commit f65b31d61a88df113686e0d65d805f51b7423ed4)

--- src/coreComponents/physicsSolvers/solidMechanics/SolidMechanicsLagrangianFEM.cpp
+++ src/coreComponents/physicsSolvers/solidMechanics/SolidMechanicsLagrangianFEM.cpp
@@ -22,6 +22,7 @@
 #include "SolidMechanicsLagrangianFEM.hpp"
 #include "kernels/ImplicitSmallStrainNewmark.hpp"
 #include "kernels/ImplicitSmallStrainQuasiStatic.hpp"
+#include "kernels/ImplicitFiniteStrainQuasiStatic.hpp"
 #include "kernels/ExplicitSmallStrain.hpp"
 #include "kernels/ExplicitFiniteStrain.hpp"
 #include "kernels/FixedStressThermoPoromechanics.hpp"
@@ -48,6 +49,7 @@
 
 #include "physicsSolvers/LogLevelsInfo.hpp"
 #include "physicsSolvers/solidMechanics/kernels/SolidMechanicsKernelsDispatchTypeList.hpp"
+#include "physicsSolvers/solidMechanics/kernels/SolidMechanicsImplicitFiniteStrainKernelsDispatchTypeList.hpp"
 #include "physicsSolvers/solidMechanics/kernels/SolidMechanicsFixedStressThermoPoromechanicsKernelsDispatchTypeList.hpp"
 #include "physicsSolvers/fluidFlow/FlowSolverBase.hpp"
 
--- src/coreComponents/physicsSolvers/solidMechanics/kernels/ImplicitFiniteStrainQuasiStatic_impl.hpp
+++ src/coreComponents/physicsSolvers/solidMechanics/kernels/ImplicitFiniteStrainQuasiStatic_impl.hpp
@@ -20,7 +20,7 @@
 #ifndef GEOS_PHYSICSSOLVERS_SOLIDMECHANICS_KERNELS_IMPLCITFINITESTRAINQUASISTATIC_IMPL_HPP_
 #define GEOS_PHYSICSSOLVERS_SOLIDMECHANICS_KERNELS_IMPLCITFINITESTRAINQUASISTATIC_IMPL_HPP_
 
-#include "ImplicitFinitStrainQuasiStatic.hpp"
+#include "ImplicitFiniteStrainQuasiStatic.hpp"
 namespace geos
 {
 
@@ -28,10 +28,11 @@ namespace solidMechanicsLagrangianFEMKernels
 {
 
 template<typename SUBREGION_TYPE, typename CONSTITUTIVE_TYPE, typename FE_TYPE>
-ImplicitFiniteStrainQuasis<SUBREGION_TYPE, CONSTITUTIVE_TYPE, FE_TYPE> :: ImplicitFinitStrainQuasiStatic(
+ImplicitFiniteStrainQuasiStatic<SUBREGION_TYPE, CONSTITUTIVE_TYPE, FE_TYPE> :: ImplicitFiniteStrainQuasiStatic(
+    NodeManager const & nodeManager, EdgeManager const & edgeManager,
     FaceManager const & faceManager, localIndex const targetRegionIndex,
     SUBREGION_TYPE const & elementSubRegion, FE_TYPE const & finiteElementSpace,
-    CONSTITUTIVE_TYPE const & inputConstitutiveType, arrayView1d<globalIndex const> const inputDofNumber,
+    CONSTITUTIVE_TYPE & inputConstitutiveType, arrayView1d<globalIndex const> const inputDofNumber,
     globalIndex const rankOffset, CRSMatrixView<real64, globalIndex const> const inputMatrix,
     arrayView1d<real64> const inputRhs, real64 const inputDt, real64 const (&inputGravityVector)[3])
   : Base(nodeManager, edgeManager, faceManager, targetRegionIndex, elementSubRegion, finiteElementSpace,
@@ -54,7 +55,7 @@ void ImplicitFiniteStrainQuasiStatic<SUBREGION_TYPE, CONSTITUTIVE_TYPE, FE_TYPE>
   localIndex const numSupportPoints = m_finiteElementSpace.template numSupportPoints<FE_TYPE>(stack.feStack);
 
   stack.numRows = 3 * numSupportPoints;
-  stack.numRows = stack.numRows;
+  stack.numCols = stack.numRows;
 
   for (localIndex a = 0; a < numSupportPoints; ++a)
   {
@@ -63,12 +64,12 @@ void ImplicitFiniteStrainQuasiStatic<SUBREGION_TYPE, CONSTITUTIVE_TYPE, FE_TYPE>
     for (int i = 0; i < numDofPerTestSupportPoint; ++i)
     {
 #if defined(CALC_FEM_SHAPE_IN_KERNEL)
-      stack.xLocal[ a ][ i ] = m_X[ localNodeIndex ][ i ];
+      stack.xLocal[a][i] = m_X[localNodeIndex][i];
 #endif
-      stack.u_local[ a ][i] = m_disp[ localNodeIndex ][i];
-      stack.uhat_local[ a ][i] = m_uhat[ localNodeIndex ][i];
-      stack.localRowDofIndex[a*3+i] = m_dofNumber[localNodeIndex]+i;
-      stack.localColDofIndex[a*3+i] = m_dofNumber[localNodeIndex]+i;
+      stack.u_local[a][i] = m_disp[localNodeIndex][i];
+      stack.uhat_local[a][i] = m_uhat[localNodeIndex][i];
+      stack.localRowDofIndex[a * 3 + i] = m_dofNumber[localNodeIndex] + i;
+      stack.localColDofIndex[a * 3 + i] = m_dofNumber[localNodeIndex] + i;
     }
   }
 
@@ -86,7 +87,8 @@ void ImplicitFiniteStrainQuasiStatic<SUBREGION_TYPE, CONSTITUTIVE_TYPE, FE_TYPE>
   real64 const detJxW = m_finiteElementSpace.template getGradN<FE_TYPE>(k, q, stack.xLocal, stack.feStack, dNdX);
 
   real64 stress[3][3] = { {0} };
-  real64 stiffness[9][9] = { {0} };
+  // real64 stiffness[9][9] = { {0} };
+  typename CONSTITUTIVE_TYPE::KernelWrapper::DiscretizationOps stiffness;
 
   real64 dUhatdX[3][3] = { {0} };
   real64 dUdX[3][3] = { {0} };
@@ -121,6 +123,51 @@ void ImplicitFiniteStrainQuasiStatic<SUBREGION_TYPE, CONSTITUTIVE_TYPE, FE_TYPE>
                                    N,
                                    gravityForce,
                                    reinterpret_cast< real64 (&)[numNodesPerElem][3] >(stack.localResidual) );
+
+  stiffness.template BTDB<numNodesPerElem>(dNdX, -detJxW, stack.localJacobian);
+}
+
+template<typename SUBREGION_TYPE, typename CONSTITUTIVE_TYPE, typename FE_TYPE>
+GEOS_HOST_DEVICE
+GEOS_FORCE_INLINE
+real64 ImplicitFiniteStrainQuasiStatic<SUBREGION_TYPE, CONSTITUTIVE_TYPE, FE_TYPE>::complete(
+    localIndex const k, StackVariables & stack) const
+{
+  GEOS_UNUSED_VAR( k );
+  real64 maxForce = 0;
+
+  localIndex const numSupportPoints = m_finiteElementSpace.template numSupportPoints< FE_TYPE >( stack.feStack );
+
+  // #pragma unroll
+  for( int localNode = 0; localNode < numSupportPoints; ++localNode )
+  {
+    // #pragma unroll
+    for( int dim = 0; dim < numDofPerTestSupportPoint; ++dim )
+    {
+      localIndex const dof = LvArray::integerConversion< localIndex >( stack.localRowDofIndex[ numDofPerTestSupportPoint * localNode + dim ] - m_dofRankOffset );
+      if( dof < 0 || dof >= m_matrix.numRows() )
+        continue;
+      m_matrix.template addToRowBinarySearchUnsorted< parallelDeviceAtomic >( dof,
+                                                                              stack.localRowDofIndex,
+                                                                              stack.localJacobian[ numDofPerTestSupportPoint * localNode + dim ],
+                                                                              stack.numRows );
+
+      RAJA::atomicAdd< parallelDeviceAtomic >( &m_rhs[ dof ], stack.localResidual[ numDofPerTestSupportPoint * localNode + dim ] );
+      maxForce = fmax( maxForce, fabs( stack.localResidual[ numDofPerTestSupportPoint * localNode + dim ] ) );
+    }
+  }
+
+  return maxForce;
+}
+
+template<typename SUBREGION_TYPE, typename CONSTITUTIVE_TYPE, typename FE_TYPE>
+template<typename POLICY, typename KERNEL_TYPE>
+GEOS_FORCE_INLINE
+real64
+ImplicitFiniteStrainQuasiStatic<SUBREGION_TYPE, CONSTITUTIVE_TYPE, FE_TYPE>::kernelLaunch(localIndex const numElems,
+                                                                                          KERNEL_TYPE const & kernelComponent)
+{
+  return Base::template kernelLaunch< POLICY, KERNEL_TYPE >( numElems, kernelComponent );
 }
 
 } // namespace solidMechanicsLagrangianFEMKernels
