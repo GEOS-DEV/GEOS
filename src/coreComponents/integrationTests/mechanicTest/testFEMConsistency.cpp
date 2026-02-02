7d0995d6bed8fb20ac4b8ea2c1cbd82a57139c43
Omar Duran <omaryesiduran@gmail.com>
2026-02-02 11:22:18 -0800

    Update testFEMConsistency.cpp

--- src/coreComponents/integrationTests/mechanicTest/testFEMConsistency.cpp
+++ src/coreComponents/integrationTests/mechanicTest/testFEMConsistency.cpp
@@ -53,7 +53,7 @@ protected:
 <Problem>
   <Mesh>
     <VTKMesh name="mesh1" file=")xml" << meshFile << R"xml(" nodesetNames=")xml" << nodeSetNames <<
-    R"xml("/>
+      R"xml("/>
   </Mesh>
   <Geometry>
     <Box name="xnegFace" xMin="{ -0.01, -0.01, -0.01 }" xMax="{  0.01,  1.01,  1.01 }"/>
@@ -98,20 +98,20 @@ protected:
   </Constitutive>
   <FieldSpecifications>
     <FieldSpecification name="separableFace" fieldName="isFaceSeparable" initialCondition="1" setNames=")xml" << nodeSetNames <<
-    R"xml(" objectPath="faceManager" scale="1" />
+      R"xml(" objectPath="faceManager" scale="1" />
     <FieldSpecification name="frac" initialCondition="1" setNames=")xml" << nodeSetNames <<
-    R"xml(" objectPath="faceManager" fieldName="ruptureState" scale="1" />
+      R"xml(" objectPath="faceManager" fieldName="ruptureState" scale="1" />
     
     <FieldSpecification name="xneg_disp" component="0" setNames="{ xnegFace }" objectPath="nodeManager" fieldName="totalDisplacement" scale="0.0"/>
     <FieldSpecification name="yneg_disp" component="1" setNames="{ ynegFace }" objectPath="nodeManager" fieldName="totalDisplacement" scale="0.0"/>
     <FieldSpecification name="zneg_disp" component="2" setNames="{ znegFace }" objectPath="nodeManager" fieldName="totalDisplacement" scale="0.0"/>
     
     <Traction name="xpos_traction" setNames="{ xposFace }" objectPath="faceManager" tractionType="normal" scale=")xml" << s_xx <<
-    R"xml("/>
+      R"xml("/>
     <Traction name="ypos_traction" setNames="{ yposFace }" objectPath="faceManager" tractionType="normal" scale=")xml" << s_yy <<
-    R"xml("/>
+      R"xml("/>
     <Traction name="zpos_traction" setNames="{ zposFace }" objectPath="faceManager" tractionType="normal" scale=")xml" << s_zz <<
-    R"xml("/>
+      R"xml("/>
   </FieldSpecifications>
   <Tasks>
     <SolidMechanicsAugmentedLagrangianContactInitialization name="ELASTICITY.PRE.INIT.STEP" solidSolverName="mechSolver" logLevel="1"/>
@@ -324,19 +324,19 @@ INSTANTIATE_TEST_SUITE_P(
     // Mesh, s_xx, s_yy, s_zz  (Triaxial)
     // Hex meshes
     std::make_tuple( "fractured_mesh_hex_DFN_1.vtu", -1.0e7, -0.5e7, -2.0e7 ),
-    std::make_tuple( "fractured_mesh_hex_DFN_2.vtu", -1.0e7, -0.5e7, -2.0e7 ), 
-    std::make_tuple( "fractured_mesh_hex_DFN_3.vtu", -1.0e7, -0.5e7, -2.0e7 ), 
-    std::make_tuple( "fractured_mesh_hex_DFN_12.vtu", -1.0e7, -0.5e7, -2.0e7 ), 
-    std::make_tuple( "fractured_mesh_hex_DFN_13.vtu", -1.0e7, -0.5e7, -2.0e7 ), 
-    std::make_tuple( "fractured_mesh_hex_DFN_123.vtu", -1.0e7, -0.5e7, -2.0e7 ), 
+    std::make_tuple( "fractured_mesh_hex_DFN_2.vtu", -1.0e7, -0.5e7, -2.0e7 ),
+    std::make_tuple( "fractured_mesh_hex_DFN_3.vtu", -1.0e7, -0.5e7, -2.0e7 ),
+    std::make_tuple( "fractured_mesh_hex_DFN_12.vtu", -1.0e7, -0.5e7, -2.0e7 ),
+    std::make_tuple( "fractured_mesh_hex_DFN_13.vtu", -1.0e7, -0.5e7, -2.0e7 ),
+    std::make_tuple( "fractured_mesh_hex_DFN_123.vtu", -1.0e7, -0.5e7, -2.0e7 ),
 
     // Tet meshes
-    std::make_tuple( "fractured_mesh_tet_DFN_1.vtu", -1.0e7, -0.5e7, -2.0e7 ), 
-    std::make_tuple( "fractured_mesh_tet_DFN_2.vtu", -1.0e7, -0.5e7, -2.0e7 ), 
-    std::make_tuple( "fractured_mesh_tet_DFN_3.vtu", -1.0e7, -0.5e7, -2.0e7 ), 
-    std::make_tuple( "fractured_mesh_tet_DFN_12.vtu", -1.0e7, -0.5e7, -2.0e7 ), 
-    std::make_tuple( "fractured_mesh_tet_DFN_13.vtu", -1.0e7, -0.5e7, -2.0e7 ), 
-    std::make_tuple( "fractured_mesh_tet_DFN_123.vtu", -1.0e7, -0.5e7, -2.0e7 ),  
+    std::make_tuple( "fractured_mesh_tet_DFN_1.vtu", -1.0e7, -0.5e7, -2.0e7 ),
+    std::make_tuple( "fractured_mesh_tet_DFN_2.vtu", -1.0e7, -0.5e7, -2.0e7 ),
+    std::make_tuple( "fractured_mesh_tet_DFN_3.vtu", -1.0e7, -0.5e7, -2.0e7 ),
+    std::make_tuple( "fractured_mesh_tet_DFN_12.vtu", -1.0e7, -0.5e7, -2.0e7 ),
+    std::make_tuple( "fractured_mesh_tet_DFN_13.vtu", -1.0e7, -0.5e7, -2.0e7 ),
+    std::make_tuple( "fractured_mesh_tet_DFN_123.vtu", -1.0e7, -0.5e7, -2.0e7 ),
 
     // Wavy Hex meshes
     std::make_tuple( "fractured_wavy_mesh_hex_DFN_1.vtu", -1.0e7, -0.5e7, -2.0e7 ),
