f3bd6dd2736989c4ff0a788335c551a68596f84c
Omar Duran <omaryesiduran@gmail.com>
2026-02-03 11:11:31 -0800

    Update testMixedDimSinglePhaseFlow.cpp

--- src/coreComponents/integrationTests/fluidFlowTests/testMixedDimSinglePhaseFlow.cpp
+++ src/coreComponents/integrationTests/fluidFlowTests/testMixedDimSinglePhaseFlow.cpp
@@ -19,11 +19,6 @@
 #include "mainInterface/ProblemManager.hpp"
 #include "mainInterface/initialization.hpp"
 #include "codingUtilities/Utilities.hpp"
-#include "physicsSolvers/fluidFlow/FlowSolverBaseFields.hpp"
-#include "mesh/MeshLevel.hpp"
-#include "mesh/ElementRegionManager.hpp"
-#include "mesh/ElementSubRegionBase.hpp"
-#include "mesh/DomainPartition.hpp"
 
 using namespace geos;
 
@@ -194,7 +189,7 @@ TEST_P( MixedDimSinglePhaseFlowTest, Run )
       ProblemManager & pm = state.getProblemManager();
       MeshLevel & mesh = pm.getDomainPartition().getMeshBody( 0 ).getBaseDiscretization();
 
-      mesh.getElemManager().forElementSubRegions( [&]( localIndex const k, ElementSubRegionBase & subRegion )
+      mesh.getElemManager().forElementSubRegions( [&]( ElementSubRegionBase & subRegion )
       {
         string const & regionName = subRegion.getParent().getName();
         if( regionName != "Region" && regionName != "Fracture" )
@@ -205,7 +200,10 @@ TEST_P( MixedDimSinglePhaseFlowTest, Run )
         real64 const expectedPressure = ( regionName == "Region" ) ? 15.0e6 : 20.0e6;
         arrayView1d< real64 const > const pressure = subRegion.getField< fields::flow::pressure >();
         
-        ASSERT_NEAR( pressure[k], expectedPressure, 1.0e-5 );
+        for ( localIndex k = 0; k < subRegion.numElements(); ++k )
+        {
+          ASSERT_NEAR( pressure[k], expectedPressure, 1.0e-5 );
+        }
       } );
     }
 
@@ -215,7 +213,7 @@ TEST_P( MixedDimSinglePhaseFlowTest, Run )
       ProblemManager & pm = state.getProblemManager();
       MeshLevel & mesh = pm.getDomainPartition().getMeshBody( 0 ).getBaseDiscretization();
 
-      mesh.getElemManager().forElementSubRegions( [&]( localIndex const k, ElementSubRegionBase & subRegion )
+      mesh.getElemManager().forElementSubRegions( [&]( ElementSubRegionBase & subRegion )
       {
         string const & regionName = subRegion.getParent().getName();
         if( regionName != "Region" && regionName != "Fracture" )
@@ -226,18 +224,21 @@ TEST_P( MixedDimSinglePhaseFlowTest, Run )
         arrayView1d< real64 const > const pressure = subRegion.getField< fields::flow::pressure >();
         arrayView2d< real64 const > const center = subRegion.getElementCenter();
         
-        real64 const x = center[k][0];
-        real64 const expectedPressure = 20.0e6 * ( 1.0 - x ) + 15.0e6 * x;
+        for ( localIndex k = 0; k < subRegion.numElements(); ++k )
+        {
+          real64 const x = center[k][0];
+          real64 const expectedPressure = 20.0e6 * ( 1.0 - x ) + 15.0e6 * x;
 
-        // Tolerance: 1 Pa relative to >1e7 Pa is very tight.
-        // Using broader tolerance or relative error is safer.
-        // Let's use 10 Pa absolute tolerance (1e-6 relative to 10 MPa) which is quite good for FVM.
-        // Or 1000 Pa? The user said "exact", but numerical solutions are never exact.
-        // I'll stick to a reasonable tolerance, e.g. 1% of the range (5MPa). 50kPa.
-        // But for a simple 1D problem it should be very accurate.
-        // I saw 1.0e-10 in linear solver tolerance.
-        // I will use 1.0 here as absolute tolerance (1 Pa).
-        ASSERT_NEAR( pressure[k], expectedPressure, 1.0 );
+          // Tolerance: 1 Pa relative to >1e7 Pa is very tight.
+          // Using broader tolerance or relative error is safer.
+          // Let's use 10 Pa absolute tolerance (1e-6 relative to 10 MPa) which is quite good for FVM.
+          // Or 1000 Pa? The user said "exact", but numerical solutions are never exact.
+          // I'll stick to a reasonable tolerance, e.g. 1% of the range (5MPa). 50kPa.
+          // But for a simple 1D problem it should be very accurate.
+          // I saw 1.0e-10 in linear solver tolerance.
+          // I will use 1.0 here as absolute tolerance (1 Pa).
+          ASSERT_NEAR( pressure[k], expectedPressure, 1.0 );
+        }
       } );
     }
   }
