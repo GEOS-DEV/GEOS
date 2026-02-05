ddffc5fdee6f6f284d8f70c04c1cf15509097132
arng40 <arnaud.dudes@capgemini.com>
2026-02-05 13:45:00 +0100

    add FE_ALL_EXCEPT

--- src/coreComponents/common/logger/ErrorHandling.cpp
+++ src/coreComponents/common/logger/ErrorHandling.cpp
@@ -140,19 +140,22 @@ DiagnosticMsgBuilder & DiagnosticMsgBuilder::addSignal( integer const sig, bool 
     errorMsg = "Floating point error encountered: \n";
 
     if( std::fetestexcept( FE_DIVBYZERO ) )
-      errorMsg += "- Division by zero operation.\n";
+      errorMsg += "\n- Division by zero operation.";
 
     if( std::fetestexcept( FE_INVALID ) )
-      errorMsg += "- Domain error occurred in an earlier floating-point operation.\n";
+      errorMsg += "\n- Domain error occurred in an earlier floating-point operation.";
 
     if( std::fetestexcept( FE_OVERFLOW ) )
-      errorMsg += "- The result of the earlier floating-point operation was too large to be representable.\n";
+      errorMsg += "\n- The result of the earlier floating-point operation was too large to be representable.";
 
     if( std::fetestexcept( FE_UNDERFLOW ) )
-      errorMsg += "- The result of the earlier floating-point operation was subnormal with a loss of precision.\n";
+      errorMsg += "\n- The result of the earlier floating-point operation was subnormal with a loss of precision.";
 
     if( std::fetestexcept( FE_INEXACT ) )
-      errorMsg += "- Inexact result.\n";
+      errorMsg += "\n- Inexact result.";
+
+    if( !std::fetestexcept( FE_ALL_EXCEPT ) )
+      errorMsg += "\nUnknown reason.";
   }
   else
   {
