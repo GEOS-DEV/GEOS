45d80bc2c50b4f1e6da69403fa5038a6a9e1babb
Tom Byer <149726499+tjb-ltk@users.noreply.github.com>
2026-02-12 08:41:25 -0800

    lambda capture error

--- src/coreComponents/physicsSolvers/fluidFlow/wells/kernels/CompositionalMultiphaseWellKernels.cpp
+++ src/coreComponents/physicsSolvers/fluidFlow/wells/kernels/CompositionalMultiphaseWellKernels.cpp
@@ -469,7 +469,7 @@ RateInitializationKernel::
       std::vector< WellConstraintBase * >  const constraints = wellControls.getProdRateConstraints();
       // Use first rate constraint to set initial connection rates
       real64 const rateVal = constraints[0]->getConstraintValue( time );
-      forAll< parallelDevicePolicy<> >( subRegionSize, [&] GEOS_HOST_DEVICE ( localIndex const iwelem )
+      forAll< parallelDevicePolicy<> >( subRegionSize, [=] GEOS_HOST_DEVICE ( localIndex const iwelem )
       {
 
         connRate[iwelem] = LvArray::math::max( 0.1 * rateVal * phaseDens[iwelem][0][targetPhaseIndex], -1e3 );
