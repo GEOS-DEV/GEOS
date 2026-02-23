2216334c954bc556bd8300b95d6cd084b4ff0933
Omar Duran <omaryesiduran@gmail.com>
2026-02-23 00:59:26 -0800

    wip: making test faster VI

--- src/coreComponents/integrationTests/fluidFlowTests/testMixedDimHydrostaticEquilibrium.cpp
+++ src/coreComponents/integrationTests/fluidFlowTests/testMixedDimHydrostaticEquilibrium.cpp
@@ -103,7 +103,7 @@ protected:
       temperature="355.0"
       allowNegativePressure="0"
       logLevel="0">
-      <NonlinearSolverParameters newtonTol="1.0e-3" newtonMaxIter="4" lineSearchAction="None" maxAllowedResidualNorm="1.0e20"/>
+      <NonlinearSolverParameters newtonTol="1.0e-1" newtonMaxIter="4" lineSearchAction="None" maxAllowedResidualNorm="1.0e20"/>
       <LinearSolverParameters directParallel="0" reuseFactorization="0"/>
     </CompositionalMultiphaseFVM>
     <SurfaceGenerator name="SurfaceGen" targetRegions="{ Region, Fracture }" fractureRegion="Fracture" initialRockToughness="10.0e9" logLevel="0"/>
