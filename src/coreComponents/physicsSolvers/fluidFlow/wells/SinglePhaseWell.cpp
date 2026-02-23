5b9042b92c6a2c5dd4186c241054d1874e03eb4b
frankfeifan <fei2@llnl.gov>
2026-02-22 16:35:00 -0800

    temp dof offset fix

--- src/coreComponents/physicsSolvers/fluidFlow/wells/SinglePhaseWell.cpp
+++ src/coreComponents/physicsSolvers/fluidFlow/wells/SinglePhaseWell.cpp
@@ -1047,7 +1047,7 @@ SinglePhaseWell::scalingForSystemSolution( DomainPartition & domain,
 
         auto const subRegionData = thermalSinglePhaseBaseKernels::
                                      SolutionScalingKernel::
-                                     launch< parallelDevicePolicy<> >( localSolution, rankOffset, dofNumber, ghostRank,
+                                     launch< parallelDevicePolicy<> >( localSolution, rankOffset, 2, dofNumber, ghostRank,
                                                                        m_maxAbsolutePresChange, m_maxAbsoluteTempChange,
                                                                        pressureScalingFactor, temperatureScalingFactor );
 
