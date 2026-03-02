c32d8eb3efd09d7a349bf5370e727cf0d41b8b48
arng40 <arnaud.dudes@capgemini.com>
2026-03-02 15:41:12 +0100

    uniformize default allocator

--- src/coreComponents/common/StdContainerWrappers.hpp
+++ src/coreComponents/common/StdContainerWrappers.hpp
@@ -158,8 +158,8 @@ public:
  * @tparam T Type of elements in the vector.
  * @tparam Allocator Allocator type for the vector.
  */
-template< typename T, typename Allocator = std::allocator< T > >
-using stdVector = internal::StdVectorWrapper< T, Allocator, USE_STD_CONTAINER_BOUNDS_CHECKING >;
+template< typename T, typename Allocator = internal::DefaultAllocator<T> >
+using stdVector = internal::StdVectorWrapper< T, Allocator, false >;
 
 
 
