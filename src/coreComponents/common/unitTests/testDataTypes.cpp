fa00c7d5f9502054f43c7cf85182e28d71da4c12
arng40 <arnaud.dudes@capgemini.com>
2026-03-03 10:16:41 +0100

    comment geos

--- src/coreComponents/common/unitTests/testDataTypes.cpp
+++ src/coreComponents/common/unitTests/testDataTypes.cpp
@@ -15,50 +15,50 @@
 
 // Source includes
 
-#include "common/GeosxConfig.hpp"
+// #include "common/GeosxConfig.hpp"
 
-// #include "BufferAllocator.hpp" 
-#include "common/DataLayouts.hpp"
-#include "common/GeosxMacros.hpp"
-#include "common/Path.hpp"
-#include "common/StdContainerWrappers.hpp"
-#include "common/Tensor.hpp"
+// // #include "BufferAllocator.hpp" 
+// #include "common/DataLayouts.hpp"
+// #include "common/GeosxMacros.hpp"
+// #include "common/Path.hpp"
+// #include "common/StdContainerWrappers.hpp"
+// #include "common/Tensor.hpp"
 
-#include "LvArray/src/Macros.hpp"
-#include "LvArray/src/Array.hpp"
-#include "LvArray/src/ArrayOfArrays.hpp"
-#include "LvArray/src/ArrayOfSets.hpp"
-#include "LvArray/src/SparsityPattern.hpp"
-#include "LvArray/src/CRSMatrix.hpp"
-#include "LvArray/src/SortedArray.hpp"
-#include "LvArray/src/StackBuffer.hpp"
-#include "LvArray/src/ChaiBuffer.hpp"
+// #include "LvArray/src/Macros.hpp"
+// #include "LvArray/src/Array.hpp"
+// #include "LvArray/src/ArrayOfArrays.hpp"
+// #include "LvArray/src/ArrayOfSets.hpp"
+// #include "LvArray/src/SparsityPattern.hpp"
+// #include "LvArray/src/CRSMatrix.hpp"
+// #include "LvArray/src/SortedArray.hpp"
+// #include "LvArray/src/StackBuffer.hpp"
+// #include "LvArray/src/ChaiBuffer.hpp"
 
 
-// TPL includes
-#include <camp/camp.hpp>
+// // TPL includes
+// #include <camp/camp.hpp>
 
-// System includes
-#ifdef GEOS_USE_MPI
-  #include <mpi.h>
-#endif
+// // System includes
+// #ifdef GEOS_USE_MPI
+//   #include <mpi.h>
+// #endif
 
-#include <cassert>
-//#include <cmath>
-#include <cstdint>
-#include <iostream>
-#include <optional>
-#include <set>
-#include <string>
-#include <string_view>
-#include <typeindex>
-#include <typeinfo>
+// #include <cassert>
+// //#include <cmath>
+// #include <cstdint>
+// #include <iostream>
+// #include <optional>
+// #include <set>
+// #include <string>
+// #include <string_view>
+// #include <typeindex>
+// #include <typeinfo>
 
-// TPL includes
+// // TPL includes
 #include <gtest/gtest.h>
 #include <gtest/gtest-spi.h>
 
-using namespace geos;
+// using namespace geos;
 
 // TEST( testDataTypes, testBoundChecking )
 // {
