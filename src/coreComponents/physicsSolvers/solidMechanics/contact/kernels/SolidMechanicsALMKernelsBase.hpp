1a6c732881efef07aa3a7033cf329c13982d41f4
npillardou <nicolas.pillardou@gmail.com>
2026-03-06 16:28:56 +0100

    Correct compilation error

--- src/coreComponents/physicsSolvers/solidMechanics/contact/kernels/SolidMechanicsALMKernelsBase.hpp
+++ src/coreComponents/physicsSolvers/solidMechanics/contact/kernels/SolidMechanicsALMKernelsBase.hpp
@@ -153,7 +153,7 @@ struct UpdateStateKernel
         // When normal traction is lower than normalTractionTolerance, check if cohesion allows stick state
         // before deciding to transition to slip (fix for high cohesion materials)
         real64 dLimitTau_dTraction = 0.0;
-        real64 const limitTau = contactWrapper.computeLimitTangentialTractionNorm( localTractionNew[ 0 ], dLimitTau_dTraction );
+        real64 const limitTau = contactWrapper.computeLimitTangentialTractionNorm( k, localTractionNew[ 0 ], dLimitTau_dTraction );
         real64 const currentTau = LvArray::math::sqrt( localTractionNew[1] * localTractionNew[1] +
                                                        localTractionNew[2] * localTractionNew[2] );
 
