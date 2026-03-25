4cb105d86c1b312e3001690d3197e0d092137d11
kdrienCG <kevin.driencourt@capgemini.com>
2026-03-25 17:36:08 +0100

    SMLCBS: update calls to FieldSpecification

--- src/coreComponents/physicsSolvers/solidMechanics/contact/SolidMechanicsLagrangeContactBubbleStab.cpp
+++ src/coreComponents/physicsSolvers/solidMechanics/contact/SolidMechanicsLagrangeContactBubbleStab.cpp
@@ -75,13 +75,13 @@ real64 SolidMechanicsLagrangeContactBubbleStab::solverStep( real64 const & time_
       fieldSpecificationManager.applyInitialConditions( mesh );
       // Would like to do it like this but it is not working. There is a cast in Object path that tries to cast
       // all objects that derive from ElementSubRegionBase to the specified type so this obviously fails.
-      //   fieldSpecificationManager.forSubGroups< FieldSpecificationBase >( [&] ( FieldSpecificationBase const & fs )
+      //   fieldSpecificationManager.forSubGroups< FieldSpecification >( [&] ( FieldSpecification const & fs )
       //   {
       //     if( fs.initialCondition() )
       //     {
       //       FieldSpecificationImpl::apply< SurfaceElementSubRegion >( fs,
       //                                                                 mesh,
-      //                                                                 [&]( FieldSpecificationBase const & bc,
+      //                                                                 [&]( FieldSpecification const & bc,
       //                                                                      string const &,
       //                                                                      SortedArrayView< localIndex const > const & targetSet,
       //                                                                      SurfaceElementSubRegion & targetGroup,
