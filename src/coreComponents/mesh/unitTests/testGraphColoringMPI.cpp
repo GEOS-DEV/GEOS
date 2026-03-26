ee6fc983359396d594de3cd387d7731f977a18c2
arng40 <arnaud.dudes@capgemini.com>
2026-03-26 17:17:51 +0100

    :ambulance: remove double free

--- src/coreComponents/mesh/unitTests/testGraphColoringMPI.cpp
+++ src/coreComponents/mesh/unitTests/testGraphColoringMPI.cpp
@@ -162,7 +162,6 @@ int main( int argc, char * *argv )
   int result = RUN_ALL_TESTS();
 
   // Finalize MPI
-  MpiWrapper::commFree( MPI_COMM_GEOS );
   MpiWrapper::finalize( false );
 
   return result;
