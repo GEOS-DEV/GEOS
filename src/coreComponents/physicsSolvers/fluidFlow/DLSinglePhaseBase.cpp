7d032786255d8795d71e5df7c0076bcd35bdbdd0
FouadAmin <foad.m.amin@gmail.com>
2026-04-08 19:59:13 +0400

    Fix merge from upstream

--- src/coreComponents/physicsSolvers/fluidFlow/DLSinglePhaseBase.cpp
+++ src/coreComponents/physicsSolvers/fluidFlow/DLSinglePhaseBase.cpp
@@ -39,6 +39,7 @@
 #include "physicsSolvers/KernelLaunchSelectors.hpp"
 #include "physicsSolvers/fluidFlow/FlowSolverBaseFields.hpp"
 #include "physicsSolvers/fluidFlow/SinglePhaseBaseFields.hpp"
+#include "physicsSolvers/fluidFlow/SolutionCheckHelpers.hpp"
 #include "physicsSolvers/fluidFlow/kernels/singlePhase/MobilityKernel.hpp"
 #include "physicsSolvers/fluidFlow/kernels/singlePhase/SolutionCheckKernel.hpp"
 #include "physicsSolvers/fluidFlow/kernels/singlePhase/SolutionScalingKernel.hpp"
@@ -577,9 +578,9 @@ void DLSinglePhaseBase::computeHydrostaticEquilibrium( DomainPartition & domain 
                                                                           pressureValues.toView() );
 
       GEOS_THROW_IF( !equilHasConverged,
-                     getCatalogName() << " " << getDataContext() <<
-                     ": hydrostatic pressure initialization failed to converge in region " << region.getName() << "!",
-                     std::runtime_error, getDataContext() );
+                     GEOS_FMT( "Hydrostatic pressure initialization failed to converge in region {}!",
+                               region.getName() ),
+                     geos::RuntimeError, getDataContext() );
     } );
 
     // Step 3.4: create hydrostatic pressure table
@@ -1344,7 +1345,8 @@ bool DLSinglePhaseBase::checkSystemSolution( DomainPartition & domain,
   GEOS_MARK_FUNCTION;
 
   string const dofKey = dofManager.getKey( viewKeyStruct::elemDofFieldString() );
-  integer numNegativePressures = 0;
+  ElementsReporterBuffer rankNegPressureIds{ isLogLevelActive< logInfo::Solution >( getLogLevel() ),
+                                             isLogLevelActive< logInfo::SolutionDetails >( getLogLevel() ) ? 16 : 0 };
   real64 minPressure = 0.0;
 
   forDiscretizationOnMeshTargets( domain.getMeshBodies(), [&]( string const &,
@@ -1359,23 +1361,25 @@ bool DLSinglePhaseBase::checkSystemSolution( DomainPartition & domain,
       arrayView1d< globalIndex const > const dofNumber = subRegion.getReference< array1d< globalIndex > >( dofKey );
       arrayView1d< integer const > const ghostRank = subRegion.ghostRank();
       arrayView1d< real64 const > const pres = subRegion.getField< flow::pressure >();
+      auto const negPresCollector = rankNegPressureIds.createCollector( subRegion.localToGlobalMap().toViewConst() );
 
-      auto const statistics =
-        singlePhaseBaseKernels::SolutionCheckKernel::
-          launch< parallelDevicePolicy<> >( localSolution, rankOffset, dofNumber, ghostRank, pres, scalingFactor );
+      auto const statistics = singlePhaseBaseKernels::SolutionCheckKernel::
+                                launch< parallelDevicePolicy<> >( localSolution,
+                                                                  rankOffset,
+                                                                  dofNumber,
+                                                                  ghostRank,
+                                                                  pres,
+                                                                  scalingFactor,
+                                                                  negPresCollector );
 
-      numNegativePressures += statistics.first;
-      minPressure = std::min( minPressure, statistics.second );
+      minPressure = std::min( minPressure, statistics.minNegPres );
     } );
   } );
 
-  numNegativePressures = MpiWrapper::sum( numNegativePressures );
+  rankNegPressureIds.createOutput().outputTooLowValues( GEOS_FMT( "        {}: ", getName() ),
+                                                        "negative pressure", minPressure, units::Unit::Pressure );
 
-  if( numNegativePressures > 0 )
-    GEOS_LOG_LEVEL_RANK_0( logInfo::Solution, GEOS_FMT( "        {}: Number of negative pressure values: {}, minimum value: {} Pa",
-                                                        getName(), numNegativePressures, fmt::format( "{:.{}f}", minPressure, 3 ) ) );
-
-  return (m_allowNegativePressure || numNegativePressures == 0) ?  1 : 0;
+  return (m_allowNegativePressure || rankNegPressureIds.getSignaledElementsCount() == 0) ?  1 : 0;
 }
 
 void DLSinglePhaseBase::saveConvergedState( ElementSubRegionBase & subRegion ) const
