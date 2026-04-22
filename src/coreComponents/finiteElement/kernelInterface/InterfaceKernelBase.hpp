76ccf026533b017ecff857b72baed966c7c7f029
Randolph Settgast <rrsettgast@gmail.com>
2026-04-21 23:52:48 -0700

    a couple of missed CRSMatrixView instances

--- src/coreComponents/finiteElement/kernelInterface/InterfaceKernelBase.hpp
+++ src/coreComponents/finiteElement/kernelInterface/InterfaceKernelBase.hpp
@@ -177,7 +177,8 @@ template< template< typename ... > class KERNEL_TYPE,
 class InterfaceKernelFactory;
 
 /**
- * @brief Specialization of #::geos::finiteElement::InterfaceKernelFactory for the case where the kernel template has 2 template parameters (CONSTITUTIVE_TYPE, FE_TYPE).
+ * @brief Specialization of #::geos::finiteElement::InterfaceKernelFactory for the case where the kernel template has 2 template parameters
+ *(CONSTITUTIVE_TYPE, FE_TYPE).
  * @tparam KERNEL_TYPE Templated class that defines the physics kernel with 2 template parameters.
  *   Most likely derives from InterfaceKernelBase.
  * @tparam ARGS The trailing arguments forwarded to the kernel constructor in addition to the standard arguments.
@@ -247,7 +248,8 @@ private:
 };
 
 /**
- * @brief Specialization of #::geos::finiteElement::InterfaceKernelFactory for the case where the kernel template has 3 or more template parameters, including a MATRIX_VIEW (CONSTITUTIVE_TYPE, FE_TYPE, MATRIX_VIEW, ...).
+ * @brief Specialization of #::geos::finiteElement::InterfaceKernelFactory for the case where the kernel template has 3 or more template
+ *parameters, including a MATRIX_VIEW (CONSTITUTIVE_TYPE, FE_TYPE, MATRIX_VIEW, ...).
  * @tparam KERNEL_TYPE Templated class that defines the physics kernel and is parameterized on a global matrix view.
  *   Most likely derives from InterfaceKernelBase.
  * @tparam ARGS The trailing arguments forwarded to the kernel constructor in addition to the standard arguments.
--- src/coreComponents/finiteElement/kernelInterface/KernelBase.hpp
+++ src/coreComponents/finiteElement/kernelInterface/KernelBase.hpp
@@ -337,7 +337,8 @@ template< template< typename ... > class KERNEL_TYPE,
 class KernelFactory;
 
 /**
- * @brief Specialization of #::geos::finiteElement::KernelFactory for the case where the kernel template has 3 template parameters (SUBREGION_TYPE, CONSTITUTIVE_TYPE, FE_TYPE).
+ * @brief Specialization of #::geos::finiteElement::KernelFactory for the case where the kernel template has 3 template parameters
+ *(SUBREGION_TYPE, CONSTITUTIVE_TYPE, FE_TYPE).
  * @tparam KERNEL_TYPE Templated class that defines the physics kernel with 3 template parameters.
  *   Most likely derives from KernelBase.
  * @tparam ARGS The trailing arguments forwarded to the kernel constructor in addition to the standard arguments.
@@ -408,7 +409,8 @@ private:
 };
 
 /**
- * @brief Specialization of #::geos::finiteElement::KernelFactory for the case where the kernel template has 4 or more template parameters, including a MATRIX_VIEW (SUBREGION_TYPE, CONSTITUTIVE_TYPE, FE_TYPE, MATRIX_VIEW, ...).
+ * @brief Specialization of #::geos::finiteElement::KernelFactory for the case where the kernel template has 4 or more template parameters,
+ *including a MATRIX_VIEW (SUBREGION_TYPE, CONSTITUTIVE_TYPE, FE_TYPE, MATRIX_VIEW, ...).
  * @tparam KERNEL_TYPE Templated class that defines the physics kernel and is parameterized on a global matrix view.
  *   Most likely derives from KernelBase.
  * @tparam ARGS The trailing arguments forwarded to the kernel constructor in addition to the standard arguments.
