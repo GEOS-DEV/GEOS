092c73d403c048f9588c645ba1067af5ac4993b8
DENEL Bertrand <bertrand.denel@total.com>
2026-04-27 17:51:23 -0500

    Path again

--- src/docs/sphinx/advancedExamples/validationStudies/wellboreProblems/nonLinearThermalDiffusion_TemperatureDependentSinglePhaseThermalConductivity/thermalConductivityFigure.py
+++ src/docs/sphinx/advancedExamples/validationStudies/wellboreProblems/nonLinearThermalDiffusion_TemperatureDependentSinglePhaseThermalConductivity/thermalConductivityFigure.py
@@ -1,14 +1,8 @@
 import os
-import sys
-
 try:
-	_this_dir = os.path.dirname(os.path.abspath(__file__))
-	_scripts_dir = os.path.normpath(os.path.join(_this_dir, '../../../../../../../inputFiles/singlePhaseFlow/scripts'))
+    _this_dir = os.path.dirname(os.path.abspath(__file__))
 except NameError:
-	_src_dir = os.getcwd()
-	_this_dir = os.path.join(_src_dir, 'docs', 'sphinx', 'advancedExamples', 'validationStudies', 'wellboreProblems', 'nonLinearThermalDiffusion_TemperatureDependentSinglePhaseThermalConductivity')
-	_scripts_dir = os.path.join(_src_dir, 'inputFiles', 'singlePhaseFlow', 'scripts')
-sys.path.append(_scripts_dir)
-import wellboreTemperatureFigure
+    _this_dir = os.getcwd()
 
+import wellboreTemperatureFigure
 wellboreTemperatureFigure.main(xmlFilePrefix='thermalCompressible_temperatureDependentSinglePhaseThermalConductivity', outputDir=_this_dir)
