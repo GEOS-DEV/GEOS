29df4df396f5acd544e90793f7e98bdb63a0e520
DENEL Bertrand <bertrand.denel@total.com>
2026-04-27 16:05:18 -0500

    Typo

--- src/docs/sphinx/advancedExamples/validationStudies/wellboreProblems/nonLinearThermalDiffusion_TemperatureDependentVolumetricHeatCapacity/volumetricHeatCapacityFigure.py
+++ src/docs/sphinx/advancedExamples/validationStudies/wellboreProblems/nonLinearThermalDiffusion_TemperatureDependentVolumetricHeatCapacity/volumetricHeatCapacityFigure.py
@@ -3,7 +3,7 @@ import sys
 
 try:
 	_this_dir = os.path.dirname(os.path.abspath(__file__))
-	_scripts_dir = os.path.normpath(os.path.join(_this_dir, '../../../../../../../inputFiles/singlePhaseFlow/scripts')))
+	_scripts_dir = os.path.normpath(os.path.join(_this_dir, '../../../../../../../inputFiles/singlePhaseFlow/scripts'))
 except NameError:
 	_src_dir = os.getcwd()
 	_this_dir = os.path.join(_src_dir, 'docs', 'sphinx', 'advancedExamples', 'validationStudies', 'wellboreProblems', 'nonLinearThermalDiffusion_TemperatureDependentVolumetricHeatCapacity')
