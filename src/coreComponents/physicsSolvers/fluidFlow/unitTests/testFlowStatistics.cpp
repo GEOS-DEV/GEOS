49f53dfb8b6c9c94679090c2b40dd134eecf5c7d
kdrienCG <kevin.driencourt@capgemini.com>
2026-05-04 19:50:02 +0200

    (WIP) update stats test

--- src/coreComponents/physicsSolvers/fluidFlow/unitTests/testFlowStatistics.cpp
+++ src/coreComponents/physicsSolvers/fluidFlow/unitTests/testFlowStatistics.cpp
@@ -137,7 +137,7 @@ struct TestSet
         sinkMeanRate[ip] += inputs.sinkRates[timestepId][ip] * inputs.sinkRateFactor;
       }
       // mean rates calculation
-      real64 const ratesMeanDivisor = 1.0 / double( timestepCount - 1 );
+      real64 const ratesMeanDivisor = 1.0 / double( timestepCount );
       sourceMeanRate[ip] *= ratesMeanDivisor;
       sinkMeanRate[ip] *= ratesMeanDivisor;
       // totals
@@ -469,8 +469,7 @@ TestSet getTestSet()
          xMax="{ 10.01, 10.01, 0.01 }" />
   </Geometry>
 
-  <!-- We are adding 500s to the whole sim time to force the wholeSimStatsEvent to be executed -->
-  <Events maxTime="5500.0">
+  <Events maxTime="5000.0">
     <PeriodicEvent name="solverApplications"
                    forceDt="500.0"
                    target="/Solvers/testSolver" />
@@ -514,8 +513,8 @@ TestSet getTestSet()
       name="FluxRate"
       inputVarNames="{ time }"
       interpolation="lower"
-      coordinates="{    0.0,  500.0, 1000.0, 1500.0, 2000.0, 2500.0, 3000.0, 3500.0, 4000.0, 4500.0, 5000.0, 5500.0 }"
-      values="{       0.000,  0.000,  0.767,  0.894,  0.561,  0.234,  0.194,  0.178,  0.162,  0.059,  0.000,  0.000 }"
+      coordinates="{    0.0,  500.0, 1000.0, 1500.0, 2000.0, 2500.0, 3000.0, 3500.0, 4000.0, 4500.0, 5000.0 }"
+      values="{       0.000,  0.767,  0.894,  0.561,  0.234,  0.194,  0.178,  0.162,  0.059,  0.000,  0.000 }"
     />
   </Functions>
 
@@ -537,7 +536,6 @@ TestSet getTestSet()
   // FluxRate table from 0.0s to 5000.0s
   setRateTable( testInputs.sourceRates,
                 { { 0.000 },
-                  { 0.000 },
                   { 0.767 },
                   { 0.894 },
                   { 0.561 },
@@ -546,6 +544,7 @@ TestSet getTestSet()
                   { 0.178 },
                   { 0.162 },
                   { 0.059 },
+                  { 0.000 },
                   { 0.000 } } );
   testInputs.sinkRates=testInputs.sourceRates;
 
@@ -715,7 +714,7 @@ TestSet getTestSet()
   </Geometry>
 
   <!-- We are adding 500s to the whole sim time to force the wholeSimStatsEvent to be executed -->
-  <Events maxTime="5500.0">
+  <Events maxTime="5000.0">
     <PeriodicEvent name="solverApplications"
                    forceDt="500.0"
                    target="/Solvers/testSolver" />
@@ -759,16 +758,16 @@ TestSet getTestSet()
       name="FluxInjectionRate"
       inputVarNames="{ time }"
       interpolation="lower"
-      coordinates="{    0.0,  500.0, 1000.0, 1500.0, 2000.0, 2500.0, 3000.0, 3500.0, 4000.0, 4500.0, 5000.0, 5500.0 }"
-      values="{       0.000,  0.000,  0.267,  0.561,  0.194,  0.102,  0.059,  0.000,  0.000,  0.000,  0.000,  0.000 }"
+      coordinates="{    0.0,  500.0, 1000.0, 1500.0, 2000.0, 2500.0, 3000.0, 3500.0, 4000.0, 4500.0, 5000.0 }"
+      values="{       0.000,  0.267,  0.561,  0.194,  0.102,  0.059,  0.000,  0.000,  0.000,  0.000,  0.000 }"
     />
     <!-- water depletion rates in mol/s -->
     <TableFunction
       name="FluxProductionRate"
       inputVarNames="{ time }"
       interpolation="lower"
-      coordinates="{    0.0,  500.0, 1000.0, 1500.0, 2000.0, 2500.0, 3000.0, 3500.0, 4000.0, 4500.0, 5000.0, 5500.0 }"
-      values="{       0.000,  0.000,  0.003,  0.062,  0.121,  0.427,  0.502,  0.199,  0.083,  0.027,  0.000,  0.000 }"
+      coordinates="{    0.0,  500.0, 1000.0, 1500.0, 2000.0, 2500.0, 3000.0, 3500.0, 4000.0, 4500.0, 5000.0 }"
+      values="{       0.000,  0.003,  0.062,  0.121,  0.427,  0.502,  0.199,  0.083,  0.027,  0.000,  0.000 }"
     />
 
     <TableFunction name="initGasCompFracTable"
@@ -814,7 +813,6 @@ TestSet getTestSet()
   // FluxInjectionRate & FluxProductionRate table from 0.0s to 5000.0s
   setRateTable( testInputs.sourceRates,
                 { { 0.000, 0.0 },
-                  { 0.000, 0.0 },
                   { 0.267, 0.0 },
                   { 0.561, 0.0 },
                   { 0.194, 0.0 },
@@ -823,10 +821,10 @@ TestSet getTestSet()
                   { 0.000, 0.0 },
                   { 0.000, 0.0 },
                   { 0.000, 0.0 },
+                  { 0.000, 0.0 },
                   { 0.000, 0.0 } } );
   setRateTable( testInputs.sinkRates,
                 { { 0.0, 0.000 },
-                  { 0.0, 0.000 },
                   { 0.0, 0.003 },
                   { 0.0, 0.062 },
                   { 0.0, 0.121 },
@@ -835,6 +833,7 @@ TestSet getTestSet()
                   { 0.0, 0.199 },
                   { 0.0, 0.083 },
                   { 0.0, 0.027 },
+                  { 0.0, 0.000 },
                   { 0.0, 0.000 } } );
 
   testInputs.sourceRateFactor = -44e-3;
