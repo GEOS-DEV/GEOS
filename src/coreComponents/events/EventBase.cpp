3287eb58c57f554dee9ddfa7a043a6ad961d58a9
Dickson Kachuma <81433670+dkachuma@users.noreply.github.com>
2026-05-11 11:26:41 -0500

    Update src/coreComponents/events/EventBase.cpp
    Co-authored-by: Jacques Franc <49998870+jafranc@users.noreply.github.com>

--- src/coreComponents/events/EventBase.cpp
+++ src/coreComponents/events/EventBase.cpp
@@ -191,7 +191,7 @@ void EventBase::checkEvents( real64 const time,
       integer const forecast = ratio < min_ratio ? LvArray::NumericLimits< integer >::min :
                                (ratio > max_ratio ? LvArray::NumericLimits< integer >::max :
                                 static_cast< integer >(ratio));
-      this->setForecast( forecast );
+      this->setForecast(  std::clamp( static_cast<integer>( (m_beginTime - time) / dt  ),  LvArray::NumericLimits< integer >::min,  LvArray::NumericLimits< integer >::max    );
     }
   }
   else if( time >= m_endTime )
