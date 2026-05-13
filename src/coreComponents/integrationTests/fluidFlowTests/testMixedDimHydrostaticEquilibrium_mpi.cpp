e2400b4763bc738a7e484ad88fa822c32c70d0b8
Omar Duran <omaryesiduran@gmail.com>
2026-05-12 22:32:13 -0700

    wip: mpi test full span meshes targeting BC data on ghosted surface cells

--- src/coreComponents/integrationTests/fluidFlowTests/testMixedDimHydrostaticEquilibrium_mpi.cpp
+++ src/coreComponents/integrationTests/fluidFlowTests/testMixedDimHydrostaticEquilibrium_mpi.cpp
@@ -33,14 +33,6 @@ INSTANTIATE_TEST_SUITE_P(
   MixedDimHydrostaticEquilibriumTest,
   ::testing::Combine(
     ::testing::Values(
-      // Flat tet meshes - single and triple fracture only
-      "fractured_mesh_tet_DFN_1.vtu",
-      "fractured_mesh_tet_DFN_123.vtu",
-
-      // Wavy tet meshes - single and triple fracture only
-      "fractured_wavy_mesh_tet_DFN_1.vtu",
-      "fractured_wavy_mesh_tet_DFN_123.vtu",
-
       // Full span hex meshes - single and triple fracture only
       "fractured_full_span_mesh_hex_DFN_1.vtu",
       "fractured_full_span_mesh_hex_DFN_123.vtu",
