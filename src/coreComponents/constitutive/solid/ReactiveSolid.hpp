cb1e1390e5eaba968457e91ee9001950e51b89c2
Alex Zibitsker <zibitsker1@llnl.gov>
2026-07-13 10:53:03 -0700

    Updated reactive surface area computation to account more explicitly for available area at t=0 and area change as a result of precipitation

--- src/coreComponents/constitutive/solid/ReactiveSolid.hpp
+++ src/coreComponents/constitutive/solid/ReactiveSolid.hpp
@@ -76,21 +76,29 @@ public:
   {
     real64 const porosity = m_porosityUpdate.getPorosity( k, q );
     real64 const initialPorosity = m_porosityUpdate.getInitialPorosity( k, q );
+    real64 const porosity_crit = 1e-8; // critical porosity below which the surface area is set to 0
+    real64 const g = std::max(0.0, ((porosity - porosity_crit) / (initialPorosity - porosity_crit))); // accesibility factor
 
+    real64 area_total = 0.0;    
+    for( integer r=0; r < initialSurfaceArea.size(); ++r )
+    {
+      area_total += initialSurfaceArea[r];
+    }
     for( integer r=0; r < initialSurfaceArea.size(); ++r )
     {
       real64 const volumeFraction_r = m_porosityUpdate.getVolumeFractionForMineral( k, q, r );
-      real64 const volumeFraction_r_n = m_porosityUpdate.getVolumeFractionForMineral_n( k, q, r );
       real64 const initialVolumeFraction_r = m_porosityUpdate.getInitialVolumeFractionForMineral( k, q, r );
-
-      if (volumeFraction_r - volumeFraction_r_n < 0) { // dissolution
-        surfaceArea[r] = initialSurfaceArea[r] * pow( volumeFraction_r / initialVolumeFraction_r, 2.0/3.0 )
-                       * pow( porosity / initialPorosity, 2.0/3.0 );
-        if (volumeFraction_r < 1e-6) { // if the mineral is completely dissolved, set surface area to 0 to avoid numerical issues
-          surfaceArea[r] = 0.0;
-        }
-      } else { // precipitation
-        surfaceArea[r] = initialSurfaceArea[r] * pow( porosity / initialPorosity, 2.0/3.0 );
+     if (volumeFraction_r - initialVolumeFraction_r < 0) { // dissolution
+        surfaceArea[r] = initialSurfaceArea[r] * pow( volumeFraction_r / initialVolumeFraction_r, 2.0/3.0 ) * g;                        
+      } else { //precipitation
+        // Mineral area growth from precipitation
+        real64 const area_self = initialSurfaceArea[r] * pow( volumeFraction_r / initialVolumeFraction_r, 2.0/3.0 );
+        // Mineral precipitation on existing surface area of the fracture, where f_cov is the coverage factor, accounting for loss of substrate or coating
+        real64 const f_cov = exp(-std::max(0.0,(volumeFraction_r - initialVolumeFraction_r)) / initialVolumeFraction_r);
+        real64 const area_substrate = initialVolumeFraction_r * area_total * f_cov;   
+        // Nucleation area                                          
+        real64 const area_nuc = 0.0; // area_nuc = area_nuc_max * 1.0 / (1.0 + exp(-k_nuc*(Q_i - 1.0))) * (1.0 - exp(-(Q_i - Qi_crit) / (Q_i_max - Qi_crit))); // sigmoidal function for nucleation area
+        surfaceArea[r] = (area_self + area_substrate + area_nuc) * g;
       }
     }
   }
