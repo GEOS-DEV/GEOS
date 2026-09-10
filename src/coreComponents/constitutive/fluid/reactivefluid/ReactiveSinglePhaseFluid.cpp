c73e95dea4fb3246a178fd18ac4f48867761f3fb
Fan Fei <fei2@llnl.gov>
2026-09-10 14:54:44 -0700

    fix a merge bug on solventDensity

--- src/coreComponents/constitutive/fluid/reactivefluid/ReactiveSinglePhaseFluid.cpp
+++ src/coreComponents/constitutive/fluid/reactivefluid/ReactiveSinglePhaseFluid.cpp
@@ -113,14 +113,12 @@ void ReactiveSinglePhaseFluid< BASE >::postInputInitialization()
       m_numPrimarySpecies = 3;
       m_numSecondarySpecies = 0;
       m_numKineticReactions = 3;
-      m_solventDensity = serpentinizationSystem.getSolventDensity();
       break;
 
     case ChemicalSystemType::kineticCarbonate:
       m_numPrimarySpecies = 3;
       m_numSecondarySpecies = 0;
       m_numKineticReactions = 1;
-      m_solventDensity = kineticCarbonateSystem.getSolventDensity();
       break;
 
     case ChemicalSystemType::chainSerialAllKinetic:
