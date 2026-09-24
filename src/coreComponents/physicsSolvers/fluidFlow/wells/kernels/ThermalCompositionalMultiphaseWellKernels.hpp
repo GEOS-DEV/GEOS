2b33fdb3a78cb9a81a88f77c6ebf167f5e43607a
Byer <byer3@llnl.gov>
2026-09-24 09:19:06 -0700

    make isothermal and thermal residual scaling consistent

--- src/coreComponents/physicsSolvers/fluidFlow/wells/kernels/ThermalCompositionalMultiphaseWellKernels.hpp
+++ src/coreComponents/physicsSolvers/fluidFlow/wells/kernels/ThermalCompositionalMultiphaseWellKernels.hpp
@@ -190,6 +190,15 @@ public:
   {
     const WellConstraintBase * currentConstraint = wellControls.getCurrentConstraint();
     ConstraintTypeId currentControl = wellControls.getControl();
+    // Note this assumes that there is only one rate constraint
+    // This is a normalizer for the balance equations.  The normalizaer should be the current rate not the constraint value!!
+    // This is one of the reasons for restricting  constraint type for a production well
+    // Another pr will remove fix this (so the cause for difference results is isolated to one change)
+    auto const * rateConstraint = wellControls.getRateConstraints().front();
+    if( rateConstraint != nullptr )
+    {
+      m_constraintValue = rateConstraint->getConstraintValue( time );
+    }
     if( currentControl == ConstraintTypeId::BHP )
     {
       m_targetBHP = currentConstraint->getConstraintValue( time );
@@ -268,11 +277,8 @@ public:
       {
         if( m_isProducer )   // only PHASEVOLRATE is supported for now
         {
-          if( m_currentControl == ConstraintTypeId::PHASEVOLRATE )
-          {
-            // the residual is in mass units
-            normalizer = m_dt * LvArray::math::abs( m_constraintValue ) * m_phaseDens_n[iwelem][0][m_targetPhaseIndex];
-          }
+          // the residual is in mass units
+          normalizer = m_dt * LvArray::math::abs( m_constraintValue ) * m_phaseDens_n[iwelem][0][m_targetPhaseIndex];
         }
         else   // Type::INJECTOR, only TOTALVOLRATE is supported for now
         {
